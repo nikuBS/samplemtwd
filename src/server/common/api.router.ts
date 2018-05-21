47f39d433bcb562381b8e4f5b49f57a3cefd7152
조아라(Ara Jo)/Platform개발3팀/SKTECH <araara.jo@sk.com>
2018-05-21 14:42:36 +0900

    [조아라/Client개발팀] Client bypass API 설정 추가

---
+++ src/server/common/api.router.ts
@@ -0,0 +1,58 @@
+import express from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
+import { API_CMD, API_METHOD } from '../types/api-command.type';
+import ApiService from '../services/api.service';
+
+
+class ApiRouter {
+  public router: Router;
+  private apiService;
+
+  constructor() {
+    this.router = express.Router();
+    this.apiService = ApiService;
+
+    Object.keys(API_CMD).map((key) => {
+      const cmd = API_CMD[key];
+      switch ( cmd.method ) {
+        case API_METHOD.GET:
+          this.setGetApi(cmd);
+          break;
+        case API_METHOD.POST:
+          this.setPostApi(cmd);
+          break;
+        case API_METHOD.PUT:
+          this.setPutApi(cmd);
+          break;
+        case API_METHOD.DELETE:
+          this.setDeleteApi(cmd);
+          break;
+      }
+    });
+  }
+
+  private setGetApi(cmd) {
+    this.router.get(cmd.path, this.sendRequest.bind(this, cmd));
+  }
+
+  private setPostApi(cmd) {
+    this.router.post(cmd.path, this.sendRequest.bind(this, cmd));
+  }
+
+  private setPutApi(cmd) {
+    this.router.put(cmd.path, this.sendRequest.bind(this, cmd));
+  }
+
+  private setDeleteApi(cmd) {
+    this.router.delete(cmd.path, this.sendRequest.bind(this, cmd));
+  }
+
+  private sendRequest(cmd: any, req: Request, res: Response, next: NextFunction) {
+    this.apiService.request(cmd, req.query)
+      .subscribe(data => {
+        return res.json(data);
+      });
+  }
+}
+
+export default ApiRouter;
