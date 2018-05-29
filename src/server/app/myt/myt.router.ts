e0a6cfa69bb6ffa39008e917c77403c1e109da91
silion <silion.dev@gmail.com>
2018-05-29 15:12:54 +0900

    [이상형/client개발팀] 페이지 추가 - T데이터셰어이링(미가입, 해지, 해지완료) - T로밍 함께쓰기 - 팅 요금상품 - 24시간 데이터 50% 할인

--- src/server/app/myt/myt.router.ts
+++ src/server/app/myt/myt.router.ts
@@ -3,6 +3,12 @@ import MyTUsage from './controllers/myt.usage.controller';
 import MyTUsageDataShare from './controllers/myt.usage.data.share.controller';
 import MyTUsageChildren from './controllers/myt.usage.children.controller';
 import MyTUsageChange from './controllers/myt.usage.change.controller';
+import MyTUsageTDataShare from './controllers/myt.usage.tdata.share.controller';
+import MyTUsageTDataShareInfo from './controllers/myt.usage.tdata.share.info.controller';
+import MyTUsageTDataShareClose from './controllers/myt.usage.tdata.share.close.controller';
+import MyTUsageTRoamingShare from './controllers/myt.usage.troaming.share.controller';
+import MyTUsageTing from './controllers/myt.usage.ting.controller';
+import MyTUsage24hour50discount from './controllers/myt.usage.24hour.50discount.controller';
 
 class MytRouter extends TwRouter {
   constructor() {
@@ -11,6 +17,12 @@ class MytRouter extends TwRouter {
     this._controllers.push({ url: '/usage/change', controller: new MyTUsageChange() });
     this._controllers.push({ url: '/usage/children', controller: new MyTUsageChildren() });
     this._controllers.push({ url: '/usage/datashare', controller: new MyTUsageDataShare() });
+    this._controllers.push({ url: '/usage/tdatashare', controller: new MyTUsageTDataShare() });
+    this._controllers.push({ url: '/usage/tdatashare/info', controller: new MyTUsageTDataShareInfo() });
+    this._controllers.push({ url: '/usage/tdatashare/close', controller: new MyTUsageTDataShareClose() });
+    this._controllers.push({ url: '/usage/troaming', controller: new MyTUsageTRoamingShare() });
+    this._controllers.push({ url: '/usage/ting', controller: new MyTUsageTing() });
+    this._controllers.push({ url: '/usage/24hourdiscount', controller: new MyTUsage24hour50discount() });
   }
 }
 
