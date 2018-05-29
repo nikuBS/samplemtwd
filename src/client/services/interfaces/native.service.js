093d0d4367a1b6304927f0783108102edc444f7e
Ara Jo <araara.jo@sk.com>
2018-05-29 14:20:28 +0900

    [조아라/client개발팀] home class 이름 변경

--- src/client/services/interfaces/native.service.js
+++ src/client/services/interfaces/native.service.js
@@ -10,20 +10,38 @@ Tw.NativeService = function () {
 Tw.NativeService.prototype = {
   send: function (command, params, callback) {
     if ( this._bridge ) {
-      this._bridge(command, params);
+      var parameter = {
+        command: command,
+        callback: 'onNativeCallback',
+        params: params
+      }
+      if(Tw.BrowserHelper.isAndroid) {
+        parameter = JSON.stringify(parameter)
+      }
+      this._bridge(parameter);
     }
     this._callback = callback;
   },
 
   _init: function () {
     this._bridge  = Tw.BrowserHelper.isAndroid() ? window.tworld : Tw.BrowserHelper.isIos() ? window.webkit.messageHandlers.tworld : null;
-    window._onNativeCallBack = $.proxy(this._onNativeCallBack, this);
+    window.onNativeCallBack = $.proxy(this._onNativeCallBack, this);
+    window.onBack = $.proxy(this._onBack, this);
+    window.onToken = $.proxy(this._onToken, this);
+  },
+
+  _onBack: function(resp) {
+    console.log('onBack', resp);
   },
 
   _onNativeCallBack: function (resp) {
     if ( this._callback ) {
       this._callback(resp);
     }
+  },
+
+  _onToken: function(resp) {
+    console.log('onToken', resp);
   },
 
   _getRandomCode: function () {
--- src/server/app/home/home.router.ts
+++ src/server/app/home/home.router.ts
@@ -1,12 +1,12 @@
 import TwRouter from '../../common/route/tw.router';
-import HomeMainController from './controllers/home.main.controller';
-import HomeMenuController from './controllers/home.menu.controller';
+import HomeMain from './controllers/home.main.controller';
+import HomeMenu from './controllers/home.menu.controller';
 
 class HomeRouter extends TwRouter {
   constructor() {
     super();
-    this._controllers.push({ url: '/', controller: new HomeMainController() });
-    this._controllers.push({ url: '/menu', controller: new HomeMenuController() });
+    this._controllers.push({ url: '/', controller: new HomeMain() });
+    this._controllers.push({ url: '/menu', controller: new HomeMenu() });
   }
 }
 
