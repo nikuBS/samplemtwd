665e2211a76d8dca242e62d8193b2a4447ffe6a0
JimanPark <jiman.park@sk.com>
2018-06-05 16:03:46 +0900

    [박지만/client개발팀] myT 회선변경 서브페이지

--- src/server/app/myt/controllers/usage/myt.usage.change.controller.ts
+++ src/server/app/myt/controllers/usage/myt.usage.change.controller.ts
@@ -6,10 +6,63 @@ class MyTUsageChange extends TwViewController {
     super();
   }
 
-  render(req: Request, res: Response, next: NextFunction) {
-    const data = {};
+  render(req: Request, res: Response, next: NextFunction, svcInfo: any) {
+    const response = {
+      code: '결과코드',
+      msg: '결과메세지',
+      result: {
+        M: [ // 모바일
+          {
+            svcCd: 'C',
+            svcNum: '010-12**-56**',
+            svcMgmtNum: '7100000001',
+            svcNickName: '제임스폰',
+            repSvcYn: 'N',
+          },
+          {
+            svcMgmtNum: '7274875661',
+            svcNum: '010-40**-08**',
+            svcNickName: '회사폰',
+            svcCd: 'C',
+            repSvcYn: 'Y'
+          },
+          {
+            svcMgmtNum: '7274875661',
+            svcNum: '010-40**-08**',
+            svcNickName: '회사폰',
+            svcCd: 'C',
+            repSvcYn: 'Y'
+          },
+          {
+            svcMgmtNum: '7274875661',
+            svcNum: '010-40**-08**',
+            svcNickName: '회사폰',
+            svcCd: 'C',
+            repSvcYn: 'Y'
+          }
+        ],
+        W: [ // 인터넷/집전화/IPTV
+          {
+            svcCd: 'I',
+            svcNum: '010-12**-56**',
+            svcMgmtNum: '7100000001',
+            svcNickName: '제임스인터넷',
+            repSvcYn: 'Y',
+          }
+        ],
+        S: [ // 보안솔루션
+          {
+            svcCd: 'O',
+            svcNum: '010-12**-56**',
+            svcMgmtNum: '7100000001',
+            svcNickName: '제임스보안솔루션',
+            repSvcYn: 'Y',
+          }
+        ]
+      }
+    };
 
-    res.render('usage/myt.usage.change.html', data);
+    res.render('usage/myt.usage.change.html', { result: response.result, svcInfo: svcInfo });
   }
 }
 
