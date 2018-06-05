c81b0a9554685438a82135aea058b86048cc7901
1100015 <jayoon.kong@sk.com>
2018-06-05 11:24:08 +0900

    [공자윤/Client개발팀] 페이지 추가
    - myT error 페이지 추가
    - 24시간 50% 할인 페이지 추가
    - ting 페이지 추가
    - 자녀 사용량 조회 및 버튼 visibility 분기

--- src/client/types/api-command.type.js
+++ src/client/types/api-command.type.js
@@ -8,4 +8,5 @@ Tw.API_METHOD = {
 Tw.API_CMD = {
   TEST_GET_USAGE_BTN: { path: '/usageBtn', method: Tw.API_METHOD.GET },
   SESSION_CHECK: { path: '/mock/session', method: Tw.API_METHOD.GET},
+  BFF_05_0010: { path: '/core-balance/v1/children', method: Tw.API_METHOD.GET },
 };
