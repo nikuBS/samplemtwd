9a75da0bff517de3261ae7d3a73637211fb75279
JimanPark <jiman.park@sk.com>
2018-06-11 17:22:51 +0900

    [박지만/Client개발팀] myT > 선물하기 회선변경 연동확인

---
+++ src/client/app/myt/js/refillrecharge/gift/myt.gift.js
@@ -0,0 +1,26 @@
+Tw.MytGift = function () {
+  this._apiService = new Tw.ApiService();
+
+  this._cachedElement();
+  this._bindEvent();
+}
+
+Tw.MytGift.prototype = {
+  _cachedElement: function () {
+    this.$container   = $('#wrap');
+    this.$select_line = this.$container.find('#sel_line');
+  },
+
+  _bindEvent: function () {
+    this.$select_line.on('change', $.proxy(this.changeServiceLine, this));
+  },
+
+  changeServiceLine: function (e) {
+    var svcMgmtNum = $(e.currentTarget).val();
+
+    this._apiService.request(Tw.API_CMD.BFF_03_0004, {}, { headers: { "svcMgmtNum": svcMgmtNum } })
+      .done(function () {
+        window.location = window.location.href.split("?")[0];
+      });
+  }
+}
--- src/server/app/myt/controllers/refillrecharge/gift/gift.controller.ts
+++ src/server/app/myt/controllers/refillrecharge/gift/gift.controller.ts
@@ -1,13 +1,19 @@
 import TwViewController from '../../../../../common/controllers/tw.view.controller';
 import { Request, Response, NextFunction } from 'express';
+import LineService from '../../../../../services/line.service';
 
 class MyTGift extends TwViewController {
+  public lineService = new LineService();
+
   constructor() {
     super();
   }
 
   render(req: Request, res: Response, next: NextFunction, svcInfo: any) {
-    res.render('refillrecharge/gift/gift.html', {});
+    this.lineService.getMobileLineList()
+      .subscribe((response) => {
+        res.render('refillrecharge/gift/gift.html', { lineList: response.result, svcInfo: svcInfo });
+      });
   }
 }
 
