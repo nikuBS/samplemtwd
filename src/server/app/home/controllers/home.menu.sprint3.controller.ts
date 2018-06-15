da9e21e3747a722091cbb71e9562eb6fc5d63952
조아라(Ara Jo)/Platform개발3팀/SKTECH <araara.jo@sk.com>
2018-06-15 13:10:47 +0900

    [조아라/Client개발팀] 홈화면 리필,선물하기 마크업 적용
    - sprint3 home은 home/sprint3 으로 변경

--- src/server/app/home/controllers/home.menu.sprint3.controller.ts
+++ src/server/app/home/controllers/home.menu.sprint3.controller.ts
@@ -1,14 +1,14 @@
 import TwViewController from '../../../common/controllers/tw.view.controller';
 import { Request, Response, NextFunction } from 'express';
 
-class HomeMenu extends TwViewController {
+class HomeMenuSprint3 extends TwViewController {
   constructor() {
     super();
   }
 
   render(req: Request, res: Response, next: NextFunction) {
-    res.render('home.menu.html');
+    res.render('home.menu.sprint3.html');
   }
 }
 
-export default HomeMenu;
+export default HomeMenuSprint3;
