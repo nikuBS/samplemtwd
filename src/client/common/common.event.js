3950f565baa3e0e4c9891524daca1b8777a0565b
조아라(Ara Jo)/Platform개발3팀/SKTECH <araara.jo@sk.com>
2018-06-21 17:48:55 +0900

    [조아라/Client개발팀] Native 초기화 추가

--- src/client/common/common.event.js
+++ src/client/common/common.event.js
@@ -1,12 +1,18 @@
 Tw.Common = function () {
+  this.initService();
   this.setBack();
 };
 
 Tw.Common.prototype = {
+  initService: function() {
+    var native = new Tw.NativeService();
+  },
+
   setBack: function () {
     /* 뒤로가기 추가 */
     $('.common-back').on('click', function () {
-      window.history.back();
+      Tw.Logger.info('[Common Back]');
+      history.back();
     });
   }
 };
