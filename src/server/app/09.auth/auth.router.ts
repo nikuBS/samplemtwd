aabbd710352673cd93e55d2973d1e79b8447190a
Hakjoon Sim <hakjoon.sim@sk.com>
2018-07-02 14:56:13 +0900

    [심학준/Client개발팀] 회원가입 안내, markup 적용

--- src/server/app/09.auth/auth.router.ts
+++ src/server/app/09.auth/auth.router.ts
@@ -1,10 +1,12 @@
 import TwRouter from '../../common/route/tw.router';
 import AuthLogin from './controllers/auth.login.controller';
+import AuthSignupGuide from './controllers/auth.signup.guide.controller';
 
 class AuthRouter extends TwRouter {
   constructor() {
     super();
     this.controllers.push({ url: '/login', controller: new AuthLogin() });
+    this.controllers.push({ url: '/signup/guide', controller: new AuthSignupGuide() });
   }
 }
 
