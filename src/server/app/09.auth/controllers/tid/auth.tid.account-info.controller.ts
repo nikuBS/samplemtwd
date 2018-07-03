acfc81cfa8c91fb752817e1a5a612bc5cffb1664
조아라(Ara Jo)/Platform개발3팀/SKTECH <araara.jo@sk.com>
2018-07-03 16:55:43 +0900

    [조아라/Client개발팀] TID 연동 테스트 및 각 페이지 리다이렉트
    - 추후 서버 연동 필요

--- src/server/app/09.auth/controllers/tid/auth.tid.account-info.controller.ts
+++ src/server/app/09.auth/controllers/tid/auth.tid.account-info.controller.ts
@@ -6,7 +6,7 @@
 
 import TwViewController from '../../../../common/controllers/tw.view.controller';
 import { Request, Response, NextFunction } from 'express';
-import { API_CMD, TID_SVC_TYPE } from '../../../../types/api-command.type';
+import { API_CMD, API_CODE, TID_SVC_TYPE } from '../../../../types/api-command.type';
 import ParamsHelper from '../../../../utils/params.helper';
 
 class AuthTidAccountInfo extends TwViewController {
@@ -16,19 +16,28 @@ class AuthTidAccountInfo extends TwViewController {
 
   render(req: Request, res: Response, next: NextFunction, svcInfo: any) {
     const params = {
-      client_id: 'cec06f9d-d775-4e50-ae91-fcd95a538f8d',
+      client_id: 'df3c0ea4-ea4d-439f-8e2f-01f683af0c95',
       client_secret: 'eac44fbe-b96b-4f9d-9da7-0e58dfc13b90',
-      client_type: 'MWEB',
-      redirect_uri: 'http://localhost:3000/home',
-      scope: 'openid',
-      response_type: 'id_token%20token',
       state: '3646bae6eff00',
       nonce: 'df597de4c079',
-      service_type: TID_SVC_TYPE.GET_ACCOUNT
+      service_type: TID_SVC_TYPE.GET_ACCOUNT,
+      redirect_uri: 'http://localhost:3000/home',
+      client_type: 'MWEB',
+      scope: 'openid',
+      response_type: 'id_token%20token',
+      login_id: 'thanatosv'
     };
-    const url = this.apiService.getServerUri(API_CMD.OIDC) + API_CMD.OIDC.path + ParamsHelper.setQueryParams(params);
+    this.apiService.request(API_CMD.BFF_03_0007, {}).subscribe((resp) => {
+      if ( resp.code === API_CODE.CODE_00 ) {
+        params.client_id = resp.result.clientId;
+        params.client_secret = resp.result.clientSecret;
+        params.nonce = resp.result.nonce;
+        params.state = resp.result.state;
+      }
+      const url = this.apiService.getServerUri(API_CMD.OIDC) + API_CMD.OIDC.path + ParamsHelper.setQueryParams(params);
+      res.redirect(url);
+    });
 
-    res.redirect(url);
   }
 }
 
