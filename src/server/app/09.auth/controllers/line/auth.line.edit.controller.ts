c8a3459c29f3560146c886a88f9420aeecc8e6ff
조아라(Ara Jo)/Platform개발3팀/SKTECH <araara.jo@sk.com>
2018-07-03 15:14:54 +0900

    [조아라/Client개발팀] 인증관련 controller 생성 및 라우팅 구조 설정

--- src/server/app/09.auth/controllers/line/auth.line.edit.controller.ts
+++ src/server/app/09.auth/controllers/line/auth.line.edit.controller.ts
@@ -1,5 +1,5 @@
 /**
- * FileName: auth.member.management.controller.ts
+ * FileName: auth.line.edit.controller.ts
  * Author: Ara Jo (araara.jo@sk.com)
  * Date: 2018.07.03
  */
@@ -7,14 +7,14 @@
 import TwViewController from '../../../../common/controllers/tw.view.controller';
 import { Request, Response, NextFunction } from 'express';
 
-class AuthMemberManagement extends TwViewController {
+class AuthLineEdit extends TwViewController {
   constructor() {
     super();
   }
 
   render(req: Request, res: Response, next: NextFunction, svcInfo: any) {
-    res.render('member/auth.member.management.html');
+    res.render('line/auth.line.edit.html');
   }
 }
 
-export default AuthMemberManagement;
+export default AuthLineEdit;
--- src/server/app/09.auth/controllers/line/auth.line.empty-register.controller.ts
+++ src/server/app/09.auth/controllers/line/auth.line.empty-register.controller.ts
@@ -1,5 +1,5 @@
 /**
- * FileName: auth.member.management.controller.ts
+ * FileName: auth.line.empty-register.controller.ts
  * Author: Ara Jo (araara.jo@sk.com)
  * Date: 2018.07.03
  */
@@ -7,14 +7,14 @@
 import TwViewController from '../../../../common/controllers/tw.view.controller';
 import { Request, Response, NextFunction } from 'express';
 
-class AuthMemberManagement extends TwViewController {
+class AuthLineEmptyRegister extends TwViewController {
   constructor() {
     super();
   }
 
   render(req: Request, res: Response, next: NextFunction, svcInfo: any) {
-    res.render('member/auth.member.management.html');
+    res.render('line/auth.line.empty-register.html');
   }
 }
 
-export default AuthMemberManagement;
+export default AuthLineEmptyRegister;
