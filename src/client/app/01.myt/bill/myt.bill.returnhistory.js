521cf3e27a55fe2eef9f93e4648ae9fb2edcef73
KimInHwan <skt.p132150@partner.sk.com>
2018-07-05 17:32:08 +0900

    [김인환/SPTek] MyT > 나의 요금 > 요금안내서 반송내역 기본 구조 추가

---
+++ src/client/app/01.myt/bill/myt.bill.returnhistory.js
@@ -0,0 +1,15 @@
+/**
+ * FileName: myt.bill.reissue.js
+ * Author: Kim Inhwan (skt.P132150@partner.sk.com)
+ * Date: 2018.07.04
+ */
+Tw.MyTBillRetrunHistory = function (rootEl) {
+    this.$container = rootEl;
+    this._apiService = new Tw.ApiService();
+    this._bindEvent();
+};
+
+Tw.MyTBillRetrunHistory.prototype = {
+    _bindEvent: function () {
+    }
+};
