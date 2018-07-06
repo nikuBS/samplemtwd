e7dd093601b887993cc577b4114ccf8e4312c0da
조아라(Ara Jo)/Platform개발3팀/SKTECH <araara.jo@sk.com>
2018-07-06 15:23:30 +0900

    [조아라/Client개발팀] 회선관리 주의사항 링크 연동

---
+++ src/client/app/09.auth/line/auth.line.edit.js
@@ -0,0 +1,13 @@
+/**
+ * FileName: auth.line.edit.js
+ * Author: Ara Jo (araara.jo@sk.com)
+ * Date: 2018.07.06
+ */
+
+Tw.AuthLineEdit = function(rootEl) {
+  this.$container = rootEl;
+};
+
+Tw.AuthLineEdit.prototype = {
+
+};
