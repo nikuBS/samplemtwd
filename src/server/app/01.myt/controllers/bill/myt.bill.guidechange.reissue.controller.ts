4a5629f3784f24cffb2e39d94c41570891d6b3e1
KimInHwan <skt.p132150@partner.sk.com>
2018-07-10 19:03:39 +0900

    [김인환/SPTek] MyT > 나의 요금 > 요금안내서 재발행   - 재발행 월 설정코드 추가

--- src/server/app/01.myt/controllers/bill/myt.bill.guidechange.reissue.controller.ts
+++ src/server/app/01.myt/controllers/bill/myt.bill.guidechange.reissue.controller.ts
@@ -18,10 +18,13 @@ class MyTBillReissue extends TwViewController {
     Observable.combineLatest(
       this.getReissueData()
     ).subscribe(([reissueData]) => {
-      const data = {
-        reissueData
-      };
-      res.render('bill/myt.bill.guidechange.reissue.html', data);
+      const data = {};
+      if ( reissueData.result ) {
+        data['type'] = reissueData.result.billIsueTypCd;
+      }
+      data['halfYear'] = this.getHalfYearData();
+
+      res.render('bill/myt.bill.guidechange.reissue.html', { data });
     });
   }
 
@@ -34,6 +37,34 @@ class MyTBillReissue extends TwViewController {
     });
   }
 
+  private getHalfYearData(): any {
+    // 현재 시간 기준으로 6개월 전 날짜 정보 설정
+    const result: any = [];
+    const now_date = new Date();
+    const pre_date = new Date();
+    pre_date.setMonth(now_date.getMonth() - 6);
+    for ( let i = 0; i > -6; i-- ) {
+      const data = {};
+      let month_label = '';
+      pre_date.setFullYear(now_date.getFullYear());
+      pre_date.setMonth(now_date.getMonth() + i);
+      const curYear = pre_date.getFullYear();
+      const curMonth = pre_date.getMonth() + 1;
+      if ( (pre_date.getMonth() + 1) < 10 ) {
+        month_label = `${ '0' + (curMonth) }`;
+      } else {
+        month_label = `${ curMonth }`;
+      }
+      const lastDay = new Date(curYear, curMonth, 0).getDate();
+
+      data['type1'] = `${pre_date.getFullYear()}년 ${month_label}월`;
+      data['type2'] = `${pre_date.getFullYear()}.${month_label}.${lastDay}`;
+
+      result.push(data);
+    }
+
+    return result;
+  }
 }
 
 export default MyTBillReissue;
