daa4eb3b6e3899eea478f5367dd455a173c1f140
KimInHwan <skt.p132150@partner.sk.com>
2018-07-10 15:02:31 +0900

    [김인환/SPTek] MyT > 나의 요금 > 요금안내서 반송내역   - 화면 추가 및 코드 수정

--- src/client/app/01.myt/bill/myt.bill.guidechange.returnhistory.js
+++ src/client/app/01.myt/bill/myt.bill.guidechange.returnhistory.js
@@ -3,13 +3,13 @@
  * Author: Kim Inhwan (skt.P132150@partner.sk.com)
  * Date: 2018.07.04
  */
-Tw.MyTBillRetrunHistory = function (rootEl) {
-    this.$container = rootEl;
-    this._apiService = new Tw.ApiService();
-    this._bindEvent();
+Tw.MyTBillReturnHistory = function (rootEl) {
+  this.$container = rootEl;
+  this._apiService = new Tw.ApiService();
+  this._bindEvent();
 };
 
-Tw.MyTBillRetrunHistory.prototype = {
-    _bindEvent: function () {
-    }
+Tw.MyTBillReturnHistory.prototype = {
+  _bindEvent: function () {
+  }
 };
--- src/server/app/01.myt/controllers/bill/myt.bill.guidechange.returnhistory.controller.ts
+++ src/server/app/01.myt/controllers/bill/myt.bill.guidechange.returnhistory.controller.ts
@@ -4,23 +4,43 @@
  * Date: 2018.07.02
  */
 import TwViewController from '../../../../common/controllers/tw.view.controller';
-import {NextFunction, Request, Response} from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 class MyTBillReturnHistory extends TwViewController {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    render(req: Request, res: Response, next: NextFunction, svcInfo: any) {
+  render(req: Request, res: Response, next: NextFunction, svcInfo: any) {
 
-        this.renderView(res, 'bill/myt.bill.guidechange.returnhistory.html', {});
-    }
+    this.renderView(res, 'bill/myt.bill.guidechange.returnhistory.html', {});
+  }
 
-    public renderView(res: Response, view: string, data: any): any {
-        // TODO error check
-        res.render(view, data);
-    }
+  public renderView(res: Response, view: string, data: any): any {
+    // TODO error check
+    res.render(view, data);
+  }
 
+  // render(req: Request, res: Response, next: NextFunction, svcInfo: any) {
+  //   // home.main.sprint3 참조
+  //   Observable.combineLatest(
+  //     this.getReturnHistoryData()
+  //   ).subscribe(([returnData]) => {
+  //     const data = {
+  //       returnData
+  //     };
+  //     res.render('bill/myt.bill.guidechange.returnhistory.html', data);
+  //   });
+  // }
+  //
+  // private getReturnHistoryData(): Observable<any> {
+  //   // const reissueData = {};
+  //   return this.apiService.request(API_CMD.BFF_05_0039, {}).map((resp) => {
+  //     // 바로 받은 response 값은 확인 후 사용하지 않고 필요한 내용 추출하여 사용 예정
+  //     // return reissueData;
+  //     return resp;
+  //   });
+  // }
 }
 
 export default MyTBillReturnHistory;
