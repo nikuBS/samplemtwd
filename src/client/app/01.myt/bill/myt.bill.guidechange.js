fdc3a114dcc21e4d7ed7d752bd5273a54814d5e1
P130715 <skt.p130715@partnet.sk.com>
2018-07-12 08:30:14 +0900

    [SPTEK/양정규] 안내서 변경 및 재발행  - 미리보기 화면 작업중

--- src/client/app/01.myt/bill/myt.bill.guidechange.js
+++ src/client/app/01.myt/bill/myt.bill.guidechange.js
@@ -5,32 +5,33 @@
  */
 Tw.MyTBillGuidechange = function (rootEl) {
   this.$container = rootEl;
-  this._apiService = new Tw.ApiService();
+  this._apiService = Tw.Api;
   this._popupService = Tw.Popup;
   this._bindEvent();
 };
 
 Tw.MyTBillGuidechange.prototype = {
   _bindEvent: function () {
-      // 하단 안내서 플리킹 클릭 이벤트
       this.$container.on('click','.swiper-slide', $.proxy(this._changeFlicking,this) );
-      this.$container.on('click','._sel-preview', $.proxy(this._openPreview,this) );
+      this.$container.on('click','._sel-preview', $.proxy(this._openPreview, this) );
+      // this.$container.on('click','._sel-preview', $.proxy(this._openPreview, this, {'test':'hello'}) );
   },
 
+  // 하단 안내서 플리킹 클릭 이벤트
   _changeFlicking : function(e){
-      var billType = $(e.currentTarget).data('billType');
-      // Tw.Logger.log('[## TEST] ', $(e.currentTarget).attr('class') );
-      this.$container.find('._sel-desc').text(billType.desc);
-      this.$container.find('._sel-nm').text(billType.chgBtnNm);
+      // Tw.Logger.log('[## TEST] ', '_changeFlicking call' );
+      this.billType = $(e.currentTarget).data('billType');
+      this.$container.find('._sel-desc').text(this.billType.desc);
+      this.$container.find('._sel-nm').text(this.billType.chgBtnNm);
   },
   
-  // 미리보기 클릭_
-  _openPreview : function (e) {
-      Tw.Logger.log('[## TEST] ', $(e.currentTarget).attr('class') );
+  // 미리보기 클릭 이벤트
+  _openPreview : function () {
+      // Tw.Logger.log('[## TEST] ', 'preview call ', JSON.stringify(billList) );
       this._popupService.open({
-          hbs:'MY_03_03_01_L01'// hbs의 파일명
-      });
-  }  
-
+          hbs:'MY_03_03_01_L01',
+          data : {}
+      }, $.proxy(this._test,this));
+  },
 
 };
