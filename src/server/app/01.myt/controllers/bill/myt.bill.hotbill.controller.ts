b4d0ab1d623fa6d20834fab5fb722bcd932b227a
Hyeryoun Lee <ryoun80@gmail.com>
2018-07-11 20:01:18 +0900

    [이혜련/SPTek] DV001-1576: svcAttrCd mock-up data 처리

--- src/server/app/01.myt/controllers/bill/myt.bill.hotbill.controller.ts
+++ src/server/app/01.myt/controllers/bill/myt.bill.hotbill.controller.ts
@@ -21,6 +21,19 @@ class MyTBillHotBill extends TwViewController {
   }
 
   render(req: Request, res: Response, next: NextFunction, svcInfo: any) {
+    //TODO svcAttrCd 완성 되면 삭제
+    svcInfo.svcAttrCd = req.query.code || 'M1';
+    var type = '';
+    switch( svcInfo.svcAttrCd){
+      case 'M3':
+        type = 'T pocket Fi';
+        break;
+      default:
+        type = '휴대폰';
+        break;
+    }
+    svcInfo.svcType = type;
+
     Observable.combineLatest(
       this.apiService.request(API_CMD.BFF_05_0035, { gubun: PARAM.TYPE.PREVIOUS })
     ).subscribe(([]) => {
