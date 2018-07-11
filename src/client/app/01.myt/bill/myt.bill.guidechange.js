6d1fece42cbac6ebf71248f360f3193dd179686e
P130715 <skt.p130715@partnet.sk.com>
2018-07-11 12:17:34 +0900

    [SPTEK/양정규] 안내서 변경 및 재발행  - 한글 데이터 string.type.ts 로 이동  - 추가 <% include ../../../../../common/views/components/footer.component.html %>

--- src/client/app/01.myt/bill/myt.bill.guidechange.js
+++ src/client/app/01.myt/bill/myt.bill.guidechange.js
@@ -6,13 +6,15 @@
 Tw.MyTBillGuidechange = function (rootEl) {
   this.$container = rootEl;
   this._apiService = new Tw.ApiService();
+  this._popupService = Tw.Popup;
   this._bindEvent();
 };
 
 Tw.MyTBillGuidechange.prototype = {
   _bindEvent: function () {
-      // 하단 안내서 플리킹 클릭 시 이벤트
+      // 하단 안내서 플리킹 클릭 이벤트
       this.$container.on('click','.swiper-slide', $.proxy(this._changeFlicking,this) );
+      this.$container.on('click','._sel-preview', $.proxy(this._openPreview,this) );
   },
 
   _changeFlicking : function(e){
@@ -20,7 +22,15 @@ Tw.MyTBillGuidechange.prototype = {
       // Tw.Logger.log('[## TEST] ', $(e.currentTarget).attr('class') );
       this.$container.find('._sel-desc').text(billType.desc);
       this.$container.find('._sel-nm').text(billType.chgBtnNm);
-  }
+  },
+  
+  // 미리보기 클릭_
+  _openPreview : function (e) {
+      Tw.Logger.log('[## TEST] ', $(e.currentTarget).attr('class') );
+      this._popupService.open({
+          hbs:'MY_03_03_01_L01'// hbs의 파일명
+      });
+  }  
 
 
 };
