e9b9f700fc1cd74c5321c0d02330c46724ba10ff
KimInHwan <skt.p132150@partner.sk.com>
2018-07-13 17:17:52 +0900

    [김인환/SPTek] MyT > 나의 요금 > 요금안내서 반송내역   - 반송내역이 없는 경우, 반송내역이 있는 경우에 대해 처리   - 내역이 있는 경우는 추후 확인 필요(데이터가 현재 없음)

--- src/server/app/01.myt/controllers/bill/myt.bill.guidechange.returnhistory.controller.ts
+++ src/server/app/01.myt/controllers/bill/myt.bill.guidechange.returnhistory.controller.ts
@@ -13,15 +13,18 @@ class MyTBillReturnHistory extends TwViewController {
   }
 
   render(req: Request, res: Response, next: NextFunction, svcInfo: any) {
-    // home.main.sprint3 참조
+
     this.apiService.request(API_CMD.BFF_05_0039, {}).subscribe((returnData) => {
-      // 서버API 데이터 확인필요!
       const data = {
-        recCnt: 3
+        recCnt: 0
       };
-      if ( returnData.result ) {
-        data['recCnt'] = (returnData.result.recCnt > 20) ? 20 : returnData.result.recCnt;
-        // TODO:20개 기준 반송 등록일로 sort 한 다음 화면에 출력해야하나?? API 조회가 된 후 확인 필요!!!
+      if (returnData.code !== 'ZINVN8319') { // 요금반송내역이 없는 경우에 대한 코드 값 'ZINVN8319'
+        if ( returnData.result ) {
+          // 요금반송내역이 있는 경우
+          // TODO: 20개 기준 반송 등록일로 sort 한 다음 화면에 출력해야하나?? API 조회가 된 후 확인 필요!!!
+          // TODO: 현재 반송내역 데이터가 없어 있는 경우에 대해 처리가 어렵다 데이터가 확보된 이후 재 확인
+          data['recCnt'] = (returnData.result.recCnt > 20) ? 20 : returnData.result.recCnt;
+        }
       }
       res.render('bill/myt.bill.guidechange.returnhistory.html', { data });
     });
