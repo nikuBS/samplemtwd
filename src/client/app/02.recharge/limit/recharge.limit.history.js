debd785b5c84b2c1cbcb0bf030007435a77e084b
jiyoung.jo <jiyoungjo@sk.com>
2018-07-16 11:39:01 +0900

    [조지영/Client 개발팀] 데이터 한도 충전내역 더보기 버튼 추가

--- src/client/app/02.recharge/limit/recharge.limit.history.js
+++ src/client/app/02.recharge/limit/recharge.limit.history.js
@@ -9,23 +9,37 @@ Tw.RechargeLimitHistory = function (rootEl) {
   this._popupService = Tw.Popup;
   this._apiService = Tw.Api;
 
+  this._cachedElement();
   this._bindEvent();
   this._init();
 };
 
 Tw.RechargeLimitHistory.prototype = {
+  DEFAULT_LIST_COUNT: 20,
+
+  TABS: {
+    RECHARGE: 'recharge',
+    LIMIT: 'limit',
+  },
+
   _init: function () {
-    this._selectedTab = window.location.hash || "recharge";
+    this._selectedTab = window.location.hash || this.TABS.RECHARGE;
 
     this._getData();
   },
 
+  _cachedElement: function () {
+    this.$rechargeTab = this.$container.find('li.tab-recharge');
+    this.$limitTab = this.$container.find('li.tab-limit');
+  },
+
   _bindEvent: function () {
     this.$container.on('click', '.info-cancel button', $.proxy(this._openCancelAlert, this));
+    this.$container.on('click', '.gift-bt-more', $.proxy(this._handleMoreClick, this));
   },
 
   _openCancelAlert: function () {
-    this._popupService.openAlert(Tw.MSG_RECHARGE.LIMIT_A09, Tw.POPUP_TITLE.NOTIFY)
+    this._popupService.openAlert(Tw.MSG_RECHARGE.LIMIT_A09, Tw.POPUP_TITLE.NOTIFY);
   },
 
   _getData: function () {
@@ -50,27 +64,57 @@ Tw.RechargeLimitHistory.prototype = {
     Handlebars.registerPartial('rechargeItems', $('#tmpl-items-' + this._selectedTab).html());
     var contents = template({
       totalCount: this._rechargeItems.length,
-      leftCount: Math.max(0, this._rechargeItems.length - 20),
-      items: this._rechargeItems.slice(0, 20)
+      leftCount: Math.max(0, this._rechargeItems.length - this.DEFAULT_LIST_COUNT),
+      items: this._rechargeItems.slice(0, this.DEFAULT_LIST_COUNT)
     });
     tab.append(contents);
+  },
+
+  _handleMoreClick: function (e) {
+    var items, $tab;
+
+    switch (this._selectedTab) {
+      case this.TABS.LIMIT:
+        items = this._limitItems;
+        $tab = this.$limitTab;
+        break;
+      case this.TABS.RECHARGE:
+      default:
+        items = this._rechargeItems;
+        $tab = this.$rechargeTab;
+        break;
+    }
+
+    var template = Handlebars.compile('{{>' + this._selectedTab + 'Items}}');
+    var leftCount = e.target.getAttribute('data-left-count');
+    var firstIdx = items.length - leftCount;
+    var eItems = template({ items: items.slice(firstIdx, firstIdx + this.DEFAULT_LIST_COUNT) });
+    $tab.find('ul').append(eItems);
+    leftCount = leftCount - this.DEFAULT_LIST_COUNT;
+
+    if (leftCount > 0) {
+      e.target.setAttribute('data-left-count', leftCount);
+      e.target.innerText = e.target.innerText.replace(/\((.+?)\)/, '(' + leftCount + ')');
+    } else {
+      e.target.style.display = 'none';
+    }
   },
 
   _setProperRecharge: function (item) {
     if (item.opTypCd === "1") {
       item.type = {
         icon: "complete",
-        label: "충전"
+        label: Tw.RECHARGE_TYPE.RECHARGE
       }
     } else if (item.opTypCd === "3") {
       item.type = {
         icon: "auto",
-        label: "자동"
+        label: Tw.RECHARGE_TYPE.REGULAR
       }
     } else {
       item.type = {
         icon: "cancel",
-        label: "취소",
+        label: Tw.RECHARGE_TYPE.CANCEL,
         complete: true,
       }
     }
@@ -80,11 +124,12 @@ Tw.RechargeLimitHistory.prototype = {
 
   _setItems: function (resp) {
     switch (this._selectedTab) {
-      case 'recharge':
-        this._rechargeItems = resp.result.map(this._setProperRecharge);
-        break;
-      case 'limit':
+      case this.TABS.LIMIT:
         this._limitItems = resp.result;
+        break;
+      case this.TABS.RECHARGE:
+      default:
+        this._rechargeItems = resp.result.map(this._setProperRecharge);
         break;
     }
   }
--- src/server/app/02.recharge/controllers/limit/recharge.limit.history.controller.ts
+++ src/server/app/02.recharge/controllers/limit/recharge.limit.history.controller.ts
@@ -6,30 +6,6 @@
 
 import { NextFunction, Request, Response } from "express";
 import TwViewController from "../../../../common/controllers/tw.view.controller";
-import { API_CMD } from "../../../../types/api-command.type";
-import { Observable } from "../../../../../../node_modules/rxjs/Observable";
-import FormatHelper from "../../../../utils/format.helper";
-import { SSL_OP_TLS_BLOCK_PADDING_BUG } from "constants";
-
-interface IRechargeInfo extends IInfo {
-  opDt: string, //"처리일"
-  amt: string, //"충전금액"
-  opTypCd: string, //"처리유형코드"
-  opTypNm: string, //"처리유형명"
-  opOrgNm: string, //"처리영업장"
-  refundableYn: string, //"환불가능여부"
-  type?: {
-    icon: string;
-    label: string;
-  },
-}
-
-interface IInfo {
-  opDt: string, //"처리일",
-  opTypCd: string, //"처리유형코드"
-  opTypNm: string, //"처리유형명"
-  opOrgNm: string, //"처리영업장"
-}
 
 export default class RechargeLimitHistory extends TwViewController {
   constructor() {
