9ee43e83f51ade0fd869873abdde626fb53a804a
Hyeryoun Lee <ryoun80@gmail.com>
2018-07-17 15:15:17 +0900

    [이혜련/SPTek] DV001-1570 요금안내서 상세 작업중

--- src/client/app/01.myt/bill/myt.bill.billguide.subDetailSpecification.js
+++ src/client/app/01.myt/bill/myt.bill.billguide.subDetailSpecification.js
@@ -28,20 +28,20 @@ Tw.mytBillBillguideSubDetailSpecification = function (rootEl, resData) {
 Tw.mytBillBillguideSubDetailSpecification.prototype = {
   _init: function () {
     Tw.Logger.info('[Tw.mytBillBillguideSubDetailSpecification 초기화]');
+    this.$billMenu = this.$container.find('#billMenu');
+    this.$btMore = this.$container.find('#btMore');
     this._bindEvent();
+    this._getDetailSpecification();
   },
   _bindEvent: function () {
     //this.$container.on('click', '[data-target="totPaySelectBtn"]', $.proxy(this._totPaySelectFun, this));
   },
   //--------------------------------------------------------------------------[api]
-  _getDetailSpecification: function() {
-
+  _getDetailSpecification: function () {
+    //TODO API 연동
     $.ajax('http://localhost:3000/mock/myt.bill.billguide.BFF_05_00036.json')
-      .done(function(resp){
-        console.log('성공');
-        Tw.Logger.info(resp);
-      })
-      .fail(function(err) {
+      .done($.proxy(this._onDetailDataReset, this))
+      .fail(function (err) {
         console.log('실패');
         Tw.Logger.info(err);
       });
@@ -71,5 +71,47 @@ Tw.mytBillBillguideSubDetailSpecification.prototype = {
   },
   _getSelClaimDtBtn: function (str) {
     return moment(str).add(1, 'days').format('YYYY년 MM월');
+  },
+  _onDetailDataReset: function (resp) {
+    console.log('성공');
+    Tw.Logger.info(resp);
+    var groups = this._getGrouppedBillData(resp.result);
+    this._renderBillGroup(groups);
+  },
+  _getGrouppedBillData: function (result) {
+    //TODO API에 다른 데이터 요청
+    var svcInfo = result.paidAmtMonthSvcNum;
+
+    var groupBySvc = _.groupBy(result.paidAmtDetailInfo, 'svcMgmtNum');
+    var detailGroups = {};
+    $.each(groupBySvc, function (svcMgmtNum, svc) {
+      detailGroups[svcMgmtNum] = {};
+      var fieldInfo = {
+        lcl: 'billItmLclNm',
+        scl: 'billItmSclNm',
+        name: 'billItmNm',
+        value: 'invAmt'
+      };
+      detailGroups[svcMgmtNum].billItems = Tw.MyTBillHotBill.arrayToGroup(svc, fieldInfo);
+      detailGroups[svcMgmtNum].svcInfo = _.findWhere(svcInfo, { svcMgmtNum: svcMgmtNum });
+      detailGroups[svcMgmtNum].svcTotal = _.reduce(detailGroups[svcMgmtNum].billItems, function (sum, item) {
+        return sum + parseInt(item.total.replace(/,/gi, ''), 10);
+      }, 0);
+      detailGroups[svcMgmtNum].svcTotal = Tw.StringHelper.commaSeperatedString(detailGroups[svcMgmtNum].svcTotal);
+    });
+    console.log(detailGroups);
+    return detailGroups;
+  },
+
+  _renderBillGroup: function (groups) {
+    Handlebars.registerHelper('isBill', function (val, options) {
+      return (Tw.MyTBillHotBill.NO_BILL_FIELDS.indexOf(val) < 0 ) ? options.fn(this) : options.inverse(this);
+    });
+
+    var source = $('#tmplSvcBillGroup').html();
+    var template = Handlebars.compile(source);
+    var output = template({ paidAmtDetailInfo: groups });
+    $(output).insertBefore(this.$btMore);
+    skt_landing.widgets.widget_accordion2();
   }
 };
