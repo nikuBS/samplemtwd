a75bb21e082596ab39b26b9e673c8309449b6107
mackmh <skt.P130714@partner.sk.com>
2018-07-17 18:35:59 +0900

    [김명환/프론트개발] 통합청구 대표 api 연동

--- src/client/app/01.myt/bill/myt.bill.billguide.subChildBill.js
+++ src/client/app/01.myt/bill/myt.bill.billguide.subChildBill.js
@@ -22,29 +22,120 @@ Tw.mytBillBillguideSubChildBill = function (rootEl, resData) {
   this._history = new Tw.HistoryService(this.$container);
   this._history.init('hash');
 
+  this.selectDataInfo = { //조회 할때 사용할 데이터
+    selectChildPhoneId : null,
+    selectChildBillMonth : null
+  };
+
+  this.usedAmounts = null; //BFF_05_0047 사용요금 조회(본인/자녀)
+
   return this;
 };
 
 Tw.mytBillBillguideSubChildBill.prototype = {
   _init: function () {
     Tw.Logger.info('[Tw.mytBillBillguideSubChildBill 초기화]');
+    this.selectDataInfo.selectPhoneId = this.resData.circuitChildInfo[0].svcMgmtNum;
+
     this._bindEvent();
+    this._getUsedAmounts();
+  },
+  _cachedElement: function () {
+    this.$childPhonenum = $('[data-target="childPhonenum"]');//회선번호
+    //this.$childPhonenumBtn = $('[data-target="childPhonenumBtn"]');
+    this.$deviceInfo = $('[data-target="deviceInfo"]');//제품명
+
+    this.$feePlanName = $('[data-target="feePlanName"]');//요금제명
+
+    this.$billMonthName = $('[data-target="billMonthName"]');//청구월명
+    this.$useAmtTot = $('[data-target="useAmtTot"]');//사용요금
+    this.$periodOfUse = $('[data-target="periodOfUse"]');//사용기간
+    this.$discountArea = $('[data-target="discountArea"]');//할인금액
+    this.$unpaidChargesTot = $('[data-target="unpaidChargesTot"]');//미납요금
+    this.$unpaidChargesList = $('[data-target="unpaidChargesList"]');//미납요금리스트
+
+
+
   },
   _bindEvent: function () {
     //this.$container.on('click', '[data-target="totPaySelectBtn"]', $.proxy(this._totPaySelectFun, this));
   },
-  //--------------------------------------------------------------------------[api]
-  _getDetailSpecification: function() {
+  _usedAmountsInit: function() {
+    this._cachedElement();
 
-    $.ajax('http://localhost:3000/mock/myt.bill.billguide.BFF_05_00036.json')
-      .done(function(resp){
+    this.$billMonthName.html($.proxy(function(){
+      this.$billMonthName.attr( 'data-info', this.usedAmounts.invDt );
+      var htmlStr = '';
+      htmlStr += this._getSelClaimDtBtn( this.usedAmounts.invDt );
+      htmlStr += '<span class="ico">';
+      return htmlStr;
+    }, this));
+
+    this.$useAmtTot.html($.proxy(function(){
+      var htmlStr = '';
+      htmlStr = Tw.FormatHelper.addComma( this.usedAmounts.useAmtTot );
+      return htmlStr;
+    }, this));
+
+    this.$periodOfUse.html($.proxy(function(){
+        return this._getSelPeriod( this.usedAmounts.invDt );
+    }, this));
+
+    this.$discountArea.html($.proxy(function(){
+      var tempMonth = Tw.DateHelper.getShortDateWithFormat(this.usedAmounts.invDt, 'M월');//청구월
+      var usageFee = Tw.FormatHelper.addComma( this.usedAmounts.useAmtTot );//사용요금
+      var discountRate = Tw.FormatHelper.addComma( String( Math.abs( Number(this.usedAmounts.deduckTotInvAmt) ) ) );//할인요금
+      var htmlStr = '';
+      htmlStr += '<p>';
+      htmlStr += '<span class="num">' + tempMonth +'</span>';
+      htmlStr += '월 사용 요금은 <span class="num">' + usageFee + '</span>원이고<br />';
+      htmlStr += '<span class="num">' + discountRate + '</span>원을 할인 받으셨습니다.';
+      htmlStr += '</p>';
+      return htmlStr;
+    }, this));
+
+    this.$unpaidChargesTot.html($.proxy(function(){
+      var htmlStr = '';
+      htmlStr = Tw.FormatHelper.addComma( this.usedAmounts.unPayAmt );
+      return htmlStr;
+    }, this));
+
+    this.$unpaidChargesList.html($.proxy(function(){
+
+      var tempArr = this.usedAmounts.unPayAmtList;
+      var htmlStr = '';
+
+      for (var i=0, len=tempArr.length; i<len; i++) {
+        var NonPaymentMonth = this._getSelClaimDtBtn( tempArr[i].invDt );
+        var NonPaymentNum = tempArr[i].comBat;
+
+        htmlStr += '<li>';
+        htmlStr += '<dl class="pdt-wrap">';
+        htmlStr += '<dt class="pdt-tit">' + NonPaymentMonth + '</dt>';
+        htmlStr += '<dd class="pdt-price"><span class="num">' + NonPaymentNum + '</span>원</dd>';
+        htmlStr += '</dl>';
+        htmlStr += '</li>';
+      }
+      return htmlStr;
+    }, this));
+
+  },
+  //--------------------------------------------------------------------------[api]
+  _getUsedAmounts: function() {//BFF_05_0047 사용요금 조회(본인/자녀)
+
+    $.ajax('http://localhost:3000/mock/myt.bill.billguide.BFF_05_00047.json')
+      .done($.proxy(function(resp){
         console.log('성공');
-        Tw.Logger.info(resp);
-      })
+        Tw.Logger.info('[BFF_05_00047]', resp);
+        this.usedAmounts = resp.result;
+        this._usedAmountsInit();
+
+      }, this))
       .fail(function(err) {
         console.log('실패');
-        Tw.Logger.info(err);
+        Tw.Logger.info('[BFF_05_00047]', err);
       });
+
 
     // this._apiService.request(Tw.API_CMD.BFF_05_0036, { detailYn: 'Y' })
     //   .done(function(resp){
@@ -71,5 +162,13 @@ Tw.mytBillBillguideSubChildBill.prototype = {
   },
   _getSelClaimDtBtn: function (str) {
     return moment(str).add(1, 'days').format('YYYY년 MM월');
+  },
+  _getSelPeriod: function(str) {
+    var startDate = moment(str).format('YYYY.MM') + '.01';
+    var endDate = moment(str).format('YYYY.MM.DD');
+    return startDate + ' ~ ' + endDate;
+
   }
+
+
 };
--- src/client/app/01.myt/bill/myt.bill.billguide.subSelPayment.js
+++ src/client/app/01.myt/bill/myt.bill.billguide.subSelPayment.js
@@ -6,7 +6,7 @@
  */
 
 Tw.mytBillBillguideSubSelPayment = function (rootEl, resData) {
-  this.thisMain = this;
+
   this.resData = resData;
   this.init = this._init;
   Tw.Logger.info('[서버에서 데이터 받음 mytBillBillguideSubSelPayment]', resData);
@@ -22,6 +22,15 @@ Tw.mytBillBillguideSubSelPayment = function (rootEl, resData) {
   this._history = new Tw.HistoryService(this.$container);
   this._history.init('hash');
 
+  this.autopaySchedule = null; //BFF_05_0033 미납요금 납부가능일 청구일정 조회
+  this.PaymentPossibleDay = null; //BFF_05_0031 미납요금 납부가능일 조회
+
+  //요일 셋팅
+  moment.lang('ko', {
+    weekdays: ["일요일","월요일","화요일","수요일","목요일","금요일","토요일"],
+    weekdaysShort: ["일","월","화","수","목","금","토"],
+  });
+
   return this;
 };
 
@@ -29,21 +38,153 @@ Tw.mytBillBillguideSubSelPayment.prototype = {
   _init: function () {
     Tw.Logger.info('[Tw.mytBillBillguideSubSelPayment 초기화]');
     this._bindEvent();
+    this._getAutopaySchedule();
+    this._getPaymentPossibleDay();
+  },
+  _cachedElement: function () {
+    this.$paymentSummaryArea = $('[data-target="paymentSummaryArea"]');
+    this.$selPaymentSelArea = $('[data-target="selPaymentSelArea"]');
+    this.$selPaymentSelBtn = $('[data-target="selPaymentSelBtn"]');
+    this.$cancelBtn = $('[data-target="cancelBtn"]');
+    this.$submitBtn = $('[data-target="submitBtn"]');
+
   },
   _bindEvent: function () {
-    //this.$container.on('click', '[data-target="totPaySelectBtn"]', $.proxy(this._totPaySelectFun, this));
+    this.$container.on('click', '[data-target="selPaymentSelBtn"]', $.proxy(this._selPopOpen, this));
+    this.$container.on('click', '[data-target="cancelBtn"]', $.proxy(this._cancelExe, this));
+    this.$container.on('click', '[data-target="submitBtn"]', $.proxy(this._submitExe, this));
+  },
+  _cancelExe: function() {
+    this._goBack();
+  },
+  _submitExe: function() {
+    var tg = this.$container.find('[data-target="selPaymentSelBtn"]');
+    Tw.Logger.info('[데이터 정송 _submitExe ] ', tg.attr('data-info'));
+  },
+  _dataInit: function() {
+    if ( this.autopaySchedule.payMthdCd === '02') {
+      Tw.Logger.info('[카드]', this.$paymentSummaryArea);
+      this._cachedElement();
+      this._stateTypeA();
+    } else {
+      if ( this.autopaySchedule.payMthdCd === '01' && this.autopaySchedule.drwInvCyclCd === 'S1' ) {
+        Tw.Logger.info('[은행 + 데일리]');
+        this._cachedElement();
+        this._stateTypeB();
+      }
+      else if ( this.autopaySchedule.payMthdCd === '01' && this.autopaySchedule.drwInvCyclCd === '01' ) {
+        Tw.Logger.info('[은행 + 금결원]');
+        this._cachedElement();
+        this._stateTypeC();
+      }
+    }
+  },
+  _selectBtnInit: function() {
+    this._cachedElement();
+    //납부가능일 버튼 활성화
+    var useObjYn = this.PaymentPossibleDay.useObjYn;
+    (useObjYn === 'Y') ? this.$selPaymentSelArea.show() : this.$selPaymentSelArea.hide();
+  },
+  //--------------------------------------------------------------------------[셀릭트 팝업]
+  _selPopOpen: function() {
+    var $target = $(event.currentTarget);
+    var arrOption = [];
+    for ( var i=0, len=this.PaymentPossibleDay.maxPtpDtList.length; i<len; i++ ) {
+      arrOption.push({
+        'attr' : 'data-info="' + this.PaymentPossibleDay.maxPtpDtList[i] + '"',
+        text : Tw.DateHelper.getShortDateWithFormat( this.PaymentPossibleDay.maxPtpDtList[i], 'YYYY년 MM월 DD일' )
+      });
+    }
+    console.info('arrOption : ', arrOption);
+    this._popupService.openChoice('납부가능일 선택', arrOption, 'type1', $.proxy(this._selPopOpenEvt, this, $target));
+  },
+  _selPopOpenEvt: function($target, $layer) {
+    $layer.on('click', '.popup-choice-list', $.proxy(this._selPopOpenEvtExe, this, $target, $layer));
+  },
+  _selPopOpenEvtExe: function($target, $layer, event) {
+    var $selectedValue = $(event.currentTarget);
+    var tg = $target.find('[data-target="selPaymentSelBtn"]');
+    tg.text($selectedValue.text());
+    tg.attr( 'data-info', $selectedValue.find('button').attr('data-info') );
+    this._popupService.close();
+  },
+  //--------------------------------------------------------------------------[카드]
+  _stateTypeA: function() {
+    var dataArr = this.autopaySchedule.autoPayHistoryList;
+    var html = '';
+
+    for (var i=0, len=dataArr.length; i < len; i++) {
+      html += '<li><strong>' + dataArr[i].billNum + '차 청구</strong>';
+      html += '<p>' + dataArr[i].autoPayDrawList[0].seq + '회차 ' + this._getDateTypeA(dataArr[i].autoPayDrawList[0].drwDt) + '</p>';
+      html += '</li>';
+    }
+
+    this.$paymentSummaryArea.html($.proxy(function() {
+      return html;
+    }, this));
+  },
+  //--------------------------------------------------------------------------[[은행 + 데일리]]
+  _stateTypeB: function() {
+    var dataArr = this.autopaySchedule.autoPayHistoryList;
+    var pStartDay = dataArr[0].autoPayDrawList[0].pStartDay;
+    var pEndDay = dataArr[0].autoPayDrawList[0].pEndDay;
+    var tStartDay = dataArr[0].autoPayDrawList[0].tStartDay;
+    var tEndDay = dataArr[0].autoPayDrawList[0].tEndDay;
+
+    var html = '';
+    html += '<li><strong>1차 청구</strong>';
+    html += '<p>' + Tw.DateHelper.getShortDateNoDot( pStartDay ) + '일~' + Tw.DateHelper.getShortDateNoDot( pEndDay ) + '일까지 매일<br /><span>(인출일이 휴일인 경우 다음날 인출) </span></p>';
+    html += '<p>' + Tw.DateHelper.getShortDateNoDot( tStartDay ) + '일~' + Tw.DateHelper.getShortDateNoDot( tEndDay ) + '일까지 매일<br /><span>(인출일이 휴일인 경우 다음날 인출) </span></p>';
+    html += '</li>';
+
+    html += '<li><strong>2차 청구</strong>';
+    html += '<div class="list-dot-wrap">';
+    html += '<ul class="list-dot">';
+    html += '<li class="list">' + dataArr[1].autoPayDrawList[0].seq + '회차 ' + this._getDateTypeA(dataArr[1].autoPayDrawList[0].drwDt) + '</li>';
+    html += '<li class="list">' + dataArr[1].autoPayDrawList[1].seq + '회차 ' + this._getDateTypeA(dataArr[1].autoPayDrawList[1].drwDt) + '</li>';
+    html += '</ul>';
+    html += '</div>';
+    html += '</li>';
+
+    this.$paymentSummaryArea.html($.proxy(function() {
+      return html;
+    }, this));
+  },
+  //--------------------------------------------------------------------------[은행 + 금결원]
+  _stateTypeC: function() {
+    var dataArr = this.autopaySchedule.autoPayHistoryList;
+    var html = '';
+
+    for (var i=0, len=dataArr.length; i < len; i++) {
+      html += '<li><strong>' + dataArr[i].billNum + '차 청구</strong>';
+
+      var drawListLen = dataArr[i].autoPayDrawList.length;
+
+      for (var a=0; a < drawListLen; a++ ) {
+        html += '<p>' + dataArr[i].autoPayDrawList[a].seq + '회차 ' + this._getDateTypeA(dataArr[i].autoPayDrawList[a].drwDt) + '</p>';
+      }
+
+      html += '</li>';
+    }
+
+    this.$paymentSummaryArea.html($.proxy(function() {
+      return html;
+    }, this));
   },
   //--------------------------------------------------------------------------[api]
-  _getDetailSpecification: function() {
+  _getAutopaySchedule: function() {
 
-    $.ajax('http://localhost:3000/mock/myt.bill.billguide.BFF_05_00036.json')
-      .done(function(resp){
+    $.ajax('http://localhost:3000/mock/myt.bill.billguide.BFF_05_00033_1.json')
+      .done($.proxy(function(resp){
         console.log('성공');
-        Tw.Logger.info(resp);
-      })
+        Tw.Logger.info('[BFF_05_00033]', resp);
+        this.autopaySchedule = resp.result;
+        this._dataInit();
+
+      }, this))
       .fail(function(err) {
         console.log('실패');
-        Tw.Logger.info(err);
+        Tw.Logger.info('[BFF_05_00033]', err);
       });
 
     // this._apiService.request(Tw.API_CMD.BFF_05_0036, { detailYn: 'Y' })
@@ -51,6 +192,21 @@ Tw.mytBillBillguideSubSelPayment.prototype = {
     //     Tw.Logger.info('[청구요금 | 상세요금조회]', resp);
     //   })
     //   .fail(function(err){})
+  },
+  _getPaymentPossibleDay: function() {
+    $.ajax('http://localhost:3000/mock/myt.bill.billguide.BFF_05_00031.json')
+      .done($.proxy(function(resp){
+        console.log('성공');
+        Tw.Logger.info('[BFF_05_00031]', resp);
+        this.PaymentPossibleDay = resp.result;
+        this._selectBtnInit();
+
+      }, this))
+      .fail(function(err) {
+        console.log('실패');
+        Tw.Logger.info('[BFF_05_00033]', err);
+      });
+
   },
 
   //--------------------------------------------------------------------------[공통]
@@ -71,5 +227,8 @@ Tw.mytBillBillguideSubSelPayment.prototype = {
   },
   _getSelClaimDtBtn: function (str) {
     return moment(str).add(1, 'days').format('YYYY년 MM월');
+  },
+  _getDateTypeA: function (str) {
+    return moment(str).format('YYYY.MM.DD (ddd)');
   }
 };
