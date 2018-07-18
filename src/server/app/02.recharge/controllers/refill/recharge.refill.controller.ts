6d2e3c9633ad06e7de5a735a79b4ffca7fa69fb3
1100015 <jayoon.kong@sk.com>
2018-07-18 15:37:40 +0900

    [공자윤/Client개발팀] 리필하기 회선정보 변경

--- src/server/app/02.recharge/controllers/refill/recharge.refill.controller.ts
+++ src/server/app/02.recharge/controllers/refill/recharge.refill.controller.ts
@@ -30,7 +30,7 @@ class RechargeRefill extends TwViewController {
   }
 
   private getLineList(): any {
-    return this.apiService.request(API_CMD.BFF_03_0003_C, { svcCtg: LINE_NAME.MOBILE });
+    return this.apiService.request(API_CMD.BFF_03_0004, {});
   }
 
   private getUsageOptions(): any {
@@ -76,8 +76,9 @@ class RechargeRefill extends TwViewController {
   }
 
   private getData(lineList: any, usageOptions: any, usageData: any, svcInfo: any): any {
+    console.log(lineList.result);
     return {
-      lineList: lineList.result,
+      lineList: lineList.result.m,
       usageOptions,
       usageData,
       svcInfo
