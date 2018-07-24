22753cfc022c258353b360b245782c8ba258ea4e
조아라(Ara Jo)/Platform개발3팀/SKTECH <araara.jo@sk.com>
2018-07-24 15:17:36 +0900

    [조아라/Client개발팀] 서버 세션 저장시 클라이언트 세션 저장하도록 변경
    - 세션만료, 처음 진입 구분

--- src/server/services/redis.service.ts
+++ src/server/services/redis.service.ts
@@ -20,7 +20,7 @@ class RedisService {
     store: new this.RedisStore(this.redisOption),
     cookie: { maxAge: 60 * 60 * 1000 }, // 1hours
     secret: 'sktechx',
-    saveUninitialized: true, // don't create session until something stored,
+    saveUninitialized: false, // don't create session until something stored,
     resave: false, // don't save session if unmodified
   });
 
