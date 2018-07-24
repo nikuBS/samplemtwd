b60ff75978548663bafcd33cc321a3d5a6efd8bb
JimanPark <jiman.park@sk.com>
2018-07-24 09:27:55 +0900

    [박지만/Client개발팀] 고객센터 submain 프레임 페이지 추가

---
+++ src/client/app/08.customer/customer.main.js
@@ -0,0 +1,18 @@
+Tw.CustomerMain = function (rootEl) {
+  this.$container = rootEl;
+  console.log('test');
+
+  this._cachedElement();
+  this._bindEvent();
+};
+
+Tw.CustomerMain.prototype = {
+
+  _bindEvent: function () {
+
+  },
+
+  _cachedElement: function () {
+
+  }
+};
