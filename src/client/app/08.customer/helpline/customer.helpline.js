b576554721d0a7b7a8bee30745f3a61e685682af
jiyoung.jo <jiyoungjo@sk.com>
2018-07-26 14:27:56 +0900

    [조지영/Client개발팀] 고객 상담 예약 관련 팝업 이벤트 바인딩

--- src/client/app/08.customer/helpline/customer.helpline.js
+++ src/client/app/08.customer/helpline/customer.helpline.js
@@ -8,6 +8,8 @@ Tw.CustomerHelpline = function (rootEl) {
   this.$container = rootEl;
   this._apiService = Tw.Api;
   this._popupService = Tw.Popup;
+  this._history = new Tw.HistoryService(rootEl);
+  this._history.init();
 
   this._cachedElement();
   this._bindEvent();
@@ -26,6 +28,8 @@ Tw.CustomerHelpline.prototype = {
   },
 
   _cachedElement: function () {
+    this.$btnType = this.$container.find('#fe-btn-type');
+    this.$btnArea = this.$container.find('#fe-btn-area');
     this.$btnTime = this.$container.find('#fe-btn-time');
   },
 
@@ -58,7 +62,7 @@ Tw.CustomerHelpline.prototype = {
         style_class: 'bt-red1',
         txt: Tw.BUTTON_LABEL.CONFIRM
       }]
-    });
+    }, $.proxy(this._handleSelectType, this));
   },
 
   _openSelectAreaPopup: function () {
@@ -67,13 +71,13 @@ Tw.CustomerHelpline.prototype = {
       'title': Tw.POPUP_TITLE.SET_HELPLINE_AREA,
       'close_bt': true,
       'list': [
-        { 'attr': 'data-type="1"', text: Tw.HELPLINE_AREA.CAPITAL },
-        { 'attr': 'data-type="5"', text: Tw.HELPLINE_AREA.CENTER },
-        { 'attr': 'data-type="4"', text: Tw.HELPLINE_AREA.EAST },
-        { 'attr': 'data-type="3"', text: Tw.HELPLINE_AREA.DAEGU },
-        { 'attr': 'data-type="2"', text: Tw.HELPLINE_AREA.BUSAN },
+        { 'attr': 'data-area-code="1"', text: Tw.HELPLINE_AREA.CAPITAL },
+        { 'attr': 'data-area-code="5"', text: Tw.HELPLINE_AREA.CENTER },
+        { 'attr': 'data-area-code="4"', text: Tw.HELPLINE_AREA.EAST },
+        { 'attr': 'data-area-code="3"', text: Tw.HELPLINE_AREA.DAEGU },
+        { 'attr': 'data-area-code="2"', text: Tw.HELPLINE_AREA.BUSAN },
       ]
-    });
+    }, $.proxy(this._handleSelectArea, this));
   },
 
   _openSelectTimePopup: function () {
@@ -88,7 +92,51 @@ Tw.CustomerHelpline.prototype = {
         style_class: 'bt-red1',
         txt: Tw.BUTTON_LABEL.CONFIRM
       }]
-    });
+    }, $.proxy(this._handleSelectTime, this));
+  },
+
+  _handleSelectType: function ($layer) {
+    $layer.on('click', '.btn-box button', $.proxy(this._handleConfirmType, this, $layer));
+  },
+
+  _handleSelectArea: function ($layer) {
+    $layer.on('click', '.popup-choice-list button', $.proxy(this._handleAreaClick, this));
+  },
+
+  _handleSelectTime: function ($layer) {
+    $layer.on('click', '.btn-box button', $.proxy(this._handleConfirmTime, this, $layer));
+  },
+
+  _handleAreaClick: function (e) {
+    var selectedArea = e.target.getAttribute('data-area-code');
+
+    if (selectedArea !== this._reservationArea) {
+      this._reservationArea = selectedArea;
+      this.$btnArea.text(e.target.innerText);
+      this.$btnArea.removeClass('placeholder');
+    }
+    this._popupService.close();
+  },
+
+  _handleConfirmTime: function ($layer) {
+    var selectedTime = $layer.find('.select-list li.checked input').attr('value');
+
+    if (selectedTime !== this._reservationTime) {
+      this._reservationTime = selectedTime;
+      this.$btnTime.text(selectedTime + ':00');
+    }
+    this._popupService.close();
+  },
+
+  _handleConfirmType: function ($layer) {
+    var selectedItem = $layer.find('.select-list li.checked input');
+    var selectedType = selectedItem.attr('value');
+
+    if (selectedType !== this._reservationType) {
+      this._reservationType = selectedType;
+      this.$btnType.text(selectedItem.attr('title'));
+    }
+    this._popupService.close();
   },
 
   _getTimeData: function (time) {
