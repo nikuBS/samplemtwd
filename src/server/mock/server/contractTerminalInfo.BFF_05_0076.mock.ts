b83269113982ae7a425ba4f2c7c99cefdd21cea0
mackmh <skt.P130714@partner.sk.com>
2018-07-27 10:58:55 +0900

    [김명환/프론트개발] ts 목데이터 싱긍쿼터로 수정

--- src/server/mock/server/contractTerminalInfo.BFF_05_0076.mock.ts
+++ src/server/mock/server/contractTerminalInfo.BFF_05_0076.mock.ts
@@ -1,12 +1,12 @@
 const contractTerminalInfo_BFF_05_0076 = {
-  "code": "00",
-  "msg": "",
-  "result":
+  'code': '00',
+  'msg': '',
+  'result':
     {
-      "invoDt": "2018.05.12",
-      "invCnt": "19",
-      "penEstDcAmt": "-18750",
-      "perEstRt": "0"
+      'invoDt': '2018.05.12',
+      'invCnt': '19',
+      'penEstDcAmt': '-18750',
+      'perEstRt': '0'
     }
 };
 
