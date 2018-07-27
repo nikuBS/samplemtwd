cea4417b0e4dcfd8aaf4fd7dd6d47704ad44b2e2
양지훈 SKtechx <jihun202@sk.com>
2018-07-27 16:03:53 +0900

    [양지훈/Client개발팀] 아웃링크 types 생성

---
+++ src/server/types/outlink.type.ts
@@ -0,0 +1,221 @@
+export const CUSTOMER_PREVENTDAMAGE_GUIDE_VIDEO = [
+  {
+    TEXT: '이메일을 통한 피싱주의',
+    LINK: 'http://m.tv.naver.com/v/3063505'
+  },
+  {
+    TEXT: '안전결제를 사칭한 피싱 사이트 주의',
+    LINK: 'http://m.tv.naver.com/v/1959799'
+  },
+  {
+    TEXT: '문자 메시지를 통한 악성 앱 설치 유도 주의',
+    LINK: 'http://tv.naver.com/v/564945'
+  },
+  {
+    TEXT: '안전한 공유기 사용',
+    LINK: 'http://tv.naver.com/v/564931'
+  },
+  {
+    TEXT: '로그인 피싱',
+    LINK: 'http://tv.naver.com/v/564923'
+  },
+  {
+    TEXT: '공용 와이파이 주의사항',
+    LINK: 'http://tv.naver.com/v/564907'
+  },
+  {
+    TEXT: '공용 컴퓨터 사용시 주의사항',
+    LINK: 'http://tv.naver.com/v/564894'
+  },
+  {
+    TEXT: '안전한 공인인증서 보관 방법',
+    LINK: 'http://tv.naver.com/v/564881'
+  },
+  {
+    TEXT: '파일 다운로드 시 주의사항',
+    LINK: 'http://tv.naver.com/v/564846'
+  },
+  {
+    TEXT: '안전한 PC 사용을 위한 SW 업데이트의 중요성',
+    LINK: 'http://tv.naver.com/v/564858'
+  },
+  {
+    TEXT: '랜섬웨어 감영에 주의하세요',
+    LINK: 'http://tv.naver.com/v/1037441'
+  }
+];
+
+export const CUSTOMER_PREVENTDAMAGE_GUIDE_LATEST = [
+  {
+    TEXT: '랜섬웨어 대응 가이드라인',
+    LINK: 'https://m.blog.naver.com/kisa118/221218861926'
+  },
+  {
+    TEXT: '꼭 지켜야 할 정보보호 실천수칙 10가지',
+    LINK: ''
+  },
+  {
+    TEXT: '무심코 스쳐간 광고! "악성코드에 감염되셨습니다"',
+    LINK: 'https://m.post.naver.com/viewer/postView.nhn?volumeNo=5894346&memberNo=3326308&navigationType=push'
+  },
+  {
+    TEXT: '사이버보안을 위한 10가지 행동 수칙',
+    LINK: 'https://m.post.naver.com/viewer/postView.nhn?volumeNo=5878746&memberNo=3326308&navigationType=push'
+  },
+  {
+    TEXT: '데이터 인질극, 랜섬웨어 예방법',
+    LINK: 'https://m.post.naver.com/viewer/postView.nhn?volumeNo=5544739&memberNo=3326308&navigationType=push'
+  },
+  {
+    TEXT: '소프트웨어 업데이트, 이렇게 하세요!',
+    LINK: 'https://m.post.naver.com/viewer/postView.nhn?volumeNo=5281268&memberNo=3326308&navigationType=push'
+  },
+  {
+    TEXT: '맞춤형 개인정보 통합 클린서비스',
+    LINK: 'https://m.post.naver.com/viewer/postView.nhn?volumeNo=5188513&memberNo=3326308&navigationType=push'
+  },
+  {
+    TEXT: '내 스마트폰을 지키는 악성앱 치료서비스',
+    LINK: 'https://m.post.naver.com/viewer/postView.nhn?volumeNo=5073390&memberNo=3326308&navigationType=push'
+  },
+  {
+    TEXT: '휴대폰 분실 전과 후, 어떻게 대처할까?',
+    LINK: 'http://blog.skbroadband.com/1969'
+  },
+  {
+    TEXT: '해외여행 체크리스트, 요금 폭탄 피하는 법과 로밍 TIP',
+    LINK: 'http://blog.skbroadband.com/1944'
+  },
+  {
+    TEXT: '내 컴퓨터가 스스로 움직인다? 원격제어 악성코드',
+    LINK: 'http://blog.skbroadband.com/1933'
+  },
+  {
+    TEXT: '타깃형 택배 스미싱 등장! 테러 동영상도 주의!',
+    LINK: 'http://blog.skbroadband.com/1895'
+  },
+  {
+    TEXT: '공용 와이파이 주의! 와이파이 악성코드 \'Skype Voice\'',
+    LINK: 'http://blog.skbroadband.com/1886'
+  },
+  {
+    TEXT: '맞춤형 백신, \'전용백신\' 제대로 알고 사용하기',
+    LINK: 'http://blog.skbroadband.com/1872'
+  },
+  {
+    TEXT: '인터넷 브라우저, 깨끗하고 안전하게 사용하기',
+    LINK: 'http://blog.skbroadband.com/1857'
+  },
+  {
+    TEXT: '사이버 응급실 \'모바일 응급 사이버 치료체계\'를 아시나요?',
+    LINK: 'http://blog.skbroadband.com/1848'
+  },
+  {
+    TEXT: '이것만은 꼭 알아두고 가세요! 최신 파싱 및 파밍 유형들',
+    LINK: 'http://blog.skbroadband.com/1841'
+  },
+  {
+    TEXT: '급여 명세서를 사칭한 악성코드 주의!',
+    LINK: 'http://blog.skbroadband.com/1835'
+  },
+  {
+    TEXT: '추석연휴, 스미싱·보이스피싱·승차권사기 보안수칙!',
+    LINK: 'http://blog.skbroadband.com/1833'
+  },
+  {
+    TEXT: '\'어도비 플래시 플레이어\' 사칭 스마트폰 랜섬웨어 주의하세요!',
+    LINK: 'http://blog.skbroadband.com/1829'
+  },
+  {
+    TEXT: '스미싱도 진짜 가짜가 있다? 가짜 메시지 \'혹스(Hoax)\'',
+    LINK: 'http://blog.skbroadband.com/1827'
+  },
+  {
+    TEXT: '메르스를 이용한 신종 스미싱·보이스피싱·악성코드 주의보 발령',
+    LINK: 'http://blog.skbroadband.com/1754'
+  },
+  {
+    TEXT: 'QR코드 금융사기 \'큐싱(Qshing)\'주의보',
+    LINK: 'http://blog.skbroadband.com/1749'
+  },
+  {
+    TEXT: '스마트폰 분실 시 대처법',
+    LINK: 'http://blog.skbroadband.com/1729'
+  },
+  {
+    TEXT: '한국판 랜섬웨어 ‘크립토락커’를 아시나요?',
+    LINK: 'http://blog.skbroadband.com/1707'
+  },
+  {
+    TEXT: '오토런 바이러스로부터 내 컴퓨터 안전하게 보호하는 방법',
+    LINK: 'http://blog.skbroadband.com/1693'
+  },
+  {
+    TEXT: '명의도용 피해예방법',
+    LINK: ''
+  },
+  {
+    TEXT: '소액결제 피해예방법',
+    LINK: ''
+  },
+  {
+    TEXT: '결합상품 피해예방법',
+    LINK: ''
+  },
+  {
+    TEXT: '해외 로밍 피해예방법',
+    LINK: ''
+  },
+  {
+    TEXT: '무선 공유기 해킹 주의보!',
+    LINK: ''
+  },
+  {
+    TEXT: 'PC타고 스마트폰까지 침투하는 악성코드 주의보',
+    LINK: 'http://blog.skbroadband.com/1612'
+  },
+  {
+    TEXT: '스미싱 피해예방 안내',
+    LINK: ''
+  },
+  {
+    TEXT: '메모리 해킹 주의보',
+    LINK: 'http://blog.skbroadband.com/764'
+  },
+  {
+    TEXT: '파밍 피해예방 방법',
+    LINK: 'http://blog.skbroadband.com/1215'
+  },
+  {
+    TEXT: '클라우드 보안법',
+    LINK: 'http://blog.skbroadband.com/1398'
+  },
+  {
+    TEXT: '스마트폰 보안 수칙',
+    LINK: 'http://blog.skbroadband.com/642'
+  },
+  {
+    TEXT: '스팸 메시지 대응방법',
+    LINK: 'http://blog.skbroadband.com/950'
+  },
+  {
+    TEXT: '안전한 무선인터넷 사용법',
+    LINK: 'http://blog.skbroadband.com/1368'
+  },
+  {
+    TEXT: '인터넷프라이버시 가이드',
+    LINK: 'http://blog.skbroadband.com/979'
+  },
+  {
+    TEXT: '웹 보안 서비스 안내',
+    LINK: 'http://blog.skbroadband.com/805'
+  },
+  {
+    TEXT: 'PC보호 프로그램 안내',
+    LINK: 'http://blog.skbroadband.com/383'
+  },
+  {
+    TEXT: '유출된 개인정보로 인한 피해 최소화 방법',
+    LINK: 'http://blog.skbroadband.com/1729'
+  }
+];
