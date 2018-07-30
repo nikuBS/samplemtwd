686eccf6b874706d7a1ff89faf84f0d05b49c6da
KimInHwan <skt.p132150@partner.sk.com>
2018-07-30 09:58:42 +0900

    [김인환/SPTek] MyT > 나의 가입서비스 > 가입정보 > 고객보호설정   - MY_01_02_07 화면 사용하지 않고 CO_01_02_03_P01로 변경되어     작업중 (완료되면 이전화면 제거예정)
    * 공통모듈 > InputHelper > 입력 시 숫자만 입력 받도록 하는
      기능 추가

--- src/client/app/01.myt/joinService/myt.joinService.protect.inquiry.js
+++ src/client/app/01.myt/joinService/myt.joinService.protect.inquiry.js
@@ -3,10 +3,10 @@
  * Author: Kim Inhwan (skt.P132150@partner.sk.com)
  * Date: 2018.07.24
  */
-Tw.MyTJSProtectInquiry = function ($element) {
+Tw.MyTJSProtectInquiry = function ($element, svcInfo) {
+  this._svcInfo = svcInfo;
   this.$container = $element;
-  this._apiService = Tw.Api;
-  this._popupService = Tw.Popup;
+  this._loginService = new Tw.MyTJSProtectPwd();
   this._rendered();
   this._bindEvent();
 };
@@ -26,32 +26,166 @@ Tw.MyTJSProtectInquiry.prototype = {
 
   _onOkClicked: function (/*event*/) {
     // 비밀번호 확인
-    this._popupService.openConfirm(Tw.POPUP_TITLE.NOTIFY, Tw.MSG_MYT.JOIN_SERVICE_PROTECT_TEST, null, null, $.proxy(this._onOkPopupClicked, this));
+    var params = {
+      mdn: this._svcInfo.svcNum,
+      serviceNumber: this._svcInfo.svcMgmtNum,
+      callback: $.proxy(this._onLayerOpened, this)
+    };
+    this._loginService.openLayer(params);
+  }
+};
+
+Tw.MyTJSProtectPwd = function () {
+  this._apiService = Tw.Api;
+  this._popupService = Tw.Popup;
+
+  // error when login
+  this._loginErrorCode = {
+    UNDER_3: 'ICAS3213',       // 고객보호비밀번호 오입력(3회 미만)
+    ERROR_3: 'ICAS3212',       // 고객보호비밀번호 오입력(3회)
+    ERROR_4: 'ICAS3215',       //고객보호비밀번호 오입력 (4회)
+    BLOCKED: 'ICAS3216'        //고객보호비밀번호 잠김 (지점 내점 안내 노출)
+  };
+
+  this._lineChangeErrorCode = {
+    ERROR_1: 'ICAS3481',       // 고객보호비밀번호 입력 오류 1회
+    ERROR_2: 'ICAS3482',       // 고객보호비밀번호 입력 오류 2회
+    ERROR_3: 'ICAS3483',       // 고객보호비밀번호 입력 오류 3회
+    ERROR_4: 'ICAS3484',       // 고객보호비밀번호 입력 오류 4회
+    BLOCKED: 'ICAS3215'       // 고객보호비밀번호 입력 오류 5회 (잠김예정)
+  };
+
+};
+
+Tw.MyTJSProtectPwd.prototype = {
+  _onPopupOpend: function ($layer) {
+    this.$container = $layer;
+
+    this.$container.find('.badge').hide();
+    this.$container.find('.circuit-tx').text(this._mdn);
+    this._cachedElement();
+    this._bindEvent();
+  },
+  _cachedElement: function () {
+    this.$inputBox = this.$container.find('.inputbox');
+    this.$pwdInput = this.$inputBox.find('input');
+    this.$deleteIcon = this.$container.find('.ico');
+    this.$errMsg = this.$container.find('.error-txt');
+    this.$pwd = this.$container.find('input');
+    this.$pwd.val('');
+  },
+  _bindEvent: function () {
+    this.$container.on('click', '.bt-red1 > button', $.proxy(this._requestLogin, this));
+    this.$deleteIcon.on('click', $.proxy(this._removePwd, this));
+    this.$pwdInput.on('keydown', $.proxy(this._onKeyDown, this))
+      .on('keyup', $.proxy(this._onKeyUp, this))
+      .on('focusout', $.proxy(this._onFocusOut, this));
+  },
+  _removePwd: function () {
+    this.$pwd.val('');
+  },
+  _requestLogin: function () {
+    var pwd = this.$pwd.val();
+    var api = Tw.API_CMD.BFF_05_0069;
+    var data = {
+      svcPwd: pwd
+    };
+    this._apiService.request(api, data)
+      .done($.proxy(this._onLoginReuestDone, this))
+      .fail(function (err) {
+        Tw.Logger.error('auth.login.service.pwd Fail', err);
+      });
+  },
+  _onLoginReuestDone: function (res) {
+    if ( res.code === Tw.API_CODE.CODE_00 ) {
+      this._onSuccess(res);
+    }
+    else {
+      var errCount = 0;
+      var unexpectedError = false;
+      switch ( res.code ) {
+        case this._lineChangeErrorCode.ERROR_1:
+          errCount = 1;
+          break;
+        case this._lineChangeErrorCode.ERROR_2:
+          errCount = 2;
+          break;
+        case this._lineChangeErrorCode.ERROR_3:
+        case this._loginErrorCode.ERROR_3:
+          errCount = 3;
+          break;
+        case this._lineChangeErrorCode.ERROR_4:
+        case this._loginErrorCode.ERROR_4:
+          errCount = 4;
+          break;
+        case this._lineChangeErrorCode.BLOCKED:
+        case this._loginErrorCode.BLOCKED:
+          this._showFail();
+          return;
+        default:
+          unexpectedError = true;
+          break;
+      }
+
+      if ( errCount >= 1 ) {
+        this.$inputBox.addClass('error');
+        this.$deleteIcon.removeClass('none');
+        this.$errMsg.removeClass('none');
+
+        this.$errMsg.text(this._changeCount(this.$errMsg.text(), errCount));
+      }
+
+      var errorMsg = unexpectedError ? res.code + ' ' + res.msg : Tw.MSG_AUTH.LOGIN_A01;
+      this._popupService.openAlert(errorMsg);
+    }
+  },
+  _onKeyUp: function(event) {
+    Tw.InputHelper.keyup(event);
   },
 
-  _onOkPopupClicked: function () {
-    this._requestProtectPwd();
+  _onKeyDown: function(event) {
+    Tw.InputHelper.keydown(event);
   },
 
-  _goToComplete: function () {
-    // api 성공 후 처리
+  _onFocusOut: function(event) {
+    var $target = $(event.target);
+    Tw.InputHelper.focusout($target);
   },
 
-  _requestProtectPwd: function (/*event*/) {
-    // var api = Tw.API_CMD.BFF_05_0069;
-    // this._apiService
-    //   .request(api, {})
-    //   .done($.proxy(this._onApiSuccess, this))
-    //   .fail($.proxy(this._onApiError, this));
+  _changeCount: function (msg, count) {
+    return msg.replace(/\d/, count);
+  },
+  _onPwdPopupClosed: function () {
+    if ( !this._isCloseCallbackNeeded ) {
+      return;
+    }
+    if ( this._pwdSuccess ) {
+      this._callback();
+    }
+    else {
+      this._popupService.open({ hbs: 'CO_01_02_03_P01_P01' }, function ($layer) {
+        $layer.on('click', '.bt-red1 > button', function () {
+          Tw.Popup.close();
+        });
+        // TODO: Insert href for a tags when routings ready
+      });
+    }
+  },
+  _onSuccess: function () {
+    this._pwdSuccess = true;
+    this._isCloseCallbackNeeded = true;
     this._popupService.close();
   },
-
-  _onApiSuccess: function (params) {
-    Tw.Logger.info(params);
+  _showFail: function () {
+    this._pwdSuccess = false;
+    this._isCloseCallbackNeeded = true;
+    this._popupService.close();
   },
-
-  _onApiError: function (params) {
-    // API 호출 오류
-    Tw.Logger.warn(params);
+  openLayer: function (params) {
+    this._mdn = params.mdn;
+    this._callback = params.callback;
+    this._popupService.open({ hbs: 'CO_01_02_03_P01' },
+      $.proxy(this._onPopupOpend, this),
+      $.proxy(this._onPwdPopupClosed, this));
   }
 };
--- src/client/utils/input.helper.js
+++ src/client/utils/input.helper.js
@@ -4,7 +4,49 @@ Tw.InputHelper = (function () {
     $input.val($input.val().replace(/[^0-9]/g, ''));
   }
 
+  function inputKeyDown(event) {
+    // input keydown event (only input number)
+    var key = event.which;
+    if ( key === 0 || key === 8 || key === 46 || key === 9 ) {
+      if ( typeof event.stopPropagation !== 'undefined' ) {
+        event.stopPropagation();
+      }
+      else {
+        event.cancelBubble = true;
+      }
+      return;
+    }
+    if ( key < 48 || (key > 57 && key < 96) || key > 105 || event.shiftKey ) {
+      if ( event.preventDefault ) {
+        event.preventDefault();
+      }
+      else {
+        event.returnValue = false;
+      }
+    }
+  }
+
+  function inputKeyUp(event) {
+    // input keyup event (only input number)
+    var key = event.which;
+    if ( key === 8 || key === 46 || key === 37 || key === 39 ) {
+      return;
+    }
+    else {
+      event.target.value = event.target.value.replace(/[^0-9]/g, '');
+    }
+  }
+
+  function inputFocusOut($elem) {
+    // input focusout event (only input number)
+    $elem.val($elem.val().replace(/[^0-9]/g, ''));
+  }
+
   return {
-    inputNumberOnly: inputNumberOnly
+    inputNumberOnly: inputNumberOnly,
+    // 내용 확인하여 수정필요
+    focusout: inputFocusOut,
+    keyup: inputKeyUp,
+    keydown: inputKeyDown
   };
 })();
