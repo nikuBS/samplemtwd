2d2468a0f15219d27064a33888343f16dcd9b85a
양지훈 SKtechx <jihun202@sk.com>
2018-07-30 17:26:06 +0900

    [양지훈/Client개발팀] 이용자 피해예방 주의보, 공지사항

--- src/client/app/08.customer/preventdamage/customer.preventdamage.latestwarning.js
+++ src/client/app/08.customer/preventdamage/customer.preventdamage.latestwarning.js
@@ -6,6 +6,9 @@
 
 Tw.CustomerPreventdamageLatestwarning = function(rootEl) {
   this.$container = rootEl;
+  this._apiService = Tw.Api;
+  this._template = Handlebars.compile($('#tpl_latestwarning_list_item').html());
+  this._page = 1;
 
   this._cachedElement();
   this._bindEvent();
@@ -13,10 +16,45 @@ Tw.CustomerPreventdamageLatestwarning = function(rootEl) {
 
 Tw.CustomerPreventdamageLatestwarning.prototype = {
   _cachedElement: function() {
-    //
+    this.$list = this.$container.find('.fe-list');
+    this.$btnMoreList = this.$container.find('.fe-btn_more_list');
   },
 
   _bindEvent: function() {
-    //
+    this.$btnMoreList.on('click', $.proxy(this._loadMoreList, this));
+  },
+
+  _loadMoreList: function() {
+    this._apiService.request(Tw.API_CMD.BFF_08_0033, {
+      page: this._page, size: 20
+    }).done($.proxy(this._appendMoreList, this));
+  },
+
+  _getRemainCount: function(param) {
+    var count = param.total - ((++param.page) * param.size);
+    return count < 0 ? 0 : count;
+  },
+
+  _appendMoreList: function(res) {
+    if (res.code !== Tw.API_CODE.CODE_00) return this._apiError(res);
+    this.$list.append(this._template({
+      list: res.result.content
+    }));
+
+    if (res.result.last) this.$btnMoreList.remove();
+    else {
+      this.$btnMoreList.html(Tw.BUTTON_LABEL.MORE + '<span>(' + this._getRemainCount({
+        total: res.result.totalElements,
+        page: res.result.pageable.pageNumber,
+        size: res.result.pageable.pageSize
+      })  + ')</span>');
+      this._page++;
+    }
+  },
+
+  _apiError: function (err) {
+    Tw.Logger.error(err.code, err.msg);
+    this._popupService.openAlert(Tw.MSG_COMMON.SERVER_ERROR + '<br />' + err.code + ' : ' + err.msg);
+    return false;
   }
 };
