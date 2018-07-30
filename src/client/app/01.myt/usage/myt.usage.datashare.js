de813f69985f0f88ca9c1c2414dbf918ffcb916f
jungminlee <wpeofh01@gmail.com>
2018-07-30 17:27:17 +0900

    [이정민/SPTEK] * myt > 실시간 사용량 - 데이터함께쓰기 mock데이터 적용

--- src/client/app/01.myt/usage/myt.usage.datashare.js
+++ src/client/app/01.myt/usage/myt.usage.datashare.js
@@ -1,29 +1,54 @@
 Tw.MytUsageDataShare = function (rootEl) {
   this.$container = rootEl;
-  this._apiService = new Tw.ApiService();
+  this._apiService = Tw.Api;
+  this._popupService = Tw.Popup;
+  this._history = new Tw.HistoryService(rootEl);
 
   this._bindEvent();
 };
 
 Tw.MytUsageDataShare.prototype = {
   _bindEvent: function () {
-    this.$container.on('click', '.get-usage-data', $.proxy(this._getUsageData, this));
+    this.$container.on('click', '.prev-step', $.proxy(this._onClickPrevStep, this));
+    this.$container.on('click', '.fe-btn-used-data', $.proxy(this._onClickBtnUsedData, this));
   },
-  _getUsageData: function ($event) {
+  _onClickBtnUsedData: function ($event) {
     $event.preventDefault();
 
     var targetSelector = $($event.target);
-    var cSvcMgmtNum = targetSelector.data('value');
+    var svcMgmtNum = targetSelector.data('svcmgmtnum');
 
-    this._apiService.request(Tw.API_CMD.BFF_05_0009, { cSvcMgmtNum: cSvcMgmtNum })
-      .done($.proxy(this._success, this, targetSelector))
-      .fail($.proxy(this._fail, this));
+    this._apiService.request(Tw.API_CMD.BFF_05_0009, { cSvcMgmtNum: svcMgmtNum })
+      .done($.proxy(this._requestDone, this, targetSelector))
+      .fail($.proxy(this._requestFail, this));
   },
-  _success: function (targetSelector, res) {
-    targetSelector.siblings('em').text(res.result.used);
-    targetSelector.hide();
+
+  _requestDone: function (targetSelector, resp) {
+    if ( resp.code === '00' ) {
+      targetSelector.siblings('em').text(resp.result.used);
+      targetSelector.hide();
+    } else {
+      if ( resp.data ) {
+        this._showErrorAlert(resp.data && resp.data.msg);
+      } else {
+        if ( resp.error ) {
+          this._showErrorAlert(resp.error.msg);
+        } else {
+          this._showErrorAlert(resp.msg);
+        }
+      }
+    }
   },
-  _fail: function (err) {
-    console.log('data-sharing child api error', err);
+
+  _requestFail: function (resp) {
+    this._showErrorAlert(resp.data && resp.data.msg);
+  },
+
+  _showErrorAlert: function (msg) {
+    this._popupService.openAlert(msg);
+  },
+
+  _onClickPrevStep: function () {
+    this._history.goBack();
   }
 };
--- src/server/app/01.myt/controllers/usage/myt.usage.data-share.controller.ts
+++ src/server/app/01.myt/controllers/usage/myt.usage.data-share.controller.ts
@@ -2,7 +2,7 @@ import TwViewController from '../../../../common/controllers/tw.view.controller'
 import { Request, Response, NextFunction } from 'express';
 import DateHelper from '../../../../utils/date.helper';
 import { API_CMD, API_CODE } from '../../../../types/api-command.type';
-import { USER_CNT } from '../../../../types/string.type';
+import { DATA_UNIT, USER_CNT } from '../../../../types/string.type';
 import FormatHelper from '../../../../utils/format.helper';
 import { UNIT } from '../../../../types/bff-common.type';
 import MyTUsage from './myt.usage.controller';
@@ -19,8 +19,17 @@ class MyTUsageDataShare extends TwViewController {
     Observable.combineLatest(
       this.getUsageData()
     ).subscribe(([usageData]) => {
-      this.myTUsage.renderView(res, 'usage/myt.usage.data-share.html', this.getData(usageData, svcInfo));
+      this.renderView(res, 'usage/myt.usage.data-share.html', this.getData(usageData, svcInfo));
     });
+  }
+
+  private renderView(res: Response, view: string, data: any): any {
+    if ( data.usageData.code === undefined ) {
+      res.render(view, data);
+    } else {
+      res.render(view, data);
+      // res.render(MYT_VIEW.ERROR, data);
+    }
   }
 
   private getUsageData(): Observable<any> {
@@ -30,22 +39,60 @@ class MyTUsageDataShare extends TwViewController {
   }
 
   private getResult(resp: any, usageData: any): any {
-    if (resp.code === API_CODE.CODE_00) {
-      usageData = this.parseData(resp.result);
+    if ( resp.code === API_CODE.CODE_00 ) {
+      usageData = this.parseData(resp.result.data);
     } else {
       usageData = resp;
     }
     return usageData;
   }
 
-  private parseData(usageData: any): any {
-    usageData.userCnt = USER_CNT[usageData.childList.length - 1];
-    usageData.showTotal = FormatHelper.convDataFormat(usageData.data.used, UNIT[usageData.data.unit]);
-    usageData.childList.map((data) => {
-      data.setDate = DateHelper.getShortDateNoDot(data.auditDtm);
-      data.showUsed = FormatHelper.convDataFormat(data.used, UNIT[data.unit]);
-    });
-    return usageData;
+  private parseData(_usageData: any): any {
+    const data = {
+      'used': '900',
+      'childList': [{
+        'svcNum': '010-45**-12**',
+        'svcMgmtNum': '7200XXXX',
+        'feeProdId': '상품ID',
+        'feeProdNm': 'LTE함께쓰기Basic',
+        'auditDtm': '20150113'
+      }, {
+        'svcNum': '010-45**-11**',
+        'svcMgmtNum': '7200XXXX',
+        'feeProdId': '상품ID',
+        'feeProdNm': 'LTE함께쓰기Basic',
+        'auditDtm': '20150113'
+      }]
+    };
+    data.used = FormatHelper.convDataFormat(data.used, DATA_UNIT.KB);
+    if ( data.used['unit'] !== DATA_UNIT.KB ) {
+      data['usedSub'] = this.getSubData(data.used);
+    }
+    if ( data.childList.length > 0 ) {
+      data.childList.map(function (child) {
+        child['auditDtm'] = DateHelper.getShortDateNoDot(child['auditDtm']);
+      });
+      data['korLengStr'] = (data.childList.length < 6) ? USER_CNT[data.childList.length - 1] : data.childList.length;
+    }
+
+    return data;
+  }
+
+
+  private getSubData(data: any): any {
+    const subData = {
+      data: FormatHelper.convNumFormat(data.data.replace(/,/gi, '') * 1024)
+    };
+    switch ( data.unit ) {
+      case DATA_UNIT.GB:
+        subData['unit'] = DATA_UNIT.MB;
+        break;
+      case DATA_UNIT.MB:
+        subData['unit'] = DATA_UNIT.KB;
+        break;
+    }
+    return subData;
+
   }
 
   private getData(usageData: any, svcInfo: any): any {
