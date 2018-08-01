de749b44e63beaecff42dc61f7475914f80ac667
양지훈 SKtechx <jihun202@sk.com>
2018-08-01 17:36:33 +0900

    [양지훈/Client개발팀] 고객센터 > 이용자피해예방가이드 아웃링크 openAlert 처리, 미사용 js 제거

--- src/client/app/08.customer/preventdamage/customer.preventdamage.relatesite.js
+++ src/client/app/08.customer/preventdamage/customer.preventdamage.relatesite.js
@@ -6,17 +6,29 @@
 
 Tw.CustomerPreventdamageRelatesite = function(rootEl) {
   this.$container = rootEl;
+  this._popupService = Tw.Popup;
+  this._history = new Tw.HistoryService();
 
-  this._cachedElement();
   this._bindEvent();
 };
 
 Tw.CustomerPreventdamageRelatesite.prototype = {
-  _cachedElement: function() {
-    //
-  },
 
   _bindEvent: function() {
-    //
+    this._popupService.close();
+    if (Tw.BrowserHelper.isApp()||true) {
+      this.$container.on('click', '.fe-outlink', $.proxy(this._openOutlink, this));
+    }
+  },
+
+  _openOutlink: function(e) {
+    this._popupService.openAlert('3G/LTE망 사용시 데이터 요금이 발생됩니다.', null, $.proxy(function() {
+      window.open($(e.currentTarget).attr('href'));
+      this._popupService.close();
+    }, this));
+
+    e.preventDefault();
+    e.stopPropagation();
   }
+
 };
