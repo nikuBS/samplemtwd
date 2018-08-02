9af8f6a6824a3514da110d5c0a76feee66962ee6
조아라(Ara Jo)/Platform개발3팀/SKTECH <araara.jo@sk.com>
2018-08-02 14:45:40 +0900

    [조아라/Client개발팀] 간편로그인 에러케이스 추가

--- src/client/app/09.auth/login/auth.login.easy-ios.js
+++ src/client/app/09.auth/login/auth.login.easy-ios.js
@@ -7,6 +7,7 @@
 Tw.AuthLoginEasyIos = function (rootEl) {
   this.$container = rootEl;
   this._apiService = Tw.Api;
+  this._popupService = Tw.Popup;
 
   this.$inputName = null;
   this.$inputBirth = null;
@@ -24,6 +25,10 @@ Tw.AuthLoginEasyIos.prototype = {
     '1': 'MALE',
     '2': 'FEMALE'
   },
+  ERROR_CODE: {
+    ATH1004: 'ATH1004',   // 입력하신 정보가 일치하지 않습니다. 확인 후 재입력해 주세요.
+    ATH1005: 'ATH1005'    // 휴대폰번호 입력오류
+  },
   _bindEvent: function () {
     this.$inputName = this.$container.find('#fe-input-name');
     this.$inputBirth = this.$container.find('#fe-input-birth');
@@ -37,13 +42,27 @@ Tw.AuthLoginEasyIos.prototype = {
     this.$btLogin.on('click', $.proxy(this._onClickLogin, this));
     this.$inputMdn.on('keyup', $.proxy(this._onKeyupMdn, this));
     this.$inputGender.on('click', $.proxy(this._onClickGender, this));
+    this.$inputBirth.on('input', $.proxy(this._onInputBirth, this));
+    this.$inputCert.on('input', $.proxy(this._onInputCert, this));
   },
   _onKeyupMdn: function () {
     var mdnLength = this.$inputMdn.val().length;
-    if ( mdnLength === 10 || mdnLength === 11 ) {
+    if ( mdnLength === Tw.MIN_MDN_LEN || mdnLength === Tw.MAX_MDN_LEN ) {
       this.$btCert.attr('disabled', false);
     } else {
       this.$btCert.attr('disabled', true);
+    }
+  },
+  _onInputBirth: function () {
+    var inputBirth = this.$inputBirth.val();
+    if ( inputBirth.length >= Tw.IOS_BIRTH_LEN ) {
+      this.$inputBirth.val(inputBirth.slice(0, Tw.IOS_BIRTH_LEN));
+    }
+  },
+  _onInputCert: function () {
+    var inputCert = this.$inputCert.val();
+    if ( inputCert.length >= Tw.DEFAULT_CERT_LEN ) {
+      this.$inputCert.val(inputCert.slice(0, Tw.DEFAULT_CERT_LEN));
     }
   },
   _onClickGender: function ($event) {
@@ -56,23 +75,32 @@ Tw.AuthLoginEasyIos.prototype = {
     $currentTarget.attr('aria-checked', true);
   },
   _onClickCert: function () {
-    var mdn = this.$inputMdn.val();
-    this._apiService.request(Tw.API_CMD.BFF_03_0019, {}, {}, mdn)
-      .done($.proxy(this._successRequestCert, this));
+    if ( this._checkCertValidation() ) {
+      var mdn = this.$inputMdn.val();
+      this._apiService.request(Tw.API_CMD.BFF_03_0019, {}, {}, mdn)
+        .done($.proxy(this._successRequestCert, this));
+    }
   },
   _successRequestCert: function (resp) {
     if ( resp.code === Tw.API_CODE.CODE_00 ) {
       this.$btLogin.attr('disabled', false);
+      this._popupService.openAlert(Tw.MSG_AUTH.EASY_LOGIN_L04);
     }
   },
   _onClickLogin: function () {
-    var params = {
-      svcNum: this.$inputMdn.val(),
-      mbrNm: this.$inputName.val(),
-      birthDt: this.$inputBirth.val(),
-      gender: this.GENDER_CODE[this.$inputGender.filter(':checked').val()],
-      authNum: this.$inputCert.val()
-    };
+    var inputCert = this.$inputCert.val();
+    if ( this._checkLoginValidation(inputCert) ) {
+      var params = {
+        svcNum: this.$inputMdn.val(),
+        mbrNm: this.$inputName.val(),
+        birthDt: this.$inputBirth.val(),
+        gender: this.GENDER_CODE[this.$inputGender.filter(':checked').val()],
+        authNum: inputCert
+      };
+      this._requestLogin(params);
+    }
+  },
+  _requestLogin: function (params) {
     this._apiService.request(Tw.API_CMD.BFF_03_0018, params)
       .done($.proxy(this._successRequestLogin, this));
   },
@@ -81,5 +109,30 @@ Tw.AuthLoginEasyIos.prototype = {
     if ( resp.code === Tw.API_CODE.CODE_00 ) {
 
     }
+  },
+  _checkCertValidation: function () {
+    var inputName = this.$inputName.val();
+    var inputBirth = this.$inputBirth.val();
+    if ( Tw.FormatHelper.isEmpty(inputName) ) {
+      this._popupService.openAlert(Tw.MSG_AUTH.EASY_LOGIN_L11);
+      return false;
+    } else if ( Tw.FormatHelper.isEmpty(inputBirth) ) {
+      this._popupService.openAlert(Tw.MSG_AUTH.EASY_LOGIN_L12);
+      return false;
+    } else if ( this.$inputGender.filter(':checked').length === 0 ) {
+      this._popupService.openAlert(Tw.MSG_AUTH.EASY_LOGIN_L13);
+      return false;
+    } else if ( inputBirth.length !== Tw.IOS_BIRTH_LEN ) {
+      this._popupService.openAlert(Tw.MSG_AUTH.EASY_LOGIN_L02);
+      return false;
+    }
+    return true;
+  },
+  _checkLoginValidation: function (inputCert) {
+    if ( Tw.FormatHelper.isEmpty(inputCert) ) {
+      this._popupService.openAlert(Tw.MSG_AUTH.EASY_LOGIN_L61);
+      return false;
+    }
+    return true;
   }
 };
