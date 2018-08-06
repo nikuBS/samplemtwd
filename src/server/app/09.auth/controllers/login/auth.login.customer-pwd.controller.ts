2ec289b10d3fcf87656c0d6d323e5b9040d00b2c
Hakjoon Sim <hakjoon.sim@sk.com>
2018-08-06 14:02:59 +0900

    [심학준/Client개발팀] Change url for service-pwd
     - service-pwd -> customer-pwd
     - for build order

--- src/server/app/09.auth/controllers/login/auth.login.customer-pwd.controller.ts
+++ src/server/app/09.auth/controllers/login/auth.login.customer-pwd.controller.ts
@@ -1,5 +1,5 @@
 /**
- * FileName: auth.login.service-pwd-fail.controller.ts
+ * FileName: auth.login.customer-pwd.controller.ts
  * Author: Hakjoon Sim (hakjoon.sim@sk.com)
  * Date: 2018.07.11
  */
@@ -7,14 +7,14 @@
 import TwViewController from '../../../../common/controllers/tw.view.controller';
 import { Request, Response, NextFunction } from 'express';
 
-class AuthLoginServicePwdFail extends TwViewController {
+class AuthLoginCustomerPwd extends TwViewController {
   constructor() {
     super();
   }
 
   render(req: Request, res: Response, next: NextFunction, svcInfo: any) {
-    res.render('login/auth.login.service-pwd-fail.html', { svcInfo: svcInfo });
+    res.render('login/auth.login.customer-pwd.html', { svcInfo: svcInfo });
   }
 }
 
-export default AuthLoginServicePwdFail;
+export default AuthLoginCustomerPwd;
