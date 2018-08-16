7568d8a8312cb8abff86ee690068c97c87d046a8
양지훈 SKtechx <jihun202@sk.com>
2018-08-16 14:55:23 +0900

    [양지훈/Client개발팀] MyT > 나의 가입 서비스 > 요금상품 파일생성
    - 인터넷
    - 요금제 변경 가능일 알림 서비스

--- src/server/app/01.myt/controllers/join/myt.join.product-service.controller.ts
+++ src/server/app/01.myt/controllers/join/myt.join.product-service.controller.ts
@@ -4,16 +4,39 @@
  * Date: 2018.08.13
  */
 import { NextFunction, Request, Response } from 'express';
+import { API_CMD } from '../../../../types/api-command.type';
 import TwViewController from '../../../../common/controllers/tw.view.controller';
+import FormatHelper from '../../../../utils/format.helper';
+import { Observable } from '../../../../../../node_modules/rxjs';
+import { SVC_CDNAME } from '../../../../types/bff.type';
 
 class MytJoinProductServiceController extends TwViewController {
   constructor() {
     super();
   }
 
+  private _convertDataFeePlan(data: any): any {
+    if (FormatHelper.isEmpty(data)) {
+      return {
+        tClassProList: []
+      };
+    }
+  }
+
   render(req: Request, res: Response, next: NextFunction, svcInfo: any) {
-    res.render('join/myt.join.product-service.html', {
-      svcInfo: svcInfo
+    const feePlanWirelessApi: Observable<any> = this.apiService.request(API_CMD.BFF_05_0136, {}, {});
+    const feePlanWireApi: Observable<any> = this.apiService.request(API_CMD.BFF_05_0128, {}, {});
+
+    Observable.combineLatest(
+      feePlanWireApi,
+      feePlanWirelessApi
+    ).subscribe(([productServiceList]) => {
+      console.log(productServiceList);
+      res.render('join/myt.join.product-service.html', {
+        svcInfo: svcInfo,
+        svcCdName: SVC_CDNAME,
+        feePlan: this._convertDataFeePlan([])
+      });
     });
   }
 }
