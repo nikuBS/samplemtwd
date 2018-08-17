e9795a4ed5d69dc82d761945e2f6e987beae7f84
jiyoung.jo <jiyoungjo@sk.com>
2018-08-17 13:24:36 +0900

    [조지영/Client개발팀] 가입상품 결합상품 조회 페이지 추가

--- src/server/app/01.myt/controllers/join/myt.join.product-service.controller.ts
+++ src/server/app/01.myt/controllers/join/myt.join.product-service.controller.ts
@@ -7,8 +7,22 @@ import { NextFunction, Request, Response } from 'express';
 import { API_CMD } from '../../../../types/api-command.type';
 import TwViewController from '../../../../common/controllers/tw.view.controller';
 import FormatHelper from '../../../../utils/format.helper';
-import { Observable } from '../../../../../../node_modules/rxjs';
+import { Observable } from 'rxjs';
 import { SVC_CDNAME } from '../../../../types/bff.type';
+import { Combinations } from '../../../../mock/server/myt.join.product-service.mock';
+import { MYT_COMBINATION_TYPE } from '../../../../types/string.type';
+
+interface ICombination {
+  prodId: string;  // 상품 코드
+  prodNm: string;  // 상품명
+  scrbDt: string;  // 가입 일자
+  prodSmryDesc: string;  // 상품 요약 설명
+  items: { icon: string; description: string; }[]
+};
+
+interface ICombinationList {
+  [key: string]: ICombination;
+}
 
 class MytJoinProductServiceController extends TwViewController {
   constructor() {
@@ -26,6 +40,7 @@ class MytJoinProductServiceController extends TwViewController {
   render(req: Request, res: Response, next: NextFunction, svcInfo: any) {
     const feePlanWirelessApi: Observable<any> = this.apiService.request(API_CMD.BFF_05_0136, {}, {});
     const feePlanWireApi: Observable<any> = this.apiService.request(API_CMD.BFF_05_0128, {}, {});
+    const combinations: ICombinationList = Combinations;
 
     Observable.combineLatest(
       feePlanWireApi,
@@ -35,9 +50,55 @@ class MytJoinProductServiceController extends TwViewController {
       res.render('join/myt.join.product-service.html', {
         svcInfo: svcInfo,
         svcCdName: SVC_CDNAME,
-        feePlan: this._convertDataFeePlan([])
+        feePlan: this._convertDataFeePlan([]),
+        combinations
       });
     });
+  }
+
+  private getCombinations = (): Observable<ICombinationList> => {
+    return this.apiService.request(API_CMD.BFF_05_0133, {}).map((resp: { code: string, result: { combinationWireMemberList?: any[], combinationWirelessMemberList?: any[] } }) => {
+      const combinations: ICombinationList = {};
+      const wireless = resp.result.combinationWirelessMemberList;
+      const wire = resp.result.combinationWireMemberList;
+
+      if (wireless) {
+        for (let i = 0; i < wireless.length; i++) {
+          const item = wireless[i];
+          combinations[item.expsOrder] = this.getProperCombination(item);
+        }
+      }
+
+      if (wire) {
+        for (let i = 0; i < wire.length; i++) {
+          const item = wire[i];
+          combinations[item.expsOrder] = this.getProperCombination(item);
+        }
+      }
+
+      return combinations;
+    });
+  }
+
+  private getProperCombination = (item: any): ICombination => {
+    const nItem: ICombination = {
+      prodId: item.prodId,
+      prodNm: item.prodNm,
+      scrbDt: item.scrbDt,
+      prodSmryDesc: item.prodSmryDesc,
+      items: []
+    };
+
+    switch (item.prodId) {
+      case 'TW20000008':
+        nItem.items.push({
+          icon: 'line',
+          description: MYT_COMBINATION_TYPE.MULTI_ONE
+        });
+        break;
+    }
+
+    return nItem;
   }
 }
 
