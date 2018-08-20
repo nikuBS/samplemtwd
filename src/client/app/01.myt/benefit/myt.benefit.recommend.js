1264146a4d1f9f7e66c2272a1e866122dc9041ea
mackmh <skt.P130714@partner.sk.com>
2018-08-20 11:29:26 +0900

    [김명환/프론트개발]  추천혜택 개발

--- src/client/app/01.myt/benefit/myt.benefit.recommend.js
+++ src/client/app/01.myt/benefit/myt.benefit.recommend.js
@@ -21,25 +21,78 @@ Tw.MytBenefitRecommend = function (rootEl, resData) {
   this._history = new Tw.HistoryService(this.$container);
   this._history.init('hash');
 
+  this.bffListData = null; //원본 리스트 데이터
+
+  this.detailListObj = {
+    listData: null,
+    curLen : 0, //현재 데이터 카운트
+    startCount: Tw.DEFAULT_LIST_COUNT, // 시작 데이터 카운트
+    addCount: Tw.DEFAULT_LIST_COUNT, // 추가 데이터 카운트
+    viewData: [] // 잘라서 넣는 데이터
+  };
+
   this._init();
 };
 
 Tw.MytBenefitRecommend.prototype = {
   _init: function () {
     Tw.Logger.info('[Client Init]');
-    // this._getDetailList();
-    // this._bindEvent();
+    this._proData();
+    this._ctrlInit();
+    this._bindEvent();
   },
   _cachedElement: function () {
-    // this.$tgTpl = $('#fe-contract-terminal-detail');
-    // this.$tgDetailList = $('[data-target="detailList"]');
-    // this.$curNum = $('[data-target="curNum"]');
-    // this.$addBtnArea = $('[data-target="addBtnArea"]');
+    this.$tgTpl = $('#fe-contract-terminal-detail');
+    this.$tgDetailList = $('[data-target="detailList"]');
+    this.$curNum = $('[data-target="curNum"]');
+    this.$addBtnArea = $('[data-target="addBtnArea"]');
   },
   _bindEvent: function () {
-    // this.$container.on('click', '[data-target="addBtn"]', $.proxy(this._addView, this));
-    // this.$container.on('click', '[data-target="closeBtn"]', $.proxy(this._goBack, this));
+    this.$container.on('click', '[data-target="addBtn"]', $.proxy(this._addView, this));
 
+  },
+  _proData: function() { //데이터 가공
+    Tw.Logger.info('[ _proData ]');
+    var tempLen = this.resData.recommendKind.typeListA.length;
+    this.detailListObj.listData = [];
+
+    for(var i=0; i < tempLen; i++) {
+      var tempKey = this.resData.recommendKind.typeListA[i];
+      this.detailListObj.listData.push( this.resData.recommendInfo[tempKey] );
+    }
+
+    this.detailListObj.curLen = this.detailListObj.listData.length;
+
+    Tw.Logger.info('[ _proData end ]', this.detailListObj);
+  },
+  _ctrlInit: function() {
+    this._cachedElement();
+    this._dataSplice( this.detailListObj.listData, this.detailListObj.startCount );
+    var template = Handlebars.compile( this.$tgTpl.html() );
+    var output = template( { tempData: this.detailListObj.viewData } );
+    this.$tgDetailList.append(output);
+    this.$curNum.html('( ' + this.detailListObj.curLen + ' )');
+
+    Tw.Logger.info('[ detailListObj.curLen ]', this.detailListObj.curLen);
+    if( this.detailListObj.curLen <= 0 ) {
+      this.$addBtnArea.hide();
+    }
+
+  },
+  _addView: function() {
+    if ( this.detailListObj.curLen <= 0 ) { return; }
+
+    this._cachedElement();
+    this._dataSplice( this.detailListObj.listData, this.detailListObj.addCount );
+    var template = Handlebars.compile( this.$tgTpl.html() );
+    var output = template( { tempData: this.detailListObj.viewData } );
+    this.$tgDetailList.append(output);
+    this.$curNum.html('( ' + this.detailListObj.curLen + ' )');
+
+    Tw.Logger.info('[ detailListObj.curLen 2 ]', this.detailListObj.curLen);
+    if( this.detailListObj.curLen <= 0 ) {
+      this.$addBtnArea.hide();
+    }
   },
   //--------------------------------------------------------------------------[service]
   _dataSplice: function( listData, count ) {
