e571e4f9dc223f623dac70f1333621f3e15ffa3a
Hyeryoun Lee <ryoun80@gmail.com>
2018-08-20 17:16:25 +0900

    [이혜련/SPTek] resolve conflicts

--- src/server/app/01.myt/myt.router.ts
+++ src/server/app/01.myt/myt.router.ts
@@ -53,8 +53,8 @@ import MytJoinProductServiceFeeAlarmChangeController from './controllers/join/my
 import MytBenefitDiscount from './controllers/benefit/myt.benefit.discount.main.controller';
 import MytBenefitDiscountDetail from './controllers/benefit/myt.benefit.discount.detail.controller';
 import MyTBenefitPoint from './controllers/benefit/myt.benefit.point.controller';
-// import MyTBenefitRainbowPoint from './controllers/benefit/myt.benefit.rainbow-point.controller';
-
+import MyTBenefitRainbowPoint from './controllers/benefit/myt.benefit.rainbow-point.controller';
+import MyTBenefitRainbowPointInfo from './controllers/benefit/myt.benefit.rainbow-point-info.controller';
 
 import MytJoinProductServiceCombinationController from './controllers/join/mty.join.product-service.combination.controller';
 
@@ -132,7 +132,8 @@ class MytRouter extends TwRouter {
     this.controllers.push({ url: '/benefit/discount', controller: new MytBenefitDiscount() });
     this.controllers.push({ url: '/benefit/discount/detail', controller: new MytBenefitDiscountDetail() });
     this.controllers.push({ url: '/benefit/point', controller: new MyTBenefitPoint() });
-    // this.controllers.push({ url: '/benefit/rainbow-point', controller: new MyTBenefitRainbowPoint() });
+    this.controllers.push({ url: '/benefit/rainbow-point', controller: new MyTBenefitRainbowPoint() });
+    this.controllers.push({ url: '/benefit/rainbow-point/info', controller: new MyTBenefitRainbowPointInfo() });
     this.controllers.push({ url: '/benefit/recommend', controller: new MyTBenefitRecommendController() });
     this.controllers.push({ url: '/benefit/recommend/detail', controller: new MyTBenefitRecommendDetailController() });
 
