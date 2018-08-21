72bdd15e0a58f0898e5e0cd6c7c191b4d050d2e7
양지훈 SKtechx <jihun202@sk.com>
2018-08-21 10:15:51 +0900

    [양지훈/Client개발팀] MyT > 가입 상품/서비스 > 요금제 알람 변경 가능일 js refactoring

--- src/client/app/01.myt/join/myt.join.product-service.fee-alarm.change.js
+++ src/client/app/01.myt/join/myt.join.product-service.fee-alarm.change.js
@@ -15,6 +15,20 @@ Tw.MyTJoinProductServiceFeeAlarmChange = function (rootEl) {
 
 Tw.MyTJoinProductServiceFeeAlarmChange.prototype = {
 
+  _data: {
+    key: null,
+    put: {
+      API_CMD: Tw.API_CMD.BFF_05_0126,
+      CONFIRM_ALERT: Tw.MSG_MYT.FEE_ALARM_ALERT_A01,
+      SUCCESS_ALERT: Tw.MSG_MYT.FEE_ALARM_ALERT_A01_SUCCESS
+    },
+    del: {
+      API_CMD: Tw.API_CMD.BFF_05_0127,
+      CONFIRM_ALERT: Tw.MSG_MYT.FEE_ALARM_ALERT_A02,
+      SUCCESS_ALERT: Tw.MSG_MYT.FEE_ALARM_ALERT_A02_SUCCESS
+    }
+  },
+
   _cachedElement: function() {
     this.$btnClose = this.$container.find('.fe-btn_close');
     this.$btnBack = this.$container.find('.fe-btn_back');
@@ -25,48 +39,39 @@ Tw.MyTJoinProductServiceFeeAlarmChange.prototype = {
   _bindEvents: function() {
     this.$btnClose.on('click', $.proxy(this._closeBack, this, true));
     this.$btnBack.on('click', $.proxy(this._closeBack, this, false));
-    this.$btnDelete.on('click', $.proxy(this._delete, this));
-    this.$btnPut.on('click', $.proxy(this._put, this));
+    this.$btnDelete.on('click', $.proxy(this._proc, this, 'del'));
+    this.$btnPut.on('click', $.proxy(this._proc, this, 'put'));
   },
 
   _closeBack: function(isClose) {
-    isClose ? this._historyService.go(-2) : this._historyService.goBack();
+    if (isClose) {
+      return this._historyService.go(-2);
+    }
+
+    this._historyService.goBack();
   },
 
-  _delete: function() {
-    this._popupService.openConfirm(Tw.POPUP_TITLE.CONFIRM, Tw.MSG_MYT.FEE_ALARM_ALERT_A01, null, null, function() {
-      this._popupService.close();
-      this._apiService.request(Tw.API_CMD.BFF_05_0126, {})
-        .done($.proxy(this._deleteResult, this));
-    }.bind(this));
+  _proc: function(key) {
+    this._data.key = key;
+    this._popupService.openConfirm(Tw.POPUP_TITLE.CONFIRM, this._data[key].CONFIRM_ALERT, null, null, $.proxy(this._procRequest, this));
   },
 
-  _deleteResult: function(res) {
+  _procRequest: function() {
+    this._popupService.close();
+    this._apiService.request(this._data[this._data.key].API_CMD, {})
+        .done($.proxy(this._procResult, this));
+  },
+
+  _procResult: function(res) {
     if (res.code !== Tw.API_CODE.CODE_00) {
       return this._apiError(res);
     }
 
-    this._popupService.openAlert(Tw.MSG_MYT.FEE_ALARM_ALERT_A01_SUCCESS, Tw.POPUP_TITLE.NOTIFY, null, function() {
-      location.reload();
-    }.bind(this));
+    this._popupService.openAlert(this._data[this._data.key].SUCCESS_ALERT, Tw.POPUP_TITLE.NOTIFY, null, $.proxy(this._reload, this));
   },
 
-  _put: function() {
-    this._popupService.openConfirm(Tw.POPUP_TITLE.CONFIRM, Tw.MSG_MYT.FEE_ALARM_ALERT_A02, null, null, function() {
-      this._popupService.close();
-      this._apiService.request(Tw.API_CMD.BFF_05_0127, {})
-          .done($.proxy(this._putResult, this));
-    }.bind(this));
-  },
-
-  _putResult: function(res) {
-    if (res.code !== Tw.API_CODE.CODE_00) {
-      return this._apiError(res);
-    }
-
-    this._popupService.openAlert(Tw.MSG_MYT.FEE_ALARM_ALERT_A02_SUCCESS, Tw.POPUP_TITLE.NOTIFY, null, function() {
-      location.reload();
-    }.bind(this));
+  _reload: function() {
+    this._historyService.reload();
   },
 
   _apiError: function (res) {
