ce3013be1efbf7e7dbec4ac2c072d4f706e1b0ea
Hakjoon Sim <hakjoon.sim@sk.com>
2018-08-22 17:48:43 +0900

    [심학준/client개발팀] 2 depth category dynamic
     - second depth category should be changed dynamically as first
       depth menu changed.

--- src/server/app/08.customer/controllers/faq/customer.faq.category.controller.ts
+++ src/server/app/08.customer/controllers/faq/customer.faq.category.controller.ts
@@ -7,6 +7,7 @@
 import TwViewController from '../../../../common/controllers/tw.view.controller';
 import {Request, Response, NextFunction} from 'express';
 import { API_CMD, API_CODE } from '../../../../types/api-command.type';
+import { Observable } from 'rxjs/Observable';
 
 export default class CustomerFaqCategoryController extends TwViewController {
   constructor() {
@@ -16,44 +17,84 @@ export default class CustomerFaqCategoryController extends TwViewController {
   render(req: Request, res: Response, next: NextFunction, svcInfo: any) {
     const code = req.query.code;
     const title = req.query.title;
+    const htmlPath = 'faq/customer.faq.category.html';
 
-    this.apiService.request(API_CMD.BFF_08_0051, { ifaqGrpCd: code })
-      .subscribe((resp) => {
-        if (resp.code === API_CODE.CODE_00) {
-          const is2depth = resp.result.faq2DepthGrp.length > 0 ? true : false;
-          this.apiService.request(API_CMD.BFF_08_0052, {
-            faqDepthGrpCd: is2depth ? resp.result.faq1DepthGrp[0].ifaqGrpCd : code,
-            faqDepthCd: 2,
-            page: 0,
-            size: 20
-          }).subscribe((response) => {
-            if (response.code === API_CODE.CODE_00) {
-              response.result.content = response.result.content.map((item) => {
-                item.answCtt = this.purify(item.answCtt);
-                return item;
-              });
-              res.render('faq/customer.faq.category.html', {
-                svcInfo: svcInfo,
-                title: title,
-                is2depth: is2depth,
-                rootCategory: code,
-                depth1: resp.result.faq1DepthGrp,
-                depth2: resp.result.faq2DepthGrp,
-                list: response.result.content,
-                isLast: response.result.last
-              });
-            } else {
-              this.logger.warn('[FAQ category list]', response);
-            }
-          }, (err) => {
-            this.logger.error('[FAQ]', err);
-          });
-        } else {
-          this.logger.warn('[FAQ category]', resp);
+    const data = {
+      svcInfo: svcInfo,
+      title: title,
+      is2depth: false,
+      rootCategory: code,
+      depth1: [],
+      depth2: [],
+      list: [],
+      isLast: false
+    };
+
+    this.checkIf2Depth(code).subscribe((depthChecked) => {
+      data.depth1 = depthChecked.depth1;
+      if (depthChecked.is2depth) {
+        Observable.combineLatest(
+          this.get2depth(depthChecked.depth1[0].ifaqGrpCd),
+          this.getFaqList(depthChecked.depth1[0].ifaqGrpCd)
+        ).subscribe((resp) => {
+          data.is2depth = true;
+          data.depth2 = resp[0];
+          data.list = resp[1].list;
+          data.isLast = resp[1].isLast;
+          res.render(htmlPath, data);
+        }, (err) => {
+        });
+
+      } else {
+        this.getFaqList(code).subscribe((faqList) => {
+          data.list = faqList.list;
+          data.isLast = faqList.isLast;
+          res.render(htmlPath, data);
+        }, (err) => {
+        });
+
+      }
+    }, (err) => {
+    });
+  }
+
+  private checkIf2Depth(code: String): Observable<any> {
+    return this.apiService.request(API_CMD.BFF_08_0051, { ifaqGrpCd: code })
+      .map((res) => {
+        if (res.code === API_CODE.CODE_00) {
+          return {
+            depth1: res.result.faq1DepthGrp,
+            is2depth: res.result.faq2DepthGrp.length > 0 ? true : false
+          };
         }
-      }, (err) => {
-        this.logger.error('[FAQ]', err);
       });
+  }
+
+  private get2depth(code: String): Observable<any> {
+    return this.apiService.request(API_CMD.BFF_08_0051, { ifaqGrpCd: code })
+      .map((res) => {
+        if (res.code === API_CODE.CODE_00) {
+          return res.result.faq1DepthGrp;
+        }
+        return null;
+      });
+  }
+
+  private getFaqList(groupCode: String): Observable<any> {
+    return this.apiService.request(API_CMD.BFF_08_0052, {
+      faqDepthGrpCd: groupCode,
+      faqDepthCd: 2,
+      page: 0,
+      size: 20
+    }).map((res) => {
+      if (res.code === API_CODE.CODE_00) {
+        return {
+          list: res.result.content,
+          isLast: res.result.last
+        };
+      }
+      return null;
+    });
   }
 
   private purify(text: string): String {
