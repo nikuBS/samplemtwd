f16dfa257438c2938e7fb9cde9ee7637c1d0285b
Hyeryoun Lee <ryoun80@gmail.com>
2018-08-27 17:51:14 +0900

    [이혜련/SPTek] DV001-3546 포인트 항목 formatting

--- src/client/app/01.myt/benefit/myt.benefit.rainbow-point-history.js
+++ src/client/app/01.myt/benefit/myt.benefit.rainbow-point-history.js
@@ -9,6 +9,7 @@ Tw.MyTBenefitRainbowPointHistory = function (rootEl) {
   this._dateHelper = Tw.DateHelper;
   this._popupService = Tw.Popup;
   this._hashService = Tw.Hash;
+  this._formatHelper = Tw.FormatHelper;
 
   this.LIST_SIZE = 20; //리스트 아이템 노출 최대수
 
@@ -94,8 +95,8 @@ Tw.MyTBenefitRainbowPointHistory.prototype = {
 
     this.$btPeriod.text((this.$fromDate.val() + ' ~ ' + this.$toDate.val()).replace(/-/g, '.'));
     if ( resp.code === Tw.API_CODE.CODE_00 ) {
-      this.$usedPoint.text(Tw.FormatHelper.addComma(resp.result.usdPoint));
-      this.$earnPoint.text(Tw.FormatHelper.addComma(resp.result.erndPoint));
+      this.$usedPoint.text(this._formatHelper.addComma(resp.result.usdPoint));
+      this.$earnPoint.text(this._formatHelper.addComma(resp.result.erndPoint));
 
       if ( resp.result.history.length < 1 ) {
         this.$listWrapper.empty();
@@ -103,11 +104,31 @@ Tw.MyTBenefitRainbowPointHistory.prototype = {
       } else {
         this._currentPage = parseInt(page);
         this._totalPage = Math.ceil(resp.result.totRecCnt / this.LIST_SIZE);
+        this._parseData(resp.result);
         this._renderList(this._currentPage, resp.result.history);
       }
     } else {
       this._popupService.openAlert(resp.code + ' ' + resp.msg);
     }
+  },
+
+  _parseData: function (data) {
+    var self = this;
+    _.each(data.history, function (item) {
+      item.opDt = self._dateHelper.getShortDateWithFormat(item.opDt, 'YYYY.MM.DD');
+      item.point = self._formatHelper.addComma(item.point);
+      switch ( item.opClCd ) {
+        case 'E':
+          item.opClDesc = Tw.NO_CONTRACT_TYPE.SAVE;
+          break;
+        case 'U':
+          item.opClDesc = Tw.NO_CONTRACT_TYPE.USE;
+          break;
+        case 'X':
+          item.opClDesc = Tw.NO_CONTRACT_TYPE.EXTINCTION;
+          break;
+      }
+    });
   },
 
   _renderList: function (page, items) {
