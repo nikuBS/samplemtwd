869d576eeba1280ea2eb3ca2ac92febbe0ecf34d
JimanPark <jiman.park@sk.com>
2018-09-03 15:11:57 +0900

    [박지만/Client개발팀] 목소리 인증 불필요 로직 제거

--- src/server/app/08.customer/controllers/voice/customer.voice.controller.ts
+++ src/server/app/08.customer/controllers/voice/customer.voice.controller.ts
@@ -14,19 +14,19 @@ class CustomerVoice extends TwViewController {
   }
 
   render(req: Request, res: Response, next: NextFunction, svcInfo?: any, layerType?: string): void {
-    svcInfo = this.convertData(svcInfo);
+    const convertSvcInfo = this.convertData(svcInfo);
 
     if ( req.params.type === 'info' ) {
-      res.render('voice/customer.voice.info.html', { svcInfo: svcInfo });
+      res.render('voice/customer.voice.info.html', { svcInfo: convertSvcInfo });
     }
 
     if ( req.params.type === 'sms' ) {
-      res.render('voice/customer.voice.sms.html', { svcInfo: svcInfo });
+      res.render('voice/customer.voice.sms.html', { svcInfo: convertSvcInfo });
     }
   }
 
   convertData(svcInfo) {
-    return Object.assign(svcInfo, { svcNum: FormatHelper.conTelFormatWithDash(svcInfo.svcNum) });
+    return Object.assign({}, svcInfo, { svcNum: FormatHelper.conTelFormatWithDash(svcInfo.svcNum) });
   }
 }
 
