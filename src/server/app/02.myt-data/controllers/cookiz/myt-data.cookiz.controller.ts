88a6bad5aa95f3b80829f1e13425fe56078a498e
JimanPark <jiman.park@sk.com>
2018-09-14 16:09:40 +0900

    [박지만/Client개발팀] 데이터 통화/관리 페이지 추가
    - router 추가

--- src/server/app/02.myt-data/controllers/cookiz/myt-data.cookiz.controller.ts
+++ src/server/app/02.myt-data/controllers/cookiz/myt-data.cookiz.controller.ts
@@ -1,20 +1,20 @@
 /**
- * FileName: myt-data.gift.controller.ts
+ * FileName: myt-data.cookiz.controller.ts
  * Author: 박지만 (jiman.park@sk.com)
- * Date: 2018.09.10
+ * Date: 2018.09.13
  */
 
 import { NextFunction, Request, Response } from 'express';
 import TwViewController from '../../../../common/controllers/tw.view.controller';
 
-class MytDataGift extends TwViewController {
+class MytDataCookiz extends TwViewController {
   constructor() {
     super();
   }
 
   render(req: Request, res: Response, next: NextFunction, svcInfo: any) {
-    res.render('gift/myt-data.gift.html');
+    res.render('cookiz/myt-data.cookiz.html');
   }
 }
 
-export default MytDataGift;
+export default MytDataCookiz;
--- src/server/app/02.myt-data/myt-data.router.ts
+++ src/server/app/02.myt-data/myt-data.router.ts
@@ -1,12 +1,18 @@
 import TwRouter from '../../common/route/tw.router';
 import MyTDataUsage from './controllers/usage/myt-data.usage.controller';
 import MytDataGift from './controllers/gift/myt-data.gift.controller';
+import MytDataTing from './controllers/ting/myt-data.ting.controller';
+import MytDataLimit from './controllers/limit/myt-data.limit.controller';
+import MytDataCookiz from './controllers/cookiz/myt-data.cookiz.controller';
 
 class MytDataRouter extends TwRouter {
   constructor() {
     super();
     this.controllers.push({ url: '/usage', controller: new MyTDataUsage() });
     this.controllers.push({ url: '/gift', controller: new MytDataGift() });
+    this.controllers.push({ url: '/ting', controller: new MytDataTing() });
+    this.controllers.push({ url: '/limit', controller: new MytDataLimit() });
+    this.controllers.push({ url: '/cookiz', controller: new MytDataCookiz() });
   }
 }
 
