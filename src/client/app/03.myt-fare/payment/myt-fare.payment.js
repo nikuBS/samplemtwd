fc9cd63c47447e4133896884b9f71166b73bc57d
1100015 <jayoon.kong@sk.com>
2018-09-17 13:27:53 +0900

    [공자윤/Client개발팀] layer 적용

--- src/client/app/03.myt-fare/payment/myt-fare.payment.js
+++ src/client/app/03.myt-fare/payment/myt-fare.payment.js
@@ -1,14 +1,25 @@
 Tw.MyTFarePayment = function (rootEl) {
   this.$container = rootEl;
+  this._apiService = Tw.Api;
+  this._popupService = Tw.Popup;
+
   this._init();
 };
 
 Tw.MyTFarePayment.prototype = {
   _init: function () {
+    this._openPaymentOption();
+
     this.account = new Tw.MyTFarePaymentAccount();
     this.auto = new Tw.MyTFarePaymentAuto();
     this.card = new Tw.MyTFarePaymentCard();
     this.point = new Tw.MyTFarePaymentPoint();
     this.sms = new Tw.MyTFarePaymentSms();
+  },
+  _openPaymentOption: function () {
+    this._popupService.open({
+      hbs: 'actionsheet_link_b_type',// hbs의 파일명
+      layer: true
+    });
   }
 };
