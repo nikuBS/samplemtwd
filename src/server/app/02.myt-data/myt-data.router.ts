96c29d4beeac9d432508b2c41aa5a79cf915af92
KimInHwan <skt.p132150@partner.sk.com>
2018-09-17 10:55:37 +0900

    [김인환/SPTek] 데이터통화관리 > 서브메인  * 마크업 추가

--- src/server/app/02.myt-data/myt-data.router.ts
+++ src/server/app/02.myt-data/myt-data.router.ts
@@ -1,4 +1,5 @@
 import TwRouter from '../../common/route/tw.router';
+import MyTSubMain from './myt-data.submain.controller';
 import MyTDataUsage from './controllers/usage/myt-data.usage.controller';
 import MytDataGift from './controllers/gift/myt-data.gift.controller';
 import MytDataTing from './controllers/ting/myt-data.ting.controller';
@@ -8,6 +9,7 @@ import MytDataCookiz from './controllers/cookiz/myt-data.cookiz.controller';
 class MytDataRouter extends TwRouter {
   constructor() {
     super();
+    this.controllers.push({ url: '/', controller: new MyTSubMain() });
     this.controllers.push({ url: '/usage', controller: new MyTDataUsage() });
     this.controllers.push({ url: '/gift', controller: new MytDataGift() });
     this.controllers.push({ url: '/ting', controller: new MytDataTing() });
