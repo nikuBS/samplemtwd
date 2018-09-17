bda0b3f80279c4bedfe4e9b8bfd21f51724336bc
mackmh <skt.P130714@partner.sk.com>
2018-09-18 08:46:18 +0900

    [김명환/프론트개발] 요금안내서 목데이터 추가

---
+++ src/server/mock/server/bill.guide.BFF_05_0036.mock.ts
@@ -0,0 +1,36 @@
+const bill_guide_BFF_05_0036 = {
+  'code': '00',
+  'msg': 'success',
+  'result': {
+    'selSvcMgmtNum': '',
+    'acntNum': '2026046400',
+    'custNum': '9421884800',
+    'invDt': '20180731',
+    'invDtArr': ['20180731', '20180630', '20180531', '20180430', '20180331', '20180228'],
+    'useAmtTot': '228470',
+    'deduckTotInvAmt': '-125228',
+    'detailYn': '',
+    'autopayYn': 'Y',
+    'repSvcYn': 'Y',
+    'coClCd': 'T',
+    'paidAmtMonthSvcCnt': 5,
+    'paidAmtMonthCnt': '6',
+    'paidAmtMonthSvcNum': [{ 'svcMgmtNum': '1144012200', 'name': '이동전화(01090**98**)' }, {
+      'svcMgmtNum': '1427017600',
+      'name': '이동전화(01089**98**)'
+    }, { 'svcMgmtNum': '7228803112', 'name': '이동전화(01087**86**)' }, {
+      'svcMgmtNum': '7233160336',
+      'name': '인터넷(광랜F) 경기 부**********************************************'
+    }, {
+      'svcMgmtNum': '7233162605',
+      'name': '유선전화(디지털전화, 003**61*****) 경기 부**********************************************'
+    }],
+    'paidAmtSvcCdList': [{ 'svcNm': '이동전화', 'amt': '216920' }, { 'svcNm': '인터넷', 'amt': '11000' }, {
+      'svcNm': '유선전화',
+      'amt': '550'
+    }]
+  }
+};
+
+export default bill_guide_BFF_05_0036;
+
