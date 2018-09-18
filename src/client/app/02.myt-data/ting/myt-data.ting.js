85b0d15a2ee4b3452a7f32ae55b4049f6f3b9822
JimanPark <jiman.park@sk.com>
2018-09-18 15:19:20 +0900

    [박지만/Client개발팀] 데이터 통화/관리 팅요금 선물하기
    - 완료 페이지 연동
    - 네이티브 연동
    - 선물하기 API 수정 및  연동
    - 히스토리 미연동

--- src/client/app/02.myt-data/ting/myt-data.ting.js
+++ src/client/app/02.myt-data/ting/myt-data.ting.js
@@ -1,57 +1,95 @@
 /**
  * FileName: myt-data.ting.js
  * Author: Jiman Park (jiman.park@sk.com)
- * Date: 2018.09.10
+ * Date: 2018.09.18
  */
 
 Tw.MyTDataTing = function (rootEl) {
   this.$container = rootEl;
   this._apiService = Tw.Api;
   this._popupService = Tw.Popup;
+  this._nativeService = Tw.Native;
+  this._historyService = new Tw.HistoryService();
 
-  // this._cachedElement();
-  // this._bindEvent();
+  this._cachedElement();
+  this._bindEvent();
   this._init();
 };
 
 Tw.MyTDataTing.prototype = {
   _init: function () {
-
-    this._getRemainDataInfo();
   },
 
-  _getRemainDataInfo: function () {
-    this._apiService.request(Tw.API_CMD.BFF_06_0014, {}).done($.proxy(this._onSuccessRemainDataInfo, this));
+  _cachedElement: function () {
+    this.$btn_send_gift = $('.fe-btn_ting_send');
+    this.$input_ting_receiver = $('.fe-input_ting_receiver');
+    this.$btn_native_contact_list = $('.fe-btn_native_contact');
+    this.$wrap_amount_select_list = $('.fe-ting_amount_select_list');
   },
 
-  _onSuccessRemainDataInfo: function (res) {
-    if ( res.code === Tw.API_CODE.CODE_00 ) {
-      // exceptions ProductId NA00001464, NA00001465
+  _bindEvent: function () {
+    this.$btn_send_gift.on('click', $.proxy(this._getReceiveUserInfo, this));
+    this.$btn_native_contact_list.on('click', $.proxy(this._onClickBtnAddr, this));
+    this.$input_ting_receiver.on('keyup', $.proxy(this._onKeyUpTingGiftNumber, this));
+  },
+
+  _onClickBtnAddr: function () {
+    this._nativeService.send(Tw.NTV_CMD.GET_CONTACT, {}, $.proxy(this._onContact, this));
+  },
+
+  _onContact: function (response) {
+    if ( response.resultCode === Tw.NTV_CODE.CODE_00 ) {
+      var params = response.params;
+      this.$input_ting_receiver.val(this._convertDashNumber(params.phoneNumber));
     }
   },
 
+  _convertDashNumber: function (sTelNumber) {
+    return Tw.StringHelper.phoneStringToDash(sTelNumber);
+  },
+
+  _onKeyUpTingGiftNumber: function () {
+    this._checkValidateSendingButton();
+    this.$input_ting_receiver.val(this._convertDashNumber(this.$input_ting_receiver.val()));
+  },
+
   _getReceiveUserInfo: function () {
-    this._apiService.request(Tw.API_CMD.BFF_06_0019, {}).done($.proxy(this._onSuccessReceiveUserInfo, this));
+    var befrSvcNum = this.$input_ting_receiver.val().match(/\d+/g).join('');
+    this._apiService.request(Tw.API_CMD.BFF_06_0022, { chrgSvcNum: befrSvcNum })
+      .done($.proxy(this._onSuccessReceiveUserInfo, this));
   },
 
   _onSuccessReceiveUserInfo: function (res) {
     if ( res.code === Tw.API_CODE.CODE_00 ) {
-
+      this._requestSendingData();
+    } else {
+      Tw.Error(res.code, res.msg).pop();
     }
   },
 
   _requestSendingData: function () {
     var htParams = {
-      befrSvcNum: '',
-      dataQty: ''
+      befrSvcNum: this.$input_ting_receiver.val().match(/\d+/g).join(''),
+      amt: this.$wrap_amount_select_list.find('li.checked input').val()
     };
 
-    this._apiService.request(Tw.API_CMD.BFF_06_0016, htParams).done($.proxy(this._onSuccessSendingData, this));
+    this._apiService.request(Tw.API_CMD.BFF_06_0023, htParams)
+      .done($.proxy(this._onSuccessSendingData, this));
   },
 
   _onSuccessSendingData: function (res) {
     if ( res.code === Tw.API_CODE.CODE_00 ) {
+      this._historyService.replaceURL('/myt/data/ting/complete');
+    } else {
+      Tw.Error(res.code, res.msg).pop();
+    }
+  },
 
+  _checkValidateSendingButton: function () {
+    if ( this.$input_ting_receiver.val().match(/\d+/g) ) {
+      this.$btn_send_gift.attr('disabled', false);
+    } else {
+      this.$btn_send_gift.attr('disabled', true);
     }
   }
 };
