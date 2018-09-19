04604bad97bbf8caa4496f452d0efee140663cd1
mackmh <skt.P130714@partner.sk.com>
2018-09-19 14:27:43 +0900

    [김명환/프론트개발] [김명환/프론트개발] 요금안내서 > 콜기프트, 로밍, 기부금/후원금 파일 생성 2

---
+++ src/server/app/03.myt-fare/controllers/bill/myt-fare.bill.guide.donation.controllers.ts
@@ -0,0 +1,47 @@
+/**
+ * FileName: myt-fare.bill.guide.call-gift.controllers.ts
+ * Author: Kim Myoung-Hwan (skt.P130714@partner.sk.com)
+ * Date: 2018.09.19
+ */
+
+import TwViewController from '../../../../common/controllers/tw.view.controller';
+import { Request, Response, NextFunction } from 'express';
+import { API_CMD, API_CODE } from '../../../../types/api-command.type';
+import { Observable } from 'rxjs/Observable';
+import StringHelper from '../../../../utils/string.helper';
+import moment = require('moment');
+import DateHelper from '../../../../utils/date.helper';
+import FormatHelper from '../../../../utils/format.helper';
+
+
+class MyTFareBillGuideDonation extends TwViewController {
+  constructor() {
+    super();
+  }
+  public reqQuery: any;
+  private _urlTplInfo: any = {
+    default: 'bill/myt-fare.bill.guide.donation.html',
+  };
+
+  render(req: Request, res: Response, next: NextFunction, svcInfo: any, layerType: string) {
+    const thisMain = this;
+    this.reqQuery = req.query;
+    this.logger.info(this, '[ svcInfo ] : ', svcInfo);
+    this.logger.info(this, '[ reqQuery ] : ', req.query);
+
+    thisMain.renderView(res, thisMain._urlTplInfo.default, {
+      reqQuery: thisMain.reqQuery,
+      svcInfo: svcInfo,
+    });
+
+  }
+
+  // -------------------------------------------------------------[클리이어튼로 전송]
+  public renderView(res: Response, view: string, data: any): any {
+    this.logger.info(this, '[ HTML ] : ', view);
+    res.render(view, data);
+  }
+
+}
+
+export default MyTFareBillGuideDonation;
