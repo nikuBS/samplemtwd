94b6f07b978af5e57bf0e872ac580a4d6e626047
JimanPark <jiman.park@sk.com>
2018-09-20 15:50:06 +0900

    [박지만/Client개발팀] 데이터 차단 요금제
    - block 단추 UI 이벤트 적용 및 마크업 반영

--- src/server/app/02.myt-data/controllers/limit/myt-data.limit.controller.ts
+++ src/server/app/02.myt-data/controllers/limit/myt-data.limit.controller.ts
@@ -6,6 +6,9 @@
 
 import { NextFunction, Request, Response } from 'express';
 import TwViewController from '../../../../common/controllers/tw.view.controller';
+import BrowserHelper from '../../../../utils/browser.helper';
+import { Observable } from 'rxjs/Observable';
+import { API_CMD, API_CODE } from '../../../../types/api-command.type';
 
 class MyTDataLimit extends TwViewController {
   constructor() {
@@ -13,7 +16,41 @@ class MyTDataLimit extends TwViewController {
   }
 
   render(req: Request, res: Response, next: NextFunction, svcInfo: any) {
-    res.render('limit/myt-data.limit.html');
+    const page = req.params.page;
+    const responseData = {
+      svcInfo: svcInfo,
+      isApp: BrowserHelper.isApp(req)
+    };
+
+    switch ( page ) {
+      case 'complete':
+        res.render('gift/myt-data.gift.complete.html', responseData);
+        break;
+      default:
+        Observable.combineLatest(
+          this.getLimitUserInfo()
+        ).subscribe(([limitUserInfo]) => {
+          const response = Object.assign(
+            {},
+            { limitUserInfo: limitUserInfo },
+            responseData
+          );
+          res.render('limit/myt-data.limit.html', response);
+        });
+    }
+  }
+
+  private getLimitUserInfo() {
+    return this.apiService.request(API_CMD.BFF_06_0034, {})
+      .map((resp) => {
+        if ( resp.code === API_CODE.CODE_00 ) {
+          const result = resp.result;
+
+          return result;
+        } else {
+          return null;
+        }
+      });
   }
 }
 
