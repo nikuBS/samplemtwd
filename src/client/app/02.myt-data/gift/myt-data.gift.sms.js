9fe8ab3fd57c21c3ca243d5912b4a13c7ca0e499
JimanPark <jiman.park@sk.com>
2018-09-21 14:18:45 +0900

    [박지만/Client개발팀] 데이터 통화 관리 페이지 및 데이터 바인딩
    - T끼리 선물하기 문자보내기 완료

--- src/client/app/02.myt-data/gift/myt-data.gift.sms.js
+++ src/client/app/02.myt-data/gift/myt-data.gift.sms.js
@@ -9,6 +9,7 @@ Tw.MyTDataGiftSms = function (rootEl) {
   this._apiService = Tw.Api;
   this._popupService = Tw.Popup;
   this._nativeService = Tw.Native;
+  this._historyService = new Tw.HistoryService();
 
   this._cachedElement();
   this._bindEvent();
@@ -17,11 +18,58 @@ Tw.MyTDataGiftSms = function (rootEl) {
 
 Tw.MyTDataGiftSms.prototype = {
   _init: function () {
+    var htQueryParams = Tw.UrlHelper.getQueryParams();
+    if ( htQueryParams ) {
+      this.phoneNumber = htQueryParams.to;
+      this._getReceiveUserInfo(this.phoneNumber);
+    }
   },
 
   _cachedElement: function () {
+    this.$btn_send_sms = this.$container.find('#fe-send_gift_sms');
+    this.$textarea_sms = this.$container.find('#fe-sms_textarea');
   },
 
   _bindEvent: function () {
+    this.$textarea_sms.on('keyup', $.proxy(this._onChangeTextArea, this));
+    this.$btn_send_sms.on('click', $.proxy(this._onClickSendSMS, this));
+  },
+
+  _getReceiveUserInfo: function (befrSvcNum) {
+    this._apiService.request(Tw.API_CMD.BFF_06_0019, {
+      befrSvcNum: befrSvcNum
+    }).done($.proxy(this._onSuccessReceiveUserInfo, this));
+  },
+
+  _onSuccessReceiveUserInfo: function (res) {
+    if ( res.code === Tw.API_CODE.CODE_00 ) {
+      $('.add-info').text(Tw.FormatHelper.getFormattedPhoneNumber(this.phoneNumber));
+      $('.add-name').text(res.result.custName);
+    } else {
+      Tw.Error(res.code, res.msg).pop();
+    }
+  },
+
+  _onChangeTextArea: function (e) {
+    if ( $(e.currentTarget).val().length !== 0 ) {
+      this.$btn_send_sms.prop('disabled', false);
+    } else {
+      this.$btn_send_sms.prop('disabled', true);
+    }
+  },
+
+  _onClickSendSMS: function () {
+    this._apiService.request(Tw.API_CMD.BFF_06_0017, {
+      befrSvcNum: this.phoneNumber,
+      msg: this.$textarea_sms.val()
+    }).done($.proxy(this._onSuccessRequestSms, this));
+  },
+
+  _onSuccessRequestSms: function (res) {
+    if ( res.code === Tw.API_CODE.CODE_00 ) {
+      this._historyService.replaceURL('/myt/data');
+    } else {
+      Tw.Error(res.code, res.msg).pop();
+    }
   }
 };
--- src/client/app/02.myt-data/limit/myt-data.limit.js
+++ src/client/app/02.myt-data/limit/myt-data.limit.js
@@ -22,6 +22,8 @@ Tw.MyTDataLimit.prototype = {
   _cachedElement: function () {
     this.$input_block_monthly = this.$container.find('#input_block_monthly');
     this.$input_block_immediately = this.$container.find('#input_block_immediately');
+    this.$wrap_monthly_select_list = $('.fe-limit_monthly_select_list');
+    this.$wrap_immediately_select_list = $('.fe-limit_immediately_select_list');
   },
 
   _bindEvent: function () {
@@ -30,23 +32,19 @@ Tw.MyTDataLimit.prototype = {
   },
 
   _onToggleBlockImmediately: function () {
-    $('#tab1-tab').find('.cont-box').each(function (nIndex, elItem) {
-      if ( $(elItem).css('display') === 'none' ) {
-        $(elItem).show();
-      } else {
-        $(elItem).hide();
-      }
-    });
+    $('#tab1-tab').find('.cont-box').each(this._toggleDisplay);
   },
 
   _onToggleBlockMonthly: function () {
-    $('#tab2-tab').find('.cont-box').each(function (nIndex, elItem) {
-      if ( $(elItem).css('display') === 'none' ) {
-        $(elItem).show();
-      } else {
-        $(elItem).hide();
-      }
-    });
+    $('#tab2-tab').find('.cont-box').each(this._toggleDisplay);
+  },
+
+  _toggleDisplay: function (nIndex, elItem) {
+    if ( $(elItem).css('display') === 'none' ) {
+      $(elItem).show();
+    } else {
+      $(elItem).hide();
+    }
   },
 
   _getRemainDataInfo: function () {
@@ -55,8 +53,28 @@ Tw.MyTDataLimit.prototype = {
 
   _onSuccessRemainDataInfo: function (res) {
     if ( res.code === Tw.API_CODE.CODE_00 ) {
-      console.log(res);
+      this._setAmountUI(Number(res.result.currentTopUpLimit));
+    } else {
+      Tw.Error(res.code, res.msg).pop();
     }
+  },
+
+  _setAmountUI: function (nLimitMount) {
+    var fnCheckedUI = function (nIndex, elInput) {
+      var $input = $(elInput);
+
+      if ( Number($input.val()) > nLimitMount ) {
+        $input.prop('disabled', true);
+        $input.parent().addClass('disabled');
+      }
+
+      if ( Number($input.val()) === nLimitMount ) {
+        $input.click();
+      }
+    };
+
+    this.$wrap_monthly_select_list.find('input').each(fnCheckedUI);
+    this.$wrap_immediately_select_list.find('input').each(fnCheckedUI);
   },
 
   _requestSendingData: function () {
@@ -70,6 +88,8 @@ Tw.MyTDataLimit.prototype = {
 
   _onSuccessSendingData: function (res) {
     if ( res.code === Tw.API_CODE.CODE_00 ) {
+
+    }else{
 
     }
   }
