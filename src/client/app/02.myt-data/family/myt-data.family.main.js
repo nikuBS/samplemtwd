12655654a477f7895084162be9a1daebda40209b
JimanPark <jiman.park@sk.com>
2018-10-01 16:11:23 +0900

    [박지만/Client개발팀] T끼리 가족모아 마크업 추가 및 라우터 설정
    - 컨트롤러 수정 및 마크업 추가

---
+++ src/client/app/02.myt-data/family/myt-data.family.main.js
@@ -0,0 +1,30 @@
+/**
+ * FileName: myt-data.family.main.js
+ * Author: Jiman Park (jiman.park@sk.com)
+ * Date: 2018.10.01
+ */
+
+Tw.MyTDataFamilyMain = function (rootEl) {
+  this.$container = rootEl;
+  this._apiService = Tw.Api;
+  this._popupService = Tw.Popup;
+  this._historyService = new Tw.HistoryService();
+
+  this._cachedElement();
+  this._bindEvent();
+  this._init();
+};
+
+Tw.MyTDataFamilyMain.prototype = {
+  _init: function () {
+
+  },
+
+  _cachedElement: function () {
+
+  },
+
+  _bindEvent: function () {
+
+  }
+};
