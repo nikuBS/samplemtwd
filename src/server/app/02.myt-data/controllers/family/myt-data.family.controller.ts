083055e2c6655602a89ba2c244dfce7561daad85
JimanPark <jiman.park@sk.com>
2018-10-04 19:58:07 +0900

    [박지만/Client개발팀] T가족모아 데이터 설정
    - 매월 자동 공유 마크업 추가
    - Date 데이터 마크업 바인딩

--- src/server/app/02.myt-data/controllers/family/myt-data.family.controller.ts
+++ src/server/app/02.myt-data/controllers/family/myt-data.family.controller.ts
@@ -10,6 +10,7 @@ import BrowserHelper from '../../../../utils/browser.helper';
 import { API_CMD, API_CODE } from '../../../../types/api-command.type';
 import FormatHelper from '../../../../utils/format.helper';
 import { DATA_UNIT } from '../../../../types/string.old.type';
+import oMockMytDataFamily from '../../../../mock/server/myt.data.family.mock';
 
 class MyTDataFamily extends TwViewController {
   constructor() {
@@ -37,38 +38,7 @@ class MyTDataFamily extends TwViewController {
   }
 
   private getRemainDataInfo() {
-    const data = {
-      code: '00',
-      msg: '',
-      result: {
-        total: '2048',
-        used: '400',
-        remained: '1648',
-        mbrList: [{
-          svcNum: '01094**04**',
-          svcMgmtNum: '7226057315',
-          custNm: '조*희',
-          repYn: 'Y',
-          prodId: 'NA00005959',
-          prodNm: 'Data 인피니티',
-          used: '0',
-          limitedYn: 'N',
-          limitation: ''
-        }, {
-          svcNum: '01012**34**',
-          svcMgmtNum: '7226057315',
-          custNm: '박*수',
-          repYn: 'N',
-          prodId: 'NA00005959',
-          prodNm: 'ting 요금제',
-          used: '0',
-          limitedYn: 'Y',
-          limitation: '300'
-        }]
-      }
-    };
-
-    let result = data.result;
+    let result = oMockMytDataFamily.BFF_06_0044_familyInfo.result;
 
     result = Object.assign(result, {
       total: this.convertTFamilyDataSet(result.total),
