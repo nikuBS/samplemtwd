559d2eb7720b8c963bcde3a914f3154b895f49aa
KimInHwan <skt.p132150@partner.sk.com>
2018-10-05 16:42:16 +0900

    [김인환/SPTek] 나의 가입정보 > 서브메인  * 마크업 적용 및 메뉴 url 추가

--- src/server/app/04.myt-join/myt-join.router.ts
+++ src/server/app/04.myt-join/myt-join.router.ts
@@ -1,4 +1,5 @@
 import TwRouter from '../../common/route/tw.router';
+import MyTJoinSubmainController from './myt-join.submain.controller';
 import MyTJoinProductFeePlan from './controllers/product/myt-join.product.fee-plan.controller';
 import MyTJoinProductAdditions from './controllers/product/myt-join.product.additions.controller';
 import MyTJoinProductCombinations from './controllers/product/myt-join.product.combinations.controller';
@@ -11,6 +12,7 @@ import MyTJoinInfoDiscountMonth from './controllers/info/myt-join.info.discount.
 class MyTJoinRouter extends TwRouter {
   constructor() {
     super();
+    this.controllers.push({ url: '/', controller: new MyTJoinSubmainController() });
     this.controllers.push({ url: '/product/fee-plan', controller: new MyTJoinProductFeePlan() });
     this.controllers.push({ url: '/product/additions', controller: new MyTJoinProductAdditions() });
     this.controllers.push({ url: '/product/combinations', controller: new MyTJoinProductCombinations() });
