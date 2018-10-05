b0e12d9c9c154fd8040c93d3ece6245f8c6519d7
mackmh <skt.P130714@partner.sk.com>
2018-10-05 14:26:56 +0900

    [김명환/프론트개발] menu.html, url.types.ts 수정

--- src/server/types/url.type.ts
+++ src/server/types/url.type.ts
@@ -16,7 +16,12 @@ export const URL = {
   '/myt/join/product/fee-alarm': { login: true, id: 'MS_05_01', title: '요금제 변경 가능일 알림' },
   '/myt/join/product/fee-alarm/change': { login: true, id: 'MS_05_01_01', title: '요금제 변경 가능일 알림 서비스' },
   '/myt/join/info/discount/month': { login: true, id: 'MS_09_01', title: '월별 상세 할인 내역' },
-
+  '/myt/bill/guide': { login: true, id: 'MF_02_01', title: '요금안내서' },
+  '/myt/bill/guide/call-gift': { login: true, id: 'MF_02_01_02', title: '요금안내서 콜기프트 사용요금 조회' },
+  '/myt/bill/guide/roaming': { login: true, id: 'MF_02_01_03', title: '요금안내서 로밍 사용요금 조회' },
+  '/myt/bill/guide/donation': { login: true, id: 'MF_02_01_04', title: '요금안내서 기부금/후원금 사용요금 조회' },
+  '/myt/join/info/discount': { login: true, id: 'MS_09', title: '약정할인 및 단말 분할상환 정보' },
+  
   // MyT-data
   '/myt/data': { login: true, id: 'DC', title: '데이터/통화 관리' },
   '/myt/data/usage': { login: true, id: 'DC_01_01', title: '실시간 데이터 잔여량' },
@@ -34,11 +39,10 @@ export const URL = {
   '/product/detail': { login: true, id: 'MP_02_02', title: '상품 상세 정보' },
   '/product/detail/contents': { login: true, id: 'MP_02_02_06', title: '상품 상세 정보 보기' },
 
-  '/bill/set/reissue': { login: true, id: 'MF_04_01', title: '요금안내서 재발행' },
-  '/bill/set/change': { login: true, id: 'MF_04_02', title: '요금안내서 변경' },
 
-  '/bill/guide': { login: true, id: 'MF_02_01', title: '요금안내서' },
-  '/bill/guide/call-gift': { login: true, id: 'MF_02_01_02', title: '요금안내서 콜기프트 사용요금 조회' },
-  '/bill/guide/roaming': { login: true, id: 'MF_02_01_03', title: '요금안내서 로밍 사용요금 조회' },
-  '/bill/guide/donation': { login: true, id: 'MF_02_01_04', title: '요금안내서 기부금/후원금 사용요금 조회' }
+  '/bill/set/reissue': { login: true, id: 'MF_04_01', title: '요금안내서 재발행' },
+  '/bill/set/change': { login: true, id: 'MF_04_02', title: '요금안내서 변경' }
+
+
+
 };
