d3ca4d1a663a197716858e92fc5ff75dcf5ac8de
jiyoung.jo <jiyoungjo@sk.com>
2018-10-08 15:18:00 +0900

    [조지영/Client 개발팀] 상품 요금제 Submain 페이지 추가

--- src/server/app/05.product/controllers/product.controller.ts
+++ src/server/app/05.product/controllers/product.controller.ts
@@ -6,14 +6,108 @@
 
 import TwViewController from '../../../common/controllers/tw.view.controller';
 import { Request, Response, NextFunction } from 'express';
-import { API_CMD } from '../../../types/api-command.type';
+import { API_CODE } from '../../../types/api-command.type';
+import {
+  PRODUCT_PROMOTION_BANNERS,
+  PRODUCT_PLAN_GROUPS,
+  PRODUCT_MY_FILTERS,
+  PRODUCT_RECOMMENDED_PLANS,
+  PRODUCT_RECOMMENDED_TAGS
+} from '../../../mock/server/product.submain.mock';
+import FormatHelper from '../../../utils/format.helper';
 
 export default class Product extends TwViewController {
+  private PLAN_CODE = 'F01100';
+
   constructor() {
     super();
   }
 
   render(req: Request, res: Response, next: NextFunction, svcInfo: any, layerType: string) {
-    res.render('product.html');
+    const productData = {
+      banners: this.getPromotionBanners(),
+      groups: this.getProductGroups(),
+      myFilters: this.getMyFilters(),
+      recommendedPlans: this.getRecommendedPlans(),
+      recommendedTags: this.getRecommendedTags()
+    };
+
+    res.render('product.html', { svcInfo, productData });
+  }
+
+  private getPromotionBanners = () => {
+    // return this.apiService.request(API_CMD.BFF_10_0024, { idxCtgCd: this.PLAN_CODE }).map(resp => {});
+    const resp = PRODUCT_PROMOTION_BANNERS;
+    if (resp.code !== API_CODE.CODE_00) {
+      return null;
+    }
+
+    return resp.result;
+  }
+
+  private getProductGroups = () => {
+    // return this.apiService.request(API_CMD.BFF_10_0026, { idxCtgCd: this.PLAN_CODE }).map(resp => {});
+    const resp = PRODUCT_PLAN_GROUPS;
+
+    if (resp.code !== API_CODE.CODE_00) {
+      return null;
+    }
+
+    return {
+      ...resp.result,
+      grpProdList: resp.result.grpProdList.map(group => {
+        return {
+          ...group,
+          prodList: group.prodList.map(plan => {
+            return {
+              ...plan,
+              basFeeInfo: FormatHelper.addComma(plan.basFeeInfo)
+            };
+          })
+        };
+      })
+    };
+  }
+
+  private getMyFilters = () => {
+    // return this.apiService.request(API_CMD.BFF_10_0025, { idxCtgCd: this.PLAN_CODE }).map(resp => {});
+    const resp = PRODUCT_MY_FILTERS;
+
+    if (resp.code !== API_CODE.CODE_00) {
+      return null;
+    }
+
+    return resp.result;
+  }
+
+  private getRecommendedPlans = () => {
+    // return this.apiService.request(API_CMD.BFF_10_0027, { idxCtgCd: this.PLAN_CODE }).map(resp => {});
+    const resp = PRODUCT_RECOMMENDED_PLANS;
+
+    if (resp.code !== API_CODE.CODE_00) {
+      return null;
+    }
+
+    return {
+      ...resp.result,
+      prodList: resp.result.prodList.map(plan => {
+        return {
+          ...plan,
+          basFeeInfo: FormatHelper.addComma(plan.basFeeInfo)
+        };
+      })
+    };
+  }
+
+  private getRecommendedTags = () => {
+    // return this.apiService.request(API_CMD.BFF_10_0029, { idxCtgCd: this.PLAN_CODE }).map(resp => {});
+
+    const resp = PRODUCT_RECOMMENDED_TAGS;
+
+    if (resp.code !== API_CODE.CODE_00) {
+      return null;
+    }
+
+    return resp.result;
   }
 }
