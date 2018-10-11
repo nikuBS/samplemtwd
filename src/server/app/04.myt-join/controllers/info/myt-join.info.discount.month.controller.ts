a2cc5ad41cbb3de6b40a73937a2c53fb54942d25
이정민 Sktechx <wpeofh01@gmail.com>
2018-10-11 15:53:56 +0900

    [이정민/SPTEK] * 가입정보 > 약정할인 및 단말상환 정보 > 월별 상세 할인 내역  - mock data -> api

--- src/server/app/04.myt-join/controllers/info/myt-join.info.discount.month.controller.ts
+++ src/server/app/04.myt-join/controllers/info/myt-join.info.discount.month.controller.ts
@@ -8,7 +8,7 @@ import { NextFunction, Request, Response } from 'express';
 import TwViewController from '../../../../common/controllers/tw.view.controller';
 import { Observable } from 'rxjs/Observable';
 import FormatHelper from '../../../../utils/format.helper';
-import { API_CODE } from '../../../../types/api-command.type';
+import { API_CMD, API_CODE } from '../../../../types/api-command.type';
 import { MYT_INFO_DISCOUNT_MONTH } from '../../../../types/string.type';
 import moment = require('moment');
 import DateHelper from '../../../../utils/date.helper';
@@ -20,11 +20,17 @@ const VIEW = {
 const MAX_ITEM_LENGTH_PER_PAGE = 20;
 
 class MyTJoinInfoDiscountMonth extends TwViewController {
+  private _svcAgrmtCdId: any; // 약정할인 ID
+  private _svcAgrmtDcCd: any; // 약정할인 코드
+
   constructor() {
     super();
   }
 
   render(req: Request, res: Response, next: NextFunction, svcInfo: any) {
+    this._svcAgrmtCdId = req.query._svcAgrmtCdId;
+    this._svcAgrmtDcCd = req.query._svcAgrmtDcCd;
+
     Observable.combineLatest(
       this.reqDiscountInfosMonth()
     ).subscribe(([discountInfosMonth]) => {
@@ -44,6 +50,8 @@ class MyTJoinInfoDiscountMonth extends TwViewController {
       const data = this.getFormattedData(discountInfosMonth);
 
       res.render(VIEW.DEFAULT, {
+        svcAgrmtCdId: this._svcAgrmtCdId,
+        svcAgrmtDcCd: this._svcAgrmtDcCd,
         data,
         svcInfo
       });
@@ -58,98 +66,101 @@ class MyTJoinInfoDiscountMonth extends TwViewController {
   }
 
   private reqDiscountInfosMonth(): Observable<any> {
-    // return this.apiService.request(API_CMD.BFF_05_0076, {});
-
-    return Observable.create((observer) => {
-      setTimeout(() => {
-        const resp = {
-          'code': '00',
-          'msg': 'success',
-          'result': {
-            'agrmt': [
-              {
-                'invoDt': '20170930',
-                'invCnt': '1',
-                'penEstDcAmt': '0',
-                'perEstRt': '0'
-              },
-              {
-                'invoDt': '20170831',
-                'invCnt': '2',
-                'penEstDcAmt': '0',
-                'perEstRt': '0'
-              },
-              {
-                'invoDt': '20160731',
-                'invCnt': '3',
-                'penEstDcAmt': '0',
-                'perEstRt': '0'
-              },
-              {
-                'invoDt': '20160630',
-                'invCnt': '4',
-                'penEstDcAmt': '0',
-                'perEstRt': '0'
-              },
-              {
-                'invoDt': '20150531',
-                'invCnt': '5',
-                'penEstDcAmt': '-4344',
-                'perEstRt': '0'
-              },
-              {
-                'invoDt': '20150531',
-                'invCnt': '6',
-                'penEstDcAmt': '-4344',
-                'perEstRt': '0'
-              },
-              {
-                'invoDt': '20140531',
-                'invCnt': '7',
-                'penEstDcAmt': '-4344',
-                'perEstRt': '0'
-              },
-              {
-                'invoDt': '20140531',
-                'invCnt': '8',
-                'penEstDcAmt': '-4344',
-                'perEstRt': '0'
-              },
-              {
-                'invoDt': '20140531',
-                'invCnt': '9',
-                'penEstDcAmt': '-4344',
-                'perEstRt': '0'
-              },
-              {
-                'invoDt': '20140531',
-                'invCnt': '10',
-                'penEstDcAmt': '-4344',
-                'perEstRt': '0'
-              },
-              {
-                'invoDt': '20140531',
-                'invCnt': '11',
-                'penEstDcAmt': '-4344',
-                'perEstRt': '0'
-              },
-              {
-                'invoDt': '20130531',
-                'invCnt': '12',
-                'penEstDcAmt': '-4344',
-                'perEstRt': '0'
-              }
-            ]
-          }
-        };
-        if ( resp.code === API_CODE.CODE_00 ) {
-          observer.next(resp);
-          observer.complete();
-        } else {
-          observer.error();
-        }
-      }, 500);
+    return this.apiService.request(API_CMD.BFF_05_0076, {
+      svcAgrmtCdId: this._svcAgrmtCdId,
+      svcAgrmtDcCd: this._svcAgrmtDcCd
     });
+
+    // return Observable.create((observer) => {
+    //   setTimeout(() => {
+    //     const resp = {
+    //       'code': '00',
+    //       'msg': 'success',
+    //       'result': {
+    //         'agrmt': [
+    //           {
+    //             'invoDt': '20170930',
+    //             'invCnt': '1',
+    //             'penEstDcAmt': '0',
+    //             'perEstRt': '0'
+    //           },
+    //           {
+    //             'invoDt': '20170831',
+    //             'invCnt': '2',
+    //             'penEstDcAmt': '0',
+    //             'perEstRt': '0'
+    //           },
+    //           {
+    //             'invoDt': '20160731',
+    //             'invCnt': '3',
+    //             'penEstDcAmt': '0',
+    //             'perEstRt': '0'
+    //           },
+    //           {
+    //             'invoDt': '20160630',
+    //             'invCnt': '4',
+    //             'penEstDcAmt': '0',
+    //             'perEstRt': '0'
+    //           },
+    //           {
+    //             'invoDt': '20150531',
+    //             'invCnt': '5',
+    //             'penEstDcAmt': '-4344',
+    //             'perEstRt': '0'
+    //           },
+    //           {
+    //             'invoDt': '20150531',
+    //             'invCnt': '6',
+    //             'penEstDcAmt': '-4344',
+    //             'perEstRt': '0'
+    //           },
+    //           {
+    //             'invoDt': '20140531',
+    //             'invCnt': '7',
+    //             'penEstDcAmt': '-4344',
+    //             'perEstRt': '0'
+    //           },
+    //           {
+    //             'invoDt': '20140531',
+    //             'invCnt': '8',
+    //             'penEstDcAmt': '-4344',
+    //             'perEstRt': '0'
+    //           },
+    //           {
+    //             'invoDt': '20140531',
+    //             'invCnt': '9',
+    //             'penEstDcAmt': '-4344',
+    //             'perEstRt': '0'
+    //           },
+    //           {
+    //             'invoDt': '20140531',
+    //             'invCnt': '10',
+    //             'penEstDcAmt': '-4344',
+    //             'perEstRt': '0'
+    //           },
+    //           {
+    //             'invoDt': '20140531',
+    //             'invCnt': '11',
+    //             'penEstDcAmt': '-4344',
+    //             'perEstRt': '0'
+    //           },
+    //           {
+    //             'invoDt': '20130531',
+    //             'invCnt': '12',
+    //             'penEstDcAmt': '-4344',
+    //             'perEstRt': '0'
+    //           }
+    //         ]
+    //       }
+    //     };
+    //     if ( resp.code === API_CODE.CODE_00 ) {
+    //       observer.next(resp);
+    //       observer.complete();
+    //     } else {
+    //       observer.error();
+    //     }
+    //   }, 500);
+    // });
   }
 
   private getFormattedData(resp: any): any {
