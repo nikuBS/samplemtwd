3c3f1d5c9f04921fcb05ef3cc202f5e0bd7a7385
jiyoung.jo <jiyoungjo@sk.com>
2018-10-11 15:05:54 +0900

    [조지영/Client 개발팀] 나의 부가상품 페이지 추가

--- src/server/app/04.myt-join/controllers/product/myt-join.product.additions.controller.ts
+++ src/server/app/04.myt-join/controllers/product/myt-join.product.additions.controller.ts
@@ -4,7 +4,12 @@
  * Date: 2018.09.19
  */
 import TwViewController from '../../../../common/controllers/tw.view.controller';
-import {NextFunction, Request, Response} from 'express';
+import { NextFunction, Request, Response } from 'express';
+import { Observable } from 'rxjs/Observable';
+import { API_CMD, API_CODE } from '../../../../types/api-command.type';
+import FormatHelper from '../../../../utils/format.helper';
+import { MYT_JOIN_ADDITONS } from '../../../../mock/server/myt.join.product.additions.mock';
+import DateHelper from '../../../../utils/date.helper';
 
 class MyTJoinProductAdditions extends TwViewController {
   constructor() {
@@ -12,8 +17,43 @@ class MyTJoinProductAdditions extends TwViewController {
   }
 
   render(req: Request, res: Response, next: NextFunction, svcInfo: any, layerType: string) {
-    //
+    if (svcInfo.svcAttrCd.includes('M')) {
+      // this.apiService.request(API_CMD.BFF_05_0137, {}).subscribe(resp => {
+      //   if (resp.code !== API_CODE.CODE_00) {
+      //     return this.error.render(res, {
+      //       svcInfo: svcInfo,
+      //       title: '나의 부가상품'
+      //     });
+      //   }
+
+      const resp = MYT_JOIN_ADDITONS;
+      const additions = this.convertMobileAdditions(resp.result.addProdList);
+
+      res.render('product/myt-join.product.additions.mobile.html', { svcInfo, additions });
+      // });
+    } else {
+      this.apiService.request(API_CMD.BFF_05_0129, {}).subscribe(resp => {
+        if (resp.code !== API_CODE.CODE_00) {
+          return this.error.render(res, {
+            svcInfo: svcInfo,
+            title: '나의 부가상품'
+          });
+        }
+
+        res.render('product/myt-join.product.additions.others.html', { svcInfo });
+      });
+    }
   }
+
+  private convertMobileAdditions = (additions: any[]) => {
+    return additions.map(addition => {
+      return {
+        ...addition,
+        basFeeTxt: FormatHelper.getFeeContents(addition.basFeeTxt),
+        scrbDt: DateHelper.getShortDateNoDot(addition.scrbDt)
+      };
+    });
+  };
 }
 
 export default MyTJoinProductAdditions;
