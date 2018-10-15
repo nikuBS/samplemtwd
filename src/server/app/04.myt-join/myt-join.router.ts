c14f62181a33fa1f90be65694bdee1bd6077db05
이규광SkTechX <1111>
2018-10-15 11:08:30 +0900

    [이규광/SPTEK] 나의 가입정보 > router source back

--- src/server/app/04.myt-join/myt-join.router.ts
+++ src/server/app/04.myt-join/myt-join.router.ts
@@ -8,14 +8,6 @@ import MyTJoinProductFeeAlarmTerminate from './controllers/product/myt-join.prod
 import MyTJoinProtectChange from './controllers/protect/myt-join.protect.change.controller';
 import MytJoinInfoDiscount from './controllers/info/myt-join.info.discount.controller';
 import MyTJoinInfoDiscountMonth from './controllers/info/myt-join.info.discount.month.controller';
-import MyTJoinWire from './controllers/wire/myt-join.wire.controller';
-import MyTJoinWireAS from './controllers/wire/myt-join.wire.as.controller';
-import MyTJoinWireASDetail from './controllers/wire/myt-join.wire.as.detail.controller';
-import MyTJoinWireDiscountRefund from './controllers/wire/myt-join.wire.discount-refund.controller';
-import MyTJoinWireGifts from './controllers/wire/myt-join.wire.gifts.controller';
-import MyTJoinWireHistory from './controllers/wire/myt-join.wire.history.controller';
-import MyTJoinWireHistoryDetail from './controllers/wire/myt-join.wire.history.detail.controller';
-import MyTJoinWireInetPhoneNumChange from './controllers/wire/myt-join.wire.netphone.change.controller';
 
 class MyTJoinRouter extends TwRouter {
   constructor() {
@@ -29,17 +21,6 @@ class MyTJoinRouter extends TwRouter {
     this.controllers.push({ url: '/protect/change', controller: new MyTJoinProtectChange() });
     this.controllers.push({ url: '/info/discount', controller: new MytJoinInfoDiscount() });
     this.controllers.push({ url: '/info/discount/month', controller: new MyTJoinInfoDiscountMonth() });
-
-    // 인터넷/집전화/IPTV 신청내역 등
-    this.controllers.push({ url: '/wire', controller: new MyTJoinWire() });
-    this.controllers.push({ url: '/wire/as', controller: new MyTJoinWireAS() });
-    this.controllers.push({ url: '/wire/as/detail', controller: new MyTJoinWireASDetail() });
-    this.controllers.push({ url: '/wire/discount-refund', controller: new MyTJoinWireDiscountRefund() });
-    this.controllers.push({ url: '/wire/gifts', controller: new MyTJoinWireGifts() });
-    this.controllers.push({ url: '/wire/history', controller: new MyTJoinWireHistory() });
-    this.controllers.push({ url: '/wire/detail', controller: new MyTJoinWireHistoryDetail() });
-    this.controllers.push({ url: '/wire/inetphone-num/change', controller: new MyTJoinWireInetPhoneNumChange() });
-
   }
 }
 
