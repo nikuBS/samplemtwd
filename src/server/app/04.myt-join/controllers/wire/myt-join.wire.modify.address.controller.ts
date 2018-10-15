ad2091c9566089816a015fb7b44d6a90efe70ed1
mackmh <skt.P130714@partner.sk.com>
2018-10-15 15:00:10 +0900

    [김명환/프론트개발] 9차_인터넷/집전화/IPTV해제/변경/신청 * B끼리 무료통화 대상자 조회 * 설치장소변경 * 상품변경 * 서비스 해지 신청

---
+++ src/server/app/04.myt-join/controllers/wire/myt-join.wire.modify.address.controller.ts
@@ -0,0 +1,132 @@
+/**
+ * FileName: myt-join.wire.modify.address.controller.ts
+ * Author: Kim Myoung-Hwan (skt.P130714@partner.sk.com)
+ * Date: 2018.10.15
+ */
+
+import TwViewController from '../../../../common/controllers/tw.view.controller';
+import { Request, Response, NextFunction } from 'express';
+import { API_CMD, API_CODE } from '../../../../types/api-command.type';
+import { Observable } from 'rxjs/Observable';
+import StringHelper from '../../../../utils/string.helper';
+import moment = require('moment');
+import DateHelper from '../../../../utils/date.helper';
+import FormatHelper from '../../../../utils/format.helper';
+import { MYT_FARE_BILL_GUIDE } from '../../../../types/string.type';
+import { MYT_JOIN_CONTRACT_TERMINAL } from '../../../../types/string.type';
+
+class MytJoinWireFreeCallCheck extends TwViewController {
+  constructor() {
+    super();
+  }
+  public reqQuery: any;
+  private _svcInfo: any;
+
+  // 데이터
+  private _resDataInfo: any = {};
+
+  // 공통데이터
+  private _commDataInfo: any = {
+  };
+
+  private _urlTplInfo: any = {
+    pageRenderView: 'wire/myt-join.wire.modify.address.html',
+  };
+
+  render(req: Request, res: Response, next: NextFunction, svcInfo: any, layerType: string) {
+    this._svcInfo = svcInfo;
+    const thisMain = this;
+    this.reqQuery = req.query;
+    this.logger.info(this, '[ svcInfo ] : ', svcInfo);
+    this.logger.info(this, '[ reqQuery ] : ', req.query);
+
+    // this._typeInit();
+
+    // const p1 = this._getPromiseApi(this.apiService.request(API_CMD.BFF_05_0063, {}), 'p1');
+    // const p1 = this._getPromiseApiMock( contractTerminal_BFF_05_0063, 'p1' );
+
+    // Promise.all([p1]).then(function(resArr) {
+    //
+    //   thisMain._resDataInfo = resArr[0].result;
+    //
+    //   thisMain._dataInit();
+    //
+    //   thisMain.logger.info(thisMain, '[_urlTplInfo.pageRenderView] : ', thisMain._urlTplInfo.pageRenderView);
+    //
+    //   thisMain.renderView(res, thisMain._urlTplInfo.pageRenderView, {
+    //     reqQuery: thisMain.reqQuery,
+    //     svcInfo: svcInfo,
+    //     commDataInfo: thisMain._commDataInfo,
+    //     resDataInfo: thisMain._resDataInfo
+    //   });
+    // }, function(err) {
+    //   thisMain.logger.info(thisMain, `[ Promise.all > error ] : `, err);
+    //   return thisMain.error.render(res, {
+    //     title: 'title',
+    //     code: err.code,
+    //     msg: err.msg,
+    //     svcInfo: svcInfo
+    //   });
+    // });
+
+    thisMain.renderView(res, thisMain._urlTplInfo.pageRenderView, {
+      reqQuery: thisMain.reqQuery,
+      svcInfo: svcInfo,
+    });
+  }
+
+
+  private _dataInit() {
+
+  }
+
+  // -------------------------------------------------------------[SVC]
+
+
+  // -------------------------------------------------------------[프로미스 생성]
+  public _getPromiseApi(reqObj, msg): any {
+    const thisMain = this;
+    const reqObjObservableApi: Observable<any> = reqObj;
+
+    return new Promise((resolve, reject) => {
+      Observable.combineLatest(
+        reqObjObservableApi
+      ).subscribe((resp) => {
+        thisMain.logger.info(thisMain, `[ ${ msg } next ] : `, resp);
+
+        if ( resp[0].code === API_CODE.CODE_00 ) {
+          resolve(resp[0]);
+        } else {
+          reject(resp[0]);
+        }
+
+      });
+    });
+
+  }
+  // -------------------------------------------------------------[프로미스 생성 - Mock]
+  public _getPromiseApiMock(mockData, msg): any {
+    return new Promise((resolve, reject) => {
+      const ms: number = Math.floor(Math.random() * 1000) + 1;
+      setTimeout(function () {
+        console.log(`[ ${ msg } _getPromiseApiMock ] : ` + mockData);
+
+        if ( mockData.code === API_CODE.CODE_00 ) {
+          resolve(mockData);
+        } else {
+          reject(mockData);
+        }
+
+      }, ms);
+    });
+  }
+
+  // -------------------------------------------------------------[클리이어튼로 전송]
+  public renderView(res: Response, view: string, data: any): any {
+    this.logger.info(this, '[ HTML ] : ', view);
+    res.render(view, data);
+  }
+
+}
+
+export default MytJoinWireFreeCallCheck;
