e5df7b5b5427e11b2a1b43569e2ab61b3e4621b7
P130715 <skt.p130715@partnet.sk.com>
2018-10-17 10:06:52 +0900

    [양정규/SPtek] 9차 > 나의 가입정보 > 망 작업 SMS 알림 신청

--- src/client/app/04.myt-join/info/myt-join.info.sms.js
+++ src/client/app/04.myt-join/info/myt-join.info.sms.js
@@ -6,6 +6,8 @@
 Tw.MyTJoinInfoSms = function (rootEl) {
   this.$container = rootEl;
   this._apiService = Tw.Api;
+  this._popupService = Tw.Popup;
+  this._historyService = new Tw.HistoryService();
   this._init();
 };
 
@@ -17,62 +19,70 @@ Tw.MyTJoinInfoSms.prototype = {
   _initVariables: function () {
     this.$btnSubmit = this.$container.find('#fe-btn-submit');
     this.$tel = this.$container.find('#fe-hp-tel');
-    this.$agree = this.$container.find('#fe-info-agree');
   },
   _bindEvent: function () {
-    this.$tel.on('keyup', $.proxy(this._onChangeTel, this));
-    this.$agree.on('change', $.proxy(this._onChangeAgree, this));
-    this.$btnSubmit.on('click', $.proxy(this._onSubmit, this));
+    this.$container.on('keyup focus change', '[data-require]', $.proxy(this._onDisableStatus, this));
+    this.$btnSubmit.on('click', $.proxy(this._reqSms, this));
   },
 
-  // 휴대폰 번호 입력 시 자동 하이픈 넣기
-  _onFormatHpNum : function (e) {
-    var _$this = $(e.currentTarget);
-    var data = _$this.val();
-    data = data.replace(/[^0-9]/g,'');
-
-    var tmp = '';
-
-    if (data.length > 3 && data.length <= 6) {
-      tmp += data.substr(0, 3);
-      tmp += '-';
-      tmp += data.substr(3);
-      data = tmp;
-    } else if (data.length > 6) {
-      tmp += data.substr(0, 3);
-      tmp += '-';
-      var size = data.length < 11 ? 3 : 4;
-      tmp += data.substr(3, size);
-      tmp += '-';
-      tmp += data.substr(3+size);
-      data = tmp;
-    }
-
-    _$this.val(data);
-  },
-
-  _onChangeTel : function (e) {
-    this._onFormatHpNum(e);
-    this._validationCheck();
-  },
-
-  _onChangeAgree : function () {
-    this._validationCheck();
-  },
-
-  _onSubmit : function () {
-
-  },
-
-  _validationCheck : function () {
+  _onDisableStatus : function () {
     var isOk = false;
-
-    var _tel = this.$tel.val().replace(/[^0-9]/g,'');
-    isOk = Tw.ValidationHelper.isCellPhone(_tel); // 핸드폰 번호 체크
-    isOk = isOk ? this.$agree.is(':checked') : isOk; // 개인정보 동의 여부
+    this.$container.find('[data-require]').each(function () {
+      var $this = $(this);
+      if ( $this.attr('type') === 'tel' ) {
+        var _tel = $this.val().replace(/[^0-9]/g,'');
+        isOk = Tw.ValidationHelper.isCellPhone(_tel); // 핸드폰 번호 체크
+        if (!isOk) return false;
+      }
+      if ( isOk && $this.attr('type') === 'checkbox' ) {
+        isOk = $this.is(':checked'); // 개인정보 동의 여부
+        if (!isOk) return false;
+      }
+    });
 
     // 이상없을 시 서브밋 버튼 활성화
     this.$btnSubmit.prop('disabled',!isOk);
+  },
+
+  // 무약정 플랜 포인트 내역 조회
+  _reqSms : function () {
+    var _tels = this.$tel.val().split('-');
+    var param = {
+      tel_num1 : _tels[0],
+      tel_num2 : _tels[1],
+      tel_num3 : _tels[2]
+    };
+    this._apiService
+      .request(Tw.API_CMD.BFF_05_0062, param)
+      .done($.proxy(this._onSuccess, this))
+      .fail($.proxy(this._onFail, this));
+  },
+
+  // Mockup
+  _mockReqSms : function () {
+    $.ajax('/mock/myt.join.sms.json')
+      .done($.proxy(this._onSuccess, this))
+      .fail($.proxy(this._onFail, this));
+  },
+
+  _onSuccess : function (resp) {
+    if ( resp.code !== Tw.API_CODE.CODE_00 ) {
+      this._onFail(resp);
+      return;
+    }
+
+    var $this = this;
+    this._popupService.openAlert(Tw.ALERT_MSG_MYT_JOIN[$this.$btnSubmit.data('alert')].MSG,
+      null, null, $.proxy(this._onClose, this));
+  },
+
+  _onClose : function () {
+    this._historyService.goBack();
+  },
+
+  // API Fail
+  _onFail: function (err) {
+    Tw.Error(err.code,err.msg).pop();
   }
 
 };
