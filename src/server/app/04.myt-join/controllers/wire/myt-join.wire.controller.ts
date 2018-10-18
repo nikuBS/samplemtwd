144ad58ca1c0e3753652688dd52eb3c600c89d84
이규광SkTechX <1111>
2018-10-18 18:20:36 +0900

    [이규광/SPTEK] #9 나의 가입정보 > 인터넷/집전화/IPTV > 최초화면 및 상세화면 api변경 적용

--- src/server/app/04.myt-join/controllers/wire/myt-join.wire.controller.ts
+++ src/server/app/04.myt-join/controllers/wire/myt-join.wire.controller.ts
@@ -18,40 +18,32 @@ class MyTJoinWire extends TwViewController {
   render(req: Request, res: Response, next: NextFunction, svcInfo: any) {
 
     Observable.combineLatest(
-      this.apiService.request(API_CMD.BFF_05_0178, {}),
       this.apiService.request(API_CMD.BFF_05_0167, {}),
       this.apiService.request(API_CMD.BFF_05_0162, {}),
       this.apiService.request(API_CMD.BFF_05_0168, {}),
       this.apiService.request(API_CMD.BFF_05_0143, {}),
       this.apiService.request(API_CMD.BFF_05_0153, {}),
       this.apiService.request(API_CMD.BFF_05_0156, { page: '1', size: '20' }))
-      .subscribe(([r0178info, r0167newJoin, r0162chgAddr, r0168prodChg, r0143periChg, r0153prodChg, r0156as]) => {
-
-        if ( r0178info.code !== API_CODE.CODE_00 ) {
-          this.error.render(res, r0178info);
-        }
-
-        let infoData = r0178info.result;
-
-        /*if ( !infoData ) {
-          infoData = {};
-          infoData = {
-            'svcNm': 'T_온가족무료250',
-            'addr': '인천 중***************'
-          };
-        }*/
+      .subscribe(([r0167newJoin, r0162chgAddr, r0168prodChg, r0143periChg, r0153prodChg, r0156as]) => {
 
         let newAndChgCnt = this._getResultCnt(r0167newJoin);
         newAndChgCnt += this._getResultCnt(r0162chgAddr);
         newAndChgCnt += this._getResultCnt(r0168prodChg);
         newAndChgCnt += this._getResultCnt(r0143periChg);
         newAndChgCnt += this._getResultCnt(r0153prodChg);
-        infoData['newAndChgCnt'] = newAndChgCnt;
 
         let asCnt: any = 0;
         if ( r0156as.code === API_CODE.CODE_00 && r0156as.result ) {
           asCnt = r0156as.result.totalCnt;
         }
+
+        let infoData = {
+          'svcNm': svcInfo.prodNm,
+          'addr': svcInfo.addr,
+          'newAndChgCnt' : newAndChgCnt,
+          'asCnt': asCnt
+        };
+
         infoData['asCnt'] = asCnt;
 
         res.render('wire/myt-join.wire.html', { svcInfo: svcInfo, data: infoData });
