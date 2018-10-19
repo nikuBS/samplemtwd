03e5e921c50e6b24894f7dda44a1ddecc85f58f2
양지훈 SKtechx <jihun202@sk.com>
2018-10-19 13:37:07 +0900

    [양지훈/Client개발팀] 상품 > 해지; 가입유도팝업 출력 로직 수정

--- src/client/app/05.product/product.terminate.js
+++ src/client/app/05.product/product.terminate.js
@@ -135,6 +135,28 @@ Tw.ProductTerminate.prototype = {
       return Tw.Error(resp.code, resp.msg).pop();
     }
 
+    this._apiService.request(Tw.API_CMD.BFF_10_0038, {}, {}, this._prodId)
+      .done($.proxy(this._isVasTerm, this));
+  },
+
+  _isVasTerm: function(resp) {
+    if (resp.code !== Tw.API_CODE.CODE_00 || Tw.FormatHelper.isEmpty(resp.result)) {
+      this._isResultPop = true;
+      return this._openTerminateResultPop();
+    }
+
+    this._openVasTermPopup(resp);
+  },
+
+  _openTerminateResultPop: function() {
+    if ( this._isGoalKeeperProduct ) {
+      return this._historyService.goLoad('/product/detail/NA00004343');
+    }
+
+    if ( !this._isResultPop ) {
+      return;
+    }
+
     var isProdMoney = this.$prodMoney && (this.$prodMoney.length > 0);
 
     this._popupService.open({
@@ -147,50 +169,47 @@ Tw.ProductTerminate.prototype = {
         isBasFeeInfo: isProdMoney,
         basFeeInfo: isProdMoney ? this.$prodMoney.text() : ''
       })
-    }, $.proxy(this._bindTerminateResPopup, this), null, 'join_success');
+    }, $.proxy(this._openResPopupEvent, this), null, 'join_success');
   },
 
-  _bindTerminateResPopup: function($popupContainer) {
+  _openResPopupEvent: function($popupContainer) {
     $popupContainer.on('click', '.fe-btn_success_close', $.proxy(this._goProductDetail, this));
-
-    this._procVasTerm();
   },
 
-  _procVasTerm: function() {
-    // @todo 가입유도 팝업 임시 하드코딩
-    var varsTermProdId = ['NA00004350', 'NA00005638', 'NA00003202', 'NA00000291', 'NA00001890', 'NA00000290'];
-
-    if (varsTermProdId.indexOf(this._prodId) !== -1) {
-      this._popupService.open({
-        'msgheader': true,
-        'msgheaderbg':'bg1',
-        'msgtit':'고객님께<br /><span>추천드려요!</span>',
-        'msgtxt':'안 받는 전화는 있어도 못 받는 전화는 없다!<br />중요한 전화를 놓치지 않도록 도와주는 콜키퍼!<br />든든한 콜키퍼에 가입해 보세요.',
-        'title': '콜키퍼란?',
-        'contents': '내가 놓친 통화의 발신번호, 시간, 스팸정보 등을 문자로 알려드리는 서비스<br />(부가세포함 월 550원)',
-        'link_list': [{
-          style_class:'fe-btn_goalkeeper',
-          txt:'콜키퍼 가입하기'
-        }],
-        'bt': [{
-          style_class: 'bt-blue1 fe-btn_back',
-          txt: '닫기'
-        }]
-      }, $.proxy(this._bindVasTermPopupEvent, this));
-    }
+  _openVasTermPopup: function(resp) {
+    // @todo 가입유도팝업 어드민 resp 내용 삽입; 정책 미확정이므로 일단 하드코딩 팝업 노출;
+    this._popupService.open({
+      hbs:'MV_01_02_02_01',
+      'msgheader': true,
+      'msgheaderbg':'bg1',
+      'msgtit':'고객님께<br /><span>추천드려요!</span>',
+      'msgtxt':'안 받는 전화는 있어도 못 받는 전화는 없다!<br />중요한 전화를 놓치지 않도록 도와주는 콜키퍼!<br />든든한 콜키퍼에 가입해 보세요.',
+      'title': '콜키퍼란?',
+      'contents': '내가 놓친 통화의 발신번호, 시간, 스팸정보 등을 문자로 알려드리는 서비스<br />(부가세포함 월 550원)',
+      'link_list': [{
+        style_class:'fe-btn_goalkeeper',
+        txt:'콜키퍼 가입하기'
+      }],
+      'bt': [{
+        style_class: 'bt-blue1 fe-btn_back',
+        txt: '닫기'
+      }]
+    }, $.proxy(this._bindVasTermPopupEvent, this), $.proxy(this._openTerminateResultPop, this));
   },
 
   _bindVasTermPopupEvent: function($popupContainer) {
-    $popupContainer.on('click', '.fe-btn_goalkeeper', $.proxy(this._goDummyGoalKeeper, this));
-    $popupContainer.on('click', '.fe-btn_back', $.proxy(this._goDummyGoalKeeperBtnBack, this));
+    $popupContainer.on('click', '.fe-btn_back>button', $.proxy(this._closeAndOpenResultPopup, this));
+    $popupContainer.on('click', '.fe-btn_goalkeeper', $.proxy(this._goGoalKeeperProduct, this));
   },
 
-  _goDummyGoalKeeper: function() {
-    this._historyService.goLoad('/product/detail/NA00004343');
+  _goGoalKeeperProduct: function() {
+    this._popupService.close();
+    this._isGoalKeeperProduct = true;
   },
 
-  _goDummyGoalKeeperBtnBack: function() {
-    this._historyService.goBack();
+  _closeAndOpenResultPopup: function() {
+    this._popupService.close();
+    this._isResultPop = true;
   },
 
   _goProductDetail: function() {
