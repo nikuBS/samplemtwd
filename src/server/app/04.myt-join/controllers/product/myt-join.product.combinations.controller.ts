80264f8e6912e1d71bdaf8e35a624f6fa778b3a4
jiyoung.jo <jiyoungjo@sk.com>
2018-10-19 16:27:55 +0900

    [조지영/Client 개발팀] 9차 > 나의 가입상품 > 결합상품 페이지 추가

--- src/server/app/04.myt-join/controllers/product/myt-join.product.combinations.controller.ts
+++ src/server/app/04.myt-join/controllers/product/myt-join.product.combinations.controller.ts
@@ -1,18 +1,15 @@
 /**
  * FileName: myt-join.product.combinations.controller.ts
- * Author: Ji Hun Yang (jihun202@sk.com)
+ * Author: Jiyoung Jo (jiyoungjo@sk.com)
  * Date: 2018.09.19
  */
+
 import TwViewController from '../../../../common/controllers/tw.view.controller';
-import {NextFunction, Request, Response} from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 class MyTJoinProductCombinations extends TwViewController {
-  constructor() {
-    super();
-  }
-
   render(req: Request, res: Response, next: NextFunction, svcInfo: any, layerType: string) {
-    //
+    res.render('product/myt-join.product.combinations.html', { svcInfo });
   }
 }
 
