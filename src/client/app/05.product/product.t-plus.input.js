c82e838aeff68bc868d6cfabfe61aa9364e23ff0
KimInHwan <skt.p132150@partner.sk.com>
2018-10-22 15:56:50 +0900

    [9차/김인환/SPTek] 상품  * T끼리 Plus할인 내 예시보기, 장기가입할인확인하기 기능 추가   ( hbs 추가)  * 선택약정할인 설정/가입 쿼리 타입 추가

---
+++ src/client/app/05.product/product.t-plus.input.js
@@ -0,0 +1,49 @@
+/**
+ * FileName: product.t-plus.input
+ * Author: Kim InHwan (skt.P132150@partner.sk.com)
+ * Date: 2018.10.22
+ *
+ */
+
+Tw.ProductTPlusInput = function (params) {
+  this.$container = params.$element;
+  this.data = params.data;
+  this._popupService = Tw.Popup;
+  this._historyService = new Tw.HistoryService();
+  this._render();
+  this._bindEvent();
+};
+
+Tw.ProductTPlusInput.prototype = {
+
+  _render: function () {
+    this.$longDiscount = this.$container.find('[data-id=plus-example]');
+    this.$plusExample = this.$container.find('[data-id=long-discount]');
+    this.$okBtn = this.$container.find('[data-id=btn-ok]');
+  },
+
+  _bindEvent: function () {
+    this.$longDiscount.on('click', $.proxy(this._onLongDiscountClicked, this));
+    this.$plusExample.on('click', $.proxy(this._onPlusExampleClicked, this));
+    this.$okBtn.on('click', $.proxy(this._onOkBtnClicked, this));
+  },
+
+  _onLongDiscountClicked: function() {
+    this._popupService.open({
+      hbs:'BS_03_02_01_02',
+      layer: true
+    }, null, null, 'BS_03_02_01_02');
+  },
+
+  _onPlusExampleClicked: function() {
+    this._popupService.open({
+      hbs:'BS_03_02_01_03',
+      layer: true,
+      info: this.data.svcInfo
+    }, null, null, 'BS_03_02_01_03');
+  },
+
+  _onOkBtnClicked: function() {
+    this._historyService.goBack();
+  }
+};
