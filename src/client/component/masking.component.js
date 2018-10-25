6304749d39fe29ba5e7cb206b45daaf55be3ab63
조아라 SKtechx <araara.jo@sk.com>
2018-10-25 11:16:01 +0900

    [조아라/Client개발팀] footer 구조 변경

---
+++ src/client/component/masking.component.js
@@ -0,0 +1,18 @@
+/**
+ * FileName: masking.component.js
+ * Author: Ara Jo (araara.jo@sk.com)
+ * Date: 2018.10.25
+ */
+Tw.MaskingComponent = function () {
+  this._bindEvent();
+};
+Tw.MaskingComponent.prototype = {
+  _bindEvent: function () {
+    $('#fe-bt-masking').on('click', $.proxy(this._onClickMasking, this));
+  },
+  _onClickMasking: function () {
+    console.log('masking');
+  }
+
+};
+ㅎ
