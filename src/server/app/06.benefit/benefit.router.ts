f0a6426899caabecfd25c087f3f31242457b8bb7
이규광SkTechX <1111>
2018-11-02 08:49:42 +0900

    [이규광/SPTEK] #9 T멤버십 > 멤버십 혜택 > 가맹점 보기,지도보기,무비/컬처 폴더이동 -> 기존소스 삭제

--- src/server/app/06.benefit/benefit.router.ts
+++ src/server/app/06.benefit/benefit.router.ts
@@ -2,9 +2,6 @@ import TwRouter from '../../common/route/tw.router';
 import BenefitMembershipJoin from './controllers/membership/benefit.membership.join';
 import BenefitIndex from './controllers/index/benefit.index.controller';
 import BenefitMyBenefitRainbowPoint from './controllers/my-benefit/benefit.my-benefit.rainbow-point.controller';
-import BenefitMembershipPartnerMovieCulture from './controllers/membership/benefit.membership.partner.movieculture';
-import BenefitMembershipPartnerShopList from './controllers/membership/benefit.membership.partner.shoplist';
-import BenefitMembershipPartnerShopMap from './controllers/membership/benefit.membership.partner.shopmap';
 
 class BenefitRouter extends TwRouter {
   constructor() {
@@ -14,9 +11,6 @@ class BenefitRouter extends TwRouter {
     this.controllers.push({ url: '/membership/join', controller: BenefitMembershipJoin });
     this.controllers.push({ url: '/index', controller: BenefitIndex });
     this.controllers.push({ url: '/my-benefit/rainbow-point', controller: BenefitMyBenefitRainbowPoint });
-    this.controllers.push({ url: '/membership/partner/movieculture', controller: BenefitMembershipPartnerMovieCulture });
-    this.controllers.push({ url: '/membership/partner/shoplist', controller: BenefitMembershipPartnerShopList });
-    this.controllers.push({ url: '/membership/partner/shopmap', controller: BenefitMembershipPartnerShopMap });
   }
 }
 
