e56330e0a22264081ee0278975ae8a94bb8d6e58
P130715 <skt.p130715@partnet.sk.com>
2018-11-02 14:08:55 +0900

    [양정규/SPtek] 멤버십 안내 > 멤버십 카드 및 등급안내  - API 연동

--- src/client/component/membership-info.layer.component.js
+++ src/client/component/membership-info.layer.component.js
@@ -31,19 +31,66 @@ Tw.MembershipInfoLayerPopup.prototype = {
 
   _openCallback: function ($element) {
     if ( this._hbs === 'BE_04_01_L02' ) {
-      this.$membershipJoinBtn = $element.find('button[data-id=join]');
-      this.$membershipJoinBtn.on('click', $.proxy(this._onClickJoinBtn, this));
+      this._reqPossibleJoin();
+      $element.find('button[data-id=join]').one('click', $.proxy(this._onClickJoinBtn, this));
+    }
+    else if ( this._hbs === 'BE_04_01_L01' ) {
+      $element.find('.fe-outlink').off().on('click', $.proxy(this._openExternalUrl,this));
     }
   },
-  _closeCallback: function () {
-    if ( this._hbs === 'BE_04_01_L02' ) {
-      this.$membershipJoinBtn.off('click');
-      this.$membershipJoinBtn = null;
+
+  _openExternalUrl : function (e) {
+    e.preventDefault();
+    var _url = $(e.currentTarget).attr('href');
+    Tw.CommonHelper.openUrlExternal(_url);
+  },
+
+  // 가입 가능여부 조회 요청
+  _reqPossibleJoin : function(){
+    skt_landing.action.loading.on({ ta: '.container', co: 'grey', size: true });
+    var mockup = function () {
+      $.ajax('/mock/membership.info.BFF_11_0015.json')
+        .done($.proxy(this._onSuccess, this))
+        .fail($.proxy(this._onFail, this));
+    };
+
+    var real = function () {
+      this._apiService
+        .request(Tw.API_CMD.BFF_11_0015, {})
+        .done($.proxy(this._onSuccess, this))
+        .fail($.proxy(this._onFail, this));
+    };
+    // TODO : API 개발완료되기 전까지 목업호출..
+    mockup.call(this);
+  },
+
+  _onSuccess : function (resp) {
+    if ( resp.code !== Tw.API_CODE.CODE_00 ) {
+      this._onFail(resp);
+      return false;
     }
+    this._isJoinOk = resp.result.cardCreatableYn;
+    skt_landing.action.loading.off({ ta: '.container' });
+  },
+
+  _closeCallback: function () {
     this._popupService.close();
   },
 
   _onClickJoinBtn: function () {
+    if (this._isJoinOk === 'N') {
+      // TODO : 가입불가 팝업 SB 나오면 처리하기!!
+      var _mailUrl = '/membership/membership_info/mbrs_0001';
+      this._popupService.afterRequestSuccess(_mailUrl, _mailUrl, 'go main', 'No Join');
+      return;
+    }
+
     this._historyService.goLoad('/benefit/membership/join');
+  },
+
+  // API Fail
+  _onFail: function (err) {
+    skt_landing.action.loading.off({ ta: '.container' });
+    Tw.Error(err.code, err.msg).pop();
   }
 };
--- src/server/types/url.type.ts
+++ src/server/types/url.type.ts
@@ -106,5 +106,5 @@ export const URL = {
   '/common/member/management': { login: true, id: 'CO_ME_01_05', title: '회원정보' },
 
   // Membership
-  '/membership/membership_info/mbrs_0001': { login: true, id: 'BE_04_01', title: '멤버십 카드 및 등급 안내' }
+  '/membership/membership_info/mbrs_0001': { login: false, id: 'BE_04_01', title: '멤버십 카드 및 등급 안내' }
 };
