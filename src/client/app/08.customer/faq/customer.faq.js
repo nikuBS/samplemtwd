b744562f98d28341e09349dece38b3e6c2358cdd
Hakjoon Sim <hakjoon.sim@sk.com>
2018-11-05 17:26:37 +0900

    [심학준/FE개발팀] SP#9 FAQ and FAQ search

---
+++ src/client/app/08.customer/faq/customer.faq.js
@@ -0,0 +1,52 @@
+/**
+ * FileName: customer.faq.js
+ * Author: Hakjoon Sim (hakjoon.sim@sk.com)
+ * Date: 2018.11.05
+ */
+
+Tw.CustomerFaq = function (rootEl) {
+  this.$container = rootEl;
+
+  this._historyService = new Tw.HistoryService();
+
+  this._cacheElements();
+  this._bindEvents();
+};
+
+Tw.CustomerFaq.prototype = {
+  _cacheElements: function () {
+    this.$btnSearch = this.$container.find('#fe-search');
+    this.$inputSearch = this.$container.find('#fe-input');
+  },
+  _bindEvents: function () {
+    this.$container.on('click', '.fe-external', $.proxy(this._onExternalLinks, this));
+    this.$container.on('click', '.cancel', $.proxy(this._onQueryDeleted, this));
+    this.$inputSearch.on('keyup', $.proxy(this._onSearchInput, this));
+    this.$btnSearch.on('click', $.proxy(this._onSearchRequested, this));
+  },
+  _onExternalLinks: function (e) {
+    if (Tw.BrowserHelper.isApp()) {
+      Tw.Native.send(Tw.NTV_CMD.OPEN_URL, {
+        type: Tw.NTV_BROWSER.EXTERNAL,
+        href: $(e.currentTarget).attr('href')
+      });
+      return false;
+    }
+    return true;
+  },
+  _onSearchInput: function (e) {
+    if (Tw.FormatHelper.isEmpty(e.currentTarget.value.trim())) {
+      this.$btnSearch.attr('disabled', 'disabled');
+    } else {
+      this.$btnSearch.removeAttr('disabled');
+    }
+  },
+  _onQueryDeleted: function () {
+    this.$btnSearch.attr('disabled', 'disabled');
+    return true;
+  },
+  _onSearchRequested: function () {
+    var keyword = this.$inputSearch.val().trim();
+    this._historyService.goLoad('/customer/faq/search?keyword=' + keyword);
+  }
+};
---
+++ src/server/app/08.customer/controllers/faq/customer.faq.search.controller.ts
@@ -0,0 +1,54 @@
+/**
+ * FileName: customer.faq.search.controller.ts
+ * Author: Hakjoon sim (hakjoon.sim@sk.com)
+ * Date: 2018.11.05
+ */
+
+import TwViewController from '../../../../common/controllers/tw.view.controller';
+import { Request, Response, NextFunction } from 'express';
+import { API_CMD, API_CODE } from '../../../../types/api-command.type';
+import { Observable } from 'rxjs/Observable';
+
+class CustomerFaqSearch extends TwViewController {
+  render(req: Request, res: Response, next: NextFunction, svcInfo: any) {
+    const keyword = req.query.keyword;
+    this.getSearchResult(keyword, res, svcInfo).subscribe(
+      (result) => {
+        res.render('faq/customer.faq.search.html', {
+          result: result.content,
+          total: parseInt(result.totalElements, 10),
+          keyword
+        });
+      },
+      (err) => {
+        this.error.render(res, {
+          code: err.code,
+          msg: err.msg,
+          svcInfo
+        });
+      }
+    );
+  }
+
+  private getSearchResult(keyword: string, res: Response, svcInfo: any): Observable<any> {
+    return this.apiService.request(API_CMD.BFF_08_0050, {
+      srchKey: encodeURIComponent(keyword),
+      page: 0,
+      size: 20
+    }).map((resp) => {
+      if (resp.code === API_CODE.CODE_00) {
+        return resp.result;
+      }
+
+      this.error.render(res, {
+        code: resp.code,
+        msg: resp.msg,
+        svcInfo
+      });
+
+      return undefined;
+    });
+  }
+}
+
+export default CustomerFaqSearch;
