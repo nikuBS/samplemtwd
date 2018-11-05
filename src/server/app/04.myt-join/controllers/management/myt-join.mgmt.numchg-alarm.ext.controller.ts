1a8d839951c4be0dc3f00c55af094f4f9db0b616
이규광SkTechX <1111>
2018-11-05 16:42:17 +0900

    [이규광/SPTEK] #9 나의 가입정보 > 번호변경안내서비스 - api연동

--- src/server/app/04.myt-join/controllers/management/myt-join.mgmt.numchg-alarm.ext.controller.ts
+++ src/server/app/04.myt-join/controllers/management/myt-join.mgmt.numchg-alarm.ext.controller.ts
@@ -12,35 +12,49 @@ class MyTJoinMgmtNumChgAlarmExt extends TwViewController {
 
   render(req: Request, res: Response, next: NextFunction, svcInfo: any, allSvc: any, childInfo: any, pageInfo: any) {
 
-    // this.apiService.request(API_CMD.BFF_05_0180, {})
-    //   .subscribe((resp) => {
-    const resp = {
-      'code': '00',
-      'msg': '결과메세지',
-      'result':
-        {
-          'oldSvcNum': '01056**78**',
-          'newSvcNum': '01012**34**',
-          'notiType': 'S',
-          'freeOfrEndDt': '20191030',
-          'notiStaDt': '20181031',
-          'notiEndDt': '20190331',
-          'extnsPsblYn': 'N',
-          'orglSktYn': 'Y'
-        }
-    };
+    this.apiService.request(API_CMD.BFF_05_0180, {})
+      .subscribe((resp) => {
+        /*const resp = {
+          'code': '00',
+          'msg': '결과메세지',
+          'result':
+            {
+              'oldSvcNum': '01056**78**',
+              'newSvcNum': '01012**34**',
+              'notiType': 'S',
+              'freeOfrEndDt': '20191030',
+              'notiStaDt': '20181031',
+              'notiEndDt': '20190331',
+              'extnsPsblYn': 'N',
+              'orglSktYn': 'Y'
+            }
+        };*/
 
         if ( resp.code === API_CODE.CODE_00 ) {
           const result = resp.result;
           result['notiStaDt'] = DateHelper.getShortDateNoDot(result['notiStaDt']);
           result['notiEndDt'] = DateHelper.getShortDateNoDot(result['notiEndDt']);
 
-          const option = { svcInfo: svcInfo, pageInfo: pageInfo, data: result};
+          const option = { svcInfo: svcInfo, pageInfo: pageInfo, data: result };
           res.render('management/myt-join.mgmt.numchg-alarm.ext.html', option);
+        } else {
+          return this.error.render(res, {
+            title: '번호변경 안내 서비스',
+            code: resp.code,
+            msg: resp.msg,
+            svcInfo: svcInfo
+          });
         }
-      // });
+      }, (resp) => {
+        return this.error.render(res, {
+          title: '번호변경 안내 서비스',
+          code: resp.code,
+          msg: resp.msg,
+          svcInfo: svcInfo
+        });
+      });
 
-    // res.render('management/myt-join.mgmt.numchg-alarm.ext.html', { svcInfo: svcInfo, pageInfo : pageInfo });
+
   }
 }
 
