8c6350735ebe467a2f4c5e6f2e4eccb1b7afbd1c
이규광SkTechX <1111>
2018-11-07 16:04:29 +0900

    [이규광/SPTEK] #9 나의 가입정보 > 번호변경안내서비스 - 서비스이용불가 메세지 alert 추가 등

--- src/server/app/04.myt-join/controllers/management/myt-join.mgmt.numchg-alarm.controller.ts
+++ src/server/app/04.myt-join/controllers/management/myt-join.mgmt.numchg-alarm.controller.ts
@@ -6,6 +6,7 @@
 import TwViewController from '../../../../common/controllers/tw.view.controller';
 import { NextFunction, Request, Response } from 'express';
 import { API_CMD, API_CODE } from '../../../../types/api-command.type';
+import StringHelper from '../../../../utils/string.helper';
 
 class MyTJoinMgmtNumChgAlarm extends TwViewController {
 
@@ -13,25 +14,27 @@ class MyTJoinMgmtNumChgAlarm extends TwViewController {
 
      this.apiService.request(API_CMD.BFF_05_0180, {})
        .subscribe((resp) => {
-        /*const resp = {
-          'code': '00',
-          'msg': '결과메세지',
-          'result':
-            {
-              'oldSvcNum': '01056**78**',
-              'newSvcNum': '01012**34**',
-              'notiType': 'S',
-              'freeOfrEndDt': '20191030',
-              'notiStaDt': '20181031',
-              'notiEndDt': '20190331',
-              'extnsPsblYn': 'N',
-              'orglSktYn': 'Y'
-            }
-        };*/
+        // const resp = {
+        //   'code': '00',
+        //   'msg': '결과메세지',
+        //   'result':
+        //     {
+        //       'oldSvcNum': '01056**78**',
+        //       'newSvcNum': '01012**34**',
+        //       'notiType': 'S',
+        //       'freeOfrEndDt': '20191030',
+        //       'notiStaDt': '20181031',
+        //       'notiEndDt': '20190331',
+        //       'extnsPsblYn': 'N',
+        //       'orglSktYn': 'Y'
+        //     }
+        // };
         
 
         if ( resp.code === API_CODE.CODE_00 ) {
           const result = resp.result;
+          result['oldSvcNum'] = StringHelper.phoneStringToDash(result['oldSvcNum']);
+          result['newSvcNum'] = StringHelper.phoneStringToDash(result['newSvcNum']);
 
           const option = { svcInfo: svcInfo, pageInfo: pageInfo, data: result};
           res.render('management/myt-join.mgmt.numchg-alarm.html', option);
