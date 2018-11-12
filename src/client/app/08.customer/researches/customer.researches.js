97553ce4f1d3610ab990fbbfb7f2983f7d001838
jiyoung.jo <jiyoungjo@sk.com>
2018-11-12 13:57:48 +0900

    [조지영/Client 개발팀] 설문조사 리스트 이벤트 추가

---
+++ src/client/app/08.customer/researches/customer.researches.js
@@ -0,0 +1,72 @@
+Tw.CustomerResearches = function(rootEl) {
+  this.$container = rootEl;
+
+  this._popupService = Tw.Popup;
+  this._apiService = Tw.Api;
+
+  this._bnidEvent();
+};
+
+Tw.CustomerResearches.prototype = {
+  _bnidEvent: function() {
+    this.$container.on('click', '.item-two > .bt-blue1', $.proxy(this._handleSubmit, this));
+    this.$container.on('click', 'ul.select-list > li', $.proxy(this._setEnableSubmit, this));
+  },
+
+  _handleSubmit: function(e) {
+    var $root = $(e.currentTarget).parents('li.acco-box');
+    var $list = $root.find('ul.select-list > li');
+    var $etcText = $root.find('textarea.poll-area');
+    var answerNumber = $root.data('answer-num');
+
+    var options = {
+      bnnrRsrchId: $root.data('research-id'),
+      bnnrRsrchTypCd: $root.data('type-code')
+    };
+
+    if (answerNumber) {
+      options.canswNum = $root.data('answer-num');
+    }
+
+    if ($etcText.length > 0) {
+      options.etcTextNum = $etcText.data('etc-idx');
+      options.etcText = $etcText.val();
+    }
+
+    for (var i = 0; i < $list.length; i++) {
+      if ($list[i].getAttribute('aria-checked') === 'true') {
+        options['rpsCtt' + (i + 1)] = i + 1;
+      }
+    }
+
+    this._apiService.request(Tw.API_CMD.BFF_08_0035, options).done($.proxy(this._successParticipation, this));
+  },
+
+  _handleSuccessSubmit: function(resp) {
+    if (resp.code !== Tw.API_CODE.CODE_00) {
+      return Tw.Error(resp.code, resp.msg).pop();
+    }
+
+    switch (resp.result) {
+      case 'DUPLICATE':
+        this._popupService.openAlert(Tw.ALERT_MSG_CUSTOMER.ALERT_RESEARCHES_A01);
+        break;
+      case 'SUCCESSY':
+      case 'SUCCESSN':
+        this._popupService.openAlert(Tw.ALERT_MSG_CUSTOMER.ALERT_RESEARCHES_A02);
+        break;
+    }
+  },
+
+  _setEnableSubmit: function(e) {
+    var $target = $(e.currentTarget);
+    var $root = $target.parents('li.acco-box');
+    var $btn = $root.find('.item-two > .bt-blue1 button');
+
+    if ($target.hasClass('checkbox') && $root.find('ul.select-list li[aria-checked="true"]').length === 0) {
+      $btn.attr('disabled', true);
+    } else if ($btn.attr('disabled')) {
+      $btn.attr('disabled', false);
+    }
+  }
+};
