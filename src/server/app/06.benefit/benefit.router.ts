ee1e75205f9c14baa4eb76a0b40a7986d2c6f84b
KimInHwan <skt.p132150@partner.sk.com>
2018-11-14 18:16:09 +0900

    [김인환/SPTek] 멤버십 안내 > 멤버십 가입하기  * 신규 url 적용에 따른 파일 이동 및 수정

--- src/server/app/06.benefit/benefit.router.ts
+++ src/server/app/06.benefit/benefit.router.ts
@@ -1,5 +1,4 @@
 import TwRouter from '../../common/route/tw.router';
-import BenefitMembershipJoin from './controllers/membership/benefit.membership.join';
 import BenefitIndex from './controllers/index/benefit.index.controller';
 import { BenefitMyBenefitRainbowPoint } from './controllers/my-benefit/benefit.my-benefit.rainbow-point.controller';
 import BenefitMyBenefitRainbowPointAdjustment from './controllers/my-benefit/benefit.my-benefit.rainbow-point.adjustment.controller';
@@ -14,7 +13,6 @@ class BenefitRouter extends TwRouter {
     super();
     // this.controllers.push({ url: '/', controller: HomeMain });
 
-    this.controllers.push({ url: '/membership/join', controller: BenefitMembershipJoin });
     this.controllers.push({ url: '/index', controller: BenefitIndex });
     this.controllers.push({ url: '/my-benefit/rainbow-point', controller: BenefitMyBenefitRainbowPoint });
     this.controllers.push({ url: '/my-benefit/rainbow-point/adjustment', controller: BenefitMyBenefitRainbowPointAdjustment });
