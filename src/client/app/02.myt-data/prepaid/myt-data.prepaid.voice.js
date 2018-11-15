b3674390717c4453045507737989b002cdb1a734
JimanPark <jiman.park@sk.com>
2018-11-15 13:59:59 +0900

    [박지만/Client개발팀] myt 선불폰 api 커맨드 추가
    - 카드 예제보기 마크업 추가

--- src/client/app/02.myt-data/prepaid/myt-data.prepaid.voice.js
+++ src/client/app/02.myt-data/prepaid/myt-data.prepaid.voice.js
@@ -17,25 +17,58 @@ Tw.MyTDataPrepaidVoice = function (rootEl) {
 
 Tw.MyTDataPrepaidVoice.prototype = {
   _init: function () {
-  //  {
-    // "code": "00",
-    // "msg": "success",
-    // "result":
-    // {
-    // "prodAmt" : "50000",
-    // "remained" : "100",
-    // "obEndDt": "20190820",
-    // "inbEndDt": "20190830",
-    // "numEndDt": "20210421"
-    // "dataYn": "Y",
-    // "dataOnlyYn": "N"
-    // }
-    // }
   },
 
   _cachedElement: function () {
+    this.$wrapExampleCard = $('.fe-wrap-example-card');
   },
 
   _bindEvent: function () {
- }
+    this.$container.on('click', '.fe-close-example-card', $.proxy(this._onCloseExampleCard, this));
+    this.$container.on('click', '.fe-btn-show-example', $.proxy(this._onShowExampleCard, this));
+    this.$container.on('click', '.fe-select-amount', $.proxy(this._onShowSelectAmount, this));
+  },
+
+  _onShowSelectAmount: function (e) {
+    var $elButton = $(e.currentTarget);
+    var fnSelectAmount = function (item) {
+      return {
+        value: item.title,
+        option: false,
+        attr: 'data-value=' + item.value
+      };
+    };
+    if (this.$cardNumber.val() !== '') {
+      this._apiService.request(Tw.API_CMD.BFF_07_0024, { cardNum: $.trim(this.$cardNumber.val()).substr(0, 6) })
+        .done($.proxy(this._getSuccess, this))
+        .fail($.proxy(this._getFail, this));
+    }
+    this._popupService.open({
+        hbs: 'actionsheet_select_a_type',
+        layer: true,
+        title: Tw.MYT_PREPAID_AMOUNT.title,
+        data: [{ list: Tw.MYT_PREPAID_AMOUNT.list.map($.proxy(fnSelectAmount, this)) }]
+      },
+      $.proxy(this._selectPopupCallback, this, $elButton),
+      null
+    );
+  },
+
+  _selectPopupCallback: function ($target, $layer) {
+    $layer.on('click', '[data-value]', $.proxy(this._setSelectedValue, this, $target));
+  },
+
+  _setSelectedValue: function ($target, e) {
+    this._popupService.close();
+    $target.text($(e.currentTarget).text());
+    $target.data('amount', $(e.currentTarget).data('value'));
+  },
+
+  _onCloseExampleCard: function () {
+    this.$wrapExampleCard.hide();
+  },
+
+  _onShowExampleCard: function () {
+    this.$wrapExampleCard.show();
+  }
 };
