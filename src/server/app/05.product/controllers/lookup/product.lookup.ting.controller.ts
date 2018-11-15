ec40ca28f44cc88d1ca13a569cb16f48d4b3d529
양지훈 SKtechx <jihun202@sk.com>
2018-11-15 09:55:14 +0900

    [양지훈/FE개발팀] 상품 > 가입설정해지
    - 모바일 요금제 > 데이터 상한금액 ; 설정
    - 모바일 요금제 > 데이터 상한금액 ; 조회

--- src/server/app/05.product/controllers/lookup/product.lookup.ting.controller.ts
+++ src/server/app/05.product/controllers/lookup/product.lookup.ting.controller.ts
@@ -7,14 +7,14 @@
 import TwViewController from '../../../../common/controllers/tw.view.controller';
 import { Request, Response, NextFunction } from 'express';
 import FormatHelper from '../../../../utils/format.helper';
+import { API_CMD, API_CODE } from '../../../../types/api-command.type';
 
 class ProductLookupTing extends TwViewController {
   constructor() {
     super();
   }
 
-  private readonly _allowedProdIdList = ['NA00003160', 'NA00003161', 'NA00002591', 'NA00002592', 'NA00002593', 'NA00002594', 'NA00003011',
-    'NA00003048', 'NA00003146', 'NA00003405', 'NA00003406', 'NA00003407', 'NA00003408'];
+  private readonly _allowedProdIdList = ['NA00002670', 'NA00002671', 'NA00002669'];
 
   render(req: Request, res: Response, next: NextFunction, svcInfo: any, allSvc: any, childInfo: any, pageInfo: any) {
     const prodId = req.params.prodId || null,
@@ -28,7 +28,19 @@ class ProductLookupTing extends TwViewController {
       return this.error.render(res, renderCommonInfo);
     }
 
-    res.render('lookup/product.lookup.ting.html', renderCommonInfo);
+    this.apiService.request(API_CMD.BFF_10_0040, {}, {}, prodId)
+      .subscribe((tingInfo) => {
+        if (tingInfo.code !== API_CODE.CODE_00) {
+          return this.error.render(res, Object.assign(renderCommonInfo, {
+            code: tingInfo.code,
+            msg: tingInfo.msg
+          }));
+        }
+
+        res.render('lookup/product.lookup.ting.html', Object.assign(renderCommonInfo, {
+          tingInfo: tingInfo.result
+        }));
+      });
   }
 }
 
