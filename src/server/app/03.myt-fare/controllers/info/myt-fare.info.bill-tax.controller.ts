955d7b61df151f7b7cd1c9d14a48cedfd909b015
이기림(Kirim Lee)/FE개발팀/SKP <kirim@sk.com>
2018-11-22 10:41:13 +0900

    [이기림/Client개발팀] 요금납부 > 세금계산서 발급내역 개월 산정
    최근 6개월간의 기록조회가 되도록 수정

--- src/server/app/03.myt-fare/controllers/info/myt-fare.info.bill-tax.controller.ts
+++ src/server/app/03.myt-fare/controllers/info/myt-fare.info.bill-tax.controller.ts
@@ -7,52 +7,88 @@
 import TwViewController from '../../../../common/controllers/tw.view.controller';
 import {Request, Response, NextFunction} from 'express';
 import {API_CMD, API_CODE} from '../../../../types/api-command.type';
-
+import {Observable} from 'rxjs/Observable';
 // import {MYT_PAY_HISTORY_TITL} from '../../../../types/bff.type';
 // import {DATE_FORMAT, MYT_BILL_HISTORY_STR} from '../../../../types/string.type';
 
 import FormatHelper from '../../../../utils/format.helper';
 import DateHelper from '../../../../utils/date.helper';
+import { isNull } from 'util';
+import { runInThisContext } from 'vm';
 
 interface Query {
   isQueryEmpty: boolean;
 }
 
+interface TaxList {
+  listId: number;
+  ctzBizName: string;
+  taxBillIsueDt: string;
+  splyPrc: string;
+  vatAmt: string;
+  totAmt: string;
+}
+
 class MyTFareInfoBillTax extends TwViewController {
   constructor() {
     super();
-  }
+  }  
 
   render(req: Request, res: Response, next: NextFunction, svcInfo: any, pageInfo: any) {
     const query: Query = {
       isQueryEmpty: FormatHelper.isEmpty(req.query)
     };
 
+    // 몇 달 전까지의 기록만 가능
+    const monthPeriod = 10;
+    // 더보기 구현 
+
+    // 분기/반기로만 조회가 가능해 6번 호출
+    Observable.combineLatest(
+      this.getBillTaxLists(DateHelper.getCurrentDate(), monthPeriod, svcInfo)
+    ).subscribe(taxlist => {
+      res.render('info/myt-fare.info.bill-tax.html', {svcInfo, pageInfo, data: {
+        limitMonth: monthPeriod,
+        items: this.mergeList(taxlist)
+      }});
+    });
+  }
 
 
-    this.apiService.request(API_CMD.BFF_07_0017, {}).subscribe((resp) => {
-      
+  private getBillTaxList = (date: string, svcInfo: any): Observable<any | null> => {
+    return this.apiService.request(API_CMD.BFF_07_0017, {selType: 'M', selSearch: date}).map((resp: {code: string, result: any}) => {
       if (resp.code !== API_CODE.CODE_00) {
-        return this.error.render(res, {
-          code: resp.code,
-          msg: resp.msg,
-          svcInfo: svcInfo,
-          // ,pageInfo: pageInfo
-        });
+        return null;
       }
 
-      resp.result.taxReprintList.map((o, i) => {
+      resp.result.taxReprintList.map((o) => {
         o.ctzBizName = svcInfo.eqpMdlNm;
-        o.listId = i;
         o.taxBillIsueDt = DateHelper.getShortDateWithFormat(o.taxBillIsueDt, 'YYYY.MM.DD');
         o.splyPrc = FormatHelper.addComma(o.splyPrc.toString());
         o.vatAmt = FormatHelper.addComma(o.vatAmt.toString());
         o.totAmt = FormatHelper.addComma(o.totAmt.toString());
       });
 
-      res.render('info/myt-fare.info.bill-tax.html', {svcInfo: svcInfo, pageInfo: pageInfo, data: {
-          items: resp.result.taxReprintList
-        }});
+      return resp.result.taxReprintList;
+    });
+  }
+
+  private getBillTaxLists = (date: Date, monthPeriod: number, svcInfo: any) => {
+    // monthPeriod 개월 전 구하기
+    date.setDate(1);
+    date.setMonth(date.getMonth() - monthPeriod);
+    const list: any[] = [];
+    for ( let i = 0; i < monthPeriod; i++) {
+      list.push(this.getBillTaxList(DateHelper.getYearMonth(new Date(date)), svcInfo));
+      date.setMonth(date.getMonth() + 1);
+    }
+    return list; 
+  }
+  
+  private mergeList = (taxlist): TaxList[] => {
+    return [].concat.apply([], taxlist).map((tax, i) => {
+      tax.listId = i;
+      return tax;
     });
   }
 
