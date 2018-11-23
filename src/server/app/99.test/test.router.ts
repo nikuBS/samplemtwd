e7c78d7d79ed1ba2fdac7afb60b3364f395870fe
양지훈 SKtechx <jihun202@sk.com>
2018-11-23 15:32:01 +0900

    [양지훈/FE개발팀] 테스트; 공지사항 case 생성

--- src/server/app/99.test/test.router.ts
+++ src/server/app/99.test/test.router.ts
@@ -3,6 +3,7 @@ import TestHome from './controllers/test.home.controller';
 import TestMyTDataSubmainController from './controllers/test.myt-data.submain.controller';
 import TestMyTFareSubmainController from './controllers/test.myt-fare.submain.controller';
 import TestMyTFareUnbill from './controllers/submain/test.myt-fare.submain.non-paymt';
+import TestCustomerSvcInfoNotice from './controllers/test.customer.svc-info.notice.controller';
 import TestMyTDataInfo from './controllers/test.myt-data.datainfo.controller';
 
 class TestRouter extends TwRouter {
@@ -12,6 +13,7 @@ class TestRouter extends TwRouter {
     this.controllers.push({ url: '/myt-data/submain', controller: TestMyTDataSubmainController });
     this.controllers.push({ url: '/myt-fare/submain', controller: TestMyTFareSubmainController });
     this.controllers.push({ url: '/myt-fare/unbill', controller: TestMyTFareUnbill });
+    this.controllers.push({ url: '/customer/svc-info/notice(/:category)?', controller: TestCustomerSvcInfoNotice });
     this.controllers.push({ url: '/myt-data/datainfo', controller: TestMyTDataInfo });
   }
 }
