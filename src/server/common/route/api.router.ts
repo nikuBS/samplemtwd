dfc78c1311f6f162fd60e45f157d5156f7b62540
양지훈 SKtechx <jihun202@sk.com>
2018-11-28 16:37:10 +0900

    [양지훈/FE개발팀] 파일 업로드 수정

--- src/server/common/route/api.router.ts
+++ src/server/common/route/api.router.ts
@@ -168,16 +168,15 @@ class ApiRouter {
   // }
 
   private upload() {
-    let returnPath = 'uploads/';
     const currentDate = new Date(),
       storage = multer.diskStorage({
         destination: (req, file, cb) => {
-          let storagePath = path.resolve(__dirname, '../../../../', returnPath);
+          let storagePath = path.resolve(__dirname, '../../../../', 'uploads/');
 
           if (!FormatHelper.isEmpty(req.body.dest)) {
             const dateFormat = dateHelper.getShortDateWithFormat(currentDate, 'YYMMDD');
 
-            storagePath += req.body.dest + '/';
+            storagePath += '/' + req.body.dest + '/';
             if (!fs.existsSync(storagePath)) {
               fs.mkdirSync(storagePath);
             }
@@ -186,8 +185,6 @@ class ApiRouter {
             if (!fs.existsSync(storagePath)) {
               fs.mkdirSync(storagePath);
             }
-
-            returnPath += req.body.dest + '/' + dateFormat + '/';
           }
 
           cb(null, storagePath);
@@ -198,16 +195,11 @@ class ApiRouter {
         limits: { fileSize: 5 * 1024 * 1024 }
       });
 
-    return {
-      path: returnPath,
-      component: multer({ storage: storage }).array('file')
-    };
+    return multer({ storage: storage }).array('file');
   }
 
   private uploadFile(req: Request, res: Response, next: NextFunction) {
-    const upload = this.upload();
-
-    upload.component(req, res, (err) => {
+    this.upload()(req, res, (err) => {
       if ( err ) {
         this.logger.error(this, err);
         res.json({ code: err.errno, msg: err.code });
@@ -223,7 +215,7 @@ class ApiRouter {
           return {
             name: file.filename,
             size: file.size,
-            path: upload.path,
+            path: file.destination.match(/uploads\/(.+)?/)[0],
             originalName: file.originalname
           };
         })
