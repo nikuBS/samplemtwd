cd1ecd64372926df2cae4c8634f3bba35f2ee162
이기림(Kirim Lee)/FE개발팀/SKP <kirim@sk.com>
2018-11-29 17:03:27 +0900

    [이기림/Client개발팀] 콘텐츠 월 이용내역

---
+++ src/server/app/03.myt-fare/controllers/billcontents/myt-fare.bill.contents.monthly.controller.ts
@@ -0,0 +1,62 @@
+/**
+ * FileName: myt-fare.bill.contents.monthly.controller.ts
+ * Author: Lee kirim (kirim@sk.com)
+ * Date: 2018. 11. 29
+ */
+
+import TwViewController from '../../../../common/controllers/tw.view.controller';
+import {Request, Response, NextFunction} from 'express';
+import DateHelper from '../../../../utils/date.helper';
+import { API_CMD, API_CODE } from '../../../../types/api-command.type';
+import FormatHelper from '../../../../utils/format.helper';
+
+interface Query {
+  gubun: string;
+  requestCnt: number;
+}
+interface Result {
+  [key: string]: string;
+}
+class MyTFareBillContentsMonthly extends TwViewController {
+  render(req: Request, res: Response, next: NextFunction, svcInfo: any, pageInfo: any) {
+
+    const query: Query = {
+      gubun: 'Done',
+      requestCnt: 1
+    };
+
+    this.apiService.request(API_CMD.BFF_07_0081, query).subscribe((resp) => {
+      console.log('\x1b[36m%s\x1b[0m', '------log auto code', resp.code, resp.result);
+      this.logger.info(this, resp.code !== API_CODE.CODE_00, resp);      
+      if (resp.code !== API_CODE.CODE_00) {
+        return this.error.render(res, {
+          code: resp.code,
+          msg: resp.msg,
+          svcInfo: svcInfo
+          //  ,pageInfo: pageInfo
+        });
+      }
+
+      const data: Result | any = resp.result;
+      if (data) {
+        data.useAmt = FormatHelper.addComma(data.tmthUseAmt);
+        data.limiitAmt = FormatHelper.addComma(data.useContentsLimitAmt);
+        data.remainAmt = FormatHelper.addComma(data.remainUseLimit);
+        data.payAmt = FormatHelper.addComma(data.tmthChrgAmt);
+        data.ableAmt = FormatHelper.addComma(data.tmthChrgPsblAmt);
+      }
+      
+      res.render('billcontents/myt-fare.bill.contents.monthly.html', {
+        svcInfo: svcInfo, 
+        pageInfo: pageInfo, 
+        data,
+        curMonth: DateHelper.getCurrentMonth() // 현재 월
+      });
+
+    });
+
+  }
+
+}
+
+export default MyTFareBillContentsMonthly;
