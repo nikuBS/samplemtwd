b38c480b5c812e475fafbf57ca4f2ab487e0adec
김승규/파인원 <ksk4788@pineone.com>
2018-11-30 09:47:46 +0900

    [김승규/파인원] 로밍 카드/쿠폰 페이지 추가 및 클릭 이벤트 수정

--- src/client/app/05.product/roaming/product.roaming.coupon.js
+++ src/client/app/05.product/roaming/product.roaming.coupon.js
@@ -7,90 +7,35 @@
 Tw.ProductRoamingCoupon = function(rootEl) {
   this.$container = rootEl;
   this._historyService = new Tw.HistoryService();
-  this._popupService = new Tw.PopupService();
+  this._popupService = Tw.Popup;
   this._apiService = Tw.Api;
-  this._cachedElement();
   this._bindEvent();
 };
 
 Tw.ProductRoamingCoupon.prototype = {
 
-  _cachedElement: function() {
-    this.$btnAlarmApply = this.$container.find('.fe-btn_alarm_apply');
-    this.$btnTerminate = this.$container.find('#inquire-btn');
-  },
-
   _bindEvent: function() {
-    this.$btnAlarmApply.on('click', $.proxy(this._alarmApply, this));
-    this.$btnTerminate.on('click', $.proxy(this._goTerminate, this));
-    this.$container.on('click', '#coupon-buy-btn', $.proxy(this._onViewclicked, this));
-    this.$container.on('click', '#coupon-register-btn', $.proxy(this._onViewclicked, this));
-    this.$container.on('click', '#coupon-inquire-btn', $.proxy(this._onViewclicked, this));
+    this.$container.on('click', '#coupon-buy-btn', $.proxy(this._onViewclicked, this, 'buy'));
+    this.$container.on('click', '#coupon-register-btn', $.proxy(this._onViewclicked, this, 'register'));
+    this.$container.on('click', '#coupon-inquire-btn', $.proxy(this._onViewclicked, this, 'inquire'));
   },
 
-  _alarmApply: function() {
-    this._apiService.request(Tw.API_CMD.BFF_05_0126, {})
-      .done($.proxy(this._applyResult, this));
-  },
-
-  _applyResult: function(resp) {
-    if (resp.code !== Tw.API_CODE.CODE_00) {
-      return Tw.Error(resp.code, resp.msg).pop();
-    }
-
-    this._popupService.openAlert(Tw.ALERT_MSG_MYT_JOIN.ALERT_2_A39.MSG, Tw.ALERT_MSG_MYT_JOIN.ALERT_2_A39.TITLE, null, $.proxy(this._reload, this));
-  },
-
-  _goTerminate: function() {
-    this._historyService.replaceURL('/product/roaming/fi/inquire');
-  },
-
-  _reload: function() {
-    this._historyService.reload();
-  },
-
-  _onActionSheetOpened: function (selected, $root) {
-    $root.on('click', '.hbs-card-type', $.proxy(this._onActionSelected, this));
-  },
-
-  _onActionSelected: function (e) {
-    if($('.hbs-card-type').hasClass('checked')){
-      $('.hbs-card-type').removeClass('checked');
-    }
-
-    $(e.target).parents('li').find('button').addClass('checked');
-  },
-
-  _onViewclicked: function (e) {
-
-    e.preventDefault();
-    e.stopPropagation();
-
-    var selected = e.target;
-    var hashName = '';
+  _onViewclicked: function (hash) {
     var hbs = '';
 
-    if(selected.id === 'coupon-buy-btn'){
-      hashName = 'buy';
+    if( hash === 'buy'){
       hbs = 'RM_13_01';
-    }else if(selected.id === 'coupon-register-btn'){
-      hashName = 'regist';
+    }else if(hash === 'register'){
       hbs = 'RM_13_02';
-    }else if(selected.id === 'coupon-inquire-btn'){
-      hashName = 'inquire';
+    }else if(hash === 'inquire'){
       hbs = 'RM_13_03';
     }
 
     this._popupService.open({
         hbs: hbs,
         layer: true,
-        data: Tw.POPUP_TPL.ROAMING_RETURN_PLACE
       },
-      $.proxy(this._onActionSheetOpened, this, selected),
-      $.proxy(this._onActionSheetClosed, this, selected), hashName);
-  },
-
-  _onActionSheetClosed: function () {
-    this._popupService.close();
+      null,
+      null, hash);
   }
 };
