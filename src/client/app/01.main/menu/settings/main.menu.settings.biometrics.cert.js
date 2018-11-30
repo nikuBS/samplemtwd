215adefc2f2b29b0089f4ace41267f6b75f14995
조아라 SKtechx <araara.jo@sk.com>
2018-11-30 11:07:49 +0900

    [조아라/Client개발팀] 생체인증 설정화면 에러 수정

--- src/client/app/01.main/menu/settings/main.menu.settings.biometrics.cert.js
+++ src/client/app/01.main/menu/settings/main.menu.settings.biometrics.cert.js
@@ -15,10 +15,10 @@ Tw.MainMenuSettingsBiometricsCert = function (rootEl, target) {
 
   this._certSk = new Tw.CertificationSk();
   this._certPublic = new Tw.CertificationPublic();
+  this._certNice = new Tw.CertificationNice();
 
   this._svcInfo = null;
-  this._authUrl = null;
-  this._resultUrl = null;
+
   this._bindEvent();
   this._getSvcInfo();
 };
@@ -36,8 +36,6 @@ Tw.MainMenuSettingsBiometricsCert.prototype = {
     this.$btNext.on('click', $.proxy(this._onClickNext, this));
   },
   _getSvcInfo: function () {
-    this._resultUrl = '/common/cert/complete';
-
     this._apiService.request(Tw.NODE_CMD.GET_SVC_INFO, {})
       .done($.proxy(this._successSvcInfo, this))
       .fail($.proxy(this._failSvcInfo, this));
@@ -52,31 +50,23 @@ Tw.MainMenuSettingsBiometricsCert.prototype = {
   },
   _onClickSkSms: function () {
     this._certSk.open(
-      this._svcInfo, this._authUrl, null, null, $.proxy(this._completeIdentification, this), Tw.AUTH_CERTIFICATION_METHOD.SK_SMS);
+      this._svcInfo, this._authUrl, this._authKind, null, Tw.AUTH_CERTIFICATION_METHOD.SK_SMS, $.proxy(this._completeIdentification, this));
   },
   _onClickKtSms: function () {
-    this._openCertBrowser('/common/cert/nice?authUrl=' + this._authUrl + '&resultUrl=' + this._resultUrl + '&niceType=' + Tw.AUTH_CERTIFICATION_NICE.KT);
+    this._certNice.open(
+      this._authUrl, this._authKind, Tw.NICE_TYPE.NICE, Tw.AUTH_CERTIFICATION_NICE.KT, null, $.proxy(this._completeIdentification, this));
   },
   _onClickLgSms: function () {
-    this._openCertBrowser('/common/cert/nice?authUrl=' + this._authUrl + '&resultUrl=' + this._resultUrl + '&niceType=' + Tw.AUTH_CERTIFICATION_NICE.LG);
+    this._certNice.open(
+      this._authUrl, this._authKind, Tw.NICE_TYPE.NICE, Tw.AUTH_CERTIFICATION_NICE.LG, null, $.proxy(this._completeIdentification, this));
   },
   _onClickIpin: function () {
-    this._openCertBrowser('/common/cert/ipin?authUrl=' + this._authUrl + '&resultUrl=' + this._resultUrl);
+    this._certNice.open(this._authUrl, this._authKind, Tw.NICE_TYPE.IPIN, this._niceKind, null, $.proxy(this._completeIdentification, this));
   },
   _onClickPublic: function () {
-    this._certPublic.open(this._svcInfo, this._authUrl, this._command, this._deferred, this._callback);
+    this._certPublic.open(this._svcInfo, this._authUrl, this._authKind, null, $.proxy(this._completeIdentification, this));
   },
-  _openCertBrowser: function (path) {
-    this._apiService.request(Tw.NODE_CMD.GET_DOMAIN, {})
-      .done($.proxy(this._successGetDomain, this, path));
-  },
-  _successGetDomain: function (path, resp) {
-    if ( resp.code === Tw.API_CODE.CODE_00 ) {
-      Tw.CommonHelper.openUrlInApp(resp.result.domain + path, 'status=1,toolbar=1');
-      // Tw.CommonHelper.openUrlInApp('http://150.28.69.23:3000' + path, 'status=1,toolbar=1');
-    }
-  },
-  _completeIdentification: function (result, deferred, command) {
+  _completeIdentification: function (result) {
     if ( result.code === Tw.API_CODE.CODE_00 ) {
       this.$btNext.attr('disabled', false);
     }
