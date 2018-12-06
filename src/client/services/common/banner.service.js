0bb28bedf484b95d22ffb6913504b889848f3556
jiyoung.jo <jiyoungjo@sk.com>
2018-12-06 20:26:35 +0900

    [조지영/Client 개발팀] 상품 > 요금제 서브메인 프로모션 배너 적용

---
+++ src/client/services/common/banner.service.js
@@ -0,0 +1,36 @@
+Tw.BannerService = function(rootEl) {
+  this.$conatiner = rootEl;
+  this._bindEvent();
+};
+
+Tw.BannerService.prototype = {
+  _bindEvent: function() {
+    this.$conatiner.on('click', '.fe-banner', $.proxy(this._openBannerLink, this));
+  },
+
+  _openBannerLink: function(e) {
+    var target = e.currentTarget,
+      linkType = target.getAttribute('data-link-type'),
+      link = target.getAttribute('data-link'),
+      isBill = target.getAttribute('data-is-bill'),
+
+    switch (linkType) {
+      case Tw.BANNER_LINK_TYPE.CHANNEL_APP:
+      case Tw.BANNER_LINK_TYPE.CHANNER_WEB: {
+        window.location.href = link;
+        break;
+      }
+      case Tw.BANNER_LINK_TYPE.OTHER_APP:
+      case Tw.BANNER_LINK_TYPE.OTHER_WEB: {
+        if (isBill) {
+          Tw.CommonHelper.showDataCharge(function() {
+            Tw.CommonHelper.openUrlExternal(link);
+          });
+        } else {
+          Tw.CommonHelper.openUrlExternal(link);
+        }
+        break;
+      }
+    }
+  }
+};
