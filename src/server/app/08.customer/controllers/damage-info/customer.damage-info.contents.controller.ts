8ba1ca0cc1c047bb260e1b2ef3dd6b7ac5fe4bc1
양지훈 SKtechx <jihun202@sk.com>
2018-12-06 18:30:35 +0900

    [양지훈/FE개발팀] data-menuId 및 popup-page footer 제거

--- src/server/app/08.customer/controllers/damage-info/customer.damage-info.contents.controller.ts
+++ src/server/app/08.customer/controllers/damage-info/customer.damage-info.contents.controller.ts
@@ -16,6 +16,25 @@ class CustomerDamageInfoContents extends TwViewController {
   private readonly _allowedPageNo = ['page001', 'page002', 'page003', 'page004', 'page005', 'page006', 'page007',
     'page008', 'page009', 'page010', 'page011', 'page012', 'page013', 'page014', 'page015', 'page016'];
 
+  private readonly _pageNoMenuId = {
+    page001: 'M000753',
+    page002: 'M000754',
+    page003: 'M000755',
+    page004: 'M000756',
+    page005: 'M000757',
+    page006: 'M000758',
+    page007: 'M000736',
+    page008: 'M000737',
+    page009: 'M000738',
+    page010: 'M000739',
+    page011: 'M000740',
+    page012: 'M000743',
+    page013: 'M000744',
+    page014: 'M000745',
+    page015: 'M000746',
+    page016: 'M000747'
+  };
+
   render(req: Request, res: Response, next: NextFunction, svcInfo: any, allSvc: any, childInfo: any, pageInfo: any) {
     const pageNo = req.params.pageNo,
       renderCommonInfo = {
@@ -29,7 +48,8 @@ class CustomerDamageInfoContents extends TwViewController {
     }
 
     res.render('damage-info/contents/customer.damage-info.contents.html', Object.assign(renderCommonInfo, {
-      pageTitle: CUSTOMER_DAMAGEINFO_CONTENTS_TITLE[pageNo]
+      pageTitle: CUSTOMER_DAMAGEINFO_CONTENTS_TITLE[pageNo],
+      menuId: this._pageNoMenuId[pageNo]
     }));
   }
 }
