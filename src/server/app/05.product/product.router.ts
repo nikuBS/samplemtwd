61ecb934fd6ca912e66f94e41157ebeb84f0c47a
김승규/파인원 <ksk4788@pineone.com>
2018-12-07 16:19:20 +0900

    [김승규/파인원] T파이 임대 > T파이 조회/취소 - 예약 수정, 취소 시 Alert 추가, 기존 예약 수정 Page 삭제, 예약 취소시 예약일 변경되는 현상 수정 공통 - tool tip 영역 page-id 추가

--- src/server/app/05.product/product.router.ts
+++ src/server/app/05.product/product.router.ts
@@ -61,7 +61,6 @@ import ProductRoamingCoupon from './controllers/roaming/product.roaming.coupon.c
 import ProductRoamingFiGuide from './controllers/roaming/product.roaming.fi.guide.controller';
 import ProductRoamingFiInquire from './controllers/roaming/product.roaming.fi.inquire.controller';
 import ProductRoamingFiInquireAuth from './controllers/roaming/product.roaming.fi.inquire-auth.controller';
-import ProductRoamingFiInquireEdit from './controllers/roaming/product.roaming.fi.inquire-edit.controller';
 import ProductRoamingFiReservation1step from './controllers/roaming/product.roaming.fi.reservation1step.controller';
 import ProductRoamingFiReservation2step from './controllers/roaming/product.roaming.fi.reservation2step.controller';
 import ProductRoamingFiReservation3step from './controllers/roaming/product.roaming.fi.reservation3step.controller';
@@ -141,7 +140,6 @@ class ProductRouter extends TwRouter {
     this.controllers.push({ url: '/roaming/fi/guide', controller: ProductRoamingFiGuide });
     this.controllers.push({ url: '/roaming/fi/inquire', controller: ProductRoamingFiInquire });
     this.controllers.push({ url: '/roaming/fi/inquire-auth', controller: ProductRoamingFiInquireAuth });
-    this.controllers.push({ url: '/roaming/fi/inquire-edit', controller: ProductRoamingFiInquireEdit });
     this.controllers.push({ url: '/roaming/coupon', controller: ProductRoamingCoupon });
     this.controllers.push({ url: '/roaming/fi/reservation1step', controller: ProductRoamingFiReservation1step });
     this.controllers.push({ url: '/roaming/fi/reservation2step', controller: ProductRoamingFiReservation2step });
