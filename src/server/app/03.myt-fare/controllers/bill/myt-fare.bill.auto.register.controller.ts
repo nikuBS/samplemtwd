b17a09a4aa7b07021e6e6b08d85c5443c6cc227e
1100015 <jayoon.kong@sk.com>
2018-12-13 15:11:10 +0900

    [공자윤/Frontend개발팀] 자동납부 신청 및 변경 웹에서 처리 불가하도록 수정

--- src/server/app/03.myt-fare/controllers/bill/myt-fare.bill.auto.register.controller.ts
+++ src/server/app/03.myt-fare/controllers/bill/myt-fare.bill.auto.register.controller.ts
@@ -9,6 +9,7 @@ import TwViewController from '../../../../common/controllers/tw.view.controller'
 import {Observable} from 'rxjs/Observable';
 import {API_CMD, API_CODE} from '../../../../types/api-command.type';
 import {MYT_FARE_PAYMENT_TITLE, MYT_FARE_PAYMENT_NAME, MYT_FARE_PAYMENT_TYPE} from '../../../../types/bff.type';
+import BrowserHelper from '../../../../utils/browser.helper';
 
 class MyTFareBillAutoRegister extends TwViewController {
   constructor() {
@@ -16,23 +17,29 @@ class MyTFareBillAutoRegister extends TwViewController {
   }
 
   render(req: Request, res: Response, next: NextFunction, svcInfo: any, allSvc: any, childInfo: any, pageInfo: any) {
-    Observable.combineLatest(
-      this.getPaymentOption()
-    ).subscribe(([paymentOption]) => {
-      if (paymentOption.code === API_CODE.CODE_00) {
-        res.render('bill/myt-fare.bill.auto.register.html', {
-          svcInfo: svcInfo,
-          pageInfo: pageInfo,
-          paymentOption: this.parseData(paymentOption.result)
-        });
-      } else {
-        this.error.render(res, {
-          code: paymentOption.code,
-          msg: paymentOption.msg,
-          svcInfo: svcInfo
-        });
-      }
-    });
+    if (BrowserHelper.isApp(req)) {
+      Observable.combineLatest(
+        this.getPaymentOption()
+      ).subscribe(([paymentOption]) => {
+        if (paymentOption.code === API_CODE.CODE_00) {
+          res.render('bill/myt-fare.bill.auto.register.html', {
+            svcInfo: svcInfo,
+            pageInfo: pageInfo,
+            paymentOption: this.parseData(paymentOption.result)
+          });
+        } else {
+          this.error.render(res, {
+            code: paymentOption.code,
+            msg: paymentOption.msg,
+            svcInfo: svcInfo
+          });
+        }
+      });
+    } else {
+      res.render('share/common.share.app-install.info.html', {
+        svcInfo: svcInfo, isAndroid: BrowserHelper.isAndroid(req)
+      });
+    }
   }
 
   private getPaymentOption(): Observable<any> {
