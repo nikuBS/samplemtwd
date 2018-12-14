4c315eea2cd5be2ebfd79ad5e31fe103190d44c4
양지훈 SKtechx <jihun202@sk.com>
2018-12-14 16:35:36 +0900

    [양지훈/FE개발팀][DV001-9806] QA bug fix

--- src/client/types/string.type.js
+++ src/client/types/string.type.js
@@ -817,8 +817,8 @@ Tw.MYT_DATA_FAMILY_SUCCESS_SHARE = {
 
 Tw.MYT_DATA_FAMILY_CONFIRM_SHARE_MONTHLY = {
   TITLE: '매월 자동으로 데이터를 공유 하시겠습니까?',
-  CONTENTS: '다음달부터 자동 공유되며, 이번 달 안으로 변경이 가능합니다.',
-  BTN_NAME: '변경하기'
+  CONTENTS: '다음달부터 자동 공유되며, 이번 달 안에 변경할 수 있습니다.',
+  BTN_NAME: '공유하기'
 };
 
 Tw.MYT_DATA_FAMILY_SUCCESS_SHARE_MONTHLY = {
