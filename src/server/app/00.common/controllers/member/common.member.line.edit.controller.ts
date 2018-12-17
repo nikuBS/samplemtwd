f3deec45096a6f78fd0752fd7c34acce0a313a5b
조아라 SKtechx <araara.jo@sk.com>
2018-12-18 08:14:00 +0900

    [조아라/Client개발팀] 회선관리/회선편집 데이터 오류 수정

--- src/server/app/00.common/controllers/member/common.member.line.edit.controller.ts
+++ src/server/app/00.common/controllers/member/common.member.line.edit.controller.ts
@@ -36,7 +36,7 @@ class CommonMemberLineEdit extends TwViewController {
   }
 
   private parseLineList(lineList): any {
-    const curLine = lineList[this.category];
+    const curLine = lineList[this.category] || lineList[this.category.toUpperCase()];
     if ( !FormatHelper.isEmpty(curLine) ) {
       return this.convLineData(curLine);
     }
