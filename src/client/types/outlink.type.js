769a8596ab020bcec9358990beb813ba300f98c6
조아라 SKtechx <araara.jo@sk.com>
2018-12-17 10:35:40 +0900

    [조아라/Client개발팀] [DV001-9695, DV001-9746] Store 다이렉트샵 링크 연동

--- src/client/types/outlink.type.js
+++ src/client/types/outlink.type.js
@@ -5,5 +5,11 @@ Tw.OUTLINK = {
   MOBILE_TWORLD: 'http://m2.tworld.co.kr/jsp/op.jsp?p=w70&f=a0001',
   GOOGLE_STORE: 'https://play.google.com/store/apps/details?id=Com.sktelecom.minit',
   ONE_STORE: 'https://www.onestore.co.kr/userpoc/game/view?pid=0000032819',
-  APP_STORE: 'https://itunes.apple.com/kr/app/%EB%AA%A8%EB%B0%94%EC%9D%BCtworld/id428872117?mt=8'
+  APP_STORE: 'https://itunes.apple.com/kr/app/%EB%AA%A8%EB%B0%94%EC%9D%BCtworld/id428872117?mt=8',
+  DIRECT_SUPPORT: 'http://shop.tworld.co.kr/handler/Dantong-SKT?UPDATE_YN=N&_ga=2.226795357.1794270396.1498012356-98061374.1498012356',
+  DIRECT_HOME: 'http://shop.tworld.co.kr/handler/ShopTworldMain-Start',
+  DIRECT_ACCESSORY: 'http://shop.tworld.co.kr/handler/AccessoryMall-MainAcc',
+  DIRECT_PHONE: '',
+  DIRECT_TABLET: '',
+  DIRECT_NUGU: ''
 };
