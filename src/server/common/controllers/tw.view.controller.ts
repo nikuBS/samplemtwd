c042d9fc99524e62e0d8e9ebd2c09a01263b8181
조아라 SKtechx <araara.jo@sk.com>
2018-12-17 13:55:55 +0900

    [조아라/Client개발팀] 성능테스트 로그인/로그아웃/SMS 인증 추가

--- src/server/common/controllers/tw.view.controller.ts
+++ src/server/common/controllers/tw.view.controller.ts
@@ -63,11 +63,11 @@ abstract class TwViewController {
     this._loginService.setCurrentReq(req, res);
 
     this.setChannel(req, res).subscribe((resp) => {
-      if ( this.checkLogin(req.session) ) {
-        this.sessionLogin(req, res, next, path);
+      if ( this.existId(tokenId, userId) ) {
+        this.login(req, res, next, path, tokenId, userId);
       } else {
-        if ( this.existId(tokenId, userId) ) {
-          this.login(req, res, next, path, tokenId, userId);
+        if ( this.checkLogin(req.session) ) {
+          this.sessionLogin(req, res, next, path);
         } else {
           this.sessionCheck(req, res, next, path);
         }
@@ -91,7 +91,7 @@ abstract class TwViewController {
         this.failLogin(req, res, next, error.code);
       });
     } else {
-      if ( /\/test/i.test(req.baseUrl) && /\/home/i.test(req.path) ) {
+      if ( /\/test/i.test(req.baseUrl) && /\/login/i.test(req.path) ) {
         this.apiService.requestLoginLoadTest(userId).subscribe((resp) => {
           this.renderPage(req, res, next, path); // noticeTpyCd
         }, (error) => {
@@ -155,11 +155,11 @@ abstract class TwViewController {
             this.render(req, res, next, svcInfo, allSvc, childInfo, urlMeta);
             // 현재 로그인 방법으론 이용할 수 없음
             // if ( svcInfo.loginType === LOGIN_TYPE.EASY ) {
-              // res.redirect('/common/member/slogin/fail');
+            // res.redirect('/common/member/slogin/fail');
             // } else {
-              // TODO: ERROR 케이스 (일반로그인에서 권한이 없는 케이스)
-              // res.redirect('/common/member/slogin/fail');
-              // this.errorAuth(req, res, next, svcInfo);
+            // TODO: ERROR 케이스 (일반로그인에서 권한이 없는 케이스)
+            // res.redirect('/common/member/slogin/fail');
+            // this.errorAuth(req, res, next, svcInfo);
             // }
           }
         } else {
