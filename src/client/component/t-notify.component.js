e99b9d5e81910434e21a26c758b291b9cfe6635a
조아라 SKtechx <araara.jo@sk.com>
2018-12-18 15:36:00 +0900

    [조아라/Client개발팀] T알림 데이터 연동

--- src/client/component/t-notify.component.js
+++ src/client/component/t-notify.component.js
@@ -10,29 +10,54 @@ Tw.TNotifyComponent = function () {
 };
 
 Tw.TNotifyComponent.prototype = {
-  open: function () {
-    this._popupService.open({
-      hbs: 'HO_04_06_01',
-      layer: true
-    }, $.proxy(this._onOpenTNotify, this), $.proxy(this._onCloseTNotify, this));
+  open: function (userId) {
+    this._getPushDate(userId);
 
   },
+  _openTNotify: function (list) {
+    this._popupService.open({
+      hbs: 'HO_04_06_01',
+      layer: true,
+      data: {
+        list: list,
+        showMore: list.length > Tw.DEFAULT_LIST_COUNT
+      }
+    }, $.proxy(this._onOpenTNotify, this), $.proxy(this._onCloseTNotify, this), 't-notify');
+  },
   _onOpenTNotify: function ($popupContainer) {
-
+    $popupContainer.on('click', '#fe-bt-more', $.proxy(this._onClickBtMore, this));
+    $popupContainer.on('click', '.fe-bt-link', $.proxy(this._onClickLink, this));
   },
   _onCloseTNotify: function () {
 
   },
-  _getPushDate: function () {
+  _getPushDate: function (userId) {
     this._apiService.request(Tw.API_CMD.BFF_04_0004, {
-      tid: ''
+      tid: userId
     }).done($.proxy(this._successPushData, this));
   },
   _successPushData: function (resp) {
     if ( resp.code === Tw.API_CODE.CODE_00 ) {
-
+      this._openTNotify(this._parseList(resp.result));
     } else {
       Tw.Error(resp.code, resp.msg).pop();
+    }
+  },
+  _parseList: function (list) {
+    return _.map(list, $.proxy(function (target) {
+      target.showImage = target.pushType === 'M';
+      target.showDate = Tw.DateHelper.getKoreanDateWithDay(target.regDate);
+      target.showTime = Tw.DateHelper.getKoreanTime(target.regDate);
+      return target;
+    }, this));
+  },
+  _onClickBtMore: function () {
+
+  },
+  _onClickLink: function ($event) {
+    var url = $($event.currentTarget).data('link');
+    if ( !Tw.FormatHelper.isEmpty(url) ) {
+      Tw.CommonHelper.openUrlExternal(url);
     }
   }
 };
