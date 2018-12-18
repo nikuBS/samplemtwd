12f96418554f4ecf0d5ef581681fafc3abe84e77
Jung Eunjung <dmsej@pineone.com>
2018-12-18 09:00:42 +0900

    [정은정/파인원] DV001-9651 > 상품 선택 영역 수정.

--- src/client/app/05.product/roaming/product.roaming.info.secure-troaming.js
+++ src/client/app/05.product/roaming/product.roaming.info.secure-troaming.js
@@ -7,9 +7,21 @@
 Tw.ProductRoamingSecureTroaming = function (rootEl) {
   this.$container = rootEl;
 
+  this.$container = rootEl;
+  this._history = new Tw.HistoryService(rootEl);
 
+  this._bindEvents();
 };
 
 Tw.ProductRoamingSecureTroaming.prototype = {
-
+  _bindEvents: function () {
+    this.$container.on('click', '.fe-roaming-kcj', $.proxy(this._goLoadProduct, this, 'NA00005699'));
+    this.$container.on('click', '.fe-roaming-asia', $.proxy(this._goLoadProduct, this, 'NA00005900'));
+    this.$container.on('click', '.fe-roaming-usa', $.proxy(this._goLoadProduct, this, 'NA00006038'));
+    this.$container.on('click', '.fe-roaming-europe', $.proxy(this._goLoadProduct, this, 'NA00006046'));
+  },
+  _goLoadProduct: function (prodId) {
+    var reqUrl = '/product/callplan/' + prodId;
+    this._history.goLoad(reqUrl);
+  }
 };
