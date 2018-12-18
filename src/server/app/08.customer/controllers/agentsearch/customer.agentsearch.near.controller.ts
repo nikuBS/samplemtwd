5b8d2f3a601403ec1ce9a0c2477dff95d48e6dad
Hakjoon Sim <hakjoon.sim@sk.com>
2018-12-18 09:38:58 +0900

    [심학준/FE개발팀] Add footer for customer pages

--- src/server/app/08.customer/controllers/agentsearch/customer.agentsearch.near.controller.ts
+++ src/server/app/08.customer/controllers/agentsearch/customer.agentsearch.near.controller.ts
@@ -6,10 +6,13 @@
 
 import TwViewController from '../../../../common/controllers/tw.view.controller';
 import { Request, Response } from 'express';
+import { NextFunction } from 'connect';
 
 class CustomerAgentsearchNear extends TwViewController {
-  render(req: Request, res: Response) {
-    res.render('agentsearch/customer.agentsearch.near.html');
+
+  render(req: Request, res: Response, next: NextFunction, svcInfo: any,
+         allSvc: any, childInfo: any, pageInfo: any) {
+    res.render('agentsearch/customer.agentsearch.near.html', { svcInfo, pageInfo });
   }
 }
 
