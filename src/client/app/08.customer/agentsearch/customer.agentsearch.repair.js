e29ed57527a7be4253fdfeed6465b831c52d0008
Hakjoon Sim <hakjoon.sim@sk.com>
2018-12-19 17:06:26 +0900

    [심학준/FE개발팀] Add popup view for repair
     - DV001-10167

---
+++ src/client/app/08.customer/agentsearch/customer.agentsearch.repair.js
@@ -0,0 +1,24 @@
+/**
+ * FileName: customer.agentsearch.repair.js
+ * Author: Hakjoon Sim (hakjoon.sim@sk.com)
+ * Date: 2018.12.19
+ */
+
+Tw.CustomerAgentsearchRepair = function (rootEl) {
+  this.$container = rootEl;
+
+  this._popupService = Tw.Popup;
+
+  this._bindEvents();
+};
+
+Tw.CustomerAgentsearchRepair.prototype = {
+  _bindEvents: function () {
+    this.$container.on('click', '#fe-view', $.proxy(this._onView, this));
+  },
+  _onView: function () {
+    this._popupService.open({
+      hbs: 'CS_03_01_L01'
+    });
+  }
+};
