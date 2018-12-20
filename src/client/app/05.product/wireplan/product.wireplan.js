0c1c1a1deacb3f4551f44eb653e641aafffe58e1
jiyoung.jo <jiyoungjo@sk.com>
2018-12-20 18:27:42 +0900

    [조지영/Client개발팀][DV001-10324] 인터넷/전화/TV 가입상담 예약 버튼 관련 SB 변경사항 반영

--- src/client/app/05.product/wireplan/product.wireplan.js
+++ src/client/app/05.product/wireplan/product.wireplan.js
@@ -8,63 +8,7 @@ Tw.ProductWireplan = function(rootEl) {
 
 Tw.ProductWireplan.prototype = {
   _bindEvent: function() {
-    this.container.on('click', '#fe-reservation', $.proxy(this._handleClickReservation, this));
-  },
-
-  _handleClickReservation: function() {
-    if (this._hasReservation) {
-      if (this._hasReservation.isNeedDocument) {
-        this._openGoToHistoryPopup();
-      } else {
-        this._openSelectProductType();
-      }
-    } else {
-      this._apiService.request(Tw.API_CMD.BFF_10_0078, {}).done($.proxy(this._handleGetReservation, this));
-    }
-  },
-
-  _handleGetReservation: function(resp) {
-    if (resp.code !== Tw.API_CODE.CODE_00) {
-      return Tw.Error(resp.code, resp.msg).pop();
-    }
-
-    this._hasReservation = this._convertReservation(resp.result);
-    if (Tw.FormatHelper.isEmpty(this._hasReservation) || !this._hasReservation.isNeedDocument) {
-      this._openSelectProductType();
-    } else {
-      this._openGoToHistoryPopup();
-    }
-  },
-
-  _convertReservation: function(result) {
-    var latestItem = result.necessaryDocumentInspectInfoList[0];
-
-    if (latestItem && latestItem.nextSchddt) {
-      isExpired = Tw.DateHelper.getDifference(latestItem.nextSchddt) <= 0;
-      return {
-        isNeedDocument:
-          (!isExpired && latestItem.ciaInsptRslt === PRODUCT_CALLPLAN.CIA_INSPT_RSLT) || (isExpired && latestItem.abnSaleOpClCd === '000'),
-        isProcess: !isExpired && FormatHelper.isEmpty(latestItem.abnSaleOpClCd)
-      };
-    }
-
-    return {};
-  },
-
-  _openGoToHistoryPopup: function() {
-    var ALERT = Tw.PRODUCT_CONFIRM_GO_TO_DOCUMENT;
-    this._popupService.openConfirmButton(
-      null,
-      ALERT.TITLE,
-      $.proxy(this._go, this, '/product/wireplan/join/require-document/apply'),
-      $.proxy(this._openSelectProductType, this),
-      ALERT.GO_RESERVATION_BTN,
-      ALERT.GO_DOCUMENT_BTN
-    );
-  },
-
-  _go: function(url) {
-    window.location.href = url;
+    this.container.on('click', '#fe-reservation', $.proxy(this._openSelectProductType, this));
   },
 
   _openSelectProductType: function() {
@@ -94,7 +38,7 @@ Tw.ProductWireplan.prototype = {
 
   _closeProductType: function() {
     if (this._typeCode) {
-      this._go('/product/wireplan/join/reservation?type_cd=' + this._typeCode);
+      window.location.href = '/product/wireplan/join/reservation?type_cd=' + this._typeCode;
     }
   }
 };
