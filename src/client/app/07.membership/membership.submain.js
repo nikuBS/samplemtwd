5975fbf78a497824da03decb8f54fdff7df92633
Jung Eunjung <dmsej@pineone.com>
2018-12-28 10:50:33 +0900

    [정은정/파인원][12차][멤버쉽] submain 정보 영역 수정 및 바코드 팝업 적용.

--- src/client/app/07.membership/membership.submain.js
+++ src/client/app/07.membership/membership.submain.js
@@ -1,47 +1,86 @@
 /**
- * FileName: product.roaming.fi.guide.js
- * Author: SeungKyu Kim (ksk4788@pineone.com)
- * Date: 2018.11.07
+ * FileName: membership.submain.js
+ * Author: EunJung Jung
+ * Date: 2018.12.26
  */
 
-Tw.ProductRoamingFiGuide = function(rootEl) {
+Tw.MembershipSubmain = function(rootEl, membershipData) {
   this.$container = rootEl;
+  this._membershipData = membershipData;
+  this._nativeService = Tw.Native;
   this._historyService = new Tw.HistoryService();
   this._popupService = Tw.Popup;
   this._apiService = Tw.Api;
+  this._map = undefined;
   this._cachedElement();
   this._bindEvent();
-  this._nearBrandRequest();
+  this._checkCurrentLocation();
 };
 
-Tw.ProductRoamingFiGuide.prototype = {
+Tw.MembershipSubmain.prototype = {
 
   _cachedElement: function() {
-    this.$btnInquire = this.$container.find('#inquire-btn');
+    this.$barCode = this.$container.find('#fe-barcode-img');
   },
 
   _bindEvent: function() {
-    this.$btnInquire.on('click', $.proxy(this._goInquire, this));
-    this.$btnReservation.on('click', $.proxy(this._goReservation, this));
-    this.$btnProduct.on('click', $.proxy(this._goProductPage, this));
+    this.$barCode.on('click', $.proxy(this._showBarCodePopup, this));
+  },
+  _checkCurrentLocation: function () {
+    if (Tw.BrowserHelper.isApp()) {
+      this._onCurrentLocation(location);
+    } else {
+      this._askCurrentLocation();
+    }
+  },
+  _askCurrentLocation: function() {
+    if (Tw.BrowserHelper.isApp()) {
+      this._nativeService.send(Tw.NTV_CMD.GET_LOCATION, {}, $.proxy(function (res) {
+        if (res.resultCode === 401) {
+          this._historyService.goBack();
+          return;
+        } else {
+          //this._checkLocationAgreement(res.params);
+        }
+      }, this));
+    } else {
+      if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition($.proxy(function (location) {
+          this._onCurrentLocation({
+            longitude: location.coords.longitude,
+            latitude: location.coords.latitude
+          });
+        }, this));
+      }
+    }
+  },
+  _onCurrentLocation: function (location) {
+    Tw.Logger.info(location);
+  },
+  _showBarCodePopup : function () {
+    var cardNum = this.$barCode.data('cardnum');
+    this._mbrGrStr = this._membershipData.mbrGrStr;
+    this._mbrGrCd = this._membershipData.mbrGrCd;
+    this._usedAmt = this._membershipData.showUsedAmount;
+
+    this._popupService.open({
+      hbs: 'HO_01_01_02',
+      layer: true,
+      data: {
+        mbrGr: this._mbrGrStr.toLowerCase(),
+        mbrGrStr: this._mbrGrStr,
+        cardNum: cardNum,
+        usedAmount: this._usedAmt
+      }
+    }, $.proxy(this._onOpenBarcode, this, cardNum));
+
   },
 
-  _nearBrandRequest: function(){
-    this._apiService.request(Tw.API_CMD.BFF_11_0017, {keyword : ''}).done($.proxy(this._handleSuccessSearchCountry, this));
-  },
-
-  _goReservation: function() {
-    this._historyService.replaceURL('/product/roaming/fi/reservation1step');
-  },
-
-  _goInquire: function() {
-    this._historyService.replaceURL('/product/roaming/fi/inquire');
-  },
-
-  _goProductPage: function(e) {
-    //T파이 이용 가능 요금제 상세 페이지 이동
-    var productId = $(e.target).parents('button').attr('id') === undefined ? $(e.target).attr('id') : $(e.target).parents('button').attr('id');
-    this._historyService.goLoad('/product/callplan/' + productId);
+  _onOpenBarcode: function (cardNum, $popupContainer) {
+    var extendBarcode = $popupContainer.find('#fe-membership-barcode-extend');
+    if ( !Tw.FormatHelper.isEmpty(cardNum) ) {
+      extendBarcode.JsBarcode(cardNum);
+    }
   },
 
   _reload: function() {
