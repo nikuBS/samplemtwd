06822188cf7f1695d24a17ebd1d88283e81810f9
jiyoung.jo <jiyoungjo@sk.com>
2018-12-28 10:51:33 +0900

    [조지영/Client개발팀][11차][DV001-10766] T 가족모아 한도 설정 기존과 동일 설정이더라도 저장버튼 활성화되도록 추가

--- src/client/app/02.myt-data/familydata/myt-data.familydata.js
+++ src/client/app/02.myt-data/familydata/myt-data.familydata.js
@@ -59,17 +59,18 @@ Tw.MyTDataFamily.prototype = {
   },
 
   _handleOpenChangeLimitation: function(mgmtNum, limitation, $layer) {
-    $layer.on('click', '.bt-red1', $.proxy(this._handleSubmitLimitation, this, mgmtNum));
+    $layer.on('click', '.bt-red1', $.proxy(this._handleSubmitLimitation, this, mgmtNum, limitation));
     $layer.on('change', 'input[type="radio"]', $.proxy(this._handleChangeLimitType, this, $layer, limitation));
-    $layer.on('keyup', 'span.input input', $.proxy(this._handleChangeLimitation, this, $layer, limitation));
+    $layer.on('keyup', 'span.input input', $.proxy(this._handleChangeLimitation, this, $layer));
   },
 
-  _handleSubmitLimitation: function(mgmtNum) {
+  _handleSubmitLimitation: function(mgmtNum, originLimit) {
     var limitation = this._limitation;
-    this._popupService.close();
 
     if (limitation === false) {
       this._apiService.request(Tw.API_CMD.BFF_06_0051, {}, {}, mgmtNum).done($.proxy(this._successChangeLimitation, this));
+    } else if (originLimit == limitation) {
+      this._popupService.openAlert(Tw.ALERT_MSG_MYT_DATA.A5);
     } else {
       this._apiService
         .request(Tw.API_CMD.BFF_06_0050, {
@@ -86,16 +87,16 @@ Tw.MyTDataFamily.prototype = {
     var value = $layer.find('span.input input').val();
 
     if (inputId === 'sradio2') {
-      $btn.attr('disabled', value.length === 0 || Number(value) === limitation);
+      $btn.attr('disabled', value.length === 0);
     } else {
       $btn.attr('disabled', !limitation);
       this._limitation = false;
     }
   },
 
-  _handleChangeLimitation: function($layer, limitation, e) {
+  _handleChangeLimitation: function($layer, e) {
     var value = e.currentTarget.value;
-    $layer.find('.bt-red1 > button').attr('disabled', value.length === 0 || Number(value) === limitation);
+    $layer.find('.bt-red1 > button').attr('disabled', value.length === 0);
     this._limitation = value;
   }
 };
