712a377af5cd2e7dac6786cfb56346233ab650d4
조아라 SKtechx <araara.jo@sk.com>
2018-12-28 10:57:54 +0900

    [조아라/Client개발팀] [DV001-10791] 광고정보수신동의 완료페이지 추가

--- src/client/component/line-marketing.component.js
+++ src/client/component/line-marketing.component.js
@@ -29,12 +29,6 @@ Tw.LineMarketingComponent.prototype = {
   },
 
   _openMarketingOfferPopup: function (showName, svcNum, agr201Yn, agr203Yn) {
-    console.log({
-      showName: showName,
-      svcNum: svcNum,
-      agr201Yn: agr201Yn === 'Y',
-      agr203Yn: agr203Yn === 'Y'
-    });
     this._popupService.open({
       hbs: 'CO_01_05_02_06',
       layer: true,
@@ -74,7 +68,7 @@ Tw.LineMarketingComponent.prototype = {
   },
   _closeOpenMarketingOfferPopup: function () {
     if ( this._complete ) {
-      // this._openCompleteMarketingPopup();
+      this._openCompleteMarketingPopup();
     }
   },
   _successAgreeMarketing: function (resp) {
