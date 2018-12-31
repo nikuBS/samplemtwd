d86522d25cf344ed15393fc8b4dd7d6b88ec474f
jiyoung.jo <jiyoungjo@sk.com>
2018-12-31 14:55:03 +0900

    [조지영/Client개발팀][11차][DV001-10454] BFF 명세 변경 사항 반영

--- src/server/app/05.product/controllers/apps/product.apps.detail.controller.ts
+++ src/server/app/05.product/controllers/apps/product.apps.detail.controller.ts
@@ -73,7 +73,7 @@ export default class ProductAppsDetail extends TwViewController {
       return (resp.result.recommendAppList || []).map(app => {
         return {
           ...app,
-          prodIconImgUrl: app.prodIconImgUrl && ProductHelper.getImageUrlWithCdn(app.prodIconImgUrl)
+          appIconImgUrl: app.prodIconImgUrl && ProductHelper.getImageUrlWithCdn(app.appIconImgUrl)
         };
       });
     });
