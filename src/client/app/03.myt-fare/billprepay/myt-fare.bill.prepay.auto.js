ab3790f277373bf13cc62a82b75e362f113dac71
1100015 <jayoon.kong@sk.com>
2019-01-02 10:48:48 +0900

    [공자윤/Frontend개발팀][DV001-10412] 카드번호 오류 케이스별 메시지 수정

--- src/client/app/03.myt-fare/billprepay/myt-fare.bill.prepay.auto.js
+++ src/client/app/03.myt-fare/billprepay/myt-fare.bill.prepay.auto.js
@@ -216,7 +216,7 @@ Tw.MyTFareBillPrepayAuto.prototype = {
     }
   },
   _getFail: function () {
-    this.$cardNumber.siblings('.fe-error-msg').show();
+    this.$cardNumber.siblings('.fe-error-msg').empty().text(Tw.ALERT_MSG_MYT_FARE.ALERT_2_V28).show();
     this.$cardNumber.focus();
     this.$isCardValid = false;
   },
