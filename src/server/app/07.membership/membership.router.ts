eed60ca07541db3111337f9487ad2cb695941549
KimInHwan <skt.p132150@partner.sk.com>
2019-01-03 17:45:28 +0900

    [김인환/SPTek] [12차] T 멤버십 > 멤버십혜택 > T-day 추가

--- src/server/app/07.membership/membership.router.ts
+++ src/server/app/07.membership/membership.router.ts
@@ -3,6 +3,7 @@ import MembershipJoin from './controllers/join/membership.join';
 import MembershipBenefitBrandList from './controllers/benefit/membership.benefit.brand.list.controller';
 import MembershipBenefitBrandMap from './controllers/benefit/membership.benefit.brand.map.controller';
 import MembershipBenefitMovieculture from './controllers/benefit/membership.benefit.movieculture';
+import MembershipBenefitTday from './controllers/benefit/membership.benefit.tday';
 import MembershipInfoGrade from './controllers/info/membership.info.grade.controller';
 import MembershipBenefitBrand from './controllers/benefit/membership.benefit.brand.controller';
 import MembershipBenefitBrandBenefit from './controllers/benefit/membership.benefit.brand-benefit.controller';
@@ -21,6 +22,7 @@ class MembershipRouter extends TwRouter {
     this.controllers.push({ url: '/benefit/map', controller: MembershipBenefitBrandMap });
     this.controllers.push({ url: '/benefit/brand/list', controller: MembershipBenefitBrandList });
     this.controllers.push({ url: '/benefit/plus', controller: MembershipBenefitPlus });
+    this.controllers.push({ url: '/benefit/t-day', controller: MembershipBenefitTday });
     this.controllers.push({ url: '/benefit/movieculture', controller: MembershipBenefitMovieculture });
     this.controllers.push({ url: '/membership_info/mbrs_0001', controller: MembershipInfoGrade });
     this.controllers.push({ url: '/submain', controller: MembershipSubmain });
