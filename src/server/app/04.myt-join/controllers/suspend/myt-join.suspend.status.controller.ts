79c75e890eb1ee6405803f1df792243e439566cc
Hyeryoun Lee <ryoun80@gmail.com>
2019-01-08 17:34:08 +0900

    [이혜련/SPTek][11차][DV001-10588] 입대일로부터 24개월 이상의 경우 일시정지 재신청 버튼 비활성화

--- src/server/app/04.myt-join/controllers/suspend/myt-join.suspend.status.controller.ts
+++ src/server/app/04.myt-join/controllers/suspend/myt-join.suspend.status.controller.ts
@@ -23,24 +23,6 @@ class MyTJoinSuspendStatus extends TwViewController {
       this.apiService.request(API_CMD.BFF_05_0149, {}),
       this.apiService.request(API_CMD.BFF_05_0194, {})
     ).subscribe(([suspendStatus, progress]) => {
-
-      /* tslint:disable */
-/*      suspendStatus = {
-        'code': '00',
-        'msg': '',
-        'result': {
-          'svcStCd': 'SP',
-          'reservedYn': 'N',
-          'fromDt': '20180523',
-          'toDt': '20180923',
-          'reFormDt': '20180909',
-          'svcChgRsnCd': '21',
-          'svcChgRsnNm': '분실정지',
-          'reason': '고객요청'
-        }
-      };*/
-      /* tslint:enable */
-
       const apiError = this.error.apiError([suspendStatus]);
       if ( !FormatHelper.isEmpty(apiError) ) {
         return this.error.render(res, {
@@ -68,8 +50,14 @@ class MyTJoinSuspendStatus extends TwViewController {
         if ( suspendStatus.result.svcChgRsnCd === '21'
           || suspendStatus.result.svcChgRsnCd === '22' ) { // 장기일시정지(case 6)
           status['type'] = 'long-term';
+          status['resuspendable'] = true;
           if ( suspendStatus.result.reFormDt ) { // 장기일시정지(case 7)
             status['resuspend'] = DateHelper.getShortDateWithFormat(suspendStatus.result.reFormDt, 'YYYY.MM.DD.');
+          } else if ( suspendStatus.result.svcChgRsnCd === '21' ) {
+            const months = DateHelper.getDiffByUnit(DateHelper.getCurrentDate(), suspendStatus.result.fromDt, 'months');
+            if ( months >= 24 ) {
+              status['resuspendable'] = false;
+            }
           }
         } else { // 일시정지(case 1)
           status['type'] = 'temporary';
@@ -78,50 +66,6 @@ class MyTJoinSuspendStatus extends TwViewController {
       } else {
         options['status'] = null;
       }
-
-      /* tslint:disable */
-  /*    progress = {
-        'code': '00',
-        'msg': '',
-        'result': {
-          'seq': '222',
-          // 'opStateCd': 'R',
-          // 'opState: '접수중',
-          'opStateCd': 'D',
-          'opState': '서류확인',
-          // 'opStateCd': 'A',
-          // 'opState':'서류확인중',
-          // 'opStateCd': 'F',
-          // 'opState': '서류확인중',
-          // 'opStateCd': 'C',
-          // 'opState': '처리완료',
-          'opReasonCd': '30',
-          'opReason': '구비서류미비',
-          'opDtm': '20181001122000',
-          'reasonCd': '??',
-          'reason': '군입대',
-          'receiveCd': '??',
-          'receive': '걸기/받기 모두 정지',
-          'rgstDt': '20180901',
-          'fromDt': '20181231',
-          'toDt': '20201231',
-          'attFileList': [
-            {
-              'seq': '001',
-              'fileName': 'adfads.gif',
-              'auditDtm': '20181011'
-            },
-            {
-              'seq': '001',
-              'fileName': 'adfads.gif',
-              'auditDtm': '20181011'
-            }
-          ],
-          'cntcNumRelNm': '친구',
-          'cntcNum': '222'
-        }
-      };*/
-      /* tslint:enable */
       if ( progress.code === API_CODE.CODE_00 ) { //  현재 장기일시 정지 미신청 상태에 대한 코드가 없음
         const _progress = progress.result;
         _progress.rgstDt = DateHelper.getShortDateWithFormat(_progress.rgstDt, 'YYYY.MM.DD.');
@@ -131,17 +75,6 @@ class MyTJoinSuspendStatus extends TwViewController {
 
         if ( _progress.toDt ) {
           _progress.toDt = DateHelper.getShortDateWithFormat(_progress.toDt, 'YYYY.MM.DD.');
-        }
-        switch ( _progress.opStateCd ) {
-          case 'R': // 접수중(case 2)
-          case 'D': // 서류접수중
-            break;
-          case 'A': // 서류재첨부필요(case 3)
-            break;
-          case 'F': // 처리불가(case 4)
-            break;
-          case 'C': // 처리완료(case 5)
-            break;
         }
         options['progress'] = _progress;
         if ( options['status'] ) {
