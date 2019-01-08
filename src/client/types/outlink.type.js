cc9e455d7637d687445b2c203e8bf462d69f1189
1100015 <jayoon.kong@sk.com>
2019-01-08 15:16:25 +0900

    [공자윤/Frontend개발팀] app link 이동 수정 (link 추가)

--- src/client/types/outlink.type.js
+++ src/client/types/outlink.type.js
@@ -4,7 +4,7 @@ Tw.OUTLINK = {
   DATA_FACTORY: 'http://tmembership.tworld.co.kr/web/html/data/dataFactory.jsp',
   MOBILE_TWORLD: 'http://m2.tworld.co.kr/jsp/op.jsp?p=w70&f=a0001',
   GOOGLE_STORE: 'https://play.google.com/store/apps/details?id=Com.sktelecom.minit',
-  ONE_STORE: 'https://www.onestore.co.kr/userpoc/game/view?pid=0000032819',
+  ONE_STORE: 'https://m.onestore.co.kr/mobilepoc/apps/appsDetail.omp?prodId=0000032819',
   APP_STORE: 'https://itunes.apple.com/kr/app/%EB%AA%A8%EB%B0%94%EC%9D%BCtworld/id428872117?mt=8',
   DIRECT_SUPPORT: 'http://shop.tworld.co.kr/handler/Dantong-SKT?UPDATE_YN=N&_ga=2.226795357.1794270396.1498012356-98061374.1498012356',
   DIRECT_HOME: 'http://shop.tworld.co.kr/handler/ShopTworldMain-Start',
