bfbb695bf33bf5222b6fa263058272d852749e95
KimInHwan <skt.p132150@partner.sk.com>
2019-01-09 17:17:40 +0900

    [김인환/SPTek] [11차] [DV001-11584]  - 서브메인 회선 변경 후 토스트팝업 노출 관련 수정

--- src/client/app/03.myt-fare/myt-fare.submain.js
+++ src/client/app/03.myt-fare/myt-fare.submain.js
@@ -527,11 +527,11 @@ Tw.MyTFareSubMain.prototype = {
 
   // 회선 변경 후 처리
   _onChangeSessionSuccess: function () {
+    if ( Tw.BrowserHelper.isApp() ) {
+      this._popupService.toast(Tw.REMNANT_OTHER_LINE.TOAST);
+    }
     setTimeout($.proxy(function () {
       this._historyService.reload();
-      if ( Tw.BrowserHelper.isApp() ) {
-        this._popupService.toast(Tw.REMNANT_OTHER_LINE.TOAST);
-      }
     }, this), 500);
   },
 
