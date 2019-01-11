faa22dae98934f07810fc38d29744a27691130ff
Jung Eunjung <dmsej@pineone.com>
2019-01-11 14:00:55 +0900

    [정은정/파인원][12차] T멤버십 > 내주변 제휴 브랜드 지도보기 연결. [정은정/파인원][12차] T멤버십 > 멤버십 아이콘 추가.

--- src/client/app/07.membership/membership.submain.js
+++ src/client/app/07.membership/membership.submain.js
@@ -4,9 +4,11 @@
  * Date: 2018.12.26
  */
 
-Tw.MembershipSubmain = function(rootEl, membershipData) {
+Tw.MembershipSubmain = function(rootEl, membershipData, svcInfo, membershipCheckData) {
   this.$container = rootEl;
   this._membershipData = membershipData;
+  this._svcInfo = svcInfo;
+  this._membershipCheckData = membershipCheckData;
   this._nativeService = Tw.Native;
   this._historyService = new Tw.HistoryService();
   this._popupService = Tw.Popup;
@@ -36,7 +38,39 @@ Tw.MembershipSubmain.prototype = {
     this.$container.on('click', '.fe-membership-grade', $.proxy(this._goMembershipGrade, this));
     this.$container.on('click', '.box-app-down', $.proxy(this._goTmembership, this));
     this.$container.on('click', '.data-plus', $.proxy(this._goChocolate, this));
+    this.$container.on('click', '.coalition-brand-list .map', $.proxy(this._getBrandDetailInfo, this));
+    this.$container.on('click', '#fe-membership-join', $.proxy(this._goMembershipJoin, this));
+    this.$container.on('click', '.fe-mebership-my', $.proxy(this._goMyMembership, this));
+  },
+  _goMyMembership: function() {
+    this._historyService.goLoad('/membership/my');
+  },
+  _goMembershipJoin: function () {
+    this._historyService.goLoad('/membership/join');
+  },
+  _getBrandDetailInfo: function (event) {
+    this.currentTarget = $(event.currentTarget);
+    var params = {
+      coCd: this.currentTarget.data('cocd'),
+      joinCd: this.currentTarget.data('joincd')
+    };
 
+    this._apiService.request(Tw.API_CMD.BFF_11_0024, params)
+        .done($.proxy(this._handleSuccessDetailInfo, this))
+        .fail($.proxy(this._handleFailCallBack, this));
+  },
+  _handleSuccessDetailInfo: function (resp) {
+    Tw.Logger.info('_handleSuccessDetailInfo resp', resp);
+    var _result = resp.result;
+    var param = {
+      coCd: _result.coCd,
+      joinCd: _result.joinCd,
+      mapX: _result.googleMapCoordX,
+      mapY: _result.googleMapCoordY,
+      brandCd:_result.brandCd,
+      cateCd: _result.cateCd
+    };
+    this._historyService.goLoad('/membership/benefit/map?' + $.param(param));
   },
   _goChocolate: function () {
     Tw.CommonHelper.openUrlExternal(Tw.URL_PATH.CHOCOLATE_MALL,'');
@@ -111,15 +145,15 @@ Tw.MembershipSubmain.prototype = {
     return iconArr;
   },
   _handleSuccessNeaBrand: function (resp) {
-    var _result = resp.result.list;
-    for(var idx in _result){
-      _result[idx].showIcoGrd1 = this._changeIcoGrade(_result[idx].icoGrdChk1);
-      _result[idx].showIcoGrd2 = this._changeIcoGrade(_result[idx].icoGrdChk2);
-      _result[idx].showIcoGrd3 = this._changeIcoGrade(_result[idx].icoGrdChk3);
-      _result[idx].showIcoGrd4 = this._changeIcoGrade(_result[idx].icoGrdChk4);
+    this.nearBrandData = resp.result.list;
+    for(var idx in this.nearBrandData){
+      this.nearBrandData[idx].showIcoGrd1 = this._changeIcoGrade(this.nearBrandData[idx].icoGrdChk1);
+      this.nearBrandData[idx].showIcoGrd2 = this._changeIcoGrade(this.nearBrandData[idx].icoGrdChk2);
+      this.nearBrandData[idx].showIcoGrd3 = this._changeIcoGrade(this.nearBrandData[idx].icoGrdChk3);
+      this.nearBrandData[idx].showIcoGrd4 = this._changeIcoGrade(this.nearBrandData[idx].icoGrdChk4);
     }
-    Tw.Logger.info('near brand resp :', _result);
-    this.$nearBrand.append(this._nearBrandTmpl({ list : _result }));
+    Tw.Logger.info('near brand resp :', this.nearBrandData);
+    this.$nearBrand.append(this._nearBrandTmpl({ list : this.nearBrandData }));
   },
   _handleFailCallBack: function () {
 
