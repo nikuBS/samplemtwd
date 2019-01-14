7a689fb1b998b34dbed839dd76330499c1714369
KimInHwan <skt.p132150@partner.sk.com>
2019-01-14 14:45:12 +0900

    [김인환/SPTek] 오타수정

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-Ívar gulp       = require('gulp'),
+var gulp       = require('gulp'),
     gutil      = require('gulp-util'),
     uglify     = require('gulp-uglify'),
     concat     = require('gulp-concat'),
