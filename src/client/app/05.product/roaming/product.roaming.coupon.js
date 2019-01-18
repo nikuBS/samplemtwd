a7dc4b7f26e905eeeba99656358ff13bb5d2132b
김승규/파인원 <ksk4788@pineone.com>
2019-01-18 13:41:57 +0900

    [김승규/파인원][12차] 로밍 > 카드/쿠폰 - BPCP 페이지 연동 [김승규/파인원][12차] 멤버십 > 나의T멤버십 - API연동 에러코드 추가

--- src/client/app/05.product/roaming/product.roaming.coupon.js
+++ src/client/app/05.product/roaming/product.roaming.coupon.js
@@ -15,27 +15,38 @@ Tw.ProductRoamingCoupon = function(rootEl) {
 Tw.ProductRoamingCoupon.prototype = {
 
   _bindEvent: function() {
-    this.$container.on('click', '#coupon-buy-btn', $.proxy(this._onViewclicked, this, 'buy'));
-    this.$container.on('click', '#coupon-register-btn', $.proxy(this._onViewclicked, this, 'register'));
-    this.$container.on('click', '#coupon-inquire-btn', $.proxy(this._onViewclicked, this, 'inquire'));
+    this.$container.on('click', '#coupon-buy-btn', $.proxy(this._onViewclicked, this, 'BUY'));
+    this.$container.on('click', '#coupon-register-btn', $.proxy(this._onViewclicked, this, 'REGISTER'));
+    this.$container.on('click', '#coupon-inquire-btn', $.proxy(this._onViewclicked, this, 'INQUIRE'));
+    this.$container.on('click', '.card-coupon-inner', $.proxy(this._onViewclicked, this, 'COUPON'));
   },
 
-  _onViewclicked: function (hash) {
-    var hbs = '';
+  _onViewclicked: function (state, event) {
+    if(state === 'COUPON'){
+      state = event.currentTarget.id;
+    }
+    var url = Tw.OUTLINK.ROAMING_COUPON[state];
+    this._title = state === 'REGISTER' ? 'REGISTER' : 'BUY';
 
-    if( hash === 'buy'){
-      hbs = 'RM_13_01';
-    }else if(hash === 'register'){
-      hbs = 'RM_13_02';
-    }else if(hash === 'inquire'){
-      hbs = 'RM_13_03';
+    this._getBPCP(url);
+  },
+
+  _getBPCP: function(url) {
+    var replaceUrl = url.replace('BPCP:', '');
+    this._apiService.request(Tw.API_CMD.BFF_01_0039, { bpcpServiceId: replaceUrl })
+      .done($.proxy(this._responseBPCP, this));
+  },
+
+  _responseBPCP: function(resp) {
+    if (resp.code !== Tw.API_CODE.CODE_00) {
+      return Tw.Error(resp.code, resp.msg).pop();
     }
 
-    this._popupService.open({
-        hbs: hbs,
-        layer: true
-      },
-      null,
-      null, hash);
-  }
+    var url = resp.result.svcUrl;
+    if (!Tw.FormatHelper.isEmpty(resp.result.tParam)) {
+      url += (url.indexOf('?') !== -1 ? '&tParam=' : '?tParam=') + resp.result.tParam;
+    }
+
+    Tw.CommonHelper.openUrlInApp(url, null, Tw.ROAMING_BPCP[this._title]);
+  },
 };
--- src/client/types/string.type.js
+++ src/client/types/string.type.js
@@ -1610,3 +1610,8 @@ Tw.ROAMING_SVCTIME_SETTING_ERR_CASE = {
   ERR_END_EVT_START : '서비스 개시일 설정은 종료일 이전만 설정 가능합니다.',
   ERR_END_EVT_END : '서비스 종료일 설정은 개시일 이후만 가능합니다.'
 };
+
+Tw.ROAMING_BPCP = {
+  BUY : '쿠폰 구매·선물',
+  REGISTER : 'T로밍쿠폰 상세'
+};
