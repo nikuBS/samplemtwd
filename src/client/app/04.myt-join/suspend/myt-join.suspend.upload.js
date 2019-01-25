f98191c758dcaa9801ccf4d4292dad4bc5c8733c
Hyeryoun Lee <ryoun80@gmail.com>
2019-01-25 16:59:00 +0900

    [이혜련/SPTek][11차] 안드로이드 4.4 upload 확장자 전달 방식 변경

--- src/client/app/04.myt-join/suspend/myt-join.suspend.upload.js
+++ src/client/app/04.myt-join/suspend/myt-join.suspend.upload.js
@@ -14,6 +14,7 @@ Tw.MytJoinSuspendUpload.prototype = {
     this._fileCount = fileCount || 1;
     this._fileInfo = fileInfo || new Array(this._fileCount);
     oldFiles = oldFiles || [];
+    this._acceptExt = ['jpg', 'jpeg', 'png', 'docx', 'doc', 'pdf', 'hwp'];
     this._fileInfo = _.map(this._fileInfo, function (info, idx) {
       return _.defaults(info, Tw.MytJoinSuspendUpload.DEFAULT_FILE, { oldFile: oldFiles[idx] });
     });
@@ -80,7 +81,7 @@ Tw.MytJoinSuspendUpload.prototype = {
     if ( this._isLowerVersionAndroid() ) {
       this._nativeService.send(Tw.NTV_CMD.OPEN_FILE_CHOOSER, {
         dest: 'suspend',
-        acceptExt: Tw.MytJoinSuspendUpload.DEFAULT_FILE,
+        acceptExt: this._acceptExt,
         limitSize: Tw.MAX_FILE_SIZE.toString()
       }, $.proxy(this._nativeFileChooser, this, $target));
     }
@@ -91,16 +92,16 @@ Tw.MytJoinSuspendUpload.prototype = {
       var params = response.params;
       var fileInfo = params.fileData.result[0];
 
-      if(!this._validateFile(fileInfo)){
-        return;
-      }
-
       if ( fileInfo ) {
         var $elFileName = $target.find('input.fileview');
         $elFileName.val(fileInfo.originalName);
       }
       this._nativeUploaded[$target.data('index')] = fileInfo;
       this._setFileButton($target, false);
+    } else if ( response.resultCode === Tw.NTV_CODE.CODE_01 ) {
+      this._popupService.openAlert(Tw.UPLOAD_FILE.CONFIRM_A01);
+    } else if ( response.resultCode === Tw.NTV_CODE.CODE_02 ) {
+      this._popupService.openAlert(Tw.UPLOAD_FILE.CONFIRM_A02);
     }
   },
 
@@ -146,7 +147,7 @@ Tw.MytJoinSuspendUpload.prototype = {
 
       if ( Tw.BrowserHelper.isApp() && this._isLowerVersionAndroid() ) {
         this._callback(this._nativeUploaded, true);
-      }else{
+      } else {
         var uploadFile = [];
         this.$inputFile.each(function () {
           if ( this.files.length !== 0 ) {
@@ -162,13 +163,13 @@ Tw.MytJoinSuspendUpload.prototype = {
   _onClickFileButton: function (e) {
     var $btFile = $(e.target);
     var $inputBox = $btFile.parents('.inputbox');
-    if($inputBox.find('input.file').attr('disabled')){// 파일삭제
+    if ( $inputBox.find('input.file').attr('disabled') ) {// 파일삭제
       // 파일삭제시 input 처리
       //TOOD 파일 삭제
       this._setFileButton($inputBox, true);
 
-    }else{
-      if( Tw.BrowserHelper.isApp() && this._isLowerVersionAndroid() ){
+    } else {
+      if ( Tw.BrowserHelper.isApp() && this._isLowerVersionAndroid() ) {
         this._openCustomFileChooser($inputBox);
       }
     }
