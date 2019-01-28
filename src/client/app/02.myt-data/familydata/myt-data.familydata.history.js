aea537e65b3e3e79839490732bc1362338504293
jiyoung.jo <jiyoungjo@sk.com>
2019-01-28 15:22:47 +0900

    [조지영/Client개발팀][13차] T가족모아 데이터 BFF 에러 케이스 처리 로직 추가

--- src/client/app/02.myt-data/familydata/myt-data.familydata.history.js
+++ src/client/app/02.myt-data/familydata/myt-data.familydata.history.js
@@ -7,46 +7,77 @@
 Tw.MyTDataFamilyHistory = function(rootEl) {
   this.$container = rootEl;
   this._apiService = Tw.Api;
+  this._popupService = Tw.Popup;
 
   this._bindEvent();
+  this._init();
 };
 
 Tw.MyTDataFamilyHistory.prototype = {
+  _init: function() {
+    this._ingTmpl = Handlebars.compile($('#fe-tmpl-ing').html());
+    this._afterTmpl = Handlebars.compile($('#fe-tmpl-after').html());
+    this._noneTmpl = Handlebars.compile($('#fe-tmpl-after-none').html());
+  },
+
   _bindEvent: function() {
     this.$container.on('click', '.fe-before', $.proxy(this._handleRetrieveData, this));
+    this.$container.on('click', '.fe-edit', $.proxy(this._handleClickEditData, this));
   },
 
   _handleRetrieveData: function(e) {
     var $target = $(e.currentTarget),
-      serialNumber = $target.data('serial-number');
+      serialNumber = $target.data('serial-number'),
+      $parent = $target.parent('li');
 
     $target.addClass('none');
-    $target.siblings('.fe-ing').removeClass('none');
-    this._requestRetrieve(serialNumber, 0, $target);
+    $parent.append(this._ingTmpl());
+    this._requestRetrieve(serialNumber, 0, $target, $parent);
   },
 
-  _requestRetrieve: function(serialNumber, requestCount, $before) {
+  _requestRetrieve: function(serialNumber, requestCount, $before, $parent) {
     this._apiService
       .request(Tw.API_CMD.BFF_06_0072, { reqCnt: requestCount, shrpotSerNo: serialNumber })
-      .done($.proxy(this._handleSuccessRetrieve, this, serialNumber, $before));
+      .done($.proxy(this._handleDoneRetrieve, this, serialNumber, $before, $parent));
   },
 
-  _handleSuccessRetrieve: function(serialNumber, $before, resp) {
-    var $ing = $before.siblings('.fe-ing'),
-      $after = $before.siblings('.fe-after');
-    if (resp.code !== Tw.API_CODE.CODE_00) {
-      Tw.Error(resp.code, resp.msg).pop();
-      $before.removeClass('none');
-      $ing.addClass('none');
-    } else {
-      if (resp.result && resp.result.nextReqYn === 'Y') {
-        setTimeout($.proxy(this._requestRetrieve, this, serialNumber, resp.result.reqCnt, $before), 3000);
-        return;
-      } else {
-        $ing.addClass('none');
-        $after.removeClass('none');
-        $after.find('strong').text(resp.result.remGbGty + 'GB');
-      }
+  _handleDoneRetrieve: function(serialNumber, $before, $parent, resp) {
+    if (resp.code !== Tw.API_CODE.CODE_00 || !resp.result) {
+      this._setRetrieveStatus($before);
+      return;
     }
+
+    if (resp.result.nextReqYn === 'Y') {
+      setTimeout($.proxy(this._requestRetrieve, this, serialNumber, resp.result.reqCnt, $before, $parent), 3000);
+    } else {
+      this._handleSuccessRetrieve(resp.result, $before, $parent);
+    }
+  },
+
+  _setRetrieveStatus: function($before) {
+    $before.removeClass('none');
+    $before.siblings('.fe-ing').addClass('none');
+    this._popupService.openAlert(Tw.ALERT_MSG_MYT_DATA.ALERT_2_A218, Tw.POPUP_TITLE.NOTIFY);
+  },
+
+  _handleSuccessRetrieve: function(share, $before, $parent) {
+    if (!share.remGbGty || share.remGbGty.length === 0) {
+      this._setRetrieveStatus($before);
+      return;
+    }
+
+    var serial = $before.data('serial-number');
+    $before.siblings('.fe-ing').remove();
+
+    var nData = Number(share.remGbGty) || 1;
+    if (nData > 0) {
+      $parent.append(this._afterTmpl({ data: nData, serial: serial }));
+    } else {
+      $parent.append(this._noneTmpl({}));
+    }
+  },
+
+  _handleClickEditData: function(e) {
+    // e.currentTarget.getAttribute('data-serial-number');
   }
 };
