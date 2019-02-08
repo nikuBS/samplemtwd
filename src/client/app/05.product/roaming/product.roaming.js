a16c06d3d4c218ff83c0f8cee33c7ad05fc8bf2f
김주호 pineone <jhkim@pineone.com>
2019-02-08 15:05:09 +0900

    [김주호/파인원][DV001-13269] 원복 -> T로밍 서브메인 리디자인 적용

--- src/client/app/05.product/roaming/product.roaming.js
+++ src/client/app/05.product/roaming/product.roaming.js
@@ -7,8 +7,6 @@
 Tw.ProductRoaming = function(rootEl, options) {
   this.$container = rootEl;
   this._popupService = Tw.Popup;
-  this._historyService = new Tw.HistoryService();
-
   this._options = options;
 
   this._cachedElement();
@@ -21,7 +19,6 @@ Tw.ProductRoaming.prototype = {
     this.$formInfoBtnList = this.$container.find('.info-link-inner');
   },
   _bindEvent: function () {
-    this.$container.on('click', '.fe-home-internal', $.proxy(this._onClickInternal, this));
     this.$formInfoBtnList.on('click', $.proxy(this._onClickFormInfo, this));
   },
   _init : function() {
@@ -38,14 +35,9 @@ Tw.ProductRoaming.prototype = {
     this.$prevBtn.on('click', $.proxy(this._onClickPrevBtn, this));
     this.$nextBtn.on('click', $.proxy(this._onClickNextBtn, this));
 
-    this._updateFormInfo();
-  },
-  _onClickInternal: function (event) {
-    var url = $(event.currentTarget).data('url');
-    this._historyService.goLoad(url);
+    $layer.find('.popup-closeBtn').on('click', $.proxy(function() { this._popupService.closeAll(); }, this));
 
-    event.preventDefault();
-    event.stopPropagation();
+    this._updateFormInfo();
   },
   _onClickFormInfo: function(e) {
     this.idxSelect = $(e.currentTarget).data('index');
