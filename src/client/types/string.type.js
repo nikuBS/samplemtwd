deef6b0d4c464a996b66c5de2fa5545a76d14c17
Hakjoon Sim <hakjoon.sim@sk.com>
2019-02-19 17:41:02 +0900

    [심학준/FE개발팀][DV001-14515]
     - Fix words

--- src/client/types/string.type.js
+++ src/client/types/string.type.js
@@ -1041,7 +1041,7 @@ Tw.MYT_DATA_FAMILY_SUCCESS_CHANGE_DATA = '데이터 변경';
 Tw.MYT_DATA_FAMILY_CHANGE_DATA_ERRORS = {
   NOT_CMPLETE: '공유가 완료되지 않은 데이터는 변경이 불가능합니다.',
   NOT_THIS_MONTH: '이번 달 공유한 데이터만 변경이 가능합니다.',
-  NOT_MINE: '다른 가족이 공유한 내역은 변경이 불가능합니다.', 
+  NOT_MINE: '다른 가족이 공유한 내역은 변경이 불가능합니다.',
   NOT_SAME_PLAN: '요금제가 변경되었습니다.<br />공유할 때 이용 중이었던 요금제일 경우에만 데이터 변경이 가능합니다.',
   GREATER_THAN_CHANGABLE: '신청량이 가능 데이터보다 커, 변경이 불가능합니다.'
 };
@@ -1359,8 +1359,8 @@ Tw.BRANCH_SEARCH_OPTIONS = {
   0: '전체',
   1: '지점',
   2: '대리점',
-  premium: 'T Premium  Store',
-  direct: '바로픽업',
+  premium: 'T프리미엄스토어',
+  direct: '바로 픽업',
   rent: '임대폰',
   skb: 'SK브로드밴드',
   apple: '애플취급점',
