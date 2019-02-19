877296afdd9d8cda5fa98ca3e5922fa7fe2e693b
조아라 SKtechx <araara.jo@sk.com>
2019-02-19 09:38:36 +0900

    [조아라/Client개발팀] 공유하기에서 단축 URL사용으로 변경

--- src/server/common/route/shortcut.router.ts
+++ src/server/common/route/shortcut.router.ts
@@ -6,6 +6,7 @@ import { REDIS_KEY } from '../../types/redis.type';
 import { API_CODE } from '../../types/api-command.type';
 import DateHelper from '../../utils/date.helper';
 import { SHORTCUT_LOGIN_TYPE } from '../../types/bff.type';
+import FormatHelper from '../../utils/format.helper';
 
 
 class ShortcutRouter {
@@ -25,7 +26,19 @@ class ShortcutRouter {
           if ( resp.code === API_CODE.CODE_00 ) {
             this.redirectTarget(req, res, next, resp.result);
           } else {
-            next();
+            const menuUrl = path.replace('/', '');
+            this.redisService.getData(REDIS_KEY.MENU_URL + menuUrl).subscribe((menuResp) => {
+              if ( menuResp.code === API_CODE.CODE_00 ) {
+                console.log(menuResp);
+                if ( !FormatHelper.isEmpty(menuResp.result.menuUrl) ) {
+                  res.redirect('/common/share/bridge?target=' + menuResp.result.menuUrl );
+                } else {
+                  next();
+                }
+              } else {
+                next();
+              }
+            });
           }
         });
 
