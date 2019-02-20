aea52985e409190188c47690ab0778478c62723e
JimanPark <jiman.park@sk.com>
2019-02-20 17:56:06 +0900

    [박지만/Client개발팀][DV001-14283][나의데이터/통화_즉시충전] 데이터 한도 요금제 화면 내 토글버튼 조작 시 노출되는 토스트 팝업 문구 기획서상이

--- src/client/app/02.myt-data/limit/myt-data.limit.immediately.js
+++ src/client/app/02.myt-data/limit/myt-data.limit.immediately.js
@@ -47,9 +47,9 @@ Tw.MyTDataLimitImmediately.prototype = {
   _onSuccessBlockImmediately: function (sCheckType, res) {
     if ( res.code === Tw.API_CODE.CODE_00 ) {
       if ( sCheckType === 'block' ) {
-        Tw.CommonHelper.toast(Tw.TOAST_TEXT.MYT_DATA_LIMIT_BLOCK);
-      } else {
         Tw.CommonHelper.toast(Tw.TOAST_TEXT.MYT_DATA_LIMIT_UNBLOCK);
+      } else {
+        Tw.CommonHelper.toast(Tw.TOAST_TEXT.MYT_DATA_LIMIT_BLOCK);
       }
     } else {
       this._popupService.openAlert(res.msg + Tw.MYT_DATA_TING.ERROR_LIMIT.CONTENT, Tw.MYT_DATA_TING.ERROR_LIMIT.TITLE, null, $.proxy(this._goSubmain, this));
