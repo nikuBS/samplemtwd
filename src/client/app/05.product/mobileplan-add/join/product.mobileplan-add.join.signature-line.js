91052587f46ffc89891ff52338a671dcbaa6aa4d
양지훈 SKtechx <jihun202@sk.com>
2019-02-21 19:51:06 +0900

    [양지훈/FE개발팀][DV001-14818] QA bug fix

--- src/client/app/05.product/mobileplan-add/join/product.mobileplan-add.join.signature-line.js
+++ src/client/app/05.product/mobileplan-add/join/product.mobileplan-add.join.signature-line.js
@@ -57,7 +57,7 @@ Tw.ProductMobileplanAddJoinSignatureLine.prototype = {
   _addNum: function() {
     var number = this.$inputNumber.val().replace(/-/gi, '');
 
-    if (this.$lineList.find('li').length > 3) {
+    if (this.$lineList.find('li').length > 4) {
       return this._popupService.openAlert(Tw.ALERT_MSG_PRODUCT.ALERT_3_A9.MSG,
         Tw.ALERT_MSG_PRODUCT.ALERT_3_A9.TITLE);
     }
