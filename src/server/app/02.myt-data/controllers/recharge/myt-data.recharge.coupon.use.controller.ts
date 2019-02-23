99ee1b5b958e903802fcdf09795212379461e702
Hakjoon Sim <hakjoon.sim@sk.com>
2019-02-23 11:51:52 +0900

    [심학준/FE개발팀][DV001-14489]
     - Fix 0Plan large data amount issue

--- src/server/app/02.myt-data/controllers/recharge/myt-data.recharge.coupon.use.controller.ts
+++ src/server/app/02.myt-data/controllers/recharge/myt-data.recharge.coupon.use.controller.ts
@@ -51,7 +51,7 @@ export default class MyTDataRechargeCouponUse extends TwViewController {
     ['NA00004705', 0],
     ['NA00005957', 15], // T plan large
     ['NA00005958', 20], // T plan family
-    ['NA00006157', 15] // 0 plan large
+    ['NA00006157', 20] // 0 plan large
   ]);
 
   render(req: Request, res: Response, next: NextFunction, svcInfo: any,
