9b48f6c7eeaf643a46376fe8f27881fcbef4bfff
jiyoung.jo <jiyoungjo@sk.com>
2019-03-11 16:25:18 +0900

    [조지영/Client개발팀][DV001-16798] 칭찬합니다 대상 변경시 clear 버튼 none 처리 추가

--- src/client/app/08.customer/praise/customer.praise.js
+++ src/client/app/08.customer/praise/customer.praise.js
@@ -293,6 +293,7 @@ Tw.CustomerPraise.prototype = {
     this.$divRole.find('input').val('');
     this.$subject.find('input').val('');
     this.$reasons.val('');
+    this.$container.find('.cancel').css('display', 'none');
     delete this._selectedType;
     delete this._selectedArea;
     this.$typeBtn.text(Tw.CUSTOMER_PRAISE_DEFAULT.TYPE);
