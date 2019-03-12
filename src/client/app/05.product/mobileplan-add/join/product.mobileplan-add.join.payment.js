6e695f2a5d900c0b8bae2b2b78746a0ac73e6819
양지훈 SKtechx <jihun202@sk.com>
2019-03-12 15:17:38 +0900

    [양지훈/FE개발팀][DV001-16947] 가입 시 5분이내 4회 초과하여 [인증번호받기] 선택 시 노출되는 오류문구가 SB와 상이한 문제

--- src/client/app/05.product/mobileplan-add/join/product.mobileplan-add.join.payment.js
+++ src/client/app/05.product/mobileplan-add/join/product.mobileplan-add.join.payment.js
@@ -153,6 +153,10 @@ Tw.ProductMobileplanAddJoinPayment.prototype = {
   },
 
   _replaceErrMsg: function(code, msg) {
+    if (code === 'ATH2003') {
+      return Tw.SMS_VALIDATION.WAIT_NEXT_TIME;
+    }
+
     if (code === 'ATH2006') {
       return Tw.SMS_VALIDATION.EXPIRE_NEXT_TIME;
     }
