8b7dec9bb36772c9ade10bd1e37ff38207b86915
김승규/파인원 <ksk4788@pineone.com>
2019-03-12 19:19:08 +0900

    [김승규/파인원][DV001-17007] 로밍 카드/쿠폰 BPCP 연동

--- src/client/app/05.product/roaming/product.roaming.coupon.js
+++ src/client/app/05.product/roaming/product.roaming.coupon.js
@@ -9,7 +9,9 @@ Tw.ProductRoamingCoupon = function(rootEl) {
   this._historyService = new Tw.HistoryService();
   this._popupService = Tw.Popup;
   this._apiService = Tw.Api;
+  this._tidLanding = new Tw.TidLandingComponent();
   this._bindEvent();
+  this._init();
 };
 
 Tw.ProductRoamingCoupon.prototype = {
@@ -19,6 +21,12 @@ Tw.ProductRoamingCoupon.prototype = {
     this.$container.on('click', '#coupon-register-btn', $.proxy(this._onViewclicked, this, 'REGISTER'));
     this.$container.on('click', '#coupon-inquire-btn', $.proxy(this._onViewclicked, this, 'INQUIRE'));
     this.$container.on('click', '.card-coupon-inner', $.proxy(this._onViewclicked, this, 'COUPON'));
+
+    $(window).on('message', $.proxy(this._getWindowMessage, this));
+  },
+
+  _init: function(){
+    history.replaceState(null, document.title, location.origin + '/product/roaming/coupon');
   },
 
   _onViewclicked: function (state, event) {
@@ -39,16 +47,50 @@ Tw.ProductRoamingCoupon.prototype = {
   },
 
   _responseBPCP: function(resp) {
+    if (resp.code === 'BFF0003') {
+      return this._tidLanding.goLogin(location.origin + '/product/roaming/coupon');
+    }
+
     if (resp.code !== Tw.API_CODE.CODE_00) {
-      this._onFail(resp);
+      return Tw.Error(resp.code, resp.msg).pop();
     }
 
     var url = resp.result.svcUrl;
+    if (Tw.FormatHelper.isEmpty(url)) {
+      return Tw.Error(null, Tw.ALERT_MSG_PRODUCT.BPCP).pop();
+    }
+
     if (!Tw.FormatHelper.isEmpty(resp.result.tParam)) {
       url += (url.indexOf('?') !== -1 ? '&tParam=' : '?tParam=') + resp.result.tParam;
     }
 
-    Tw.CommonHelper.openUrlInApp(url, null, Tw.ROAMING_BPCP[this._title]);
+    url += '&ref_origin=' + encodeURIComponent(location.origin);
+
+    this._popupService.open({
+      hbs: 'product_bpcp',
+      iframeUrl: url
+    }, null, $.proxy(function() {
+      this._historyService.replaceURL('/product/roaming/coupon');
+    }, this));
+  },
+
+  _getWindowMessage: function(e) {
+    var data = e.data || e.originalEvent.data;
+
+    // BPCP 팝업 닫기
+    if (data === 'popup_close') {
+      this._popupService.close();
+    }
+
+    // BPCP 팝업 닫고 링크 이동
+    if (data.indexOf('goLink:') !== -1) {
+      this._popupService.closeAllAndGo(data.replace('goLink:', ''));
+    }
+
+    // BPCP 팝업 닫고 로그인 호출
+    if (data.indexOf('goLogin:') !== -1) {
+      this._tidLanding.goLogin(location.origin + '/product/roaming/coupon&' + $.param(JSON.parse(data.replace('goLogin:', ''))));
+    }
   },
 
   _onFail: function(err) {
