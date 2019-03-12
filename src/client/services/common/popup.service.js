9a5889a624ddf21dfe18538743f500f724103c84
1100015 <jayoon.kong@sk.com>
2019-03-12 14:33:15 +0900

    [공자윤/FE개발팀][DV001-16549][웹접근성] popup 닫을 때 focus 이동 안되는 이슈 수정

--- src/client/services/common/popup.service.js
+++ src/client/services/common/popup.service.js
@@ -95,6 +95,9 @@ Tw.PopupService.prototype = {
       closeCallback();
     }
     skt_landing.action.popup.close();
+
+    var $lastPopup = $('.tw-popup').last();
+    $lastPopup.attr('aria-hidden', 'false');
   },
   _addHash: function (closeCallback, hashName) {
     var curHash = location.hash || '#';
