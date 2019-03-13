b37513b57e92528ee7f035ec67533ecb1544a57c
양지훈 SKtechx <jihun202@sk.com>
2019-03-13 17:50:51 +0900

    [양지훈/FE개발팀][DV001-17152] 가입 시 01x 회선번호 입력 후 추가 시 신청된 회선 영역에 노출되는 회선정보의 하이픈 처리 오류

--- src/client/app/05.product/mobileplan-add/join/product.mobileplan-add.join.payment.js
+++ src/client/app/05.product/mobileplan-add/join/product.mobileplan-add.join.payment.js
@@ -230,11 +230,7 @@ Tw.ProductMobileplanAddJoinPayment.prototype = {
   },
 
   _blurInputNumber: function() {
-    if (this.$inputNumber.length > 8) {
-      this.$inputNumber.val(Tw.FormatHelper.conTelFormatWithDash(this.$inputNumber.val()));
-    } else {
-      this.$inputNumber.val(Tw.FormatHelper.getDashedCellPhoneNumber(this.$inputNumber.val()));
-    }
+    this.$inputNumber.val(Tw.FormatHelper.conTelFormatWithDash(this.$inputNumber.val()));
   },
 
   _focusInputNumber: function() {
