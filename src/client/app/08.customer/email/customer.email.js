228ab204ce162a2c076d4410c607aa076e017d3f
이기림(Kirim Lee)/FE개발팀/SKP <kirim@sk.com>
2019-03-13 17:58:25 +0900

    [이기림/Client개발팀][DV001-16986] 웹접근성 반영 납부내역

--- src/client/app/08.customer/email/customer.email.js
+++ src/client/app/08.customer/email/customer.email.js
@@ -50,6 +50,13 @@ Tw.CustomerEmail.prototype = {
     this.$container.on('click', '.fe-term-private-agree', $.proxy(this._openTermLayer, this, '37'));
     this.$container.on('click', '.fe-service-cntcNumClCd', $.proxy(this._onChangeReceiveContact, this));
     this.$container.on('click', '.fe-quality-cntcNumClCd', $.proxy(this._onChangeReceiveContact, this));
+    this.$container.on('click', '.tab-linker a', $.proxy(this._TabLinker, this));
+  },
+
+  _TabLinker: function (e) {
+    e.preventDefault(); // 링크이동되는 것을 막음
+    // 기본 적용 기능 li 에 aria-selected 를 주고 닫음 , 읽히게 하려면 a 에 주어야 함
+    $(e.currentTarget).attr('aria-selected', true).parent().siblings('li').find('a').attr('aria-selected', false); 
   },
 
   _preventDown: function(e) {
