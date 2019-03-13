cba8b310e443c3a657b8cab830ebbb4fbf76da0b
이기림(Kirim Lee)/FE개발팀/SKP <kirim@sk.com>
2019-03-14 08:37:27 +0900

    [이기림/Client개발팀][DV001-17258] 에러팝업호출방식Tw.Error 로 변경

--- src/client/app/03.myt-fare/info/myt-fare.info.overpay-account.js
+++ src/client/app/03.myt-fare/info/myt-fare.info.overpay-account.js
@@ -65,7 +65,9 @@ Tw.MyTFareInfoOverpayAccount.prototype = {
       this._popupService.openAlert(Tw.POPUP_CONTENTS.REFUND_ACCOUNT_SUCCESS,
         Tw.POPUP_TITLE.NOTIFY, Tw.BUTTON_LABEL.CONFIRM, $.proxy(this._refreshOverPay, this));
     } else {
-      if(res.code === 'ZNGME0000') res.msg = Tw.ALERT_MSG_MYT_FARE.ALERT_2_A32;
+      if(res.code === 'ZNGME0000') {
+        res.msg = Tw.ALERT_MSG_MYT_FARE.ALERT_2_A32;
+      }
       this._popupService.openAlert(res.msg, Tw.POPUP_TITLE.NOTIFY, Tw.BUTTON_LABEL.CONFIRM, null);
     }
   },
