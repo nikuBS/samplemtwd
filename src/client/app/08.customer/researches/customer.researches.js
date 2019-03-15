35aace70c8a75b157c88f2a6860195bd780f1e23
jiyoung.jo <jiyoungjo@sk.com>
2019-03-15 18:06:26 +0900

    [조지영/Client개발팀][DV001-17580] 설문조사 리스트에서 답변 선택 후 링크 이동시 답변선택 초기화하도록 수정

--- src/client/app/08.customer/researches/customer.researches.js
+++ src/client/app/08.customer/researches/customer.researches.js
@@ -39,6 +39,7 @@ Tw.CustomerResearches.prototype = {
     this.$container.on('click', '.fe-nResearch li.category-type', $.proxy(this._setSelect, this));
     this.$container.on('click', '.fe-nResearch .fe-submit', $.proxy(this._handleSubmit, this));
     this.$container.on('click', '.fe-nResearch .acco-tit', $.proxy(this._toggleShowDetail, this));
+    this.$container.on('click', 'a', $.proxy(this._clearForm, this));
   },
 
   _cachedElement: function() {
@@ -89,6 +90,13 @@ Tw.CustomerResearches.prototype = {
     $target.prop('disabled', true);
 
     this._apiService.request(Tw.API_CMD.BFF_08_0035, options).done($.proxy(this._handleSuccessSubmit, this));
+  },
+
+  _clearForm: function() {
+    var $list = this.$container.find('ul.survey-researchbox > li');
+    $list.prop('aria-checked', false);
+    $list.find('input').prop('checked', false);
+    this.$container.find('.fe-submit').prop('disabled', true);
   },
 
   _handleSuccessSubmit: function(resp) {
