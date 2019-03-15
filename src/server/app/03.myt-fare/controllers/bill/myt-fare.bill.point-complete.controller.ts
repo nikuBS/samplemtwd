0171b3a77caf733484a5e9189e6173aaac96e85f
1100015 <jayoon.kong@sk.com>
2019-03-15 09:48:54 +0900

    [공자윤/FE개발팀] 포인트요금납부 및 자동납부 ts, html 주석 추가

--- src/server/app/03.myt-fare/controllers/bill/myt-fare.bill.point-complete.controller.ts
+++ src/server/app/03.myt-fare/controllers/bill/myt-fare.bill.point-complete.controller.ts
@@ -29,39 +29,40 @@ class MyTFareBillPointComplete extends TwViewController {
     const add = !!queryObject && queryObject['add'];
 
     res.render('bill/myt-fare.bill.point-complete.html', {
-      mainTitle: this._getTitle(title, type),
-      subTitle: this._getSubTitle(title, type),
+      mainTitle: this._getTitle(title, type), // 메인타이틀 조회
+      subTitle: this._getSubTitle(title, type), // 서브타이틀 (포인트 종류)
       description: this._getDescription(title, type, add),
-      point: this._getPoint(point),
+      point: this._getPoint(point), // 납부된 포인트
       isRight: title === 'rainbow' && type === '',
-      centerName: MYT_FARE_COMPLETE_MSG.HISTORY,
-      centerUrl: '/myt-fare/info/history',
-      confirmUrl: '/myt-fare/submain',
+      centerName: MYT_FARE_COMPLETE_MSG.HISTORY, // 포인트 요금납부 내역 조회
+      centerUrl: '/myt-fare/info/history', // 포인트 요금납부 내역 조회 화면으로 이동
+      confirmUrl: '/myt-fare/submain', // 확인 버튼 클릭 시 서브메인으로 이동
       pageInfo
     });
   }
 
+  /* 포인트 종류 (제목) 조회 */
   private _getTitle(title: string, type: string): string {
     let mainTitle = '';
 
     if (title === 'CPT') {
-      mainTitle = MYT_FARE_POINT_MSG.CASHBAG;
+      mainTitle = MYT_FARE_POINT_MSG.CASHBAG; // CPT인 경우 OK cashbag
     } else if (title === 'TPT') {
-      mainTitle = MYT_FARE_POINT_MSG.TPOINT;
+      mainTitle = MYT_FARE_POINT_MSG.TPOINT; // TPT인 경우 T포인트
     } else {
-      mainTitle = MYT_FARE_POINT_MSG.RAINBOW;
+      mainTitle = MYT_FARE_POINT_MSG.RAINBOW; // 레인보우포인트
     }
 
     mainTitle += '<br />';
 
     if (type === 'auto') {
-      mainTitle += MYT_FARE_POINT_MSG.AUTO;
+      mainTitle += MYT_FARE_POINT_MSG.AUTO; // 자동납부
     } else if (type === 'change') {
-      mainTitle += MYT_FARE_POINT_MSG.CHANGE;
+      mainTitle += MYT_FARE_POINT_MSG.CHANGE; // 변경
     } else if (type === 'cancel') {
-      mainTitle += MYT_FARE_POINT_MSG.CANCEL;
+      mainTitle += MYT_FARE_POINT_MSG.CANCEL; // 해지
     } else {
-      mainTitle += MYT_FARE_POINT_MSG.RESERVATION;
+      mainTitle += MYT_FARE_POINT_MSG.RESERVATION; // 예약
     }
     return mainTitle;
   }
@@ -82,7 +83,7 @@ class MyTFareBillPointComplete extends TwViewController {
   private _getDescription(title: string, type: string, add: string): string {
     let description = '';
     if (!FormatHelper.isEmpty(add)) {
-      description = RAINBOW_FARE[add];
+      description = RAINBOW_FARE[add]; // 레인보우포인트인 경우 납부예약한 대상자 표시
     }
 
     if (title === 'rainbow' && type === 'auto') {
