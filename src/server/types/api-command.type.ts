a2167e613ef2e0af67d9305743e088bcd3709846
이정민 Sktechx <skt.p130713@partner.sk.com>
2019-03-19 19:30:53 +0900

    [이정민/SPTEK][DV001-16905] BE성능개선 대응(나의 데이터 통화 > 서브메인 충전/선물 이력 관련)

--- src/server/types/api-command.type.ts
+++ src/server/types/api-command.type.ts
@@ -401,6 +401,7 @@ export const API_CMD = {
   BFF_06_0074: { path: '/core-recharge/:version/tfamily-canceling', method: API_METHOD.POST, server: API_SERVER.BFF, bypass: true },
   BFF_06_0075: { path: '/core-bill/v1/pps-alram', method: API_METHOD.GET, server: API_SERVER.BFF, bypass: true },
   BFF_06_0076: { path: '/core-bill/v1/pps-alram', method: API_METHOD.DELETE, server: API_SERVER.BFF, bypass: true },
+  BFF_06_0077: { path: '/core-recharge/v1/refill-gift-history', method: API_METHOD.GET, server: API_SERVER.BFF, bypass: true },
 
   // PAYMENT
   BFF_07_0004: { path: '/core-bill/:version/cash-receipts-issue-history', method: API_METHOD.GET, server: API_SERVER.BFF, bypass: true },
