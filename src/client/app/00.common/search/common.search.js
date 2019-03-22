2fb30262d7675546d0a343a0d34ef0bb7987b18f
이현국 <P107003@partner.sk.com>
2019-03-22 13:20:15 +0900

    [ 이현국 / 파인원 ] GAPP DV001-18373 이슈 수정   /   DV001-17255  팝업 close 시 포커스 원래 요소로 이동하는 부분 예외 추가

--- src/client/app/00.common/search/common.search.js
+++ src/client/app/00.common/search/common.search.js
@@ -383,7 +383,7 @@ Tw.CommonSearch.prototype = {
       this._popupService.close();
       this.$container.find('.keyword-list-base').remove();
       this.$container.find('.fe-container-wrap').attr('aria-hidden',false);
-    },this),100);
+    },this));
   },
   _keywordListBaseClassCallback : function () {
     this._closeKeywordListBase();
