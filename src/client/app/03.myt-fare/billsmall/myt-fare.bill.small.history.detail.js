db1f93e65e8a22de46229272eb67ee7379dbcf45
이기림(Kirim Lee)/FE개발팀/SKP <kirim@sk.com>
2019-04-03 16:30:57 +0900

    [이기림/Client개발팀][DV001-15149] 소액결제 이용내역 인증 업무 추가 상세페이지 팝업으로 변경

--- src/client/app/03.myt-fare/billsmall/myt-fare.bill.small.history.detail.js
+++ src/client/app/03.myt-fare/billsmall/myt-fare.bill.small.history.detail.js
@@ -3,13 +3,13 @@
  * Author: Lee kirim (kirim@sk.com)
  * Date: 2018. 11. 29
  */
-Tw.MyTFareBillSmallHitstoryDetail = function (rootEl, data) {
+Tw.MyTFareBillSmallHitstoryDetail = function (rootEl, data, updateFunc) {
   this.$container = rootEl;
-  this.data = data ? JSON.parse(data) : '';
+  this.data = data;
+  this.updateFunc = updateFunc;
   this._apiService = Tw.Api;
   this._historyService = new Tw.HistoryService(rootEl);
   this._popupService = Tw.Popup;
-  this._params = Tw.UrlHelper.getQueryParams();
 
   this._cachedElement();
   this._init();
@@ -19,53 +19,37 @@ Tw.MyTFareBillSmallHitstoryDetail = function (rootEl, data) {
 Tw.MyTFareBillSmallHitstoryDetail.prototype = {
   _init: function() {
     this.rootPathName = this._historyService.pathname;
-
-    var renderedHTML;
-    
-    this.detailData = this.data; //JSON.parse(Tw.CommonHelper.getLocalStorage('detailData'));
-    
-    if(this.detailData){
-      renderedHTML = this.$template.$detailWrap(this.detailData);
-      this.$domWrapper.append(renderedHTML);
-    }
-    // 곧바로 상세 url를 치고 들어와서 조회된 데이터가 없을 시 
-    else{
-      renderedHTML = this.$template.$emptyContent({});
-      this.$domWrapper.after(renderedHTML);
-      this.$domWrapper.hide();
-    }
   },
 
   _cachedElement: function() {
-    this.$domWrapper = this.$container.find('#fe-detail-wrap'); 
-    this.$template = {
-      $detailWrap: Handlebars.compile($('#fe-detail-contents').html()),
-      $emptyContent: Handlebars.compile($('#fe-empty-contents').html())
-    };
+    this.$showBlockList = this.$container.find('#fe-btn-view-block'); // 차단내역 보기 버튼
+    this.$setBlock = this.$container.find('#fe-btn-set-block'); // 차단내역 보기 버튼
+
   },
 
   _bindEvent: function() {
     // 차단하기
-    this.$domWrapper.find('#fe-btn-set-block').on('click',$.proxy(this._setBillBlock, this));
+    this.$setBlock.on('click', $.proxy(this._setBillBlock, this));
   
     // 차단내역으로 이동
-    this.$domWrapper.find('#fe-btn-view-block').on('click',$.proxy(this._moveBlockList, this));
+    this.$showBlockList.on('click', $.proxy(this._moveBlockList, this));
   },
 
   _moveBlockList: function() {
-    this._historyService.goLoad('/myt-fare/bill/small/block');
+    this._historyService.replaceURL('/myt-fare/bill/small/block');
   },
 
   // 차단하기
-  _setBillBlock: function() {
+  _setBillBlock: function(e) {
     this._popupService.openModalTypeA(
       Tw.ALERT_MSG_MYT_FARE.ALERT_2_A94.TITLE,
       Tw.ALERT_MSG_MYT_FARE.ALERT_2_A94.MSG,
       Tw.ALERT_MSG_MYT_FARE.ALERT_2_A94.BUTTON,
       $.proxy(function(){},this),
       $.proxy(this._execBillBlock,this),
-      $.proxy(this._cancelBillBlock,this),
-      'confirmBlock'
+      null,
+      'confirmBlock',
+      $(e.currentTarget)
     );
     // this.detailData
   },
@@ -73,17 +57,13 @@ Tw.MyTFareBillSmallHitstoryDetail.prototype = {
   _execBillBlock: function() {
     this._popupService.close();
     this._apiService.request(Tw.API_CMD.BFF_05_0082, {
-      idPg: this.detailData.idpg,
-      tySvc: this.detailData.tySvc,
-      cpCode: this.detailData.cpCode,
+      idPg: this.data.idpg,
+      tySvc: this.data.tySvc,
+      cpCode: this.data.cpCode,
       state: 'A'
     })
         .done($.proxy(this._successBillBlock, this))
         .fail($.proxy(this._failBillBlock,this));
-  },
-
-  _cancelBillBlock: function() {
-    this._popupService.close();
   },
 
   _successBillBlock: function(resq) {
@@ -92,18 +72,21 @@ Tw.MyTFareBillSmallHitstoryDetail.prototype = {
       return Tw.Error(resq.code, resq.msg).pop();
     }
     // 차단완료
-    this._popupService.close();
     Tw.CommonHelper.toast(Tw.MYT_FARE_HISTORY_MICRO_BLOCK_TOAST.BLOCK);
-    // 
-    $.extend(this.detailData,{
+    
+    // view 
+    this.$setBlock.addClass('none').attr('aria-hidden', true)
+    this.$showBlockList.removeClass('none').attr('aria-hidden', false);
+    if (this.$container.find('.fe-block-state').length) {
+      this.$container.find('.fe-block-state').text(Tw.MYT_FARE_HISTORY_MICRO_BLOCK_TYPE.A1);
+    }
+
+    // data update
+    this.updateFunc(this.data.plainTime, this.data.listId, {
       cpState:'A1',
       blockState:Tw.MYT_FARE_HISTORY_MICRO_BLOCK_TYPE.A1,
       isBlocked:true
     });
-    // Tw.CommonHelper.setLocalStorage('detailData',JSON.stringify(this.detailData));
-
-    this.$domWrapper.empty().append(this.$template.$detailWrap(this.detailData));
-    this._bindEvent();
   },
 
   _failBillBlock: function(e) {
