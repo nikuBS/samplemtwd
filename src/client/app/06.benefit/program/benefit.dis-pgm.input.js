fc14a00c24f8120878f6b5ac42deb98ef3a78057
Hyeryoun Lee <ryoun80@gmail.com>
2019-04-25 18:26:59 +0900

    [이혜련/SPTEK][주석]무약정플랜

--- src/client/app/06.benefit/program/benefit.dis-pgm.input.js
+++ src/client/app/06.benefit/program/benefit.dis-pgm.input.js
@@ -3,7 +3,11 @@
  * @author Kim In Hwan (skt.P132150@partner.sk.com)
  * @since 2018.12.18
  */
-
+/**
+ * @class
+ * @desc 혜택할인 > 상품상세 > 무약정 가입
+ * @param {JSON} params
+ */
 Tw.BenefitDisPgmInput = function (rootEl, prodId, confirmOptions, selType) {
   this.$container = rootEl;
 
@@ -19,7 +23,10 @@ Tw.BenefitDisPgmInput = function (rootEl, prodId, confirmOptions, selType) {
 };
 
 Tw.BenefitDisPgmInput.prototype = {
-
+  /**
+   * @function
+   * @desc 초기설정
+   */
   _init: function () {
     if ( this._selType ) {
       var data = {};
@@ -38,12 +45,18 @@ Tw.BenefitDisPgmInput.prototype = {
     this._convConfirmOptions();
     this._bindEvent();
   },
-
+  /**
+   * @function
+   * @desc 바인드 이벤트
+   */
   _bindEvent: function () {
     // window 'env' 이벤트 발생시 페이지를 팝업으로 호출 - 상품 쪽과 동일
     $(window).on(Tw.INIT_COMPLETE, $.proxy(this._callConfirmCommonJs, this));
   },
-
+  /**
+   * @function
+   * @desc 정보확인 공통 실행
+   */
   _convConfirmOptions: function () {
     this._confirmOptions = $.extend(this._confirmOptions, {
       title: Tw.PRODUCT_TYPE_NM.JOIN,
@@ -61,7 +74,10 @@ Tw.BenefitDisPgmInput.prototype = {
       isJoinTermProducts: Tw.IGNORE_JOINTERM.indexOf(this._prodId) === -1
     });
   },
-
+  /**
+   * @function
+   * @desc 공통 정보확인 컴포넌트 호출 (isPopup false)
+   */
   _callConfirmCommonJs: function () {
     var options = $.extend(this._confirmOptions, {
       isSelectedProgram: true,
@@ -90,7 +106,10 @@ Tw.BenefitDisPgmInput.prototype = {
     }
     new Tw.ProductCommonConfirm(true, this.$container, options, $.proxy(this._prodConfirmOk, this));
   },
-
+  /**
+   * @function
+   * @desc 정보확인 콜백 시
+   */
   _prodConfirmOk: function () {
     Tw.CommonHelper.startLoading('.container', 'grey', true);
     if ( this._selType ) {
@@ -106,7 +125,11 @@ Tw.BenefitDisPgmInput.prototype = {
         .fail($.proxy(Tw.CommonHelper.endLoading('.container'), this));
     }
   },
-
+  /**
+   * @function
+   * @desc 가입하기 API 응답 처리
+   * @param resp - API 응답 값 확인 및 가입유도팝업 조회 API 요청
+   */
   _procJoinRes: function (resp) {
     Tw.CommonHelper.endLoading('.container');
     if ( resp.code !== Tw.API_CODE.CODE_00 ) {
@@ -115,7 +138,10 @@ Tw.BenefitDisPgmInput.prototype = {
     // DV001-16396 timer 삭제
     this._openSuccessPop();
   },
-
+  /**
+   * @function
+   * @desc 완료 팝업 실행
+   */
   _openSuccessPop: function () {
     this._popupService.open({
       hbs: 'complete_product',
@@ -134,25 +160,35 @@ Tw.BenefitDisPgmInput.prototype = {
 
     this._apiService.request(Tw.NODE_CMD.UPDATE_SVC, {});
   },
-
+  /**
+   * @function
+   * @desc 완료 팝업 이벤트 바인딩
+   * @param $popupContainer - 완료 팝업 컨테이너 레이어
+   */
   _bindJoinResPopup: function ($popupContainer) {
     $popupContainer.on('click','.btn-floating.tw-popup-closeBtn', $.proxy(this._closePop, this));
     $popupContainer.on('click', 'a', $.proxy(this._closeAndGo, this));
 
   },
-
+  /**
+   * @function
+   * @desc 완료 팝업 내 A 하이퍼링크 핸들링
+   * @param e - A 하이퍼링크 클릭 이벤트
+   */
   _closeAndGo: function (e) {
     e.preventDefault();
     e.stopPropagation();
 
     this._popupService.closeAllAndGo($(e.currentTarget).attr('href'));
   },
-
+  /**
+   * @function
+   * @desc 완료 팝업 내 닫기 버튼 클릭 시
+   */
   _closePop: function () {
     this._historyService.replaceURL('/product/callplan?prod_id='+this._prodId);
     this._popupService.closeAllAndGo('/product/callplan?prod_id='+this._prodId);
   },
-
   /**
    * @function
    * @desc hash 변경 시 호출
