37c3ca307b75ded5ea7ed15250da1dae32980ce5
조아라 SKtechx <araara.jo@sk.com>
2019-05-02 10:28:24 +0900

    [조아라/FE개발팀] 공통 component 주석 추가

--- src/client/component/line.component.js
+++ src/client/component/line.component.js
@@ -4,6 +4,11 @@
  * @since 2018.07.26
  */
 
+/**
+ * @class
+ * @desc 공통 > 회선변경
+ * @constructor
+ */
 Tw.LineComponent = function () {
   this._popupService = Tw.Popup;
   this._apiService = Tw.Api;
@@ -26,17 +31,40 @@ Tw.LineComponent = function () {
 };
 
 Tw.LineComponent.prototype = {
+  /**
+   * @member {object}
+   * @desc 에러코드
+   * @readonly
+   * @prop {string} BFF0012 고객비밀번호 인증이 필요합니다.
+   * @prop {string} BFF0013 고객비밀번호 잠김 해제가 필요합니다.
+   * @prop {string} BFF0014 고객비밀번호 설정이 필요합니다.
+   * @prop {string} ICAS3216 고객비밀번호 잠김
+   */
   ERROR_CODE: {
     BFF0012: 'BFF0012',    //	고객비밀번호 인증이 필요합니다.
     BFF0013: 'BFF0013',    //	고객비밀번호 잠김 해제가 필요합니다.
-    BFF0014: 'BFF0014',
+    BFF0014: 'BFF0014',    // 고객비밀번호 설정이 필요합니다.
     ICAS3216: 'ICAS3216'   // 고객비밀번호가 잠김
   },
+
+  /**
+   * @function
+   * @desc 회선 변경 레이어 팝업 오픈 요청
+   * @param selectedMgmt
+   * @param $target
+   */
   onClickLine: function (selectedMgmt, $target) {
     this._init(selectedMgmt);
     this._openLineList = true;
     this._getLineList($target);
   },
+
+  /**
+   * @function
+   * @desc 초기화
+   * @param selectedMgmt
+   * @private
+   */
   _init: function (selectedMgmt) {
     this._index = 0;
     this._selectedMgmt = selectedMgmt;
@@ -49,11 +77,26 @@ Tw.LineComponent.prototype = {
     this._svcMgmtNum = '';
     this._mdn = '';
   },
+
+  /**
+   * @function
+   * @desc 전체 회선 정보 요청
+   * @param $target
+   * @private
+   */
   _getLineList: function ($target) {
     this._apiService.request(Tw.NODE_CMD.GET_ALL_SVC, {})
       .done($.proxy(this._successGetLineList, this, $target))
       .fail($.proxy(this._failGetLineList, this));
   },
+
+  /**
+   * @function
+   * @desc 전체 회선 정보 응답 처리
+   * @param $target
+   * @param resp
+   * @private
+   */
   _successGetLineList: function ($target, resp) {
     if ( resp.code === Tw.API_CODE.CODE_00 ) {
       this._lineList = this._parseLineList(resp.result);
@@ -64,10 +107,25 @@ Tw.LineComponent.prototype = {
       Tw.Error(resp.code, resp.msg).pop();
     }
   },
+
+  /**
+   * @function
+   * @desc 전체 회선 정보 요청 실패 처리
+   * @param error
+   * @private
+   */
   _failGetLineList: function (error) {
     Tw.Logger.error(error);
     this._popupService.openAlert(Tw.TIMEOUT_ERROR_MSG);
   },
+
+  /**
+   * @function
+   * @desc 회선 변경 팝업 오픈
+   * @param lineData
+   * @param $target
+   * @private
+   */
   _openListPopup: function (lineData, $target) {
     this._popupService.open({
       hbs: 'actionsheet_line',
@@ -76,6 +134,13 @@ Tw.LineComponent.prototype = {
       btMore: this._index > Tw.DEFAULT_LIST_COUNT
     }, $.proxy(this._onOpenListPopup, this), $.proxy(this._onCloseListPopup, this), 'line', $target);
   },
+
+  /**
+   * @function
+   * @desc 회선 변경 팝업 오픈 콜백 (이벤트 바인딩)
+   * @param $popupContainer
+   * @private
+   */
   _onOpenListPopup: function ($popupContainer) {
     Tw.CommonHelper.focusOnActionSheet($popupContainer);
 
@@ -87,6 +152,11 @@ Tw.LineComponent.prototype = {
     $popupContainer.on('click', '#fe-bt-line', $.proxy(this._onClickLineButton, this));
   },
 
+  /**
+   * @function
+   * @desc 회선 변경 팝업 클로즈 콜백
+   * @private
+   */
   _onCloseListPopup: function () {
     if ( this._changeLine ) {
       this._completeLogin({ code: Tw.CALLBACK_CODE.SUCCESS });
@@ -94,9 +164,23 @@ Tw.LineComponent.prototype = {
       this._customerPwd.openLayer(this._mdn, this._svcMgmtNum, $.proxy(this._completeCustomerLogin, this));
     }
   },
+
+  /**
+   * @function
+   * @desc 팝업 클로즈 요청
+   * @private
+   */
   _closePopup: function () {
     this._popupService.close();
   },
+
+  /**
+   * @function
+   * @desc 회선 정보 파싱
+   * @param lineList
+   * @returns {Array}
+   * @private
+   */
   _parseLineList: function (lineList) {
     var category = ['MOBILE', 'INTERNET_PHONE_IPTV', 'SECURITY'];
     var result = [];
@@ -115,6 +199,14 @@ Tw.LineComponent.prototype = {
     }, this));
     return result;
   },
+
+  /**
+   * @desc 회선 데이터 화면에 나타내는 데이터로 변경
+   * @param lineData
+   * @param category
+   * @returns {Array}
+   * @private
+   */
   _convLineData: function (lineData, category) {
     var result = [];
     Tw.FormatHelper.sortObjArrAsc(lineData, 'expsSeq');
@@ -133,6 +225,13 @@ Tw.LineComponent.prototype = {
     }, this));
     return result;
   },
+
+  /**
+   * @function
+   * @desc 회선 선택 click event 처리
+   * @param $event
+   * @private
+   */
   _onSelectLine: function ($event) {
     var $selectedLine = $($event.currentTarget);
     var svcMgmtNum = $selectedLine.data('svcmgmtnum');
@@ -140,6 +239,14 @@ Tw.LineComponent.prototype = {
     this.changeLine(svcMgmtNum, mdn);
 
   },
+
+  /**
+   * @function
+   * @desc 회선 변경 요청
+   * @param svcMgmtNum
+   * @param mdn
+   * @param callback
+   */
   changeLine: function (svcMgmtNum, mdn, callback) {
     this._callback = callback;
     this._svcMgmtNum = svcMgmtNum;
@@ -149,9 +256,22 @@ Tw.LineComponent.prototype = {
       .done($.proxy(this._successChangeLine, this))
       .fail($.proxy(this._failChangeLine, this));
   },
+
+  /**
+   * @function
+   * @desc 회선 관리 버튼 click event 처리
+   * @private
+   */
   _onClickLineButton: function () {
     this._historyService.replaceURL('/common/member/line');
   },
+
+  /**
+   * @function
+   * @desc 회선 변경 응답 처리
+   * @param resp
+   * @private
+   */
   _successChangeLine: function (resp) {
     Tw.CommonHelper.endLoading('.container');
     if ( resp.code === Tw.API_CODE.CODE_00 ) {
@@ -182,11 +302,24 @@ Tw.LineComponent.prototype = {
       Tw.Error(resp.code, resp.msg).pop();
     }
   },
+
+  /**
+   * @function
+   * @desc 회선 변경 요청 실패 처리
+   * @param error
+   * @private
+   */
   _failChangeLine: function (error) {
     Tw.Logger.error(error);
     Tw.CommonHelper.endLoading('.container');
     this._popupService.openAlert(Tw.TIMEOUT_ERROR_MSG);
   },
+
+  /**
+   * @function
+   * @desc 회선 정보 더보기 버튼 click event 처리
+   * @private
+   */
   _onClickMore: function () {
     var $hideList = this.$list.filter('.none');
     var $showList = $hideList.filter(function (index) {
@@ -203,8 +336,14 @@ Tw.LineComponent.prototype = {
       this.$btMore.hide();
     }
   },
+
+  /**
+   * @function
+   * @desc 회선 변경 완료
+   * @param resp
+   * @private
+   */
   _completeLogin: function (resp) {
-    // Tw.CommonHelper.setLocalStorage(Tw.LSTORE_KEY.LINE_REFRESH, 'Y');
     if ( !Tw.FormatHelper.isEmpty(this._callback) ) {
       this._callback(resp);
     } else {
@@ -213,6 +352,13 @@ Tw.LineComponent.prototype = {
       }
     }
   },
+
+  /**
+   * @function
+   * @desc 고객보호비밀번호 완료 콜백
+   * @param resp
+   * @private
+   */
   _completeCustomerLogin: function (resp) {
     this._completeLogin(resp);
   }
