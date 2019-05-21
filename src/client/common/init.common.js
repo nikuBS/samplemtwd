c0bfef10bfb08fc3492faac85013626794dba4f6
김강타 <kangta3339@gimgangtaui-MacBook-Pro.local>
2019-05-21 15:48:09 +0900

    QA version up to QA_v5.53.0

--- src/client/common/init.common.js
+++ src/client/common/init.common.js
@@ -100,7 +100,7 @@ Tw.Init.prototype = {
 
       // Store tab height issue, toast popup blocks height calculation and scroll does not work properly
       if ( Tw.Environment.environment !== 'local' && Tw.Environment.environment !== 'prd' && /\/home/.test(location.href) ) {
-        Tw.Popup.toast('QA_v5.52.0');
+        Tw.Popup.toast('QA_v5.53.0');
       }
 
       this._initTrackerApi();
