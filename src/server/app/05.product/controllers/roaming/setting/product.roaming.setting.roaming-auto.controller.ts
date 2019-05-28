60fb71007649abaec07e44f076b214e1063f2254
김강타 <kangta3339@gimgangtaui-MacBook-Pro.local>
2019-05-28 17:39:15 +0900

    [OP002-812][FE][M] T로밍 > T로밍 요금제>T로밍 함께쓰기 3GB, 6GB

--- src/server/app/05.product/controllers/roaming/setting/product.roaming.setting.roaming-auto.controller.ts
+++ src/server/app/05.product/controllers/roaming/setting/product.roaming.setting.roaming-auto.controller.ts
@@ -29,6 +29,10 @@ class ProductRoamingSettingRoamingAuto extends TwViewController {
       });
     }
 
+    // T로밍 함께쓰기 6종 상품번호
+    const useTogether = ['NA00005690', 'NA00005691', 'NA00005692', 'NA00005693', 'NA00005694', 'NA00005695'];
+    const isUseTogether = useTogether.indexOf(prodId) > -1;
+
     Observable.combineLatest(
       this.apiService.request(API_CMD.BFF_10_0001, {}, {}, [prodId]),
       this.apiService.request(API_CMD.BFF_10_0091, {}, {}, [prodId])
@@ -52,7 +56,8 @@ class ProductRoamingSettingRoamingAuto extends TwViewController {
         prodBffInfo : prodBffInfo.result,
         prodId : prodId,
         expireDate : expireDate,
-        pageInfo : pageInfo
+        pageInfo : pageInfo,
+        isUseTogether : isUseTogether
       });
     });
 
