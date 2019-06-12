dfc3335b88cdbed56e03b8e4374dd283b6c839bd
권준호님/모바일 T월드 개발 <P120230@sktelecom.com>
2019-06-12 12:44:25 +0900

    [OP002-815] 오류수정

--- src/client/services/common/banner.service.js
+++ src/client/services/common/banner.service.js
@@ -112,15 +112,16 @@ Tw.BannerService.prototype = {
           new Tw.XtractorService(this.$banners, true);
         }
 
-        if(this._banners[0].isRolling){
+        if(this._banners[0].isRolling && this._banners.length > 1){
           //this.$banners.addClass('fe-banner-auto');
-          this.$banners.closest('widget-box').addClass('slider1-auto'); // 190610_추가
+          this.$banners.closest('.widget-box').addClass('slider1-auto').data("slider-auto", "true"); // 190610_추가
         }
         
 
         // set slick
-        if (this.$banners.closest('widget-box').hasClass('slider1-auto')) { // auto scrolling
-          this.$banners.slick({
+        var _this = this.$banners;
+        if (_this.closest('.widget-box').data('slider-auto')) { // auto scrolling
+          _this.slick({
             autoplay: true,
             autoplaySpeed: 4000,
             dots: this._banners.length !== 1,
@@ -160,7 +161,7 @@ Tw.BannerService.prototype = {
           // 190603 - 자동롤링 시 Play/Stop 버튼 기능 제공 END
 
         } else {
-          this.$banners.slick({
+          _this.slick({
             dots: this._banners.length !== 1,
             infinite: false,
             speed: 300,
