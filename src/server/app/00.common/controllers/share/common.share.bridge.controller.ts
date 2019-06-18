33d1a249584008f4c5e2f0e4830cabc9bdb033bf
이전일 SKCC <jeonil.lee@sk.com>
2019-06-18 15:35:08 +0900

    OP002-1483 바로가기 URL 수집 개선

--- src/server/app/00.common/controllers/share/common.share.bridge.controller.ts
+++ src/server/app/00.common/controllers/share/common.share.bridge.controller.ts
@@ -8,7 +8,6 @@ import TwViewController from '../../../../common/controllers/tw.view.controller'
 import { NextFunction, Request, Response } from 'express';
 import FormatHelper from '../../../../utils/format.helper';
 import BrowserHelper from '../../../../utils/browser.helper';
-
 /**
  * @desc App 안내화면 초기화를 위한 class
  */
@@ -30,8 +29,9 @@ class CommonShareBridge extends TwViewController {
   render(req: Request, res: Response, next: NextFunction, svcInfo: any, allSvc: any, childInfo: any, pageInfo: any) {
     const target = req.query.target;
     const loginType = req.query.loginType;
+    const referer = req.query.referer;
 
-    res.render('share/common.share.bridge.html', { isAndroid: BrowserHelper.isAndroid(req), target, loginType, pageInfo });
+    res.render('share/common.share.bridge.html', { isAndroid: BrowserHelper.isAndroid(req), target, loginType, referer, pageInfo });
   }
 }
 export default CommonShareBridge;
