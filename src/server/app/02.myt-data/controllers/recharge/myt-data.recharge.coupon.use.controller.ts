9a3aba93696281d0a038881b3d5b3a1e09593abc
이정민 Sktechx <skt.p130713@partner.sk.com>
2019-06-19 13:41:00 +0900

    [이정민/SPTEK][OP002-1678] 나의데이터통화 > 리필쿠폰사용 > tip 조정

--- src/server/app/02.myt-data/controllers/recharge/myt-data.recharge.coupon.use.controller.ts
+++ src/server/app/02.myt-data/controllers/recharge/myt-data.recharge.coupon.use.controller.ts
@@ -54,6 +54,7 @@ export default class MyTDataRechargeCouponUse extends TwViewController {
     ['NA00006403', 20], // 5gx standard
     ['NA00006404', 30], // 5gx prime
     ['NA00006405', 50], // 5gx platinum
+    ['NA00006534', 1.5], // T플랜 세이브
     ['NA00006535', 2.5], // T플랜 안심2.5G
     ['NA00006536', 4],  // T플랜 안심4G
     ['NA00006537', 15], // T플랜 에센스
@@ -236,8 +237,8 @@ export default class MyTDataRechargeCouponUse extends TwViewController {
               plan === 'NA00006537' || plan === 'NA00006538') { // Tplan large/family의 경우 '최대' 삽입, 0Plan large, T플랜 에센스, T플랜 스페셜 포함
             option.qttText = '최대 ' + option.qttText;
             option.copnDtlClNm = option.copnDtlClNm.replace('100%', '');
+            option.isTplan = true;
           }
-          option.isTplan = true;
           return option;
         }
       } else if (this.planAdaptive[plan]) {
