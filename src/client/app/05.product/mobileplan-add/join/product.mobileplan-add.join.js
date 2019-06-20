1f4f0f392f6348fbf4a896e5c4451faf04836562
김강타 <kangta3339@gimgangtaui-MacBook-Pro.local>
2019-06-20 20:35:31 +0900

    [OP002-435][FE][W-1903-019-00] T월드 내 분실안심990 가입/해지 적용 - SMS API 상품코드 변경

--- src/client/app/05.product/mobileplan-add/join/product.mobileplan-add.join.js
+++ src/client/app/05.product/mobileplan-add/join/product.mobileplan-add.join.js
@@ -135,11 +135,11 @@ Tw.ProductMobileplanAddJoin.prototype = {
       return Tw.Error(resp.code, resp.msg).pop();
     }
 
-    // Swing 문자 발송 API 호출 (분실안심990_예약)
+    // Swing 문자 발송 API 호출 (분실안심990_예약 가입 시)
     if ( this._prodId == 'NA00006395') {
       this._apiService.request(Tw.API_CMD.BFF_10_0181, {
         smsPhrsGrpId: 'SMART'
-      }, {}, [this._prodId]);
+      }, {}, ['NA00006397']); // SMS 발송 API 파라미터는 본상품 코드로 호출
     }
     
     this._popupService.close();
