3a331cf11471a0529a1da97e5326b66add5f5157
야만인 <yamanin@gwonjunhoui-MacBookPro.local>
2019-07-29 17:46:50 +0900

    [OP002-2224][마케팅] [FE] (W-1907-043-01) 로밍 유력상품 baro 직가입 유도

---
+++ src/server/app/05.product/controllers/roaming/product.roaming.event.right-roam.controller.ts
@@ -0,0 +1,15 @@
+/**
+ * MenuName: T로밍 > 로로밍 상품 이용 안내 (RM_17)
+ * @file product.roaming.fee-info.controller
+ * @author Hyeryoun Lee
+ * @since 2019. 5. 20.
+ */
+import TwViewController from '../../../../common/controllers/tw.view.controller';
+import { Request, Response, NextFunction } from 'express';
+
+export default class ProductRoamingEventRightRoam extends TwViewController {
+
+  render(req: Request, res: Response, _next: NextFunction, svcInfo: any, _allSvc: any, _childInfo: any, pageInfo: any) {
+    res.render('roaming/product.roaming.event.right-roam.html', { svcInfo , pageInfo});
+  }
+}
