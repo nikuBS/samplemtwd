9b5b3a0428a2dcf367affe0f24f5a69fea3b74ab
Hyeryoun Lee <ryoun80@gmail.com>
2019-08-06 10:06:11 +0900

    [이혜련/SPTEK][OP002-967](W-1905-179-01) 신규 모바일앱 유선서비스 번호변경 기능 추가 (임시저장)

--- src/server/app/04.myt-join/myt-join.submain.controller.ts
+++ src/server/app/04.myt-join/myt-join.submain.controller.ts
@@ -290,7 +290,7 @@ class MyTJoinSubmainController extends TwViewController {
         break;
       case 'S1':
       case 'S2':
-      case 'S3':
+      case 'S3': // 집전화
         // 유선
         this.type = 2;
         break;
