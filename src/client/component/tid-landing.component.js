e279917319f16487065c5549f6c457ae60ba9a89
P120790 <P120790@partner.sk.com>
2019-08-09 09:17:13 +0900

    Revert "OP002-3081 : (W-1907-149-01) iOS 스토어 심사대비 회원가입 URL 수정(원복)"
    This reverts commit 1566f8dc81e92fa098d15ac7071b69c280cac612.

--- src/client/component/tid-landing.component.js
+++ src/client/component/tid-landing.component.js
@@ -195,13 +195,7 @@ Tw.TidLandingComponent.prototype = {
    * @desc 회원가입 요청
    */
   goSignup: function () {
-    // iOS 스토어 심사대비 회원가입 URL을 out link로 연결되도록 수정(심사 후 원복 예정)
-    if ( Tw.BrowserHelper.isApp() && Tw.BrowserHelper.isIos()) {
-      Tw.CommonHelper.openUrlExternal('https://m.tworld.co.kr/common/member/signup-guide');
-    } else {
-      this._goLoad(Tw.NTV_CMD.SIGN_UP, '/common/member/signup-guide', $.proxy(this._onNativeSignup, this));
-    }
-    // this._goLoad(Tw.NTV_CMD.SIGN_UP, '/common/member/signup-guide', $.proxy(this._onNativeSignup, this));
+    this._goLoad(Tw.NTV_CMD.SIGN_UP, '/common/member/signup-guide', $.proxy(this._onNativeSignup, this));
   },
 
   /**
