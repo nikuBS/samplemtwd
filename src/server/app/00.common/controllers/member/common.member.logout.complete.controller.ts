d74e4c45acd56ca47e2bd0fb75ed33d3f7392087
P120790 <P120790@partner.sk.com>
2019-08-20 14:35:14 +0900

    OP002-3232 : [FE] 회선정보 변경 시 Xtractor용 cookie 업데이트

--- src/server/app/00.common/controllers/member/common.member.logout.complete.controller.ts
+++ src/server/app/00.common/controllers/member/common.member.logout.complete.controller.ts
@@ -27,7 +27,7 @@ class CommonMemberLogoutComplete extends TwViewController {
    * @param pageInfo
    */
   render(req: Request, res: Response, next: NextFunction, svcInfo: any, allSvc: any, childInfo: any, pageInfo: any) {
-    this.loginService.sessionGenerate(req).subscribe(() => {
+    this.loginService.sessionGenerate(req, res).subscribe(() => {
       this.logger.info(this, this.loginService.getSessionId(req));
       res.render('member/common.member.logout.complete.html', { svcInfo, pageInfo });
     });
