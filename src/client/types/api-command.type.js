2e5ea05eb088f37e093a8a6977b79ab1b84d86df
P120790 <P120790@partner.sk.com>
2019-10-04 16:08:56 +0900

    Revert "OP002-4171 : [FE] [자경단 대응] TID SSO 로그인 시 보안취약점 개선-3"
    This reverts commit 4637b5f519d69654993ceb548a90604eb92e9465.

--- src/client/types/api-command.type.js
+++ src/client/types/api-command.type.js
@@ -692,7 +692,6 @@ Tw.API_CODE = {
   NODE_1003: '1003',  // 처리 완료
   NODE_1004: '1004',  // 세션 만료
   NODE_1005: '1005',  // 세션 변경
-  NODE_1006: '1006',  // 잘못된 redirect 경로(parameter : target)
 
   NOT_FAMILY: 'ZORDE4011', // 쿠폰 선물 가능한 가족그룹 아님
   RECEIVER_LIMIT: 'ZNGME0000', // 쿠폰 받는 사람 횟수 초과
--- src/server/services/error.service.ts
+++ src/server/services/error.service.ts
@@ -1,9 +1,6 @@
 import { Response } from 'express';
 import { API_CODE } from '../types/api-command.type';
 import FormatHelper from '../utils/format.helper';
-import RedisService from '../services/redis.service';
-import { REDIS_KEY } from '../types/redis.type';
-import { TARGET_PARAM_VALIDATION_CHECK_URL } from '../types/url.type';
 
 interface ErrorOptions {
   title?: string;
@@ -16,7 +13,6 @@ interface ErrorOptions {
 
 class ErrorService {
   static instance;
-  private redisService: RedisService = RedisService.getInstance();
 
   constructor() {
     if ( ErrorService.instance ) {
@@ -71,53 +67,6 @@ class ErrorService {
     });
 
     return errorResult;
-  }
-
-  /**
-   * target Parameter의 유효성을 체크하는 middlewware
-   * @param req
-   */
-  public targetValidationCheck(req: any, res: any, next: any) {
-    let target = decodeURIComponent(req.query.target || '');
-    const path = req.path || '';
-    const domain = req.protocol + '://' + req.headers.host;
-    const unnecessaryTargetStr = '_type_back_state';
-
-    // 요청 type이 html이고, target parameter이 존재하고, check URL인 경우만 유효성을 체크한다..
-    if (req.accepts(['html', 'json']) === 'html' && !FormatHelper.isEmpty(target)) {
-
-      // 유효성 체크 대상 path가 아닐 경우는 skip
-      if (TARGET_PARAM_VALIDATION_CHECK_URL.some(url => (path === url)) === false) {
-        next();
-      }
-
-      // "_type_back_state"의 앞까지만 target로 인식
-      const redisService = ErrorService.instance.redisService;
-      if (target.indexOf(unnecessaryTargetStr) !== -1) {
-        target = target.substring(0, target.indexOf('_type_back_state'));
-      }
-
-      // target에서 parameter 부분 제거
-      if (target.indexOf('?') !== -1) {
-        target = target.substring(0, target.indexOf('?'));
-      }
-
-      // 유효성 체크 대상인 경우는 redis에서 target parameter의 정보 확인
-      redisService.getData(REDIS_KEY.URL_META + target.replace(domain, '')).subscribe((resp) => {
-        if (resp.code !== API_CODE.REDIS_SUCCESS ) {
-          res.render('error.server-error.html', {
-                svcInfo: null, 
-                code: API_CODE.NODE_1006, 
-                isBackCheck: false,
-                msg: '잘못된 접근 경로 입니다.'
-          });
-        } else {
-          next();
-        }
-      });
-    } else {
-      next();
-    }
   }
 }
 
