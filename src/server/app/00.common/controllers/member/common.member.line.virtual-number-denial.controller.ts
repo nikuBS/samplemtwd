eac9303e72a3f3174aa2273786b9231b7134551d
P120790 <P120790@partner.sk.com>
2019-11-22 16:29:38 +0900

    OP002-5352 : [FE][콘텐츠] (W-1910-011-00) '휴대전화 가상번호 시행에 따른 이용자고지' 공지사항 등록 요청(원복)

--- src/server/app/00.common/controllers/member/common.member.line.virtual-number-denial.controller.ts
+++ src/server/app/00.common/controllers/member/common.member.line.virtual-number-denial.controller.ts
@@ -35,16 +35,8 @@ class CommonMemberLineVirtualNumberDenial extends TwViewController {
     // 부가서비스 상품 유형에 따른 가입 가능한 회선 조회
     const allowedSvcAttrInfo: any = ProductHelper.getAllowedSvcAttrCd('C');
 
-    // R : 일반 법인 이동전화, D : SKT 법인 이동전화, E : SWG기준 실사용자 등록된 법인 이동전화
-    const allowdSvcGr = ['A', 'Y'];
-
     // 모바일회선 중 svcAttrCd 적합하는 회선만 추출
-    const selectedLines = allSvc[allowedSvcAttrInfo.group].filter((lineInfo) => 
-      (
-        allowedSvcAttrInfo.svcAttrCds.indexOf(lineInfo.svcAttrCd) !== -1
-        && allowdSvcGr.indexOf(lineInfo.svcGr) !== -1
-      )
-    );
+    const selectedLines = allSvc[allowedSvcAttrInfo.group].filter((lineInfo) => allowedSvcAttrInfo.svcAttrCds.indexOf(lineInfo.svcAttrCd) !== -1);
 
     if (FormatHelper.isEmpty(selectedLines)) {
       this.error.render(res, {
