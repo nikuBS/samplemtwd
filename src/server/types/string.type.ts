aa8d6ab55e9392edbf22f7df9fac57fc85a23923
Kangta Kim <kangta.kim@sktelecom.com>
2019-12-11 21:58:17 +0900

    [OP002-5559][FE]위젯 표기용 잔여량 데이터 정제 로직 이관(Native → FE) - 1차 Commit

--- src/server/types/string.type.ts
+++ src/server/types/string.type.ts
@@ -17,7 +17,9 @@ export const UNLIMIT_NAME = {
   '1': '무제한',
   B: '기본제공',
   M: '무제한',
-  REMAIN: '남음'
+  REMAIN: '남음',
+  WIDGET_UNLIMIT: '무제한', // 위젯 내 '무제한' 표기
+  WIDGET_UNLIMIT_DEFAULT: '기본제공' // 위젯 내 '기본제공' 표기 (무제한 표기 방식의 일종)
 };
 
 // 요금 안내서 설정 > 안내서 유형(복합은 컨트롤러에서 만들고 단수만 표현한다)
@@ -1458,3 +1460,7 @@ export const NODE_ERROR_MSG = {
   '1006': '잘못된 접근 경로 입니다.',
   '1007': '가입된 모바일 회선이 존재하지 않습니다.'
 };
+
+export const WIDGET_REMAINS_CODE = {
+  DATA_SUM: 'DATA_SUM'
+}
