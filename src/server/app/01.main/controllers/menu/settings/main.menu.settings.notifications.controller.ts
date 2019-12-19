c0a9305bda0e2dbdcb8c2ae0ad544a25da736c0e
P120790 <P120790@partner.sk.com>
2019-12-19 17:29:17 +0900

    OP002-5918 : [FE] (W-1912-041-01) 14세 미만 미성년자 T ID 기반 서비스 가입 시 법정대리인 동의절차 조치 안내

--- src/server/app/01.main/controllers/menu/settings/main.menu.settings.notifications.controller.ts
+++ src/server/app/01.main/controllers/menu/settings/main.menu.settings.notifications.controller.ts
@@ -9,33 +9,42 @@ import TwViewController from '../../../../../common/controllers/tw.view.controll
 import { API_CMD, API_CODE } from '../../../../../types/api-command.type';
 import { Observable } from 'rxjs/Observable';
 import FormatHelper from '../../../../../utils/format.helper';
+import { NODE_ERROR_MSG } from '../../../../../types/string.type'; 
 
 export default class MainMenuSettingsNotifications extends TwViewController {
 
   render(req: Request, res: Response, next: NextFunction, svcInfo: any,
          allSvc: any, childInfo: any, pageInfo: any) {
-    Observable.combineLatest(
-      this.checkNotiAgreedInfo(res, svcInfo, pageInfo),
-      this.checkTworldAgreedInfo(res, svcInfo, pageInfo)
-    ).subscribe(
-      ([respNoti, respTworld]) => {
-        if (FormatHelper.isEmpty(respNoti) || FormatHelper.isEmpty(respTworld)) {
-          return;
-        }
 
-        res.render('menu/settings/main.menu.settings.notifications.html', {
-          svcInfo,
-          pageInfo,
-          isServiceOn: respNoti.tnotiInfoRcvAgreeYn === 'Y' ? true : false,
-          isRecommendOn: respNoti.tnotiMrktRcvAgreeYn === 'Y' ? true : false,
-          isTplaceOn: respNoti.tplaceUseAgreeYn && respNoti.tplaceUseAgreeYn === 'Y' ? true : false,
-          isAdOn: respTworld.twdAdRcvAgreeYn === 'Y' ? true : false,
-          isPrivateInfoOn: respTworld.twdInfoRcvAgreeYn === 'Y' ? true : false,
-          isLocationOn: respTworld.twdLocUseAgreeYn === 'Y' ? true : false
-        });
-      },
-      (err) => this.showError(res, svcInfo, pageInfo, err.code, err.msg)
-    );
+    // 14세 미만 체크
+    this.checkIsOverFourteen(res, svcInfo, pageInfo).subscribe((isOverFourteen) => {
+      if (isOverFourteen) {
+        Observable.combineLatest(
+          this.checkNotiAgreedInfo(res, svcInfo, pageInfo),
+          this.checkTworldAgreedInfo(res, svcInfo, pageInfo)
+        ).subscribe(
+          ([respNoti, respTworld]) => {
+            if (FormatHelper.isEmpty(respNoti) || FormatHelper.isEmpty(respTworld)) {
+              return;
+            }
+    
+            res.render('menu/settings/main.menu.settings.notifications.html', {
+              svcInfo,
+              pageInfo,
+              isServiceOn: respNoti.tnotiInfoRcvAgreeYn === 'Y' ? true : false,
+              isRecommendOn: respNoti.tnotiMrktRcvAgreeYn === 'Y' ? true : false,
+              isTplaceOn: respNoti.tplaceUseAgreeYn && respNoti.tplaceUseAgreeYn === 'Y' ? true : false,
+              isAdOn: respTworld.twdAdRcvAgreeYn === 'Y' ? true : false,
+              isPrivateInfoOn: respTworld.twdInfoRcvAgreeYn === 'Y' ? true : false,
+              isLocationOn: respTworld.twdLocUseAgreeYn === 'Y' ? true : false
+            });
+          },
+          (err) => this.showError(res, svcInfo, pageInfo, err.code, err.msg)
+        );
+      } else {
+        this.showError(res, svcInfo, pageInfo, API_CODE.NODE_1006, NODE_ERROR_MSG[API_CODE.NODE_1008]);
+      }
+    });
   }
 
   /**
@@ -71,6 +80,24 @@ export default class MainMenuSettingsNotifications extends TwViewController {
       }
       this.showError(res, svcInfo, pageInfo, resp.code, resp.msg);
       return null;
+    });
+  }
+
+  /**
+   * @function
+   * @desc 14세 이상 확인
+   * @param  {Response} res - Response
+   * @param  {any} svcInfo - 사용자 정보
+   * @param  {any} pageInfo - page 정보
+   * @returns Observable BFF로 조회된 결과를 Observable로 return
+   */
+  private checkIsOverFourteen(res: Response, svcInfo: any, pageInfo: any): Observable<any> {
+    return this.apiService.request(API_CMD.BFF_08_0080, {/*mbrChlId : svcInfo.mbrChlId*/}).map((resp) => {
+      if (resp.code === API_CODE.CODE_00) {
+        return resp.result.age >= 14 ? true : false;
+      }
+      this.showError(res, svcInfo, pageInfo, resp.code, resp.msg);
+      return false;
     });
   }
 
