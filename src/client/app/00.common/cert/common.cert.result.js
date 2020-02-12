ad9b39028f5d01b378de2cabd6d7e6bf4793fb41
P120790 <P120790@partner.sk.com>
2020-02-12 18:10:13 +0900

    Revert "OP002-6702 : [FE] [자체개선][NICE평가정보] Chrome 80 정책 변경으로 인한 조치사항 적용-debugging-2"
    This reverts commit 5a699c404afa93cd67c3584c80b19c56e3911369.

--- src/client/app/00.common/cert/common.cert.result.js
+++ src/client/app/00.common/cert/common.cert.result.js
@@ -44,11 +44,6 @@ Tw.CommonCertResult.prototype = {
    * @private
    */
   _closeWeb: function (code, msg) {
-    Tw.Logger.info(window.opener);
-    if(Tw.FormatHelper.isEmpty(window.opener)) {
-      alert('==== window.opener is null or undefined ====');
-    }
-    
     if ( code === Tw.API_CODE.CODE_00 ) {
       if ( this._target === Tw.AUTH_CERTIFICATION_METHOD.PASSWORD ) {
         window.opener.onPopupCallbackPassword({ code: code, msg: msg });
@@ -57,9 +52,7 @@ Tw.CommonCertResult.prototype = {
       }
       window.close();
     } else {
-      // for test
-      window.opener.onPopupCallback({ code: code, msg: msg });
-      // Tw.Error(code, msg).page();
+      Tw.Error(code, msg).page();
     }
   },
 
