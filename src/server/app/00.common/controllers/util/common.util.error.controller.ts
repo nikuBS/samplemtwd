09e4add15bb78427866ae5a9b671c6dc60a50a73
양정규SPTek <skt.p130715@partner.sk.com>
2020-04-07 11:32:23 +0900

    OP002-7800: [FE] [myT] (W-2003-048-01) 미성년자 콘텐츠 이용요금 조회 제한 해제 요청-장애처리 What: Why: Task-Url:

--- src/server/app/00.common/controllers/util/common.util.error.controller.ts
+++ src/server/app/00.common/controllers/util/common.util.error.controller.ts
@@ -16,7 +16,8 @@ class CommonError extends TwViewController {
     const code = req.query.code || '',
       msg = req.query.msg || '',
       subMsg = req.query.subMsg || '',
-      isPopupCheck = !!req.query.isPopupCheck || false;
+      // 20.4.7 양정규 : query가 String으로 오기때문에 isPopupCheck 를 String값으로 비교하는걸로 바꿔줌.
+      isPopupCheck = req.query.isPopupCheck && req.query.isPopupCheck === 'true';
 
     this.error.render(res, {
       code: code,
