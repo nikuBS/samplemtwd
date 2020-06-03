6a75cc531e1e71f39f95a5f4daa433e00f75d0f2
양정규SPTek <skt.p130715@partner.sk.com>
2020-06-03 17:59:08 +0900

    OP002-8414: [myT] (W-2004-128-03) 휴대폰결제 한도 상향(2020.6)에 따른 T월드 App. 및 WEB 변경 요청 What: 컨펌 팝업 디자인 변경 Why: Task-Url:

--- src/client/types/string.type.js
+++ src/client/types/string.type.js
@@ -717,7 +717,10 @@ Tw.ALERT_MSG_MYT_FARE = {
   ALERT_2_A93: { TITLE: '알림', MSG: 'OK캐쉬백 포인트 납부 예약이 취소되었습니다.' },
   ALERT_2_A94: { TITLE: '자동결제를 차단하시겠습니까?', MSG: '차단은 다음달에 적용 됩니다.', BUTTON: '차단하기' },
   ALERT_2_A95: { TITLE: '자동결제를 해제하시겠습니까?', MSG: '해제는 다음달에 적용 됩니다.', BUTTON: '해제하기' },
-  ALERT_2_A96: { TITLE: '이용 한도를 변경하시겠습니까?', MSG: '이용 한도 변경은 월 1회만 하실 수 있습니다.</br> 이용 한도를 변경하면 <button class="bt-link-tx underline fe-open-pop">휴대폰 결제 이용약관</button> 에 재동의하는 것으로 간주됩니다.', BUTTON: '변경하기' },
+  ALERT_2_A96: { TITLE: '이용 한도를 변경하시겠습니까?',
+    MSG: '이용 한도 변경은 월 1회만 하실 수 있습니다.</br></br> 이용 한도를 변경하면 휴대폰 결제 이용약관에 재동의하는 것으로 간주됩니다.',
+    TERMS: '이용약관 보기'
+  },
   ALERT_2_A100: { TITLE: '선결제를 종료하시겠습니까?', MSG: '선결제가 종료됩니다.', BUTTON: '종료하기' },
   ALERT_2_A101: { TITLE: '요금납부를 종료하시겠습니까?', MSG: '요금납부가 종료됩니다.', BUTTON: '종료하기' },
   ALERT_2_DATA: { TITLE: '데이터 충전을 종료하시겠습니까?', MSG: '데이터 충전이 종료됩니다.', BUTTON: '종료하기' }
