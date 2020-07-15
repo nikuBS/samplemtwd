6dfd9e4a664d9a0204764503abc823fdefb8539f
p151541 <jobkim0410@gmail.com>
2020-07-15 11:06:23 +0900

    dockerFile edit

--- src/server/app/00.common/controllers/share/common.share.bridge.controller.ts
+++ src/server/app/00.common/controllers/share/common.share.bridge.controller.ts
@@ -10,8 +10,7 @@ import FormatHelper from '../../../../utils/format.helper';
 import BrowserHelper from '../../../../utils/browser.helper';
 
 import request from 'request';
-// import cheerio from 'cheerio';
-// import client from 'cheerio-httpcli';
+import client from 'cheerio-httpcli';
 
 
 /**
@@ -37,22 +36,22 @@ class CommonShareBridge extends TwViewController {
     const loginType = req.query.loginType;
     const referer = req.query.referer;
 
-    // client.fetch(`http://${req['headers']['host']}${req.query['target']}`, {}, function (err, $, response, body) {
-    //   let title = $("meta[property='og:title']").attr('content')
-    //   let description = $("meta[property='og:description']").attr('content')
-    //   res.render('share/common.share.bridge.html', { 
-    //     isAndroid: BrowserHelper.isAndroid(req), 
-    //     target, 
-    //     loginType, 
-    //     referer, 
-    //     pageInfo,
-    //     ogTitle: title,
-    //     ogDesc: description
-    //   });
+    client.fetch(`http://${req['headers']['host']}${req.query['target']}`, {}, function (err, $, response, body) {
+      let title = $("meta[property='og:title']").attr('content')
+      let description = $("meta[property='og:description']").attr('content')
+      res.render('share/common.share.bridge.html', { 
+        isAndroid: BrowserHelper.isAndroid(req), 
+        target, 
+        loginType, 
+        referer, 
+        pageInfo,
+        ogTitle: title,
+        ogDesc: description
+      });
 
-    // });
-    const ogDesc = '';
-    res.render('share/common.share.bridge.html', { isAndroid: BrowserHelper.isAndroid(req), target, loginType, referer, pageInfo, ogDesc });
+    });
+    // const ogDesc = '';
+    // res.render('share/common.share.bridge.html', { isAndroid: BrowserHelper.isAndroid(req), target, loginType, referer, pageInfo, ogDesc });
   }
 }
 export default CommonShareBridge;
