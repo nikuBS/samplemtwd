e283f81de33d435235d100255207264d47b66137
Jang-Ho Hwang <rath@xrath.com>
2020-08-24 04:21:24 +0900

    로밍모드 메인, 앱바 스크롤 효과 구현 및 Controller 연동

--- src/server/app/05.product/controllers/roaming-next/roaming.on.ts
+++ src/server/app/05.product/controllers/roaming-next/roaming.on.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 import { API_CMD, API_CODE } from '../../../../types/api-command.type';
 import { Observable } from 'rxjs/Observable';
 import FormatHelper from '../../../../utils/format.helper';
+import EnvHelper from '../../../../utils/env.helper';
 
 export default class RoamingOnController extends TwViewController {
   render(req: Request, res: Response, next: NextFunction, svcInfo: any, allSvc: any, childInfo: any, pageInfo: any) {
@@ -10,10 +11,19 @@ export default class RoamingOnController extends TwViewController {
     // if (isLogin) {
     //   this.renderAnonymous(res, svcInfo, pageInfo);
     // }
+
+    const CDN = EnvHelper.getEnvironment('CDN');
     res.render('roaming-next/roaming.on.html', {
       svcInfo,
       pageInfo,
       isLogin: isLogin,
+      country: {
+        code: 'AUS',
+        name: '호주',
+        timezoneOffset: -8,
+        flagUrl: `${CDN}/img/product/roam/flag_aus.png`,
+        backgroundUrl: `${CDN}/img/product/roam/background_aus.png`,
+      }
     });
   }
 
