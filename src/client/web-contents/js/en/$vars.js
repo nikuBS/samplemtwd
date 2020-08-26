34f7137afb12aef49d2005ce282b2c53c55bae71
mtwdevuser <mtwdevuser@partner.sk.com>
2020-08-26 15:55:26 +0900

    Web Contents Merge FROM 39f20d9ba1e

---
+++ src/client/web-contents/js/en/$vars.js
@@ -0,0 +1,12 @@
+/* jshint ignore:start */
+var skt_landing = {},
+	frontend_fn = {
+	},
+	component_list = [],
+	widget_list = [],
+	resize_fn = [], //resize시 사용
+	scroll_fn = [], //scroll시 사용
+	wheel_fn = []; //wheel시 사용
+$(document).on("ready", function () {
+});
+/* jshint ignore:end */
