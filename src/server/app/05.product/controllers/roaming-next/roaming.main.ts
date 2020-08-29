bf85d105e00898620609308e8d0047af93936f87
Jang-Ho Hwang <rath@xrath.com>
2020-08-30 03:38:23 +0900

    로밍메인, 인기있는여행지 Redis 데이터 연동
    countryCode 누락됨. mblBtnImgId URL 만드는 방법을 알 수 없음

--- src/server/app/05.product/controllers/roaming-next/roaming.main.ts
+++ src/server/app/05.product/controllers/roaming-next/roaming.main.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 import { API_CMD, API_CODE } from '../../../../types/api-command.type';
 import { Observable } from 'rxjs/Observable';
 import FormatHelper from '../../../../utils/format.helper';
+import {REDIS_KEY} from '../../../../types/redis.type';
 
 export default class RoamingMainController extends TwViewController {
   render(req: Request, res: Response, next: NextFunction, svcInfo: any, allSvc: any, childInfo: any, pageInfo: any) {
@@ -10,10 +11,35 @@ export default class RoamingMainController extends TwViewController {
     // if (isLogin) {
     //   this.renderAnonymous(res, svcInfo, pageInfo);
     // }
-    res.render('roaming-next/roaming.main.html', {
-      svcInfo,
-      pageInfo,
-      isLogin: isLogin,
+    Observable.combineLatest(
+      this.getPopularNations(),
+      this.redisService.getData(REDIS_KEY.ROAMING_NATIONS_BY_CONTINENT + ':AFR'),
+    ).subscribe(([popularNations, continents]) => {
+
+      if (popularNations.length > 6) {
+        popularNations = popularNations.slice(0, 6);
+      }
+      // popularNations = popularNations.filter(n => n.mblBtnImgId != null);
+      res.render('roaming-next/roaming.main.html', {
+        svcInfo,
+        pageInfo,
+        isLogin: isLogin,
+        popularNations,
+      });
+    });
+  }
+
+  private getPopularNations(): Observable<any> {
+    return this.redisService.getData(REDIS_KEY.ROAMING_POPULAR_NATIONS).map(resp => {
+      // countryCode
+      // countryNameEng
+      // countryNameKor
+      // expsSeq
+      // mblBtnImgId
+      // mblBtnImgAltCtt
+      return resp.result.roamingPopularNationList.sort((o1, o2) => {
+        return parseInt(o1.expsSeq, 10) - parseInt(o2.expsSeq, 10);
+      });
     });
   }
 }
