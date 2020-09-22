0350f10da979f015472fdb68f51b15731e403a57
김용연 SKCC <jobkim0410@partner.sk.com>
2020-09-22 10:01:18 +0900

    version edit

--- src/server/config/version.config.ts
+++ src/server/config/version.config.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const VERSION = '1.255.0';
-=======
-const VERSION = '1.252.0';
->>>>>>> OP002-10192: [FE] (W-2008-068-01) 안심거래 키오스크 매장 속성 추가 요청
 const VERSION_TEST = '1.0.0';
 
 export default VERSION;
