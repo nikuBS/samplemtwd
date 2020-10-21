f095768fb4365c9734753a68725a0a2a65edc857
P638175 <P164632@sktelecom.com>
2020-10-21 11:47:18 +0900

    메시지창 기본값 영문으로 변경

--- src/client/types_en/string.type.js
+++ src/client/types_en/string.type.js
@@ -90,9 +90,9 @@ Tw.SMS_VALIDATION = {
 };
 
 Tw.POPUP_TITLE = {
-  NOTIFY: '알림',
-  CONFIRM: '확인',
-  ERROR: '에러',
+  NOTIFY: 'alert',
+  CONFIRM: 'confrim',
+  ERROR: 'error',
   SELECT_BANK: '은행선택',
   SELECT_AMOUNT: '금액 선택',
   SELECT: '선택',
