004bbb4064e920668f06c61309ea58eb521d9ea2
김기남님/SKtelecom <skt.P161322@partner.sktelecom.com>
2020-10-21 11:05:12 +0900

    영문 액션시트 변경문제점 수정 및 APP 메인 문구 수정

--- src/client/services_en/common/popup.service.js
+++ src/client/services_en/common/popup.service.js
@@ -655,7 +655,12 @@ Tw.PopupService.prototype = {
         // 아래에 해당되는 액션시트가 뜰 때 history.back() 이 아닌 popupClose로 액션시트를 닫음
         var hash = window.location.hash;
         var nonback_hash = [
-          '#family_P', '#line_P', '#conditionChange_P', '#private_P', '#faq_P', '#store_info_P',
+          '#family_P', 
+          // '#line_P', 
+          '#conditionChange_P', 
+          '#private_P', 
+          '#faq_P', 
+          '#store_info_P',
         ];
         if(nonback_hash.indexOf(hash) > -1) {
           this._popupClose();
