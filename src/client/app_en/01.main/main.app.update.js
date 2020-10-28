3bd45a0c3430e006107ad7f53da1d4db9c88fe2a
김기남님/SKtelecom <skt.P161322@partner.sktelecom.com>
2020-10-28 23:38:12 +0900

    [영문] 수정

--- src/client/app_en/01.main/main.app.update.js
+++ src/client/app_en/01.main/main.app.update.js
@@ -31,48 +31,57 @@ Tw.MainAppUpdate.prototype = {
    * @desc 
    */
   _init: function () {
-    this._onAppUpdateCheck();
+    this._apiService.request(Tw.NODE_CMD.GET_ENVIRONMENT, {})
+      .done($.proxy(this._onAppUpdateCheck, this));
   },
 
   /**
    * @function
    * @desc 
    */
-  _onAppUpdateCheck: function () {
-    if ( Tw.Environment.environment === 'prd' ) { // 운영 모드 (prd) 일때만 동작 
-      var userAgentString = Tw.BrowserHelper.getUserAgent();
-      // var userAgentString = 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) TWM_APP TWM_DEVICE=osType:ios|appVersion:5.0.20|osVersion:14.2|id:5BA27585-2B6A-4287-8D07-A9CD17DCFB7D|model:iPhone12_3|widget:0';
-      var version = userAgentString.match(/\|appVersion:([\.0-9]*)\|/)[1];
-      var stdVersion = '';
-      this._currentVersion = version;
+  _onAppUpdateCheck: function (resp) {
+    if ( resp.code === Tw.API_CODE.CODE_00 ) {
+      var result = resp.result;
+      Tw.Environment = result;
+      var env = result.environment;
+      alert('-->> ' + env);
 
-      if (userAgentString.indexOf('osType:aos') !== -1) {
-        this._osType = 'A';
-        var versionArray = version.split('.')
-        if (versionArray[2] % 2 === 0) { // 대문자 
-          this._currentVersion = versionArray[0] + '.' + versionArray[1] + '.' + (versionArray[2] * 1 + 1)
+      if ( Tw.Environment.environment === 'prd' ) { // 운영 모드 (prd) 일때만 동작 
+        var userAgentString = Tw.BrowserHelper.getUserAgent();
+        // var userAgentString = 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) TWM_APP TWM_DEVICE=osType:ios|appVersion:5.0.20|osVersion:14.2|id:5BA27585-2B6A-4287-8D07-A9CD17DCFB7D|model:iPhone12_3|widget:0';
+        var version = userAgentString.match(/\|appVersion:([\.0-9]*)\|/)[1];
+        var stdVersion = '';
+        this._currentVersion = version;
+
+        if (userAgentString.indexOf('osType:aos') !== -1) {
+          this._osType = 'A';
+          var versionArray = version.split('.')
+          if (versionArray[2] % 2 === 0) { // 대문자 
+            this._currentVersion = versionArray[0] + '.' + versionArray[1] + '.' + (versionArray[2] * 1 + 1)
+          }
+        } else {
+          this._osType = 'I';
         }
-      } else {
-        this._osType = 'I';
-      }
 
-      if ( this._osType === 'A' ) { // 안드로이드 기준 버전
-        stdVersion = '5.0.20';
-      } else { // 아이폰 기준 버전
-        stdVersion = '5.0.14';
-      }
+        if ( this._osType === 'A' ) { // 안드로이드 기준 버전
+          stdVersion = '5.0.20';
+        } else { // 아이폰 기준 버전
+          stdVersion = '5.0.14';
+        }
 
-      var isUpdate = Tw.ValidationHelper.checkVersionValidation(stdVersion, this._currentVersion, 3);
-      if( isUpdate ) {
-        this._popupService.openConfirmUpdateButton(
-          Tw.POPUP_CONTENTS.UPDATE_POPUP_CONTENTS,
-          Tw.POPUP_TITLE.UPDATE_POPUP_TITLE, 
-          $.proxy(this._onUpdate, this), // 'Update' 버튼을 선택하면 업데이트 화면으로 이동
-          $.proxy(this._gotoTworld, this), // 'Go To Tworld KOR' 버튼을 선택하면 국문 메인페이지로 이동
-          Tw.BUTTON_LABEL.GO_TO_TWORLD_KOR,
-          Tw.BUTTON_LABEL.UPDATE
-        );
+        var isUpdate = Tw.ValidationHelper.checkVersionValidation(stdVersion, this._currentVersion, 3);
+        if( isUpdate ) {
+          this._popupService.openConfirmUpdateButton(
+            Tw.POPUP_CONTENTS.UPDATE_POPUP_CONTENTS,
+            Tw.POPUP_TITLE.UPDATE_POPUP_TITLE, 
+            $.proxy(this._onUpdate, this), // 'Update' 버튼을 선택하면 업데이트 화면으로 이동
+            $.proxy(this._gotoTworld, this), // 'Go To Tworld KOR' 버튼을 선택하면 국문 메인페이지로 이동
+            Tw.BUTTON_LABEL.GO_TO_TWORLD_KOR,
+            Tw.BUTTON_LABEL.UPDATE
+          );
+        }
       }
+          
     }
   },
 
