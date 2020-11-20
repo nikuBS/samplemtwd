e939b827d49c8aa34bb1ba34fac8ad2a93e5dd63
p151541 <jobkim0410@gmail.com>
2020-11-20 13:46:04 +0900

    [OP002-11204] [FE] [myT] (W-2010-045-02) Welcome Page 제작api 연동

--- src/server/app/01.main/controllers/main.welcome.controller.ts
+++ src/server/app/01.main/controllers/main.welcome.controller.ts
@@ -2,14 +2,19 @@
  * @file main.welcome.controller.ts
  * @author Ara Jo (araara.jo@sk.com)
  * @since 2018.09.06
- * @desc 메인 > 홈
+ * @desc 메인 > Welcome
  */
 
 import TwViewController from '../../../common/controllers/tw.view.controller';
 import { NextFunction, Request, Response } from 'express';
+import { Observable } from 'rxjs/Observable';
+import { API_CMD, API_CODE } from '../../../types/api-command.type';
+import { PRODUCT_CODE } from '../../../types/bff.type';
+import FormatHelper from '../../../utils/format.helper';
+import ProductHelper from '../../../utils/product.helper';
 
 /**
- * @desc 환영페이지 class
+ * @desc Welcome class
  */
 class Welcome extends TwViewController {
   constructor() {
@@ -17,7 +22,7 @@ class Welcome extends TwViewController {
   }
 
   /**
-   * 메인화면-MY 렌더 함수
+   * Welcome 렌더 함수
    * @param {Request} req
    * @param {Response} res
    * @param {NextFunction} next
@@ -28,18 +33,46 @@ class Welcome extends TwViewController {
    * @return {void}
    */
   render(req: Request, res: Response, next: NextFunction, svcInfo: any, allSvc: any, childInfo: any, pageInfo: any) {
+    Observable.combineLatest([
+      this._getBestAdditions() // SK텔레콤의 대표 부가서비스
+    ]).subscribe(([bestAdditions, appList]) => {
+      const productData = {
+        bestAdditions,
+      };
+      res.render(`main.welcome.html`, {
+        pageInfo,
+        productData
+      });
+    });
+  }
 
-    if (svcInfo) { // 로그인 
-        const tLink = ['/main/home', '/myt-join/submain', '/myt-data/submain', '/customer'];
-        const myfedd = '/new/my-feed';
-        
-    } else { // 비로그인 
+  /**
+   * @desc SK텔레콤의 대표 부가서비스 요청
+   * @private
+   */
+  private _getBestAdditions = () => {
+    return this.apiService.request(API_CMD.BFF_10_0027, { idxCtgCd: PRODUCT_CODE.MOBILE_ADDITION }).map(resp => {
+      if (resp.code !== API_CODE.CODE_00) {
+        return {
+          code: resp.code,
+          msg: resp.msg
+        };
+      }
 
-    }
+      if (FormatHelper.isEmpty(resp.result)) {
+        return resp.result;
+      }
 
-    res.render(`main.welcome.html`, {
-      title: '환영페이지',
-      desc: 'T월드 페이지를 환영 합니다.'
+      return {
+        ...resp.result,
+        prodList: (resp.result.prodList || []).map(addition => {
+          return {
+            ...addition,
+            basFeeInfo: ProductHelper.convProductBasfeeInfo(addition.basFeeInfo),
+            prodIconImgUrl: addition.prodIconImgUrl && ProductHelper.getImageUrlWithCdn(addition.prodIconImgUrl)
+          };
+        })
+      };
     });
   }
 }
