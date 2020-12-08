a235e04bff9e22c1c843359bb73ee73add12f4b6
김기남님/SKtelecom <skt.P161322@partner.sktelecom.com>
2020-12-08 17:03:23 +0900

    [영문] 웹 접근성 재 수정

--- src/client/app_en/01.main/main.home.js
+++ src/client/app_en/01.main/main.home.js
@@ -169,28 +169,29 @@ Tw.MainHome.prototype = {
       skt_landing.widgets.widget_slider1();
 
       var $parent = element.parent().parent();
-      element.find('#tab1-tab > div').css('display', 'block');
-      element.find('#tab2-tab > div').css('display', 'none');
-      element.find('#tab3-tab > div').css('display', 'none');
+
+      element.find('#tab1-tab').css('visibility', 'visible');
+      element.find('#tab2-tab').css('visibility', 'hidden');
+      element.find('#tab3-tab').css('visibility', 'hidden');
 
       $parent.on('click', '#tab1', $.proxy(function() {
-        element.find('#tab1-tab > div').css('display', 'block');
-        element.find('#tab2-tab > div').css('display', 'none');
-        element.find('#tab3-tab > div').css('display', 'none');
+        element.find('#tab1-tab').css('visibility', 'visible');
+        element.find('#tab2-tab').css('visibility', 'hidden');
+        element.find('#tab3-tab').css('visibility', 'hidden');
         $parent.find('#tab1').focus();
       }, this));
 
       $parent.on('click', '#tab2', $.proxy(function() {
-        element.find('#tab1-tab > div').css('display', 'none');
-        element.find('#tab2-tab > div').css('display', 'block');
-        element.find('#tab3-tab > div').css('display', 'none');
+        element.find('#tab1-tab').css('visibility', 'hidden');
+        element.find('#tab2-tab').css('visibility', 'visible');
+        element.find('#tab3-tab').css('visibility', 'hidden');
         $parent.find('#tab2').focus();
       }, this));
 
       $parent.on('click', '#tab3', $.proxy(function() {
-        element.find('#tab1-tab > div').css('display', 'none');
-        element.find('#tab2-tab > div').css('display', 'none');
-        element.find('#tab3-tab > div').css('display', 'block');
+        element.find('#tab1-tab').css('visibility', 'hidden');
+        element.find('#tab2-tab').css('visibility', 'hidden');
+        element.find('#tab3-tab').css('visibility', 'visible');
         $parent.find('#tab3').focus();
       }, this));
     }
