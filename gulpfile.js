14ec0e25ea54fad332541dbe1470fa26fb164acf
P132150 <skt.p132150@partner.sk.com>
2021-02-03 20:04:57 +0900

    [OP002-10551] (W-2008-091-01) [UI고도화] 나의데이터 통화 서브메인 통합구현 - 문구 원복

--- gulpfile.js
+++ gulpfile.js
@@ -51,8 +51,7 @@ gulp.task('server', function () {
         res.setHeader('Access-Control-Allow-Headers', '*');
         next();
       }
-    }))
-    .pipe(gulp.task('watch'));
+    }));
 });
 
 gulp.task('js-vendor', function () {
@@ -914,7 +913,7 @@ gulp.task('ko-build', shell.task([
   'gulp ko-run --ver=' + version
 ]));
 
-gulp.task('ko-run', ['server']);
+gulp.task('ko-run', ['server', 'ko-watch']);
 
 gulp.task('ko-watch', function () {
   livereload.listen();
