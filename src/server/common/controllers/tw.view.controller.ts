e0cc651dd963012f15ac464e61d7f7721a725fb2
P132150 <skt.p132150@partner.sk.com>
2021-02-19 14:41:40 +0900

    [hotfix] 페이지 그린 분리 코드 추가

--- src/server/common/controllers/tw.view.controller.ts
+++ src/server/common/controllers/tw.view.controller.ts
@@ -230,22 +230,41 @@ abstract class TwViewController {
    * @param menuId
    * @protected
    */
-  protected getAdvancementPageVisibleCheck(menuId): Observable<any> {
+  protected getAdvancementPageVisibleCheck(params): Observable<any> {
+    const { menuId, host } = params;
     return this._apiService.request(API_CMD.BFF_01_0069, {
       property: REDIS_KEY.ADVANCEMENT_PAGE_VISIBLE
     }).map((resp) => {
       if ( resp.code === API_CODE.CODE_00 ) {
-        if (FormatHelper.isEmpty(resp.result)) {
+        // 예외 오류 케이스가 발생시 대처
+        try {
+          if (FormatHelper.isEmpty(resp.result)) {
+            return null;
+          }
+          const selectItem = resp.result.split(',').filter(item => menuId === item.split(':')[0]);
+          if (FormatHelper.isEmpty(selectItem)) {
+            return null;
+          }
+          const env = selectItem[0].split(':')[1];
+          const visible = parseInt(selectItem[0].split(':')[2], 10) === 1;
+          this.logger.error(this, 'host::: '+ host +' env:::: '+ env + ' ===> ' + env.includes('-g'));
+          // 상용환경인 경우 green, blue 구분 하기 위한 코드 추가 (green: 'prd-g', blue: 'prd')
+          // green 환경은 기존 NODE_ENV 값에 '-g'을 추가하여 처리
+          if (host.includes('gapp') && env.includes('-g')) {
+            // 무조건 그린환경인 경우
+            return {
+              env: env.replace('-g', ''),
+              visible
+            }
+          }
+          return {
+            env,
+            visible
+          };
+        } catch (e) {
+          this.logger.warn(this, '[getAdvancementPageVisibleCheck] error ::: '+ e);
           return null;
         }
-        const selectItem = resp.result.split(',').filter(item => menuId === item.split(':')[0]);
-        if (FormatHelper.isEmpty(selectItem)) {
-          return null;
-        }
-        return {
-          env: selectItem[0].split(':')[1],
-          visible: parseInt(selectItem[0].split(':')[2], 10) === 1
-        };
       } else {
         return null;
       }
@@ -300,7 +319,7 @@ abstract class TwViewController {
           if ( isLogin ) {
             Observable.combineLatest(
                 this.getPersonSmsDisableTimeCheck(),
-                this.getAdvancementPageVisibleCheck(urlMeta.menuId)
+                this.getAdvancementPageVisibleCheck({ menuId: urlMeta.menuId, host: req.hostname })
             ).subscribe(([personResp, advancementResp]) => {
               svcInfo.personSmsDisableTimeCheck = personResp;
               urlMeta.advancement = advancementResp;
--- src/server/config/version.config.ts
+++ src/server/config/version.config.ts
@@ -1,4 +1,4 @@
-const VERSION = '1.324.0';
+const VERSION = '1.325.0';
 const VERSION_TEST = '1.0.0';
 
 export default VERSION;
