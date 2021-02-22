1b483ce17044787e7af5bd1fdf3d4de0ccb18877
P132150 <skt.p132150@partner.sk.com>
2021-02-22 13:18:35 +0900

    * 특정 페이지 블루, 그린 분리 코드 오류 수정 - includes -> indexOf 로 변경

--- src/server/common/controllers/tw.view.controller.ts
+++ src/server/common/controllers/tw.view.controller.ts
@@ -247,10 +247,10 @@ abstract class TwViewController {
           }
           const env = selectItem[0].split(':')[1];
           const visible = parseInt(selectItem[0].split(':')[2], 10) === 1;
-          this.logger.error(this, 'host::: '+ host +' env:::: '+ env + ' ===> ' + env.includes('-g'));
+          this.logger.error(this, 'host::: '+ host +' env:::: '+ env + ' ===> ' + (env.indexOf('-g') > -1));
           // 상용환경인 경우 green, blue 구분 하기 위한 코드 추가 (green: 'prd-g', blue: 'prd')
           // green 환경은 기존 NODE_ENV 값에 '-g'을 추가하여 처리
-          if (host.includes('gapp') && env.includes('-g')) {
+          if (host.indexOf('gapp') > -1 && env.indexOf('-g') > -1) {
             // 무조건 그린환경인 경우
             return {
               env: env.replace('-g', ''),
