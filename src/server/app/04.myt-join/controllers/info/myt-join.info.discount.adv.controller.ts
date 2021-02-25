7b8077f9c8d23d06c5def59bd747e71e03b641d5
P132150 <skt.p132150@partner.sk.com>
2021-02-25 20:51:24 +0900

    [OP002-11001] [FE] (W-2009-045-01) [고도화] 약정할인/기기상환정보 - 자주하는 질문 영역 및 기능 추가 - 종료된 약정 영역 추가

--- src/server/app/04.myt-join/controllers/info/myt-join.info.discount.adv.controller.ts
+++ src/server/app/04.myt-join/controllers/info/myt-join.info.discount.adv.controller.ts
@@ -9,8 +9,11 @@
 import { Request, Response, NextFunction } from 'express';
 import { API_CMD, API_CODE, API_VERSION } from '../../../../types/api-command.type';
 import MytJoinInfoDiscount from './myt-join.info.discount.controller';
+import { Observable } from 'rxjs';
+import FormatHelper from '../../../../utils/format.helper';
 
 class MytJoinInfoDiscountAdvController extends MytJoinInfoDiscount {
+  private finishInfoList: any = [];
 
   constructor() {
     super();
@@ -31,32 +34,72 @@ class MytJoinInfoDiscountAdvController extends MytJoinInfoDiscount {
 
   _render(req, res, next, svcInfo, allSvc, child, pageInfo) {
     this._setDataInfo(req, svcInfo, pageInfo);
-    this.apiService.request(API_CMD.BFF_05_0063, {}, null, [], API_VERSION.V2)
-      .subscribe((discountResp) => {
-        if (discountResp.code !== API_CODE.CODE_00) {
-          return this.error.render(res, {
-            // title: 'title',
-            code: discountResp.code,
-            msg: discountResp.msg,
-            pageInfo: pageInfo,
-            svcInfo: svcInfo
-          });
-        }
-        this.resDataInfo = discountResp.result;
-        this._dataInit();
-        /**
-         * TODO: 약정위약금, 할인반환금, 종료된 약정 등 추가 기능 개발 필요
-         * AS-IS 기준으로 1차 개발
-         */
-
-        this.renderView(res, 'info/myt-join.info.discount.adv.html', {
-          reqQuery: this.reqQuery,
-          svcInfo: svcInfo,
-          pageInfo: this.pageInfo,
-          commDataInfo: this.commDataInfo,
-          resDataInfo: this.resDataInfo
+    Observable.combineLatest(
+      this.apiService.request(API_CMD.BFF_05_0063, {}, null, [], API_VERSION.V2),
+      this.apiService.request(API_CMD.BFF_05_0238, {}),
+      this._getFaqRequest()
+    ).subscribe(([discountResp, agrmtInfosResp, faqList]) => {
+      if ( discountResp.code !== API_CODE.CODE_00 ) {
+        return this.error.render(res, {
+          // title: 'title',
+          code: discountResp.code,
+          msg: discountResp.msg,
+          pageInfo: pageInfo,
+          svcInfo: svcInfo
         });
+      }
+      console.log('################', agrmtInfosResp)
+      if ( agrmtInfosResp.code === API_CODE.CODE_00 ) {
+        if ( agrmtInfosResp.result.agrmtInfoList.length ) {
+
+        }
+      }
+
+      this.resDataInfo = discountResp.result;
+      this._dataInit();
+      /**
+       * 기능 추가 부분 - 할인반환금,
+       */
+
+      this.renderView(res, 'info/myt-join.info.discount.adv.html', {
+        reqQuery: this.reqQuery,
+        svcInfo: svcInfo,
+        pageInfo: this.pageInfo,
+        commDataInfo: this.commDataInfo,
+        resDataInfo: this.resDataInfo,
+        faqList,
+        finishInfoList: this.finishInfoList
       });
+    });
+  }
+
+  _getFaqRequest() {
+    return Observable.combineLatest(
+      this.apiService.request(API_CMD.BFF_08_0073, { ifaqId: '1606010553' }),
+      this.apiService.request(API_CMD.BFF_08_0073, { ifaqId: '1000056771' }),
+      this.apiService.request(API_CMD.BFF_08_0073, { ifaqId: '1000056772' })
+    ).map(([item, item1, item2]) => {
+      const faqList: any = [];
+      if ( item.code === API_CODE.CODE_00 && !FormatHelper.isEmpty(item.result) ) {
+        faqList.push({
+          title: item.result.inqCtt,
+          answer: item.result.answCtt
+        });
+      }
+      if ( item1.code === API_CODE.CODE_00 && !FormatHelper.isEmpty(item1.result) ) {
+        faqList.push({
+          title: item1.result.inqCtt,
+          answer: item1.result.answCtt
+        });
+      }
+      if ( item2.code === API_CODE.CODE_00 && !FormatHelper.isEmpty(item2.result) ) {
+        faqList.push({
+          title: item2.result.inqCtt,
+          answer: item2.result.answCtt
+        });
+      }
+      return faqList;
+    });
   }
 }
 
