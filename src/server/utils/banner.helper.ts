f14d982fba497859bb5888540052e5e005062015
kimyongyeon <jobkim0410@gmail.com>
2021-03-03 15:16:09 +0900

    [OP002-13530][FE][공통] TextBanner 상용 업무로그 확인용 로직 추가

--- src/server/utils/banner.helper.ts
+++ src/server/utils/banner.helper.ts
@@ -136,11 +136,8 @@ class BannerHelper {
         let serialNums = '';
         let realTimeBanner, campaignBanner;
 
-        this.logger.error(this, `[textbanner]=>${code}, ${userId}`);
-
         return this.redisService.getData(REDIS_KEY.BANNER_TOS_LINK + code)
             .switchMap((resp) => {  // TOS 정보를 호출함
-                this.logger.error(this, `[textbanner]=>${REDIS_KEY.BANNER_TOS_LINK + code}`, resp);
                 if (resp.code === API_CODE.CODE_00) {
                     if (resp.result.bltnYn === 'N') {
                         return of({
@@ -167,13 +164,10 @@ class BannerHelper {
                 }
             })
             .switchMap((resp) => {// 조회된 TOS정보 중 실시간배너(R)인것만 추출하여 배너를 조회함
-                this.logger.error(this, `[textbanner]=>${REDIS_TOS_KEY.BANNER_TOS_KEY + code + ':' + userId + ':' + svcMgmtNum}`, resp);
                 if (resp.code === API_CODE.CODE_00) {
                     serialNums = (resp.result || '').trim();
                     realTimeBanner = serialNums.split('|').filter(e => e.indexOf('R') > -1);
                     campaignBanner = serialNums.split('|').filter(e => e.indexOf('C') > -1);
-
-                    this.logger.error(this, `[textbanner]=>${serialNums},${realTimeBanner},${campaignBanner}`);
 
                     if (serialNums === '') {
                         return this.redisService.getData(REDIS_KEY.BANNER_TOS_INFO + 'D' + code);
@@ -192,7 +186,6 @@ class BannerHelper {
                                     n.result.imgList.forEach(e => p.push(Object.assign({}, n.result.summary, e)));
                                     return p;
                                 }, []);
-                                this.logger.error(this, `[textbanner]=>${imgList}`);
                             return of({
                                 code: API_CODE.CODE_00,
                                 result: {
