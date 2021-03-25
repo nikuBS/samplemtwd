98e0e505d84a81dd1fb038b63e96dfdb1b371a1d
P132150 <skt.p132150@partner.sk.com>
2021-03-25 17:07:03 +0900

    [OP002-13862][FE][myT] (W-2103-082-01) [고도화 잔여 업무] 회선리스트 심화 및 추가 적용 - 사용하지 않는 부분 원복

--- src/client/app/02.myt-data/myt-data.submain.js
+++ src/client/app/02.myt-data/myt-data.submain.js
@@ -169,7 +169,7 @@ Tw.MyTDataSubMain.prototype = {
     this._getTosAdminMytDataBanner();
 
     // OP002-5303 : [개선][FE](W-1910-078-01) 회선선택 영역 확대
-    // this._lineComponent = new Tw.LineComponent(this.$container, '.fe-bt-line', true, null);
+    this._lineComponent = new Tw.LineComponent(this.$container, '.fe-bt-line', true, null);
   },
 
   /**
